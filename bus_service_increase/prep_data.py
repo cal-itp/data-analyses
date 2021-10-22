30bc514492fa7682a8813d485893e4e0ab3ac168
tiffanychu90 <tiffany.chu@dot.ca.gov>
2021-10-22 20:11:17 +0000

    Move to new GCS bucket

--- bus_service_increase/prep_data.py
+++ bus_service_increase/prep_data.py
@@ -18,7 +18,13 @@ import utils
 # Download LEHD data from Urban Institute
 URBAN_URL = "https://urban-data-catalog.s3.amazonaws.com/drupal-root-live/"
 DATE_DOWNLOAD = "2021/04/19/"
-datasets = ["wac_pri_tract_minus_fed", "wac_fed_tract"]
+
+# Doing all_se01-se03 is the same as primary jobs
+# summing up to tract level gives same df.describe() results
+datasets = ["wac_all_se01_tract_minus_fed", 
+            "wac_all_se02_tract_minus_fed",
+            "wac_all_se03_tract_minus_fed", 
+            "wac_fed_tract"]
 
 for dataset in datasets:
     utils.import_export(DATASET_NAME = f"{URBAN_URL}{DATE_DOWNLOAD}{dataset}", 
@@ -46,16 +52,38 @@ def process_lehd(df):
 
 
 # Merge and clean up 
-def merge_and_process(df1, df2):
-    df = pd.merge(df1, 
-              df2.rename(columns = {"num_jobs": "fed_jobs"}), 
-              on = "Tract", how = "left", validate = "1:1")
+def merge_and_process(data_to_merge = []):
+    """
+    data_to_merge: list. List of pandas.DataFrames to merge.
+    """    
+    # Use a loop to merge dataframes
+    # For 1st df, make a copy and rename column
+    # For subsequent dfs, merge and rename column
+    # We want num_jobs to be stored as num_jobs0, num_jobs1, etc to keep track
+    final = pd.DataFrame()
+    i = 0
     
-    df = df.assign(
-        wac_num_jobs = df[["num_jobs", "fed_jobs"]].sum(axis=1).astype(int)
-    )[["Tract", "wac_num_jobs"]]
+    for d in data_to_merge:
+        new_col = f"num_jobs{i}"
+        if i == 0:
+            final = d.copy()
+            final = final.rename(columns = {"num_jobs": new_col})
+            
+        else:
+            final = final.merge(d,
+                on = "Tract", how = "left", validate = "1:1"
+            ).rename(columns = {"num_jobs": new_col})
+            
+        i += 1
     
-    return df    
+    # Sum across to get total number of jobs for tract
+    jobs_cols = [col for col in final.columns if "num_jobs" in col]
+    
+    final = final.assign(
+        num_jobs = final[jobs_cols].sum(axis=1).astype(int),
+    ).drop(columns = jobs_cols)
+        
+    return final   
     
     
 def merge_calenviroscreen_lehd(calenviroscreen, lehd):
@@ -63,10 +91,13 @@ def merge_calenviroscreen_lehd(calenviroscreen, lehd):
     
     # Merge LEHD with CalEnviroScreen
     df = pd.merge(gdf, lehd, 
-                  on = "Tract", how = "left", validate = "1:1")
+                  on = "Tract", how = "left", validate = "1:1"
+                 )
     
+    # Calculate jobs per sq mi
     df = df.assign(
-        wac_num_jobs = df.wac_num_jobs.fillna(0).astype(int)
+        num_jobs = df.num_jobs.fillna(0).astype(int),
+        jobs_per_sqmi = df.num_jobs / df.Population,
     )
     
     return df
@@ -75,22 +106,22 @@ def merge_calenviroscreen_lehd(calenviroscreen, lehd):
 #--------------------------------------------------------#
 ### Function to make cleaned data
 #--------------------------------------------------------#
-def generate_calenviroscreen_lehd_data():
+def generate_calenviroscreen_lehd_data(lehd_datasets):
     # CalEnviroScreen data (gdf)
     CALENVIROSCREEN_FILE = 'calenviroscreen40shp_F_2021/CES4_final.shp'
     gdf = gpd.read_file(f"./{CALENVIROSCREEN_FILE}")
     
     # LEHD Data
-    primary_nofed = pd.read_parquet((f"gs://{utils.BUCKET_NAME}/"
-                                     "wac_pri_tract_minus_fed.parquet")
-                                   )
-    fed = pd.read_parquet(f"gs://{utils.BUCKET_NAME}/"
-                          "wac_fed_tract.parquet")
+    lehd_dfs = {}
+    for d in lehd_datasets:
+        lehd_dfs[d] = pd.read_parquet(f"{utils.GCS_FILE_PATH}{d}.parquet")
+    
+    cleaned_dfs = []
+    for key, value in lehd_dfs.items():
+        processed_df = process_lehd(value)
+        cleaned_dfs.append(processed_df)
 
-    primary_nofed = process_lehd(primary_nofed)
-    fed = process_lehd(fed)
-
-    lehd = merge_and_process(primary_nofed, fed)
+    lehd = merge_and_process(data_to_merge = cleaned_dfs)
     
     # Merge together
     df = merge_calenviroscreen_lehd(gdf, lehd)
