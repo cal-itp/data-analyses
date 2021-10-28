114c70f8c03f75af40b589fd68122b5f2a597268
tiffanychu90 <tiffany.chu@dot.ca.gov>
2021-10-28 20:20:01 +0000

    Add more to utils

--- bus_service_increase/utils.py
+++ bus_service_increase/utils.py
@@ -95,6 +95,7 @@ def prep_calenviroscreen(df):
     
     return df3
 
+
 def get_recent_dates():
     '''
     Return a dict with ISO date strings for the most recent thursday, saturday, and sunday.
@@ -109,3 +110,78 @@ def get_recent_dates():
         dates.append(last_day.isoformat())
     
     return dict(zip(['thurs', 'sat', 'sun'], dates))
+
+
+# There are multiple feeds, with different trip_keys but same trip_ids
+# Only keep calitp_url_number == 0 EXCEPT LA Metro
+def include_exclude_multiple_feeds(df, id_col = "itp_id",
+                                   include_ids = [182], exclude_ids = [200]):
+    """
+    df: pandas.DataFrame.
+    id_col: str, column name for calitp_itp_id, such as "itp_id"
+    include_ids: list, 
+            list of itp_ids that are allowed to have multiple feeds 
+            (Ex: LA Metro) 
+    exclude_ids: list, list of itp_ids to drop. (Ex: MTC, regional feed)
+    """
+    # If there are explicit regional feeds to drop, put that in exclude_ids
+    group_cols = [id_col, "trip_id", "stop_id", 
+                  "calitp_url_number"]
+    df2 = (df[~df[id_col].isin(exclude_ids)]
+           .sort_values(group_cols)
+           .drop_duplicates(subset=group_cols)
+           .reset_index(drop=True)
+          )
+    
+    print(f"# obs in original df: {len(df)}")
+    print(f"# obs in new df: {len(df2)}")
+    
+    # There are still multiple operators here
+    # But, seems like for those trip_ids, they are different values 
+    # between url_number==0 vs url_number==1
+    multiple_urls = list(df2[df2.calitp_url_number==1][id_col].unique())
+    print(f"These operators have multiple calitp_url_number values: {multiple_urls}")    
+    
+    return df2
+
+
+def aggregate_by_tract(gdf, group_cols, 
+                       sum_cols = [], count_cols = []):
+    '''
+    gdf: geopandas.GeoDataFrame, on which the aggregating to tract is done
+        It must include the tract's geometry column
+    
+    group_cols: list. List of columns to do the groupby, but exclude geometry.
+    sum_cols: list. List of columns to calculate a sum with the groupby.
+    count_cols: list. List of columns to calculate a count with the groupby.
+    
+    Returns a geopandas.GeoDataFrame.
+    '''
+    tract_geometry = gdf[["Tract", "geometry"]].drop_duplicates()
+    df2 = gdf[group_cols].drop_duplicates().reset_index()
+    
+    if len(sum_cols) > 0:
+        sum_df = gdf.pivot_table(index=group_cols, 
+                                values=sum_cols, 
+                                aggfunc="sum").reset_index()
+        df2 = pd.merge(df2, sum_df,
+                      on=group_cols, how="left", validate="1:1"
+                     )
+  
+    if len(count_cols) > 0:
+        count_df = gdf.pivot_table(index=group_cols, 
+                                  values=count_cols, 
+                                  aggfunc="count").reset_index()
+        df2 = pd.merge(df2, count_df, 
+                     on=group_cols, how="left", validate="1:1")
+        
+    # Merge tract geometry back in
+    df3 = pd.merge(
+        tract_geometry,
+        df2,
+        on = "Tract",
+        how = "inner", 
+        validate = "1:1"
+    ).drop(columns = "index")
+    
+    return df3
