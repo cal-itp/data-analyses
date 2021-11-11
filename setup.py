664c2bac77209de559a371f49aaaca6f943195fd
tiffanychu90 <tiffany.chu@dot.ca.gov>
2021-11-11 01:06:14 +0000

    Initial shared utilities

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='shared_utils',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Shared utility functions for data analyses',
+    author='Cal-ITP',
+    license='Apache',
+    include_package_data=True,
+    package_dir={"shared_utils": "shared_utils"},
+    install_requires=["calitp", "geopandas", "numpy", "pandas"],
+)
