a6a7b2612991f4e0be29390d18a6be8299947c85
tiffanychu90 <tiffany.chu@dot.ca.gov>
2021-11-12 20:36:35 +0000

    Add example notebook

--- shared_utils/geography_utils.py
+++ shared_utils/geography_utils.py
@@ -10,38 +10,55 @@ CA_NAD83Albers = "EPSG:3310" # units are in meters
 
 SQ_MI_PER_SQ_M = 3.86 * 10**-7
 
-
-def aggregate_by_geography(gdf, group_cols, 
-                       sum_cols = [], count_cols = [], nunique_cols = []):
+def aggregate_by_geography(df, group_cols, 
+                       sum_cols = [], mean_cols = [], 
+                       count_cols = [], nunique_cols = []):
     '''
-    gdf: geopandas.GeoDataFrame, on which the aggregating to tract is done
-        It must include the tract's geometry column
+    df: pandas.DataFrame or geopandas.GeoDataFrame., 
+        The df on which the aggregating is done.
+        If it's a geodataframe, it must exclude the tract's geometry column
     
-    group_cols: list. List of columns to do the groupby, but exclude geometry.
-    sum_cols: list. List of columns to calculate a sum with the groupby.
-    count_cols: list. List of columns to calculate a count with the groupby.
+    group_cols: list. 
+        List of columns to do the groupby, but exclude geometry.
+    sum_cols: list. 
+        List of columns to calculate a sum with the groupby.
+    mean_cols: list. 
+        List of columns to calculate an average with the groupby 
+        (beware: may want weighted averages and not simple average!!).
+    count_cols: list. 
+        List of columns to calculate a count with the groupby.
+    nunique_cols: list. 
+        List of columns to calculate the number of unique values with the groupby.
     
-    Returns a geopandas.GeoDataFrame.
+    Returns a pandas.DataFrame or geopandas.GeoDataFrame (same as input).
     '''
-    df2 = gdf[group_cols].drop_duplicates().reset_index()
+    df2 = df[group_cols].drop_duplicates().reset_index()
     
     if len(sum_cols) > 0:
-        sum_df = gdf.pivot_table(index=group_cols, 
+        sum_df = df.pivot_table(index=group_cols, 
                                 values=sum_cols, 
                                 aggfunc="sum").reset_index()
         df2 = pd.merge(df2, sum_df,
                       on=group_cols, how="left", validate="1:1"
                      )
   
+    if len(mean_cols) > 0:
+        mean_df = df.pivot_table(index=group_cols, 
+                                values=mean_cols, 
+                                aggfunc="mean").reset_index()
+        df2 = pd.merge(df2, mean_df,
+                      on=group_cols, how="left", validate="1:1"
+                     )
+        
     if len(count_cols) > 0:
-        count_df = gdf.pivot_table(index=group_cols, 
+        count_df = df.pivot_table(index=group_cols, 
                                   values=count_cols, 
                                   aggfunc="count").reset_index()
         df2 = pd.merge(df2, count_df, 
                      on=group_cols, how="left", validate="1:1")
     
     if len(nunique_cols) > 0:
-        nunique_df = gdf.pivot_table(index=group_cols,
+        nunique_df = df.pivot_table(index=group_cols,
                                      values=nunique_cols,
                                      aggfunc="nunique").reset_index()
         df2 = pd.merge(df2, nunique_df, 
