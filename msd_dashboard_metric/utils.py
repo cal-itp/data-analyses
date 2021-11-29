9a5121bb535b49814097a7983d6355c59e765bfb
Eric Dasmalchi <edasmalchi@gmail.com>
2021-11-29 23:51:59 +0000

    initial new metrics, pending cleanup and share

--- msd_dashboard_metric/utils.py
+++ msd_dashboard_metric/utils.py
@@ -13,34 +13,54 @@ import requests
 from ipyleaflet import Map, GeoJSON, projections, basemaps, GeoData, LayersControl, WidgetControl, GeoJSON, LegendControl
 from ipywidgets import Text, HTML
 
-def get_accessible_stops_and_trips():
-    accessible_stops = (tbl.gtfs_schedule.stops()
-                    >> filter(_.wheelchair_boarding == '1')
+def get_stops_and_trips(filter_accessible):
+    '''
+    Returns a basic view of stops x modes serving stop.
+    
+    If filter_accessible == True, return only stops that explicitly
+    provide wheelchair boarding and are served by a wheelchair accessible trip
+    '''
+    stops = (tbl.gtfs_schedule.stops()
                     >> select(_.calitp_itp_id, _.calitp_url_number, _.stop_id,
                               _.stop_lat, _.stop_lon, _.wheelchair_boarding)
                    )
-    accessible_trips = (tbl.gtfs_schedule.trips()
-                    >> filter(_.wheelchair_accessible == '1')
+    trips = (tbl.gtfs_schedule.trips()
                     >> select(_.calitp_itp_id, _.calitp_url_number, _.trip_id,
-                                _.wheelchair_accessible)
+                                _.wheelchair_accessible, _.route_id)
                    )
-    accessible_stops_trips = (tbl.gtfs_schedule.stop_times()
+    if filter_accessible:
+        stops = stops >> filter(_.wheelchair_boarding == '1')
+        trips = trips >> filter(_.wheelchair_accessible == '1')
+    
+    route_mode = (tbl.gtfs_schedule.routes()
+                    >> select(_.calitp_itp_id, _.calitp_url_number, _.route_id,
+                                _.route_type)
+                 )
+    trips_route_joined = trips >> inner_join(_, route_mode, on=['calitp_itp_id',
+                            'calitp_url_number', 'route_id'])
+    print('before collect...')
+    stops_trips = (tbl.gtfs_schedule.stop_times()
       >> select(_.calitp_itp_id, _.calitp_url_number, _.trip_id,
                _.stop_id)
-      >> inner_join(_, accessible_trips, on=['calitp_itp_id',
+      >> inner_join(_, trips_route_joined, on=['calitp_itp_id',
                             'calitp_url_number', 'trip_id'])
-      >> inner_join(_, accessible_stops, on=['calitp_itp_id',
+      >> inner_join(_, stops, on=['calitp_itp_id',
                             'calitp_url_number', 'stop_id'])
-      >> collect()
+      # >> collect()
       ## actually a trip count could be cool? (another use for a frequency table...)
-      >> distinct(_.stop_id, _keep_all = True)
-      >> select(-_.trip_id)
+      # >> distinct(_.stop_id, _.route_type, _keep_all = True)
+      >> distinct(_.stop_id, _.route_type, _.stop_lon, _.stop_lat,
+                  _.calitp_itp_id, _.calitp_url_number, _.wheelchair_boarding,
+                 _.wheelchair_accessible)
+      # >> select(-_.trip_id)
+      >> collect()
      )
-    accessible_stops_trips = gpd.GeoDataFrame(accessible_stops_trips,
-                        geometry=gpd.points_from_xy(accessible_stops_trips.stop_lon,
-                                                   accessible_stops_trips.stop_lat),
+    print('to_gdf')
+    stops_trips = gpd.GeoDataFrame(stops_trips,
+                        geometry=gpd.points_from_xy(stops_trips.stop_lon,
+                                                   stops_trips.stop_lat),
                         crs = 'EPSG:4326').to_crs('EPSG:6414') ## https://epsg.io/6414 (meters)
-    return accessible_stops_trips
+    return stops_trips
 
 ca_counties = requests.get('https://api.census.gov/data/2019/acs/acs5?get=NAME,B01001_001E&for=county:*&in=state:06')
 ca_county_codes = [x[-1] for x in ca_counties.json()[1:]]
@@ -55,7 +75,7 @@ https://api.census.gov/data/2019/acs/acs5?get=NAME,\
 {census_vars}&for={geography}:*&in=state:06%20county:{county}\
 '''
         r = requests.get(query)
-        # print(query)
+        print(query)
         # print(r.status_code)
         json = r.json()
         cols = json[0]
