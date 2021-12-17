5f4e836a15757dd4c84533c57b9d9da22480c3a9
Eric Dasmalchi <edasmalchi@gmail.com>
2021-12-17 20:48:02 +0000

    quick coverage maps

--- msd_dashboard_metric/utils.py
+++ msd_dashboard_metric/utils.py
@@ -123,6 +123,40 @@ def simple_map(gdf, mouseover=None):
             
         
     geo_data = GeoData(geo_dataframe = gdf.to_crs('EPSG:4326'),
+                   style={'color': 'black', 'fillColor': '#998ec3',
+                                'opacity':0.2, 'weight':.5, 'dashArray':'2', 'fillOpacity':0.3},
+                   hover_style={'fillColor': 'red' , 'fillOpacity': 0.3},
+                   name = 'data')
+
+    m.add_layer(geo_data)
+        
+    if mouseover:
+        geo_data.on_hover(update_html)
+
+    m.add_control(LayersControl())
+
+    return m
+
+def simple_map(gdf, mouseover=None):
+    
+    x = gdf.to_crs('EPSG:4326').geometry.iloc[0].centroid.x
+    y = gdf.to_crs('EPSG:4326').geometry.iloc[0].centroid.y
+    
+    m = Map(basemap=basemaps.CartoDB.Positron, center=[y, x], zoom=11)
+
+    if mouseover:
+        html = HTML(f'hover to see {mouseover}')
+        html.layout.margin = '0px 20px 20px 20px'
+        control = WidgetControl(widget=html, position='topright')
+        m.add_control(control)
+
+        def update_html(feature,  **kwargs):
+            html.value = '''
+                <h3><b>{}</b></h3>
+            '''.format(feature['properties'][mouseover])
+            
+        
+    geo_data = GeoData(geo_dataframe = gdf.to_crs('EPSG:4326'),
                    style={'color': 'black', 'fillColor': '#2ca25f',
                                 'opacity':0.2, 'weight':.5, 'dashArray':'2', 'fillOpacity':0.3},
                    hover_style={'fillColor': 'red' , 'fillOpacity': 0.3},
