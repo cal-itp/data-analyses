71dec3e719eea591adeb60656a1d0fffe422b439
Eric Dasmalchi <edasmalchi@gmail.com>
2021-12-22 21:45:51 +0000

    fix delayed update at origin bug

--- rt_delay/utils.py
+++ rt_delay/utils.py
@@ -9,6 +9,8 @@ BUCKET_NAME = "calitp-analytics-data"
 BUCKET_DIR = "data-analyses/rt_delay"
 GCS_FILE_PATH = f"gs://{BUCKET_NAME}/{BUCKET_DIR}/"
 
+MPH_PER_MPS = 2.237 ## use to convert meters/second to miles/hour
+
 def simple_map(gdf, mouseover=None):
     
     gdf = gdf.copy()
