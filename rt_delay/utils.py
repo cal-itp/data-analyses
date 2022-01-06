36544e6f20ec88a68c91d604f818aaaf017163b8
Eric Dasmalchi <edasmalchi@gmail.com>
2022-01-06 01:10:25 +0000

    a better detailled map

--- rt_delay/utils.py
+++ rt_delay/utils.py
@@ -17,3 +17,18 @@ def reversed_colormap(existing):
         colors=list(reversed(existing.colors)),
         vmin=existing.vmin, vmax=existing.vmax
     )
+
+def primary_cardinal_direction(origin, destination):
+    distance_east = destination.x - origin.x
+    distance_north = destination.y - origin.y
+    
+    if abs(distance_east) > abs(distance_north):
+        if distance_east > 0:
+            return('Eastbound')
+        else:
+            return('Westbound')
+    else:
+        if distance_north > 0:
+            return('Northbound')
+        else:
+            return('Southbound')
