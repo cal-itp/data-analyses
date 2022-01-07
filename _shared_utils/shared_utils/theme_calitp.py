286b5c652cbce059199fa6cc06e4cc69863eb0e2
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-01-07 00:37:08 +0000

    Create custom altair theme and png config

--- _shared_utils/shared_utils/theme_calitp.py
+++ _shared_utils/shared_utils/theme_calitp.py
@@ -6,16 +6,25 @@ and color palettes.
 import altair as alt
 import calitp_color_palette as cp
 
-"""
-Setting altair theme to resemble Urban: 
-https://towardsdatascience.com/consistently-beautiful-visualizations-with-altair-themes-c7f9f889602
+from plotnine import *
 
-GH code: 
-https://github.com/chekos/altair_themes_blog/tree/master/notebooks
+#https://github.com/CityOfLosAngeles/los-angeles-citywide-data-style
 
-Download Google font: 
-https://gist.github.com/ravgeetdhillon/0063aaee240c0cddb12738c232bd8a49
-"""
+#--------------------------------------------------------------#
+# Chart parameters for altair
+#--------------------------------------------------------------#
+font_size = 18
+chart_width = 400
+chart_height = 250
+
+markColor = '#8CBCCB'
+axisColor = '#cbcbcb'
+guideLabelColor = '#474747'
+guideTitleColor = '#333'
+blackTitle = '#333'
+font="Calibri" #"Raleway"
+labelFont="Arial" #"Nunito Sans"
+backgroundColor = "white"
 
 PALETTE = {
     "category_bright": cp.CALITP_CATEGORY_BRIGHT_COLORS,
@@ -24,131 +33,23 @@ PALETTE = {
     "sequential": cp.CALITP_SEQUENTIAL_COLORS,
 }
 
-def calitp_theme():
-    font_size = 18
-    chart_width = 400
-    chart_height = 250
+"""
+Setting custom Altair theme as .py (Urban): 
+https://towardsdatascience.com/consistently-beautiful-visualizations-with-altair-themes-c7f9f889602
 
-    markColor = '#8CBCCB'
-    axisColor = '#cbcbcb'
-    guideLabelColor = '#474747'
-    guideTitleColor = '#333'
-    blackTitle = '#333'
-    font="Calibri" #"Raleway"
-    labelFont="Arial" #"Nunito Sans"
-    sourceFont=labelFont
-    backgroundColor = "white"
-    
-    gridColor=axisColor
-    
-    # Colors
+GH code: 
+https://github.com/chekos/altair_themes_blog/tree/master/notebooks
 
-    return {
-        # width and height are configured outside the config dict because they are Chart configurations/properties not chart-elements' configurations/properties.
-        "width": 685, # from the guide
-        "height": 380, # not in the guide
-        "config": {
-            "title": {
-                "fontSize": 18,
-                "font": font,
-                "anchor": "start", # equivalent of left-aligned.
-                "fontColor": "#000000"
-            },
-            "axisX": {
-                "domain": True,
-                "domainColor": axisColor,
-                "domainWidth": 1,
-                "grid": False,
-                "labelFont": labelFont,
-                "labelFontSize": 12,
-                "labelAngle": 0, 
-                "tickColor": axisColor,
-                "tickSize": 5, # default, including it just to show you can change it
-                "titleFont": font,
-                "titleFontSize": 12,
-                "titlePadding": 10, # guessing, not specified in styleguide
-                "title": "X Axis Title (units)", 
-            },
-            "axisY": {
-                "domain": False,
-                "grid": True,
-                "gridColor": gridColor,
-                "gridWidth": 1,
-                "labelFont": labelFont,
-                "labelFontSize": 12,
-                "labelAngle": 0, 
-                "ticks": False, # even if you don't have a "domain" you need to turn these off.
-                "titleFont": font,
-                "titleFontSize": 12,
-                "titlePadding": 10, # guessing, not specified in styleguide
-                "title": "Y Axis Title (units)", 
-                # titles are by default vertical left of axis so we need to hack this 
-                "titleAngle": 0, # horizontal
-                "titleY": -10, # move it up
-                "titleX": 18, # move it to the right so it aligns with the labels 
-            },
-            "range": {
-                "category": PALETTE["category_bright"],
-                "diverging": PALETTE["diverging"],
-            },
-            "legend": {
-                "labelFont": labelFont,
-                "labelFontSize": 12,
-                "symbolType": "square", # just 'cause
-                "symbolSize": 100, # default
-                "titleFont": font,
-                "titleFontSize": 12,
-                "title": "", # set it to no-title by default
-                "orient": "top-left", # so it's right next to the y-axis
-                "offset": 0, # literally right next to the y-axis.
-            },
-            "view": {
-                "stroke": "transparent", # altair uses gridlines to box the area where the data is visualized. This takes that off.
-            },
-            "background": {
-                "color": "#FFFFFF", # white rather than transparent
-            },
-            ### MARKS CONFIGURATIONS ###
-            "area": {
-               "fill": markColor,
-           },
-           "line": {
-               "color": markColor,
-               "stroke": markColor,
-               "strokeWidth": 5,
-           },
-           "trail": {
-               "color": markColor,
-               "stroke": markColor,
-               "strokeWidth": 0,
-               "size": 1,
-           },
-           "path": {
-               "stroke": markColor,
-               "strokeWidth": 0.5,
-           },
-           "point": {
-               "filled": True,
-           },
-           "text": {
-               "font": sourceFont,
-               "color": markColor,
-               "fontSize": 11,
-               "align": "right",
-               "fontWeight": 400,
-               "size": 11,
-           }, 
-           "bar": {
-                "size": 40,
-                "binSpacing": 1,
-                "continuousBandSize": 30,
-                "discreteBandSize": 30,
-                "fill": markColor,
-                "stroke": False,
-            },
-}
-    }
+Download Google fonts:
+https://gist.github.com/ravgeetdhillon/0063aaee240c0cddb12738c232bd8a49
 
+Altair GH issue setting custom theme: 
+https://github.com/altair-viz/altair/issues/1333
+
+https://discourse.holoviz.org/t/altair-theming/1421/2
+
+https://stackoverflow.com/questions/33061785/can-i-load-google-fonts-with-matplotlib-and-jupyter
+"""
 
 '''
 # Run this in notebook
@@ -157,3 +58,154 @@ def calitp_theme():
 @import url('https://fonts.googleapis.com/css?family=Lato');
 </style>
 '''
+
+def calitp_theme(font=font, labelFont=labelFont, font_size = font_size, 
+                 chart_width = chart_width, chart_height = chart_height,
+                 markColor = markColor, axisColor = axisColor,
+                 guideLabelColor = guideLabelColor, guideTitleColor = guideTitleColor,
+                 blackTitle = blackTitle, backgroundColor = backgroundColor,
+                 PALETTE=PALETTE):
+    # Typography
+    # At Urban it's the same font for all text but it's good to keep them separate in case you want to change one later.
+    labelFont = labelFont 
+    sourceFont = labelFont
+     
+    return {
+            # width and height are configured outside the config dict because they are Chart configurations/properties not chart-elements' configurations/properties.
+            "width": chart_width, # from the guide
+            "height": chart_height, # not in the guide
+            "background": backgroundColor,
+            "config": {
+                "title": {
+                    "fontSize": font_size,
+                    "font": font,
+                    "anchor": "middle", 
+                    "fontColor": blackTitle,
+                    "fontWeight": "bold", #300 was default. can also use lighter, bold, normal, bolder
+                    "offset": 20,
+                },
+                "header": {
+                    "labelFont": labelFont,
+                    "titleFont": font,
+                }, 
+                "axis": {
+                    "domain": True,
+                    "domainColor": axisColor,
+                    "grid": True,
+                    "gridColor": axisColor,
+                    "gridWidth": 1,
+                    "labelColor": guideLabelColor,
+                    "labelFontSize": 10,
+                    "titleColor": guideTitleColor,
+                    "tickColor": axisColor,
+                    "tickSize": 10,
+                    "titleFontSize": 12,
+                    "titlePadding": 10,
+                    "labelPadding": 4,
+                  },
+                "axisBand": {
+                    "grid": False,
+                  },
+                "range": {
+                    "category_bright": PALETTE["category_bright"],
+                    "category_bold": PALETTE["category_bold"],
+                    "diverging": PALETTE["diverging"],
+                    "sequential": PALETTE["sequential"],
+                }, 
+                "legend": {
+                    "labelFont": labelFont,
+                    "labelFontSize": 11,
+                    "symbolType": "square", 
+                    "symbolSize": 30, # default
+                    "titleFont": font,
+                    "titleFontSize": 14,
+                    "titlePadding": 10,
+                    "padding": 1,
+                    "orient": "right", 
+                    #"offset": 0, # literally right next to the y-axis.
+                    "labelLimit": 0, # legend can fully display text instead of truncating it
+                },
+                "view": {
+                    "stroke": "transparent", # altair uses gridlines to box the area where the data is visualized. This takes that off.
+                },    
+                "group": {
+                    "fill": backgroundColor,
+                },
+                ### MARKS CONFIGURATIONS ###
+                "arc": {
+                    "fill": markColor,
+                }, 
+                "area": {
+                   "fill": markColor,
+               },
+               "line": {
+                   #"color": markColor,
+                   "stroke": markColor,
+                   "strokeWidth": 2,
+               },
+               "trail": {
+                   "color": markColor,
+                   "stroke": markColor,
+                   "strokeWidth": 0,
+                   "size": 1,
+               },
+               "path": {
+                   "stroke": markColor,
+                   "strokeWidth": 0.5,
+               },
+               "rect": {
+                   "fill": markColor,
+               },
+               "point": {
+                   "filled": True,
+                   "shape": "circle",
+               },
+               "shape": {
+                   "stroke": markColor
+               },
+               "text": {
+                   "font": sourceFont,
+                   "color": markColor,
+                   "fontSize": 11,
+                   "align": "center",
+                   "fontWeight": 400,
+                   "size": 11,
+               }, 
+               "bar": {
+                    #"size": 40,
+                    "binSpacing": 2,
+                    #"continuousBandSize": 30,
+                    #"discreteBandSize": 30,
+                    "fill": markColor,
+                    "stroke": False,
+                },
+            }
+        }
+
+
+# Let's add in more top-level chart configuratinos
+# But, we can always adjust elements by adjusting chart_width or chart_height in a notebook
+# Need to add more since altair_saver will lose a lot of the theme applied
+# Has been adjusted to match the calitp_theme above.
+def preset_chart_config(chart):
+    chart = (chart.properties(
+                width = chart_width, height = chart_height,
+            ).configure(background=backgroundColor, font=font)
+             .configure_axis(
+                 domainColor=axisColor, grid=True,
+                 gridColor=axisColor, gridWidth=1,
+                 labelColor=guideLabelColor, labelFont=labelFont, labelFontSize=10,
+                 titleColor=guideTitleColor, titleFont=font, tickColor=axisColor, 
+                 tickSize=10,titleFontSize=12, titlePadding=10,labelPadding=4,
+             ).configure_axisBand(grid=False)
+             .configure_title(font=font, fontSize=font_size, anchor='middle',
+                              fontWeight=300, offset=20,)
+             .configure_header(labelFont=labelFont, titleFont=font)
+             .configure_legend(labelColor=blackTitle, labelFont=labelFont, labelFontSize=11,
+                               padding=1,symbolSize=30,symbolType= 'square',
+                               titleColor=blackTitle, titleFont=font, titleFontSize=14,
+                               titlePadding=10,
+                               labelLimit=0
+                              )
+            )
+    return chart
