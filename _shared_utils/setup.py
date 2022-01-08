6b1486937a695916b9069cac07eabd0d9bffb6b5
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-01-08 00:41:47 +0000

    Add altair_saver back to setup

--- _shared_utils/setup.py
+++ _shared_utils/setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='shared_utils',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='Shared utility functions for data analyses',
     author='Cal-ITP',
     license='Apache',
     include_package_data=True,
     package_dir={"_shared_utils": "shared_utils"},
-    install_requires=[],
+    install_requires=["altair_saver"],
 )
