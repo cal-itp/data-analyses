a27b0543bc5448bb913c7811e555407a3ae1ea60
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-01-12 17:18:41 +0000

    Dissolve highways more and overlay

--- _shared_utils/shared_utils/geography_utils.py
+++ _shared_utils/shared_utils/geography_utils.py
@@ -18,6 +18,8 @@ CA_StatePlane = "EPSG:2229" # units are in feet
 CA_NAD83Albers = "EPSG:3310" # units are in meters
 
 SQ_MI_PER_SQ_M = 3.86 * 10**-7
+FEET_PER_MI = 5_280
+SQ_FT_PER_SQ_MI = 2.788 * 10**7
 
 def aggregate_by_geography(df, group_cols, 
                        sum_cols = [], mean_cols = [], 
