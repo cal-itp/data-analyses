dbf36501aab5eb6bc929fbe638461f3692727c85
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-02-05 00:35:48 +0000

    Move adding zeros to outside the loop by day

--- bus_service_increase/scripts/create_analysis_data.py
+++ bus_service_increase/scripts/create_analysis_data.py
@@ -5,8 +5,11 @@ import pandas as pd
 import create_calenviroscreen_lehd_data
 import utils
 import shared_utils
+import warehouse_queries
 
+from calitp.tables import tbl
 from siuba import *
+
 
 #------------------------------------------------------------------#
 ## Functions to create operator-route-level dataset
@@ -33,6 +36,7 @@ def get_time_calculations(df):
     
     return df
 
+
 def add_zeros(one_operator_df):
     '''
     Creates rows with 0 trips for hour for all departure hours x shape_ids where that shape_id wasn't running at all
@@ -43,10 +47,18 @@ def add_zeros(one_operator_df):
     '''
     shape_frequency = one_operator_df
     ## insert nulls or 0 for missing values as appropriate
-    shapes_routes = shape_frequency[['shape_id', 'route_id']].set_index('shape_id').to_dict()['route_id']
-    iterables = [shape_frequency.shape_id.unique(), ['Thursday', 'Saturday', 'Sunday'], range(0, 24)]
-    multi_ix = pd.MultiIndex.from_product(iterables, names=['shape_id', 'day_name', 'departure_hour'])
+    shapes_routes = (shape_frequency[['shape_id', 'route_id']]
+                     .set_index('shape_id').to_dict()['route_id'])
+    
+    # Currently, day_name_list is ['Thursday', 'Saturday', 'Sunday']...from pre-selected dates
+    # Generalize, in case we change it
+    day_name_list = list(one_operator_df.day_name.unique())
+    iterables = [shape_frequency.shape_id.unique(), day_name_list, range(0, 24)]
+    
+    multi_ix = pd.MultiIndex.from_product(iterables, 
+                                          names=['shape_id', 'day_name', 'departure_hour'])
     shape_frequency = shape_frequency.set_index(['shape_id', 'day_name', 'departure_hour'])
+    
     try:
         shape_frequency = shape_frequency.reindex(multi_ix).reset_index()
     except ValueError: ## aggregate rare shape_ids with multiple routes (these may be errors)
@@ -56,8 +68,13 @@ def add_zeros(one_operator_df):
     # out of an ix of {len(shape_frequency.index)}
     #     ''')
         assert len(duplicate_indicies) < len(shape_frequency.index) / 100, 'too many duplicate shape/day/departure_hour'
-        shape_frequency = shape_frequency.groupby(['shape_id', 'day_name', 'departure_hour']).agg(
-            {'calitp_itp_id':max, 'route_id':max, 'trips_per_hour':sum, 'mean_runtime_min':np.mean})
+        shape_frequency = (shape_frequency.groupby(['shape_id', 'day_name', 'departure_hour'])
+                           .agg({'calitp_itp_id':max, 
+                                 'route_id':max, 
+                                 'trips_per_hour':sum, 
+                                 'mean_runtime_min':"mean"})
+                          )
+        
         shape_frequency = shape_frequency.reindex(multi_ix).reset_index()
 
     shape_frequency['calitp_itp_id'] = (shape_frequency['calitp_itp_id']
@@ -65,7 +82,9 @@ def add_zeros(one_operator_df):
                                         .fillna(method='ffill')
                                        )
     shape_frequency['trips_per_hour'] = shape_frequency['trips_per_hour'].fillna(0)
+    
     return shape_frequency
+
 
 def add_all_zeros(shape_frequency_df):
     '''
@@ -75,10 +94,14 @@ def add_all_zeros(shape_frequency_df):
     df_with_zeros = (df_with_zeros.reset_index(drop=True)
              .astype({'calitp_itp_id': 'int64'}) ##fix type
              [['calitp_itp_id', 'shape_id', 'departure_hour',
-               'day_name', 'route_id', 'trips_per_hour', 'mean_runtime_min']]) ##order to match original
+               'day_name', 'route_id', 'trips_per_hour', 
+               'mean_runtime_min']]) ##order to match original
+    
     return df_with_zeros
 
+
 def calculate_runtime_hourlytrips(df):
+        
     # Calculate run time for a trip
     # Find the median stop (keep that observation)
     def find_runtime(df):
@@ -110,12 +133,6 @@ def calculate_runtime_hourlytrips(df):
                            )
     )
     
-    debug_me = middle_stops[middle_stops.mean_runtime_min.isna()][
-        ["calitp_itp_id", "shape_id", "trip_key"]]
-    print("Debug errors for NaN mean runtimes")
-    print(debug_me.head())
-    # Why are there some NaNs from this, when NaNs were dropped before?
-    # Some are due to no departure_time (handle it above by dropping NaTs)
     
     middle_stops = middle_stops.assign(
         mean_runtime_min = (middle_stops.mean_runtime_min.dropna()     
@@ -154,11 +171,8 @@ def calculate_runtime_hourlytrips(df):
     
     # Now, drop ITP_ID==200 to use individual operator feeds
     shape_frequency3 = shape_frequency2 >> filter(_.calitp_itp_id != 200)
-    
-    # Add zeros for each hour when existing service doesn't run at all
-    shape_frequency4 = add_all_zeros(shape_frequency3)
      
-    return shape_frequency4
+    return shape_frequency3
 
 
 def attach_funding(all_operators_df):
@@ -186,23 +200,24 @@ def attach_funding(all_operators_df):
 
 # Loop through and grab weekday/Sat/Sun subsets of joined data, calculate runtimes
 def create_service_estimator_data():
-    #DATA_PATH = "./data/test/"
     DATA_PATH = "gs://calitp-analytics-data/data-analyses/bus_service_increase/test/"
     GCS_FILE_PATH = DATA_PATH
     
     time0 = dt.datetime.now()
+    
+    dates = warehouse_queries.dates
     
     processed_dfs = {}
     for key in dates.keys():
         start_time_loop = dt.datetime.now()
 
         print(f"Grab selected trips for {key}")
-        days_st = pd.read_parquet(f"{DATA_PATH}trips_{key}.parquet")
+        #days_st = pd.read_parquet(f"{DATA_PATH}trips_{key}.parquet")
 
         print(f"Do time calculations for {key}")
-        st_trips_joined = get_time_calculations(days_st)
-        #st_trips_joined.to_parquet(f"{DATA_PATH}timecalc_{key}")
+        #st_trips_joined = get_time_calculations(days_st)
         
+        st_trips_joined = pd.read_parquet(f"{DATA_PATH}timecalc_{key}.parquet")
         print(f"Calculate runtimes for {key}")    
         processed_dfs[key] = calculate_runtime_hourlytrips(st_trips_joined)
 
@@ -215,12 +230,13 @@ def create_service_estimator_data():
     # Append into 1 dataframe and export
     all_operators_shape_frequency = pd.DataFrame()
     for key, value in processed_dfs.items():
-        #Difference between concat, append
-        #https://stackoverflow.com/questions/15819050/pandas-dataframe-concat-vs-append/48168086
         all_operators_shape_frequency = pd.concat([
             all_operators_shape_frequency,
             value], ignore_index=True, axis=0)
 
+    # Add zeros for each hour when existing service doesn't run at all
+    all_operators_shape_frequency = add_all_zeros(all_operators_shape_frequency)    
+    
     all_operators_shape_frequency.to_parquet(
         f"{GCS_FILE_PATH}shape_frequency.parquet")
     
@@ -238,7 +254,7 @@ def create_service_estimator_data():
     print(f"Total execution time: {funding_time - time0}")
     
     # Cleanup local files
-    os.remove(f"{DATA_PATH}trips_*.parquet")
+    #os.remove(f"{DATA_PATH}trips_*.parquet")
     
 
 #------------------------------------------------------------------#
