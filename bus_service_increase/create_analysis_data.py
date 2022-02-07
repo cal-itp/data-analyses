4781bba776e7413a747cc21f7972f11a2d55b684
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-02-07 17:57:24 +0000

    Add sanity check notebook to debug operator differences

--- bus_service_increase/create_analysis_data.py
+++ bus_service_increase/create_analysis_data.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import geopandas as gpd
+import numpy as np
 import pandas as pd
 
 import create_calenviroscreen_lehd_data
@@ -9,7 +10,6 @@ import warehouse_queries
 
 from calitp.tables import tbl
 from siuba import *
-
 
 #------------------------------------------------------------------#
 ## Functions to create operator-route-level dataset
@@ -257,6 +257,100 @@ def create_service_estimator_data():
     #os.remove(f"{DATA_PATH}trips_*.parquet")
     
 
+## Start functions related to A2
+# Categorize tracts and add back further process the operator-route-level df
+def generate_shape_categories(shapes_df):
+    shapes_df = (shapes_df.reset_index(drop=True)
+                 .to_crs(shared_utils.geography_utils.CA_NAD83Albers)
+                )
+    
+    shapes_df = shapes_df.assign(
+        geometry = shapes_df.geometry.simplify(tolerance=1),
+    )
+    
+    ## quick fix for invalid geometries?
+    ces_df = (create_calenviroscreen_lehd_data.generate_calenviroscreen_lehd_data()
+              .to_crs(shared_utils.geography_utils.CA_NAD83Albers)
+             )
+
+    ces_df = ces_df.assign(
+        tract_type = ces_df['pop_sq_mi'].apply(lambda x: 'urban' if x > 2400 
+                                               else 'suburban' if x > 800 
+                                               else 'rural'),
+        ## quick fix for invalid geometries (comes up in dissolve later)
+        geometry = ces_df.geometry.buffer(0),
+    )
+    
+    category_dissolved = ces_df[["tract_type", "geometry"]].dissolve(by='tract_type')
+    # Since CRS is in meters, tolerance is anything < 1m off gets straightened
+    category_dissolved["geometry"] = category_dissolved.geometry.simplify(tolerance=1)
+    
+
+    urban = shapes_df.clip(category_dissolved.loc[['urban']])
+    suburban = shapes_df.clip(category_dissolved.loc[['suburban']])
+    rural = shapes_df.clip(category_dissolved.loc[['rural']])
+
+    shapes_df['pct_urban'] = urban.geometry.length / shapes_df.geometry.length
+    shapes_df['pct_suburban'] = suburban.geometry.length / shapes_df.geometry.length
+    shapes_df['pct_rural'] = rural.geometry.length / shapes_df.geometry.length
+    
+    shapes_df['pct_max'] = shapes_df[
+        ['pct_urban', 'pct_suburban', 'pct_rural']].max(axis=1)
+    
+    return shapes_df
+
+
+def categorize_shape(row):
+    if row.pct_urban == row.pct_max:
+        row['tract_type'] = 'urban'
+    elif row.pct_suburban == row.pct_max:
+        row['tract_type'] = 'suburban'
+    elif row.pct_rural == row.pct_max:
+        row['tract_type'] = 'rural'
+    else:
+        row['tract_type'] = np.nan
+    return row
+
+
+def create_shapes_tract_categorized():
+    DATA_PATH = "./data/test/"
+    #DATA_PATH = "gs://calitp-analytics-data/data-analyses/bus_service_increase/test/"
+    
+    time0 = dt.datetime.now()
+    
+    # Move creating linestring to this    
+    itp_ids = pd.read_parquet(f"{DATA_PATH}shape_frequency.parquet")
+    
+    itp_ids = list(itp_ids.calitp_itp_id.unique())    
+    print(f"Grab ITP IDs")
+    print(itp_ids)
+    
+    all_shapes = shared_utils.geography_utils.make_routes_shapefile(
+        ITP_ID_LIST = itp_ids)
+    
+    time1 = dt.datetime.now()
+    print(f"Execution time to make routes shapefile: {time1-time0}")
+    
+    # THIS IS GEOPARQUET, MUST USE SHARED_UTILS TO UPLOAD TO GCS
+    all_shapes.to_parquet(f"{DATA_PATH}shapes_initial.parquet")
+    #shared_utils.utils.geoparquet_gcs_export(all_shapes, utils.GCS_FILE_PATH, 'shapes_initial')
+    
+    #all_shapes = gpd.read_parquet(f'{utils.GCS_FILE_PATH}shapes_initial.parquet')
+    all_shapes = gpd.read_parquet(f"{DATA_PATH}shapes_initial.parquet")
+    
+    time2 = dt.datetime.now()
+    processed_shapes = generate_shape_categories(all_shapes)
+    processed_shapes = processed_shapes.apply(categorize_shape, axis=1)
+        
+    print(f"Execution time to categorize routes: {time2-time1}")
+    processed_shapes.to_parquet(f"{DATA_PATH}shapes_processed.parquet")
+    #shared_utils.utils.geoparquet_gcs_export(processed_shapes, utils.GCS_FILE_PATH, 
+    #                                         'shapes_processed')
+    
+    print(f"Total execution time: {time2-time0}")
+    
+   
+    
 #------------------------------------------------------------------#
 ## Functions to create tract-level dataset
 #------------------------------------------------------------------#
@@ -312,7 +406,8 @@ if __name__ == "__main__":
     # Run this to get the static parquet files
     
     # Get analysis dataset for service increase estimator?
-    create_service_estimator_data()
+    #create_service_estimator_data()
+    create_shapes_tract_categorized()
     
     # Get analysis dataset for bus arrivals by tract
-    create_bus_arrivals_by_tract_data()
+    #create_bus_arrivals_by_tract_data()
