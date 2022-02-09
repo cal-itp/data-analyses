0e63c90912c7da44fe890864da66ec642cf584f2
amandaha8 <amandahastuyck@gmail.com>
2022-02-09 23:23:37 +0000

    5311: data prep modifications, parquet, more charting

--- 5311/data_prep.py
+++ 5311/data_prep.py
@@ -11,6 +11,13 @@ from plotnine import *
 import intake
 from shared_utils import geography_utils
 
+import altair as alt
+import altair_saver
+from shared_utils import geography_utils
+from shared_utils import altair_utils
+from shared_utils import calitp_color_palette as cp
+from shared_utils import styleguide
+
 GCS_FILE_PATH = "gs://calitp-analytics-data/data-analyses/5311 /"
 
 
@@ -180,6 +187,28 @@ def GTFS():
 '''
 charting functions 
 '''
+#Labels
+def labeling(word):
+    # Add specific use cases where it's not just first letter capitalized
+    LABEL_DICT = { "prepared_y": "Year",
+              "dist": "District",
+              "total_requested": "Total Requested",
+              "fed_requested":"Fed Requested",
+              "ac_requested": "Advance Construction Requested",
+              "nunique":"Number of Unique",
+              "project_no": "Project Number"}
+    
+    if (word == "mpo") or (word == "rtpa"):
+        word = word.upper()
+    elif word in LABEL_DICT.keys():
+        word = LABEL_DICT[word]
+    else:
+        #word = word.replace('n_', 'Number of ').title()
+        word = word.replace('unique_', "Number of Unique ").title()
+        word = word.replace('_', ' ').title()
+    
+    return word
+
 # Bar
 def basic_bar_chart(df, x_col, y_col):
     
