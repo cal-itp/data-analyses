21b80c8bf8f94a3e061d65ef59a2872f59ef88eb
Charles Costanzo <cvc5185@gmail.com>
2022-02-23 11:33:34 -0500

    analyses(pre-commit): ran black on shared utils directory

--- _shared_utils/shared_utils/__init__.py
+++ _shared_utils/shared_utils/__init__.py
@@ -4,7 +4,10 @@ from . import calitp_color_palette, styleguide, altair_utils
 __version__ = "0.1.1"
 
 __all__ = [
-    "utils", "geography_utils", 
-    "map_utils", "altair_utils",
-    "calitp_color_palette", "styleguide", 
+    "utils",
+    "geography_utils",
+    "map_utils",
+    "altair_utils",
+    "calitp_color_palette",
+    "styleguide",
 ]
