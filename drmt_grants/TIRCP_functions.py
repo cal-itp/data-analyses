3437ef61d175cd94aec8e2f4a3beec3748c748b9
amandaha8 <amandahastuyck@gmail.com>
2022-03-03 16:17:20 +0000

    Replaced counties & districts for mapping

--- drmt_grants/TIRCP_functions.py
+++ drmt_grants/TIRCP_functions.py
@@ -246,7 +246,7 @@ def semi_annual_report():
     })
     )
     ### GCS ###
-    with pd.ExcelWriter(f"{GCS_FILE_PATH}FUNCTION_TEST_TIRCP_SAR.xlsx") as writer:
+    with pd.ExcelWriter(f"{GCS_FILE_PATH}Semi_Annual_Report.xlsx") as writer:
         summary_table_2.to_excel(writer, sheet_name="Summary", index=True)
         df_pivot.to_excel(writer, sheet_name="FY", index=True)
     return df_pivot
@@ -347,7 +347,7 @@ def program_allocation_plan():
     df_2020 = pivot(df_combined.loc[df_combined['Award_Year'] == 2020])
     
     #GCS 
-    with pd.ExcelWriter(f'{GCS_FILE_PATH}FUNCTION_TEST_PAP.xlsx') as writer:
+    with pd.ExcelWriter(f'{GCS_FILE_PATH}Program_Allocation_Plan.xlsx') as writer:
         df_2015.to_excel(writer, sheet_name="2015_Cycle_1", index=True)
         df_2016.to_excel(writer, sheet_name="2016_Cycle_2", index=True)
         df_2018.to_excel(writer, sheet_name="2018_Cycle_3", index=True)
@@ -363,7 +363,7 @@ Tableau
 ### SHEET CONNECTED TO TABLEAU ### 
 def tableau():
     #Keeping only the columns we want
-    df = project() 
+    df = project()
     
     #Getting percentages & filling in with 0
     df['Expended_Percent'] = df['Expended_Amt_project_sheet']/df['Allocated_Amount']
@@ -426,6 +426,25 @@ def tableau():
                                             6:'District 6: Fresno / Bakersfield',
                                             1:'District 1: Eureka'
                                                  }))
+    #Renaming counties
+    df['County'] = (df['County'].replace({'LA': 'Los Angeles', 
+                                          'VAR': 'Various', 
+                                          'MON': 'Mono', 
+                                          'ORA':'Orange', 
+                                          'SAC':'Sacramento', 
+                                          'SD':'San Diego', 
+                                          'SF': 'San Francisco', 
+                                          'SJ':'San Joaquin', 
+                                          'SJ ': 'San Joaquin', 
+                                          'FRE':"Fresno",
+                                           'SBD':'San Bernandino', 
+                                          'SCL':'Santa Clara', 
+                                          'ALA':'Alameda', 
+                                          'SM':'San Mateo', 
+                                          'SB': 'San Barbara', 
+                                          'SON, MRN': 'Various', 
+
+                                                 }))
     
     #Which projects are large,small, medium
     p75 = df.TIRCP_project_sheet.quantile(0.75).astype(float)
@@ -448,7 +467,10 @@ def tableau():
                                                 'TIRCP_project_sheet': "TIRCP_Amount"}
                   ))
     ### GCS ###
-    df.to_csv(f'{GCS_FILE_PATH}df_tableau_sheet.csv', index = False)
+    with pd.ExcelWriter(f"{GCS_FILE_PATH}Tableau_Sheet.xlsx") as writer:
+        df.to_excel(writer, sheet_name="Data", index=False)
+    return df
+
     return df 
 
 '''
