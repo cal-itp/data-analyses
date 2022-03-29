07c9493f95848f9eb5e52b34d09d078c0311db4c
Andrew Vaccaro <atvaccaro@gmail.com>
2022-03-29 17:06:18 -0400

    fmt

--- _shared_utils/shared_utils/geography_utils.py
+++ _shared_utils/shared_utils/geography_utils.py
@@ -13,9 +13,9 @@ WGS84 = "EPSG:4326"
 CA_StatePlane = "EPSG:2229"  # units are in feet
 CA_NAD83Albers = "EPSG:3310"  # units are in meters
 
-SQ_MI_PER_SQ_M = 3.86 * 10 ** -7
+SQ_MI_PER_SQ_M = 3.86 * 10**-7
 FEET_PER_MI = 5_280
-SQ_FT_PER_SQ_MI = 2.788 * 10 ** 7
+SQ_FT_PER_SQ_MI = 2.788 * 10**7
 
 
 def aggregate_by_geography(
