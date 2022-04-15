cc7aa218a08e82f927361cea49950d8d02909826
natam1 <natalielamberg@gmail.com>
2022-04-15 22:14:41 +0000

    updates to utils and charting function, testing report

--- dla/e76_obligated_funds/gen_report.py
+++ dla/e76_obligated_funds/gen_report.py
@@ -20,13 +20,21 @@ from shared_utils import styleguide
 from calitp import to_snakecase
 import intake
 
+from calitp.storage import get_fs
+fs = get_fs()
+
 import _dla_utils
 
 import ipywidgets as widgets
 
-df= pd.read_parquet("dla_df.parquet")
 
-def make_charts(df, subset):     
+#GCS_FILE_PATH = 'gs://calitp-analytics-data/data-analyses/dla/e-76Obligated/'
+
+
+#df= pd.read_parquet("dla_df.parquet")
+#df = pd.read_parquet(f"{GCS_FILE_PATH}dla_df.parquet")
+
+def make_charts(subset):     
     df= pd.read_parquet("dla_df.parquet")
     
     df = (df>>filter(_.dist==subset))
@@ -37,7 +45,7 @@ def make_charts(df, subset):
     
     
     ##print statements
-    display(HTML(f"<h1><strong>District {subset} Analysis</strong></h1>"))
+    #display(HTML(f"<h1><strong>District {subset} Analysis</strong></h1>"))
     
     display(HTML("<h2>Quick Stats</h2>"))
     
@@ -60,34 +68,36 @@ def make_charts(df, subset):
 
 
     ##tables
-    
-    display(HTML("<strong>Number of Unique Prefix Codes by Agency</stromg>"))
+   
+    display(HTML("<h4><strong>Number of Unique Prefix Codes by Agency</strong></h4>"))
     display((_dla_utils.get_nunique(df, 'prefix', 'primary_agency_name'))
             .rename(columns={'primary_agency_name':'Agency',
                             'n':'Number of Unqiue Prefix Codes'})
             .head(5))
     
-    display(HTML("<strong>Number of Unique Agencies by Prefix Codes</strong>"))
+    display(HTML("<h4><strong>Number of Unique Agencies by Prefix Codes</strong></h4>"))
     display((_dla_utils.get_nunique(df, 'primary_agency_name', 'prefix'))
             .rename(columns={'prefix':'Prefix',
                             'n':'Number of Unqiue Agencies'})
             .head(5))
     
-    display(HTML("<strong>Top 5 Types of Work</strong>"))
-    display((transit>>count(_.type_of_work)>>arrange(-_.n)>>select(_.type_of_work))
+    display(HTML("<h4><strong>Top 5 Types of Work</strong></h4>"))
+    work_types =  ((df>>count(_.type_of_work)>>arrange(-_.n)>>select(_.type_of_work))
             .rename(columns={'type_of_work':'Type of Work'})
             .head(5))
+    display(work_types.style)
 
        
     #Visual Charts
 
-    display(HTML("<h2>Number of Obligations</h2"))
+    display(Markdown("<h2> Number of Obligations </h2>"))
     
     #Line chart for Obligations by Year
     chart_df = (df_top>>filter(_.variable=='prepared_y')).rename(columns= {"value":"Year"})
     
-    chart1= (_dla_utils.basic_line_chart_test_no_save(chart_df, 'Year', 'count', subset, 'Obligations by Year')).encode(x=alt.X('Year:O', title='Prepared Year'))
+    chart1= (_dla_utils.basic_line_chart_test_no_save(chart_df, 'Year', 'count', subset)).encode(x=alt.X('Year:O', title='Prepared Year'))
     
+    display(HTML("<h4><strong>Obligations by Year</strong></h4>"))
     display(chart1)
     
     # Unique Agencies by Dist
@@ -109,18 +119,21 @@ def make_charts(df, subset):
                               scale=alt.Scale(
                                   range=altair_utils.CALITP_SEQUENTIAL_COLORS),  
                                legend=alt.Legend(title="Prepared Year")
-                              )
-                              ).properties( 
-                       title="Number of Unique Agencies by District"))
+                              )))
+                              
     chart10 = styleguide.preset_chart_config(chart10)
     chart10 = _dla_utils.add_tooltip(chart10, 'prepared_y', 'Count')
+    
+    display(HTML("<h4><strong>Number of Unique Agencies by District</strong></h4>"))
     display(chart10)
     
   
     #Bar chart Agencies With The Most Obligations
     chart_df = (df_top>>filter(_.variable=='primary_agency_name')).rename(columns={"value":"Agency",
                                  "count":"Number of Obligations"})
-    chart2= (_dla_utils.basic_bar_chart_no_save(chart_df, 'Agency', 'Number of Obligations', 'Agency', subset, 'Agencies With The Most Obligations'))
+    chart2 = (_dla_utils.basic_bar_chart_no_save(chart_df, 'Agency', 'Number of Obligations', 'Agency', subset))
+    
+    display(HTML("<h4><strong>Agencies With The Most Obligations</strong></h4>"))
     display(chart2)
     
     
@@ -143,23 +156,28 @@ def make_charts(df, subset):
                                   range=altair_utils.CALITP_SEQUENTIAL_COLORS),  
                                legend=alt.Legend(title="District")
                               )
-                              ).properties( 
-                       title="Number of Unique Prefix Codes by District"))
+                              ))
     chart11 = styleguide.preset_chart_config(chart11)
     chart11 = _dla_utils.add_tooltip(chart11, 'prepared_y','Count')
+    
+    display(HTML("<h4><strong>Number of Unique Prefix Codes by District</strong></h4>"))
     display(chart11)
 
     #Bar chart with the Most Used Prefix Counts
     chart_df = (df_top>>filter(_.variable=='prefix')).rename(columns={"value":"Prefix",
                                  "count":"Number of Obligations"})
-    chart9= (_dla_utils.basic_bar_chart_no_save(chart_df, 'Prefix', 'Number of Obligations', 'Prefix', subset, 'Most Used Prefix Codes'))
+    chart9= (_dla_utils.basic_bar_chart_no_save(chart_df, 'Prefix', 'Number of Obligations', 'Prefix', subset))
+    
+    display(HTML("<h4><strong>Most Used Prefix Codes</strong></h4>"))
     display(chart9)
     
     
     #Bar chart Agencies With The Most Unique Prefix Codes
     
     chart3 = (_dla_utils.basic_bar_chart_no_save(((_dla_utils.get_nunique(df, 'prefix', 'primary_agency_name')).head(30)),
-                            'primary_agency_name', 'n', 'primary_agency_name', subset, 'Agencies With The Most Unique Prefix Codes'))
+                            'primary_agency_name', 'n', 'primary_agency_name', subset))
+    
+    display(HTML("<h4><strong>Agencies With The Most Unique Prefix Codes</strong></h4>"))
     display(chart3)
     
     
@@ -168,9 +186,11 @@ def make_charts(df, subset):
     #Bar chart Average Total Requested Funds by Agency
     chart4=(_dla_utils.basic_bar_chart_no_save((((_dla_utils.calculate_data_all(df, 'adjusted_total_requested', 'primary_agency_name', aggfunc="mean"))
                           >>arrange(-_.adjusted_total_requested)).head(30)
-                        ), 'primary_agency_name','adjusted_total_requested', 'primary_agency_name', subset,
-                           'Average Total Requested Funds by Agency ($2021)'
+                        ), 'primary_agency_name','adjusted_total_requested', 'primary_agency_name', subset
+                          
                        ))
+    
+    display(HTML("<h4><strong>Average Total Requested Funds by Agency ($2021)</strong></h4>"))
     display(chart4)
     
 
@@ -178,15 +198,17 @@ def make_charts(df, subset):
     #Bar chart Bottom Average Total Requested Funds by Agency
     avg_funds_bottom = (df>>group_by(_.primary_agency_name)>>summarize(avg_funds=_.adjusted_total_requested.mean())>>arrange(-_.avg_funds)).tail(50)
 
-    chart5=( _dla_utils.basic_bar_chart_no_save((avg_funds_bottom.tail(40)), 'primary_agency_name','avg_funds', 'primary_agency_name', subset, 
-                          'Lowest Average Total Funds by Agency ($2021'))
+    chart5=( _dla_utils.basic_bar_chart_no_save((avg_funds_bottom.tail(40)), 'primary_agency_name','avg_funds', 'primary_agency_name', subset))
+    
+    display(HTML("<h4><strong>Lowest Average Total Funds by Agency ($2021)</strong></h4>"))
     display(chart5)
     
     # Bar chart Average Total Requested Funds by Prefix
     chart8 = (_dla_utils.basic_bar_chart_no_save((((_dla_utils.calculate_data_all(df, 'adjusted_total_requested', 'prefix', aggfunc="mean"))
-                          >>arrange(-_.adjusted_total_requested)).head(30)), 'prefix','adjusted_total_requested', 'prefix', subset,
-                           'Average Total Requested Funds by Prefix ($2021)'
+                          >>arrange(-_.adjusted_total_requested)).head(30)), 'prefix','adjusted_total_requested', 'prefix', subset
                        ))
+    
+    display(HTML("<h4><strong>Average Total Requested Funds by Prefix ($2021)</strong></h4>"))
     display(chart8)
     
     
@@ -213,6 +235,9 @@ def make_charts(df, subset):
     for ii in enumerate(work_cat): 
         tab2.set_title(ii[0], f"{_dla_utils.labeling(ii[1])}")
 
+    display(tab2)
+    
+    
     with out1:
         _dla_utils.project_cat(df, 'active_transp') 
     with out2:
@@ -228,6 +253,6 @@ def make_charts(df, subset):
     with out7:
         _dla_utils.project_cat(df, 'congestion_relief')
 
-    display(tab2)
+    
     
     
