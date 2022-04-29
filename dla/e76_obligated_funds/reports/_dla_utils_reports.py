5d586f0b618341f34cc3711ebeeaa5448802308d
natam1 <natalielamberg@gmail.com>
2022-04-29 21:44:29 +0000

    add all dsitrict reports, take out widget tabs

--- dla/e76_obligated_funds/reports/_dla_utils_reports.py
+++ dla/e76_obligated_funds/reports/_dla_utils_reports.py
@@ -230,7 +230,6 @@ def project_cat(df, i, district):
     chart = add_tooltip(chart, "Agency", "Funding Amount")
     chart = chart.properties(width=400,height=70)
     #chart.save(f"./chart_outputs/project_cat/bar_{district}_{i}.png")
-
     #path = (f'/chart_outputs/project_cat/bar_{district}_{i}.png')
     #path2= ('/chart_outputs/project_cat/bar_4_active_transp.png')
     table = (subset_2.style.hide(axis='index').format(formatter={("Percent of Category"): "{:.2f}%"}))
