4e60dbc94132c3af1b60c6451d9cba74d926163a
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-05-04 16:30:24 +0000

    Stripplots with top 15 routes for each group, change sorting

--- bus_service_increase/parallel_corridors_utils.py
+++ bus_service_increase/parallel_corridors_utils.py
@@ -37,9 +37,10 @@ def operator_parallel_competitive_stats(itp_id, pct_trips_competitive_cutoff):
     parallel_df = setup_parallel_trips_with_stops.subset_to_parallel_routes(df)
     
     competitive_df = catalog.competitive_route_variability.read()
-    competitive_df = competitive_df[(competitive_df.calitp_itp_id == itp_id) & 
-                                    (competitive_df.pct_trips_competitive > pct_trips_competitive_cutoff)
-                                   ]
+    competitive_df = competitive_df[
+        (competitive_df.calitp_itp_id == itp_id) & 
+        (competitive_df.pct_trips_competitive > pct_trips_competitive_cutoff)
+    ]
     
     operator_dict = {
         "num_routes": df.route_id.nunique(),
@@ -55,6 +56,7 @@ def operator_parallel_competitive_stats(itp_id, pct_trips_competitive_cutoff):
 #------------------------------------------------------------#
 # Color to designate p25, p50, p75, fastest trip?
 DARK_GRAY = "#323434"
+NAVY = cp.CALITP_CATEGORY_BOLD_COLORS[0]
 
 def labeling(word):
     label_dict = {
@@ -91,19 +93,24 @@ def set_yaxis_range(df, y_col):
 
 
 def make_stripplot(df, y_col="bus_multiplier", Y_MIN=0, Y_MAX=5):  
+    # max_trip_route_group is in hours, convert to minutes to match bus_difference units
+    plus25pct_travel_minutes = df.max_trip_route_group.iloc[0] * 60 * 0.25
+    df = df.assign(
+        cutoff2 = plus25pct_travel_minutes
+    )
+    
     # We want to draw horizontal line on chart
     if y_col == "bus_multiplier":
         df = df.assign(cutoff=2)
-    else:
+    elif y_col == "bus_difference":
         df = df.assign(cutoff=0)
     
     # Use the same sorting done in the wrangling
     route_sort_order = list(df.sort_values(["calitp_itp_id", 
                                             "pct_trips_competitive", 
                                             "num_competitive",
-                                            "p50", 
-                                            f"{y_col}_spread"], 
-                                       ascending=[True, False, False, True, True]
+                                            "p50"], 
+                                       ascending=[True, False, False, True]
                                       )
                         .drop_duplicates(subset=["route_id"]).route_id)
         
@@ -157,6 +164,15 @@ def make_stripplot(df, y_col="bus_multiplier", Y_MIN=0, Y_MAX=5):
         )
     )
     
+    horiz_line2 = (
+        alt.Chart()
+        .mark_rule(strokeDash=[2,3])
+        .encode(
+            y=alt.Y("cutoff2:Q"),
+            color=alt.value(NAVY)
+        )
+    )
+    
     # Add labels
     # https://github.com/altair-viz/altair/issues/920
     text = (stripplot
@@ -169,9 +185,14 @@ def make_stripplot(df, y_col="bus_multiplier", Y_MIN=0, Y_MAX=5):
            ).transform_filter(alt.datum.fastest_trip==1)
         
     # Must define data with top-level configuration to be able to facet
+    if y_col == "bus_difference":
+        other_charts = p50 + horiz_line + horiz_line2 + text
+    else:
+        other_charts = p50 + horiz_line + text
+    
     chart = (
         (stripplot.properties(width=60) + 
-         p50 + horiz_line + text)
+         other_charts)
         .facet(
             column = alt.Column("route_id:N", title="Route ID",
                                 sort = route_sort_order), 
@@ -186,12 +207,44 @@ def make_stripplot(df, y_col="bus_multiplier", Y_MIN=0, Y_MAX=5):
     return chart
 
 
-def generate_report(df):
+PCT_COMPETITIVE_THRESHOLD = 0.75
+
+def generate_report(df, PCT_COMPETITIVE_THRESHOLD = PCT_COMPETITIVE_THRESHOLD):
     # Set up df for charting (cut-off at some threshold to show most competitive routes)
-    PCT_COMPETITIVE_THRESHOLD = 0.75
     plot_me = (df[df.pct_trips_competitive > PCT_COMPETITIVE_THRESHOLD]
            .drop(columns = "geometry")
     )
+    
+    
+    def top15_routes(df, route_group):
+        df2 = (df[df.route_group==route_group])
+        
+        # Set a cut-off to enable sorting, where most of the trips are 
+        # below a certain time difference cut-off, 
+        # grab top 15 routes where majority of trips are below that cut-off 
+        # cut-off done off of bus_difference because it's easier to understand
+        bus_difference_cutoff = df2.bus_difference.quantile(0.25)
+        
+        route_cols = ["calitp_itp_id", "route_id"]
+        
+        df2 = df2.assign(
+            below_cutoff = df2.apply(lambda x: 1 if x.service_hours <= bus_difference_cutoff 
+                                     else 0, axis=1),
+            num_trips = df2.groupby(route_cols)["trip_id"].transform("count")
+        )
+        
+        df2["below_cutoff"] = df2.groupby(route_cols)["below_cutoff"].transform("sum")
+        df2["pct_below_cutoff"] = df2.below_cutoff.divide(df2.num_trips)
+        
+        df3 = (df2
+               .sort_values(["calitp_itp_id", "below_cutoff", 
+                             "pct_below_cutoff", "route_id"],
+                            ascending = [True, False, False, True]
+                           )
+               .drop_duplicates(subset=["calitp_itp_id", "route_id"])
+              ).head(15)
+                
+        return list(df3.route_id)
     
     y_col1 = "bus_multiplier"
     Y_MIN1, Y_MAX1 = set_yaxis_range(plot_me, y_col1)
@@ -211,8 +264,23 @@ def generate_report(df):
             
         return multiplier_chart, difference_chart
     
-    s1, s2 = combine_stripplots(plot_me[plot_me.route_group=="short"])
+    
+    short_routes= top15_routes(plot_me, "short")
+    med_routes = top15_routes(plot_me, "medium")
+    long_routes = top15_routes(plot_me, "long")
+    
+    s1, s2 = combine_stripplots(plot_me[plot_me.route_id.isin(short_routes)])
+    m1, m2 = combine_stripplots(plot_me[plot_me.route_id.isin(med_routes)])
+    l1, l2 = combine_stripplots(plot_me[plot_me.route_id.isin(long_routes)])
             
-    display(Markdown("Short Routes"))
+    display(HTML("<h3>Short Routes</h3>"))
     display(s1)
     display(s2)
+    
+    display(HTML("<h3>Medium Routes</h3>"))
+    display(m1)
+    display(m2)
+    
+    display(HTML("<h3>Long Routes</h3>"))
+    display(l1)
+    display(l2)
