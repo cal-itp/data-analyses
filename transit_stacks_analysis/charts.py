2c1223c287cacf77ae0688499f25b8d2a0068348
amandaha8 <amandahastuyck@gmail.com>
2022-05-09 18:51:04 +0000

    Transit Stacks: updated visuals, double checked work

---
+++ transit_stacks_analysis/charts.py
@@ -0,0 +1,66 @@
+"""
+5311 Utils for Analysis and Charting 
+"""
+
+
+import numpy as np
+import pandas as pd
+from siuba import *
+from calitp import *
+from plotnine import *
+import intake
+from shared_utils import geography_utils
+
+import altair as alt
+import altair_saver
+from shared_utils import geography_utils
+from shared_utils import altair_utils
+from shared_utils import calitp_color_palette as cp
+from shared_utils import styleguide
+
+
+
+
+'''
+charting functions 
+'''
+#Labels
+def labeling(word):
+    # Add specific use cases where it's not just first letter capitalized
+    LABEL_DICT = { "prepared_y": "Year",
+              "dist": "District",
+              "nunique":"Number of Unique",
+              "project_no": "Project Number"}
+    
+    if (word == "mpo") or (word == "rtpa"):
+        word = word.upper()
+    elif word in LABEL_DICT.keys():
+        word = LABEL_DICT[word]
+    else:
+        #word = word.replace('n_', 'Number of ').title()
+        word = word.replace('unique_', "Number of Unique ").title()
+        word = word.replace('_', ' ').title()
+    
+    return word
+
+# Bar
+def basic_bar_chart(df, x_col, y_col, colorcol, chart_title=''):
+    if chart_title == "":
+        chart_title = (f"{labeling(x_col)} by {labeling(y_col)}")
+    chart = (alt.Chart(df)
+             .mark_bar()
+             .encode(
+                 x=alt.X(x_col, title=labeling(x_col), ),
+                 y=alt.Y(y_col, title=labeling(y_col),sort=('-x')),
+                 color = alt.Color(colorcol, 
+                                  scale=alt.Scale(
+                                      range=altair_utils.CALITP_CATEGORY_BRIGHT_COLORS),
+                                      legend=alt.Legend(title=(labeling(colorcol)))
+                                  ))
+             .properties( 
+                       title=chart_title)
+    )
+
+    chart=styleguide.preset_chart_config(chart)
+    chart.save(f"./bar_{x_col}_by_{y_col}.png")
+    return chart
