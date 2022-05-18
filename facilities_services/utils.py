cb3d3e07023844971116f2e26e5868cac6d55c9b
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-05-18 21:00:54 +0000

    import facilities spreadsheet, write to GCS

---
+++ facilities_services/utils.py
@@ -0,0 +1,11 @@
+"""
+Utility functions
+"""
+import pandas as pd
+
+GCS_PROJECT = "cal-itp-data-infra"
+BUCKET_NAME = "calitp-analytics-data"
+BUCKET_DIR = "data-analyses/facilities_services"
+GCS_FILE_PATH = f"gs://{BUCKET_NAME}/{BUCKET_DIR}/"
+
+DATA_PATH = "./data/"
