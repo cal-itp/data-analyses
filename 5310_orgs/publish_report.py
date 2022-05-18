8622ec382d6985ffdad2cf7e8786c4c5af1e0133
natam1 <natalielamberg@gmail.com>
2022-05-18 19:19:17 +0000

    add 5310 report

---
+++ 5310_orgs/publish_report.py
@@ -0,0 +1,36 @@
+'''
+single report script for 5310
+run in data-analyses/5310_orgs
+'''
+import dotenv
+import os
+import subprocess
+import sys
+
+import pandas as pd
+import papermill as pm 
+import requests
+
+from shared_utils import utils
+
+OUTPUT_FILENAME = "5310_report"
+
+pm.execute_notebook(
+    # notebook to execute
+    'projecttypes_of_interest.ipynb',
+    # if needed, rename the notebook as something different
+    # this will be the filename that is used when converting to HTML or PDF
+    f'{OUTPUT_FILENAME}.ipynb',
+)
+
+# shell out, run NB Convert
+OUTPUT_FORMAT = 'html'
+subprocess.run([
+    "jupyter",
+    "nbconvert",
+    "--to",
+    OUTPUT_FORMAT,
+    "--no-input",
+    "--no-prompt",
+    f"{OUTPUT_FILENAME}.ipynb",
+])
