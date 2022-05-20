61ba7f3955b1336ff7a2f828dbd06121deaa7478
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-05-20 16:56:52 +0000

    Add scripts to geocode in batches

--- facilities_services/utils.py
+++ facilities_services/utils.py
@@ -4,6 +4,9 @@ Utility functions
 import json
 import pandas as pd
 
+from calitp.storage import get_fs
+fs = get_fs()
+
 GCS_PROJECT = "cal-itp-data-infra"
 BUCKET_NAME = "calitp-analytics-data"
 BUCKET_DIR = "data-analyses/facilities_services"
