ecc3f821d3ccd6fa6a67d68c048d64c77c32ef94
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-05-20 16:56:52 +0000

    Add scripts to geocode in batches

---
+++ facilities_services/A3_prep_for_geocode.py
@@ -0,0 +1,88 @@
+"""
+Prep data for geocoding.
+
+Clean up addresses.
+Also spit out csv to do manual geocodes
+where addresses can't be constructed.
+"""
+import geocoder
+import intake
+import pandas as pd
+import re
+
+import utils
+
+DATA_PATH = "./data/"
+GCS_FILE_PATH = utils.GCS_FILE_PATH
+
+catalog = intake.open_catalog("./*.yml")
+
+def clean_up_addresses(row):
+    if row.address is not None:
+        address_string = row.address
+
+        # Replace everything within parentheses
+        address_cleaned = re.sub(r'\([^)]*\)', '', address_string)
+
+        # They prefix some locations with these
+        remove_me = ["TMC, ", "Lab, ", "Space adjustment: " ]
+
+        for word in remove_me:
+            address_cleaned = address_cleaned.replace(word, "")
+            
+        # Strip extra leading or trailing spaces
+        address_cleaned = address_cleaned.strip()
+        
+        return address_cleaned
+    else:
+        return None 
+    
+def assemble_full_address(row):
+    if (row.address_cleaned is not None) and (row.city is not None):
+        address_city = row.address_cleaned + " " + row.city
+        address_with_zip = address_city + ", CA " + str(row.zip_code)
+
+        if row.category=="office":
+            full_address = address_city + ", CA"
+        elif ((row.category=="maintenance") and (row.zip_code2 is None)):
+            full_address = address_with_zip
+        elif (row.category=="maintenance") and (row.zip_code2 is not None):
+            full_address = address_with_zip + "-" + row.zip_code2
+        elif (row.category=="equipment") or (row.category=="labs"):
+            full_address = address_with_zip
+    else:
+        full_address = "Incomplete"
+    
+    return full_address
+
+
+def prep_for_geocoding(df):
+    df = df.assign(
+        address_cleaned = df.apply(lambda x: clean_up_addresses(x), axis=1)
+    )
+    df = df.assign(
+        full_address = df.apply(lambda x: assemble_full_address(x), axis=1)
+    )
+    
+    return df
+
+
+if __name__ == "__main__":
+    df = catalog.tier1_facilities_addresses.read()
+    
+    df = prep_for_geocoding(df)
+    
+    # Do these manually
+    # if there are no digits, in addition to those we know are incomplete
+    manual_geocoding = df[~(df.full_address.str.contains(r"[0-9]"))]
+    
+    keep_cols = ["sheet_uuid", 
+                 "address", "address_cleaned", "city", "zip_code", "zip_code2",
+                 "full_address"]
+    
+    manual_geocoding[keep_cols].to_csv(f"{utils.GCS_FILE_PATH}manual_geocoding.csv")
+    
+    # These can go into a geocoder
+    for_geocoding = df[(df.full_address.str.contains(f"[0-9]"))] 
+    for_geocoding.to_parquet(f"{utils.GCS_FILE_PATH}for_geocoding.parquet")
+    
