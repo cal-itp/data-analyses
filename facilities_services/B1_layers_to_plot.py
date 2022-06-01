bcbcaa5703102d972a8a19dbd68f242b670099f2
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-06-01 23:38:40 +0000

    Add charts for districts

--- facilities_services/B1_layers_to_plot.py
+++ facilities_services/B1_layers_to_plot.py
@@ -44,10 +44,38 @@ def spatial_join_facilities_hqta():
     
     return gdf
 
+import pandas as pd
+
+# Notice there's some observations missing district info (maybe fell outside district boundaries?)
+def fill_in_missing_district(df):
+    missing_df = df[df.district.isna()]
+    non_missing_df = df[df.district.notna()]
+    
+    replacements = (df[(df.district.notna()) & 
+                        (df.county_name.isin(missing_df.county_name))]
+                      [["county_name", "district"]]
+                    # sort this way so district 3/59 will pick 59 (HQ) instead of 3
+                      .sort_values(["district", "county_name"], 
+                                   ascending=[False, True])
+                      .drop_duplicates(subset="county_name")
+                     )
+    
+    missing_df = pd.merge(
+        missing_df.drop(columns = "district"),
+        replacements,
+        on = ["county_name"],
+        how = "inner",
+    )
+    
+    df2 = pd.concat([non_missing_df, missing_df], axis=0, ignore_index=True)
+    
+    return df2
 
 def layers_to_plot():
     gdf = spatial_join_facilities_hqta()
 
+    gdf = gdf.astype({"district": "Int64"})
+    
     # Which locations
     facility_cols = ["sqft", "category", 
                      "facility_name", "facility_type",
@@ -55,6 +83,8 @@ def layers_to_plot():
                      "county_name", "district",
                     ]
     facilities = gdf[["sheet_uuid", "geometry"] + facility_cols].drop_duplicates()
+    
+    facilities = fill_in_missing_district(facilities)
     
     
     # Which HQTA corridors (polygon geom)
@@ -75,8 +105,5 @@ def layers_to_plot():
                 )
 
     hqta_corr = gpd.GeoDataFrame(hqta_corr).to_crs(geography_utils.WGS84)
-    
-    #facilities.to_parquet("./data/plot_facilities.parquet")
-    #hqta_corr.to_parquet("./data/plot_hqta.parquet")
-    
+        
     return facilities, hqta_corr
