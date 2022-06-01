ac56ad7452475bcda8544962de9c7b996cc23338
Eric Dasmalchi <edasmalchi@gmail.com>
2022-06-01 17:41:56 +0000

    update to new table, try culver

--- rt_delay/rt_analysis.py
+++ rt_delay/rt_analysis.py
@@ -250,7 +250,6 @@ class OperatorDayAnalysis:
         assert type(analysis_date) == dt.date, 'analysis date must be a datetime.date object'
         self.analysis_date = analysis_date
         self.display_date = self.analysis_date.strftime('%b %d (%a)')
-        ## get view df/gdfs TODO implement temporary caching with parquets in bucket...
         self.vehicle_positions = get_vehicle_positions(self.calitp_itp_id, self.analysis_date)
         self.trips = get_trips(self.calitp_itp_id, self.analysis_date)
         self.stop_times = get_stop_times(self.calitp_itp_id, self.analysis_date)
@@ -268,7 +267,6 @@ class OperatorDayAnalysis:
         self.routelines = get_routelines(self.calitp_itp_id, self.analysis_date)
         self.routelines = self.routelines.dropna() ## invalid geos are nones in new df...
         assert type(self.routelines) == type(gpd.GeoDataFrame()) and not self.routelines.empty, 'routelines must not be empty'
-        ## end of caching...
         self.trs = self.trips >> select(_.shape_id, _.trip_id)
         self.trs = self.trs >> inner_join(_, self.stop_times >> select(_.trip_id, _.stop_id), on = 'trip_id')
         self.trs = self.trs >> distinct(_.stop_id, _.shape_id)
@@ -397,302 +395,27 @@ class OperatorDayAnalysis:
         self.stop_delay_view = _delays
         return
     
-    def set_filter(self, start_time = None, end_time = None, route_names = None,
-                   shape_ids = None, direction_id = None, direction = None):
+    def export_views_gcs(self):
+        ''' Exports stop delay view, rt_trips, and endpoint delay view to GCS. Supports robust mapping and analysis without intensive regeneration of interpolator objects.
         '''
-        start_time, end_time: string %H:%M, for example '11:00' and '14:00'
-        route_names: list or pd.Series of route_names (GTFS route_short_name)
-        direction_id: '0' or '1'
-        direction: string 'Northbound', 'Eastbound', 'Southbound', 'Westbound' (experimental)
-        '''
-        assert start_time or end_time or route_names or direction_id or direction or shape_ids, 'must supply at least 1 argument to filter'
-        assert not start_time or type(dt.datetime.strptime(start_time, '%H:%M') == type(dt.datetime)), 'invalid time string'
-        assert not end_time or type(dt.datetime.strptime(end_time, '%H:%M') == type(dt.datetime)), 'invalid time string'
-        assert not route_names or type(route_names) == list or type(route_names) == type(pd.Series())
-        if route_names:
-            assert pd.Series(route_names).isin(self.rt_trips.route_short_name).all(), 'at least 1 route not found in self.rt_trips'
-        assert not direction_id or type(direction_id) == str and len(direction_id) == 1
-        self.filter = {}
-        if start_time:
-            self.filter['start_time'] = dt.datetime.strptime(start_time, '%H:%M').time()
-        else:
-            self.filter['start_time'] = None
-        if end_time:
-            self.filter['end_time'] = dt.datetime.strptime(end_time, '%H:%M').time()
-        else:
-            self.filter['end_time'] = None
-        self.filter['route_names'] = route_names
-        self.filter['shape_ids'] = shape_ids
-        if shape_ids:
-            shape_trips = (self.rt_trips >> filter(_.shape_id.isin(shape_ids))
-                           >> distinct(_.route_short_name, _keep_all=True)
-                           >> collect()
-                          )
-            self.filter['route_names'] = list(shape_trips.route_short_name)
-            if len(shape_ids) == 1:
-                direction = self.position_interpolators[shape_trips.trip_id.iloc[0]]['rt'].direction
-        self.filter['direction_id'] = direction_id
-        self.filter['direction'] = direction
-        if start_time and end_time:
-            self.hr_duration_in_filter = (dt.datetime.combine(self.analysis_date, self.filter['end_time'])
-                                 - dt.datetime.combine(self.analysis_date, self.filter['start_time'])
-                                ).seconds / 60**2
-        else:
-            self.hr_duration_in_filter = (self.vehicle_positions.vehicle_timestamp.max() - 
-                                         self.vehicle_positions.vehicle_timestamp.min()).seconds / 60**2
-        if self.filter['route_names'] and len(self.filter['route_names']) < 5:
-            rts = 'Route(s) ' + ', '.join(self.filter['route_names'])
-        elif self.filter['route_names'] and len(self.filter['route_names']) > 5:
-            rts = 'Multiple Routes'
-        elif not self.filter['route_names']:
-            rts = 'All Routes'
-            
-        # print(self.filter)
-        ## properly format for pm peak, TODO add other periods
-        if start_time and end_time and start_time == '15:00' and end_time == '19:00':
-            period = 'PM Peak'
-        elif not start_time and not end_time:
-            period = 'All Day'
-        else:
-            period = f'{start_time}–{end_time}'
-        elements_ordered = [rts, direction, period, self.display_date]
-        self.filter_formatted = ', ' + ', '.join([str(x) for x in elements_ordered if x])
-        return self.filter
-            
-    def reset_filter(self):
-        self.filter = None
-    
-    def _filter(self, df):
-        '''Filters a df (containing trip_id) on trip_id based on any set filter.
-        '''
-        if self.filter == None:
-            return df
-        else:
-            trips = self.rt_trips.copy()
-            print(f'view filter: {self.filter}')
-        if self.filter['start_time']:
-            trips = trips >> filter(_.median_time > self.filter['start_time'])
-        if self.filter['end_time']:
-            trips = trips >> filter(_.median_time < self.filter['end_time'])
-        if self.filter['route_names']:
-            trips = trips >> filter(_.route_short_name.isin(self.filter['route_names']))
-        if self.filter['shape_ids']:
-            trips = trips >> filter(_.shape_id.isin(self.filter['shape_ids']))
-        if self.filter['direction_id']:
-            trips = trips >> filter(_.direction_id == self.filter['direction_id'])
-        if self.filter['direction']:
-            trips = trips >> filter(_.direction == self.filter['direction'])
-        return df.copy() >> inner_join(_, trips >> select(_.trip_id), on = 'trip_id')
-    
-
+        day = str(self.analysis_date.day).zfill(2)
+        month = str(self.analysis_date.month).zfill(2)
         
-    def segment_speed_map(self, segments = 'stops', how = 'average',
-                          colorscale = ZERO_THIRTY_COLORSCALE, size = [900, 550]):
-        ''' Generate a map of segment speeds aggregated across all trips for each shape, either as averages
-        or 20th percentile speeds.
-        
-        segments: 'stops' or 'detailed' (detailed not yet implemented)
-        how: 'average' or 'low_speeds'
-        colorscale: branca.colormap
-        size: [x, y]
-        
-        '''
-        assert segments in ['stops', 'detailed']
-        assert how in ['average', 'low_speeds']
-        
-        gdf = self._filter(self.stop_delay_view)
-        all_stop_speeds = gpd.GeoDataFrame()
-        # for shape_id in tqdm(gdf.shape_id.unique()): trying self.pbar.update...
-        if type(self.pbar) != type(None):
-            self.pbar.reset(total=len(gdf.shape_id.unique()))
-            self.pbar.desc = 'Generating segment speeds'
-        for shape_id in gdf.shape_id.unique():
-            for direction_id in gdf.direction_id.unique():
-                this_shape_direction = (gdf
-                             >> filter((_.shape_id == shape_id) & (_.direction_id == direction_id))).copy()
-                if this_shape_direction.empty:
-                    print(f'{shape_id}_{direction_id}_ empty!')
-                    continue
-                # self.debug_dict[f'{shape_id}_{direction_id}_tsd'] = this_shape_direction
-                stop_speeds = (this_shape_direction
-                             >> group_by(_.trip_key)
-                             >> arrange(_.stop_sequence)
-                             >> mutate(seconds_from_last = (_.actual_time - _.actual_time.shift(1)).apply(lambda x: x.seconds))
-                             >> mutate(last_loc = _.shape_meters.shift(1))
-                             >> mutate(meters_from_last = (_.shape_meters - _.last_loc))
-                             >> mutate(speed_from_last = _.meters_from_last / _.seconds_from_last)
-                             # >> mutate(last_delay = _.delay.shift(1))
-                             >> mutate(delay_chg_sec = (_.delay_seconds - _.delay_seconds.shift(1)))
-                             # >> mutate(delay_sec = _.delay.map(lambda x: x.seconds if x.days == 0 else x.seconds - 24*60**2))
-                             >> ungroup()
-                            )
-                if stop_speeds.empty:
-                    # print(f'{shape_id}_{direction_id}_st_spd empty!')
-                    continue
-                # self.debug_dict[f'{shape_id}_{direction_id}_st_spd'] = stop_speeds
-                stop_speeds.geometry = stop_speeds.apply(
-                    lambda x: shapely.ops.substring(
-                                self.position_interpolators[x.trip_id]['rt'].shape,
-                                x.last_loc,
-                                x.shape_meters),
-                                                axis = 1)
-                stop_speeds = stop_speeds.dropna(subset=['last_loc']).set_crs(shared_utils.geography_utils.CA_NAD83Albers)
-
-                try:
-                    stop_speeds = (stop_speeds
-                         >> mutate(speed_mph = _.speed_from_last * MPH_PER_MPS)
-                         >> group_by(_.stop_sequence)
-                         >> mutate(n_trips = _.stop_sequence.size,
-                                    avg_mph = _.speed_mph.mean(),
-                                  _20p_mph = _.speed_mph.quantile(.2),
-                                   trips_per_hour = _.n_trips / self.hr_duration_in_filter
-                                  )
-                         # >> distinct(_.stop_sequence, _keep_all=True) ## comment out to enable speed distribution analysis
-                         >> ungroup()
-                         >> select(-_.arrival_time, -_.actual_time, -_.delay, -_.last_delay)
-                        )
-                    # self.debug_dict[f'{shape_id}_{direction_id}_st_spd2'] = stop_speeds
-                    assert not stop_speeds.empty, 'stop speeds gdf is empty!'
-                except Exception as e:
-                    print(f'stop_speeds shape: {stop_speeds.shape}, shape_id: {shape_id}, direction_id: {direction_id}')
-                    print(e)
-                    continue
-
-                if stop_speeds.avg_mph.max() > 80:
-                    print(f'speed above 80 for shape {stop_speeds.shape_id.iloc[0]}, dropping')
-                    stop_speeds = stop_speeds >> filter(_.avg_mph < 80)
-                if stop_speeds._20p_mph.min() < 0:
-                    print(f'negative speed for shape {stop_speeds.shape_id.iloc[0]}, dropping')
-                    stop_speeds = stop_speeds >> filter(_._20p_mph > 0)
-                all_stop_speeds = pd.concat((all_stop_speeds, stop_speeds))
-                
-                if type(self.pbar) != type(None):
-                    self.pbar.update()
-            if type(self.pbar) != type(None):
-                self.pbar.refresh
-
-        self.stop_segment_speed_view = all_stop_speeds
-        return self._show_speed_map(how = how, colorscale = colorscale, size = size)
-    
-    def _show_speed_map(self, how, colorscale, size):
-        
-        gdf = self.stop_segment_speed_view.copy()
-        # print(gdf.dtypes)
-        singletrip = gdf.trip_id.nunique() == 1
-        gdf = gdf >> distinct(_.shape_id, _.stop_sequence, _keep_all=True) ## essential here for reasonable map size!
-        orig_rows = gdf.shape[0]
-        gdf = gdf.round({'avg_mph': 1, '_20p_mph': 1, 'shape_meters': 0,
-                        'trips_per_hour': 1}) ##round for display
-        
-        how_speed_col = {'average': 'avg_mph', 'low_speeds': '_20p_mph'}
-        how_formatted = {'average': 'Average', 'low_speeds': '20th Percentile'}
-
-        gdf = gdf >> select(-_.service_date)
-        gdf = gdf >> arrange(_.trips_per_hour)
-        gdf = gdf.set_crs(CA_NAD83Albers)
-        
-        ## shift to right side of road to display direction
-        gdf.geometry = gdf.geometry.apply(try_parallel)
-        self.detailed_map_view = gdf.copy()
-        ## create clips, integrate buffer+simplify?
-        gdf.geometry = gdf.geometry.apply(arrowize_segment).simplify(tolerance=5)
-        gdf = gdf >> filter(gdf.geometry.is_valid)
-        gdf = gdf >> filter(-gdf.geometry.is_empty)
-        
-        assert gdf.shape[0] >= orig_rows*.99, 'over 1% of geometries invalid after buffer+simplify'
-        gdf = gdf.to_crs(WGS84)
-        centroid = gdf.dissolve().centroid 
-        name = self.calitp_agency_name
-
-        popup_dict = {
-            how_speed_col[how]: "Speed (miles per hour)",
-            "shape_meters": "Distance along route (meters)",
-            "route_short_name": "Route",
-            "shape_id": "Shape ID",
-            "direction_id": "Direction ID",
-            "stop_id": "Next Stop ID",
-            "stop_sequence": "Next Stop Sequence",
-            "trips_per_hour": "Trips per Hour" 
-        }
-        if singletrip:
-            popup_dict["delay_seconds"] = "Current Delay (seconds)"
-            popup_dict["delay_chg_sec"] = "Change in Delay (seconds)"
-
-        g = make_folium_choropleth_map(
-            gdf,
-            plot_col = how_speed_col[how],
-            popup_dict = popup_dict,
-            tooltip_dict = popup_dict,
-            colorscale = colorscale,
-            fig_width = size[0], fig_height = size[1],
-            zoom = 13,
-            centroid = [centroid.y, centroid.x],
-            title=f"{name} {how_formatted[how]} Vehicle Speeds Between Stops{self.filter_formatted}",
-            legend_name = "Speed (miles per hour)",
-            highlight_function=lambda x: {
-                'fillColor': '#DD1C77',
-                "fillOpacity": 0.6,
-            }
-        )
-
-        return g  
-    
-    def route_coverage_summary(self):
-        ''' A view summarizing rt coverage for each trip, by shape distance and stop.
-        For example, a trip where vehicle position reports did not start until some distance down the
-        shape will show with a min_meters and min_stop higher than the shape minimums.
-        '''
-        if hasattr(self, 'stop_delay_view'):
-            summary = (self.stop_delay_view
-             >> group_by(_.trip_key, _.trip_id, _.shape_id, _.direction_id)
-             >> summarize(min_meters = _.shape_meters.min(),
-                         min_stop = _.stop_sequence.min(),
-                         max_meters = _.shape_meters.max(),
-                         max_stop = _.stop_sequence.max())
-            )
-            return summary
-        else:
-            self._generate_stop_delay_view()
-            return self.route_coverage_summary()
-
-    def chart_delays(self):
-        '''
-        A bar chart showing delays grouped by arrival hour for current filtered selection
-        '''
-        filtered_endpoint = self._filter(self.endpoint_delay_view)
-        grouped = (filtered_endpoint >> group_by(_.arrival_hour)
-                   >> summarize(mean_end_delay = _.delay.mean())
-                  )
-        grouped['Minutes of Delay at Endpoint'] = grouped.mean_end_delay.apply(lambda x: x.seconds / 60)
-        grouped['Hour'] = grouped.arrival_hour
-        sns_plot = (sns.barplot(x=grouped['Hour'], y=grouped['Minutes of Delay at Endpoint'], ci=None, 
-                       palette=[shared_utils.calitp_color_palette.CALITP_CATEGORY_BOLD_COLORS[1]])
-            .set_title(f"{self.calitp_agency_name} Mean Delays by Arrival Hour{self.filter_formatted}")
-           )
-        chart = sns_plot.get_figure()
-        chart.tight_layout()
-        return chart
-    
-    def chart_variability(self, min_stop_seq = None, max_stop_seq = None):
-        '''
-        Chart trip speed variability, as speed between each stop segments.
-        stop_sequence_range: (min_stop, max_stop)
-        '''
-        sns.set(rc = {'figure.figsize':(13,6)})
-        assert (self.filter['shape_ids']
-                and len(self.filter['shape_ids']) == 1), 'must filter to a single shape_id'
-        to_chart = self._filter(self.stop_segment_speed_view.copy())
-        if min_stop_seq:
-            to_chart = to_chart >> filter(_.stop_sequence >= min_stop_seq)
-        if max_stop_seq:
-            to_chart = to_chart >> filter(_.stop_sequence <= max_stop_seq)
-        to_chart.stop_name = to_chart.stop_name.str.split('&').map(lambda x: x[-1])
-        to_chart = to_chart.rename(columns={'speed_mph': 'Segement Speed (mph)',
-                                      'delay_chg_sec': 'Increase in Delay (seconds)',
-                                      'stop_sequence': 'Stop Segment ID',
-                                      'stop_name': 'Segment Cross Street'})
-        plt.xticks(rotation=65)
-        variability_plt = sns.swarmplot(x = to_chart['Segment Cross Street'], y=to_chart['Segement Speed (mph)'],
-              palette=shared_utils.calitp_color_palette.CALITP_CATEGORY_BRIGHT_COLORS,
-             ).set_title(f"{self.calitp_agency_name} Speed Variability by Stop Segment{self.filter_formatted}")
-        return variability_plt
+        stop_delay_to_parquet = self.stop_delay_view.copy()
+        stop_delay_to_parquet['delay_seconds'] = stop_delay_to_parquet.delay.map(lambda x: x.seconds)
+        stop_delay_to_parquet['arrival_time'] = stop_delay_to_parquet.arrival_time.map(lambda x: x.isoformat())
+        stop_delay_to_parquet['actual_time'] = stop_delay_to_parquet.actual_time.map(lambda x: x.isoformat())
+        stop_delay_to_parquet = stop_delay_to_parquet >> select(-_.delay)
+        shared_utils.utils.geoparquet_gcs_export(stop_delay_to_parquet,
+                                         f'{GCS_FILE_PATH}stop_delay_views/',
+                                        f'{self.calitp_itp_id}_{month}_{day}'
+                                        )
+        self.rt_trips.to_parquet(f'{GCS_FILE_PATH}rt_trips/{self.calitp_itp_id}_{month}_{day}.parquet')
+        end_delay_to_parquet = self.endpoint_delay_view.copy()
+        end_delay_to_parquet['delay_seconds'] = end_delay_to_parquet.delay.map(lambda x: x.seconds)
+        end_delay_to_parquet = end_delay_to_parquet >> select(-_.delay)
+        shared_utils.utils.geoparquet_gcs_export(end_delay_to_parquet,
+                                         f'{GCS_FILE_PATH}endpoint_delay_views/',
+                                        f'{self.calitp_itp_id}_{month}_{day}_pm_peak'
+                                        )
+        return
