c2d428413c7875f79d0b258060bd94eb55479cba
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-06-02 20:57:32 +0000

    Update script to include HQTA

--- traffic_ops/arcgis_script.py
+++ traffic_ops/arcgis_script.py
@@ -7,17 +7,30 @@ print os.getcwd()
 arcpy.env.workspace = r"C:\Users\s153936\Documents\ArcGIS"
 
 # Set local variables
-in_features = ['stops_assembled/stops_assembled.shp', 
-               'routes_assembled/routes_assembled.shp']
+in_features = [
+    #'stops_assembled', 'routes_assembled',
+    'ca_hq_transit_areas','ca_hq_transit_stops',
+]
+
 out_location = 'open_data.gdb'
 
 # Execute FeatureClassToGeodatabase
-arcpy.FeatureClassToGeodatabase_conversion(in_features, out_location)
+#arcpy.FeatureClassToGeodatabase_conversion(in_features, out_location)
 
 # Export metadata using FGDC
 translator =  'C:\Program Files (x86)\ArcGIS\Desktop10.8\Metadata\Translator\ArcGIS2FGDC.xml'
 
-arcpy.ExportMetadata_conversion (out_location + '/routes_assembled', translator, 
-    'routes_assembled.xml')
-arcpy.ExportMetadata_conversion (out_location + '/stops_assembled', translator, 
-    'stops_assembled.xml')
+for f in in_features:
+    # construct the filename, which is takes form of routes_assembled/routes_assembled.shp
+    file_name = f + '/' + f + '.shp'
+
+    # Execute FeatureClassToGeodatabase
+    arcpy.FeatureClassToGeodatabase_conversion(file_name, out_location)
+    
+    # Construct XML filename
+    xml_file = f + '.xml'
+
+    arcpy.ExportMetadata_conversion (out_location + '/' + f, translator, 
+        xml_file)
+    
+
