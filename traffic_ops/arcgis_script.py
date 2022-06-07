0e60a002eeb17a020aded454eb916a8ce47c40f9
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-06-07 22:02:50 +0000

    Overwrite metadata in file gdb with outside XML

--- traffic_ops/arcgis_script.py
+++ traffic_ops/arcgis_script.py
@@ -9,8 +9,8 @@ arcpy.env.workspace = r"C:\Users\s153936\Documents\ArcGIS"
 # Set local variables
 in_features = [
     #'stops_assembled', 'routes_assembled',
-    'ca_hq_transit_areas',
-    #'ca_hq_transit_stops',
+    #'ca_hq_transit_areas',
+    'ca_hq_transit_stops',
 ]
 
 out_location = 'open_data.gdb'
@@ -25,33 +25,37 @@ translator =  directory + 'Metadata\Translator\ArcGIS2FGDC.xml'
 
 for f in in_features:
     # construct the filename, which is takes form of routes_assembled/routes_assembled.shp
-    file_name = f + '/' + f + '.shp'
+    shp_file_name = f + '/' + f + '.shp'
 
     # Execute FeatureClassToGeodatabase
-    #arcpy.FeatureClassToGeodatabase_conversion(file_name, out_location)
+    arcpy.FeatureClassToGeodatabase_conversion(shp_file_name, out_location)
     
     # Construct XML filename
-    #xml_file = f + '_orig.xml'
-    updated_xml_file = f + '.xml'
+    # Spit out one that's before it's manually changed
+    xml_file = f + '.xml'
+    # This is the one after it's manually changed. Keep separate to see what works.
+    updated_xml_file = f + '/' + f + '.xml'
     
-    #arcpy.ExportMetadata_conversion (out_location + '/' + f, translator, 
-    #    xml_file)
+    arcpy.ExportMetadata_conversion (out_location + '/' + f, 
+                                     translator, 
+                                     out_location + '/' + xml_file)
     
-    # Upgrade from FGDC to ArcGIS (this works)
+    # Upgrade from FGDC to ArcGIS (this works...need this step?)
+    # But following step imports it from FGDC...confusing
     #arcpy.conversion.UpgradeMetadata(updated_xml_file, "FGDC_TO_ARCGIS")
-
-    arcpy.conversion.ImportMetadata(updated_xml_file, "FROM_ARCGIS", f, "ENABLED")
-
-    #arcpy.SynchronizeMetadata_conversion("vegetation", "NOT_CREATED")
-
-    arcpy.MetadataImporter_conversion(updated_xml_file, f)
-    ## BELOW - NOT TESTED YET
-    #Source_Metadata = arcpy.GetParameter(0)
     
-    #for item in Source_Metadata:
-    #    print(item)
+    # Import XML into file gdb
+    #arcpy.conversion.MetadataImporter(updated_xml_file, f)
+
+    # My updated xml is in the shapefile folder (separate from original)
+    
+    # Import the updated xml, then overwrite the metadata in the file gdb    
+    arcpy.conversion.ImportMetadata(updated_xml_file, 
+                                    "FROM_FGDC", 
+                                    out_location + '/' + f, 
+                                    "ENABLED")
+
 
     
     # https://desktop.arcgis.com/en/arcmap/latest/manage-data/metadata/editing-metadata-for-many-arcgis-items.htm
     # Add changes in metadata and write it to the file
-    #arcpy.MetadataImporter_conversion(xml_file, item)
