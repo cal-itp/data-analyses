5fec37bff96b95dbd9480312c61ae0830d8080cb
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-06-07 19:55:53 +0000

    Add more arcpy commands

--- traffic_ops/arcgis_script.py
+++ traffic_ops/arcgis_script.py
@@ -9,26 +9,49 @@ arcpy.env.workspace = r"C:\Users\s153936\Documents\ArcGIS"
 # Set local variables
 in_features = [
     #'stops_assembled', 'routes_assembled',
-    'ca_hq_transit_areas','ca_hq_transit_stops',
+    'ca_hq_transit_areas',
+    #'ca_hq_transit_stops',
 ]
 
 out_location = 'open_data.gdb'
 
-# Execute FeatureClassToGeodatabase
-#arcpy.FeatureClassToGeodatabase_conversion(in_features, out_location)
+# Path to Metadata stylesheet
+directory = arcpy.GetInstallInfo("desktop")["InstallDir"] 
+#"c:\program files (x86)\arcgis\desktop10.8\"
 
 # Export metadata using FGDC
-translator =  'C:\Program Files (x86)\ArcGIS\Desktop10.8\Metadata\Translator\ArcGIS2FGDC.xml'
+translator =  directory + 'Metadata\Translator\ArcGIS2FGDC.xml'
+
 
 for f in in_features:
     # construct the filename, which is takes form of routes_assembled/routes_assembled.shp
     file_name = f + '/' + f + '.shp'
 
     # Execute FeatureClassToGeodatabase
-    arcpy.FeatureClassToGeodatabase_conversion(file_name, out_location)
+    #arcpy.FeatureClassToGeodatabase_conversion(file_name, out_location)
     
     # Construct XML filename
-    xml_file = f + '.xml'
+    #xml_file = f + '_orig.xml'
+    updated_xml_file = f + '.xml'
+    
+    #arcpy.ExportMetadata_conversion (out_location + '/' + f, translator, 
+    #    xml_file)
+    
+    # Upgrade from FGDC to ArcGIS (this works)
+    #arcpy.conversion.UpgradeMetadata(updated_xml_file, "FGDC_TO_ARCGIS")
 
-    arcpy.ExportMetadata_conversion (out_location + '/' + f, translator, 
-        xml_file)
+    arcpy.conversion.ImportMetadata(updated_xml_file, "FROM_ARCGIS", f, "ENABLED")
+
+    #arcpy.SynchronizeMetadata_conversion("vegetation", "NOT_CREATED")
+
+    arcpy.MetadataImporter_conversion(updated_xml_file, f)
+    ## BELOW - NOT TESTED YET
+    #Source_Metadata = arcpy.GetParameter(0)
+    
+    #for item in Source_Metadata:
+    #    print(item)
+
+    
+    # https://desktop.arcgis.com/en/arcmap/latest/manage-data/metadata/editing-metadata-for-many-arcgis-items.htm
+    # Add changes in metadata and write it to the file
+    #arcpy.MetadataImporter_conversion(xml_file, item)
