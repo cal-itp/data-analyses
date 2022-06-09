f2f0ee892eaf01d801d8a0700fcd8fbcbb1adef1
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-06-09 17:02:59 +0000

    Split steps apart for before/after XML updated

--- traffic_ops/arcgis_script.py
+++ traffic_ops/arcgis_script.py
@@ -8,7 +8,6 @@ arcpy.env.workspace = r"C:\Users\s153936\Documents\ArcGIS"
 
 # Set local variables
 in_features = [
-    #'stops_assembled', 'routes_assembled',
     'ca_hq_transit_areas',
     #'ca_hq_transit_stops',
 ]
@@ -17,7 +16,6 @@ out_location = 'open_data.gdb'
 
 # Path to Metadata stylesheet
 directory = arcpy.GetInstallInfo("desktop")["InstallDir"] 
-#"c:\program files (x86)\arcgis\desktop10.8\"
 
 # Export metadata using FGDC
 translator =  directory + 'Metadata\Translator\ArcGIS2FGDC.xml'
@@ -33,33 +31,27 @@ for f in in_features:
     # Construct XML filename
     # Spit out one that's before it's manually changed
     xml_file = f + '.xml'
-    # This is the one after it's manually changed. Keep separate to see what works.
-    updated_xml_file = f + '/' + f + '.xml'
     
     arcpy.ExportMetadata_conversion (out_location + '/' + f, 
                                      translator, 
                                      out_location + '/' + xml_file)
     
-    # Upgrade from FGDC to ArcGIS (this works...need this step?)
-    # But following step imports it from FGDC...confusing
-    #arcpy.conversion.UpgradeMetadata(updated_xml_file, "FGDC_TO_ARCGIS")
-    
-    # Import XML into file gdb
-    #arcpy.conversion.MetadataImporter(updated_xml_file, f)
 
-    # My updated xml is in the shapefile folder (separate from original)
-    
+
+### UPDATE XML METADATA SEPARATELY IN PYTHON OUTSIDE OF ARCGIS
+
+
+for f in in_features:
+    # This is the one after it's manually changed. Keep separate to see what works.
+    updated_xml_file = f + '/' + f + '.xml'
+            
     # Import the updated xml, then overwrite the metadata in the file gdb    
     arcpy.conversion.ImportMetadata(updated_xml_file, 
                                     "FROM_FGDC", 
                                     out_location + '/' + f, 
                                     "ENABLED")
-
+    
 
 # Compress file gdb for sending -- nope, this doesn't create a zipped file
 # Manually create zipped .gdb file
 arcpy.CompressFileGeodatabaseData_management(out_location, "Lossless compression")
-
-    
-    # https://desktop.arcgis.com/en/arcmap/latest/manage-data/metadata/editing-metadata-for-many-arcgis-items.htm
-    # Add changes in metadata and write it to the file
