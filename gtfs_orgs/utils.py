4590ee3e73f58e7fff4a01db68479d8e8f1e9ee9
natam1 <natalielamberg@gmail.com>
2022-06-23 19:16:55 +0000

    add notebooks

--- gtfs_orgs/utils.py
+++ gtfs_orgs/utils.py
@@ -4,6 +4,52 @@ from calitp import to_snakecase
 
 from siuba import *
 
+
+def add_airtable_filter(orgs):
+    #narrow down the columns
+    orgs = (orgs>>select(_.name,
+                         _.organization_type,
+                         _.ntp_id,
+                         _.itp_id,
+                         _.opm_id_drmt,
+                         _.brand, 
+                         _.alias,
+                         _.reporting_category,
+                         _.fares_v2_status,
+                         _.mobility_services_managed,
+                         _.mobility_services_operated,
+                         _.gtfs_datasets_produced,
+                         _.service_type__from_mobility_services_managed_,
+                         _.service_type__from_mobility_services_operated_,
+                         _.currently_operating__from_mobility_services_managed_,
+                         _.headquarters_place,
+                         _.caltrans_district,
+                         _.gtfs_dataset__from_mobility_services_managed_,
+                         _.gtfs_schedule_status,
+                         _['#_of_fixed_route_services'],
+                         _['#_fixed_route_or_deviated_fixed_route_services'],
+                         _.gtfs_static_status,
+                         _.gtfs_realtime_status)
+        )
+    
+    # filter for reporting category
+    orgs_2 = ((orgs.query("reporting_category == ('Core') or reporting_category ==('Other Public Transit')"))
+              >> filter(_.currently_operating__from_mobility_services_managed_.str.contains('checked')))
+    
+    # filter for service type
+    fixed = orgs_2[
+        (orgs_2["service_type__from_mobility_services_managed_"].str.contains("fixed-route")
+        ) | (orgs_2["service_type__from_mobility_services_managed_"].str.contains("deviated fixed-route")
+            ) | (orgs_2["service_type__from_mobility_services_managed_"].str.contains("on-demand fixed-route"))
+    ]
+    
+    # filter for currently operating
+    fixed = fixed[~fixed["currently_operating__from_mobility_services_managed_"].str.contains("0 checked out of")]
+    
+    return fixed
+
+
+
 def make_long(df, keep_cols = [], category_name = "managed"):
     df1 = df[["name", "gtfs_schedule_status"] + keep_cols]
     
