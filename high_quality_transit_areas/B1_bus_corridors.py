6086116a1d9d3a14e5189cac22d7fec760e301aa
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-07-08 23:41:25 +0000

    Remove test notebook

--- high_quality_transit_areas/B1_bus_corridors.py
+++ high_quality_transit_areas/B1_bus_corridors.py
@@ -29,33 +29,34 @@ date_str = analysis_date.strftime(rt_utils.FULL_DATE_FMT)
 
 ## force clear to ensure route type data present
 #trips = rt_utils.get_trips(itp_id, analysis_date, force_clear=True, route_types = ['3'])
-
 #stop_times = rt_utils.get_stop_times(itp_id, analysis_date)
-
 #stops = rt_utils.get_stops(itp_id, analysis_date)
 
 
 ## Join HQTA segment to stop
 # Find nearest stop
 def hqta_segment_to_stop(hqta_segments, stops):    
+    segment_cols = ["hqta_segment_id", "segment_sequence"]
+    
     segment_to_stop = (dask_geopandas.sjoin(
-            stops.drop(columns = ["stop_lon", "stop_lat"]),
-            hqta_segments[["hqta_segment_id", "geometry"]],
+            stops[["stop_id", "geometry"]],
+            hqta_segments,
             how = "inner",
             predicate = "intersects"
         ).drop(columns = ["index_right"])
-        .drop_duplicates(subset=["calitp_itp_id", "stop_id", "hqta_segment_id"])
+        .drop_duplicates(subset=segment_cols)
         .reset_index(drop=True)
     )
 
     # Dask geodataframe, even if you drop geometry col, retains df as gdf
     # Use compute() to convert back to df or gdf and merge
-    segment_to_stop2 = segment_to_stop.drop(columns = "geometry").compute()
+    keep_cols = ["calitp_itp_id", "calitp_url_number", "stop_id"] + segment_cols
+    segment_to_stop2 = segment_to_stop[keep_cols].compute()
     
     segment_to_stop3 = pd.merge(
-        hqta_segments[["hqta_segment_id", "geometry"]].compute(),
+        hqta_segments[segment_cols + ["geometry"]].compute(),
         segment_to_stop2,
-        on = "hqta_segment_id",
+        on = segment_cols,
         how = "inner"
     )
     
@@ -63,8 +64,10 @@ def hqta_segment_to_stop(hqta_segments, stops):
 
 
 def hqta_segment_with_max_trips(df, peak_trips_by_stop):
-    ddf = dd.from_pandas(df[["hqta_segment_id", "calitp_itp_id", "stop_id"]], 
-                         npartitions=1)
+    segment_cols = ["hqta_segment_id", "segment_sequence"]
+    stop_cols = ["calitp_itp_id", "calitp_url_number", "stop_id"]
+    
+    ddf = dd.from_pandas(df[segment_cols + stop_cols], npartitions=1)
 
     # Within a hqta_segment_id find
     # max am_peak trips and max pm_peak trips
@@ -76,7 +79,8 @@ def hqta_segment_with_max_trips(df, peak_trips_by_stop):
         how = "inner",
     )
     
-    peak_trips_by_segment = (peak_trips_by_stop_segment.groupby("hqta_segment_id")
+    peak_trips_by_segment = (peak_trips_by_stop_segment
+                             .groupby(stop_cols + segment_cols)
                          .agg({"am_max_trips": "max", 
                                "pm_max_trips": "max"})
                          .reset_index()
@@ -84,18 +88,28 @@ def hqta_segment_with_max_trips(df, peak_trips_by_stop):
     
     # Merge the geometry back in after aggregation (finding max trips along that segment)
     gdf = pd.merge(
-        df[["hqta_segment_id", "geometry"]].drop_duplicates(),
+        df[stop_cols + segment_cols + ["geometry"]].drop_duplicates(),
         peak_trips_by_segment,
-        on = "hqta_segment_id",
+        on = stop_cols + segment_cols,
         how = "left"
     )
+    
+    gdf = gdf.assign(
+        am_max_trips = gdf.am_max_trips.fillna(0).astype(int),
+        pm_max_trips = gdf.pm_max_trips.fillna(0).astype(int),
+    )
+        
+    return gdf
 
+
+#TODO: must exclude trips that run only in the AM and PM. those don't count
+def identify_hq_transit_corr(df):
     gdf = gdf.assign(
         hq_transit_corr = gdf.apply(lambda x: 
                                     True if (x.am_max_trips > 4 and (x.pm_max_trips > 4))
                                     else False, axis=1)
     )
-    
+
     return gdf
 
 
@@ -142,3 +156,4 @@ if __name__=="__main__":
         stop_times = dd.read_parquet(f"{FILE_PATH}st_{itp_id}_{date_str}.parquet")
         stops = dask_geopandas.read_parquet(f"{FILE_PATH}stops_{itp_id}_{date_str}.parquet")
         
+        gdf = bus_corridors.single_operator_hqta(routelines, trips, stop_times, stops)
