6fc4da9eeb9b6d5671373bf493c2e722cc22d45d
amandaha8 <amandahastuyck@gmail.com>
2022-07-27 22:41:21 +0000

    First attempt to recreate TPSOE and timeline sheets for PMP

---
+++ pmp_dashboard/A1_utilities.py
@@ -0,0 +1,50 @@
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+from calitp import *
+from shared_utils import utils
+
+GCS_FILE_PATH = "gs://calitp-analytics-data/data-analyses/pmp_dashboard/"
+
+
+'''
+Crosswalks
+'''
+#Narrow down division names to major groups
+div_crosswalks= {
+            "State & Fed Mass Trans": "DRMT",
+            "Statewide Planning": "DOTP",
+            "Research": "DRISI",
+            "PSR/PSSR Development": "DOTP",
+            "Rail": "DRMT",
+            "Planning Administration": "DOTP",
+            "Regional Planning": "DOTP",
+        }
+
+'''
+Variables
+& Lists
+'''
+
+
+'''
+Function that loads & cleans in raw data for 
+each accounting period
+'''
+
+'''
+Funds by 
+Division 
+'''
+
+'''
+TPSOE 
+'''
+
+'''
+Timeline 
+'''
+
+'''
+PSOE Timeline
+'''
