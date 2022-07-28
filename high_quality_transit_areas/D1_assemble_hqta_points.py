b23d7d3a4016a0efc30c516214cdca4f0107af3d
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-07-28 20:40:04 +0000

    Create hqta polygons file

--- high_quality_transit_areas/D1_assemble_hqta_points.py
+++ high_quality_transit_areas/D1_assemble_hqta_points.py
@@ -15,14 +15,14 @@ from calitp.tables import tbl
 from siuba import *
 
 import A3_rail_ferry_brt_extract as rail_ferry_brt_extract
+import utilities
 from A1_rail_ferry_brt import analysis_date
 from B1_bus_corridors import TEST_GCS_FILE_PATH
-from shared_utils import utils
-from utilities import catalog_filepath
+from shared_utils import utils, geography_utils
 
 # Input files
-MAJOR_STOP_BUS_FILE = catalog_filepath("major_stop_bus")
-STOPS_IN_CORRIDOR_FILE = catalog_filepath("stops_in_hq_corr")
+MAJOR_STOP_BUS_FILE = utilities.catalog_filepath("major_stop_bus")
+STOPS_IN_CORRIDOR_FILE = utilities.catalog_filepath("stops_in_hq_corr")
     
     
 def get_agency_names():
@@ -37,7 +37,7 @@ def get_agency_names():
     return names
 
 
-def add_agency_names(gdf):
+def add_agency_names_hqta_details(gdf):
     names_df = get_agency_names()
     
     name_dict = (names_df.set_index('calitp_itp_id_primary')
@@ -53,16 +53,28 @@ def add_agency_names(gdf):
     with_names = gdf2.compute()
     
     with_names = with_names.assign(
-        agency_name_secondary = with_names.apply(
-            lambda x: name_dict[int(x.calitp_itp_id_secondary)] if 
-            (not np.isnan(x.calitp_itp_id_secondary) and 
-             int(x.calitp_itp_id_secondary) in name_dict.keys()) 
-             else None, axis = 1, 
-         ), 
+                    agency_name_secondary = with_names.apply(
+                        lambda x: name_dict[int(x.calitp_itp_id_secondary)] if 
+                        (not np.isnan(x.calitp_itp_id_secondary) and 
+                         int(x.calitp_itp_id_secondary) in name_dict.keys()) 
+                         else None, axis = 1, 
+                     ), 
+                    hqta_details = with_names.apply(utilities.hqta_details, axis=1)
+                )
+    return with_names
+
+
+def clean_up_hqta_points(gdf):
+    gdf2 = (gdf.drop_duplicates(
+                    subset=["calitp_itp_id_primary", "hqta_type", "stop_id"])
+                   .sort_values(["calitp_itp_id_primary", "hqta_type", "stop_id"])
+                   .reset_index(drop=True)
+                   .to_crs(geography_utils.WGS84)
     )
     
-    return with_names
-    
+    return gdf2
+
+
 def delete_local_files():
     os.remove(MAJOR_STOP_BUS_FILE)
     os.remove(STOPS_IN_CORRIDOR_FILE)
@@ -85,8 +97,9 @@ if __name__=="__main__":
     time1 = dt.datetime.now()
     print(f"combined points: {time1 - start}")
     
-    # Add agency names
-    gdf = add_agency_names(hqta_points_combined)
+    # Add agency names, hqta_details, project back to WGS84
+    gdf = add_agency_names_hqta_details(hqta_points_combined)
+    gdf = clean_up_hqta_points(gdf)
     
     time2 = dt.datetime.now()
     print(f"add agency names / compute: {time2 - time1}")
