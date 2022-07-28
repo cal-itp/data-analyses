9520ec90a4019f43d23a5603db3ea2fb392f8876
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-07-28 22:12:11 +0000

    Remove shape dissolve because it's not used in B2

--- high_quality_transit_areas/B2_combine_operator_corridors.py
+++ high_quality_transit_areas/B2_combine_operator_corridors.py
@@ -4,7 +4,6 @@ Take single operator HQTA and combine across operators.
 Additional data cleaning to filter out small HQTA segments.
 
 This takes 2 min to run. 
-Quicker if getting rid of shape_dissolve (see if it's necessary to use in future stages)
 """
 import dask.dataframe as dd
 import dask_geopandas
@@ -14,29 +13,6 @@ import geopandas as gpd
 import B1_bus_corridors as bus_corridors
 from shared_utils import utils
 
-
-def clean_combined_operators(gdf):    
-    gdf = dask_geopandas.from_geopandas(gdf, npartitions=1)
-    
-    # Drop segments that are large enough
-    gdf = gdf.assign(
-        area = gdf.geometry.area 
-    )
-    
-    gdf2 = gdf[gdf.area > 50*400].compute()  ##50m width * 400m segment min
-    
-    dissolved = gdf2.dissolve(
-        by=["calitp_itp_id", "route_id", "hq_transit_corr"]).reset_index()
-
-    dissolved = dissolved.assign(
-        area = dissolved.geometry.area
-    )
-    
-    dissolved2 = (dissolved[dissolved.area > 50*3_000] ##50m width * 3000m shape min
-                 .reset_index(drop=True)
-                 )
-
-    return dissolved2 
 
 # Read first one in, to set the metadata for dask gdf
 OPERATOR_PATH = f"{bus_corridors.TEST_GCS_FILE_PATH}bus_corridors/"
@@ -65,18 +41,6 @@ if __name__ == "__main__":
     utils.geoparquet_gcs_export(gdf2, 
                                 f'{bus_corridors.TEST_GCS_FILE_PATH}intermediate/', 
                                 'all_bus')
-    
-    time1 = dt.datetime.now()
-    print(f"Exported all_bus to GCS: {time1-start}")
-    
-    gdf3 = clean_combined_operators(gdf2)
-    
-    utils.geoparquet_gcs_export(gdf3,
-                                f'{bus_corridors.TEST_GCS_FILE_PATH}intermediate/',
-                                'shape_dissolve'
-                               )
-    time2 = dt.datetime.now()
-    print(f"Exported shape_dissolve to GCS: {time2-time1}")
     
     end = dt.datetime.now()
     print(f"Execution time: {end-start}")
