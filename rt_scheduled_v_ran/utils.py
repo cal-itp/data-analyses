b3ee2ac1100d0b057983387926ffe4c25199412f
natam1 <natalielamberg@gmail.com>
2022-07-29 23:25:55 +0000

    fix utils

--- rt_scheduled_v_ran/utils.py
+++ rt_scheduled_v_ran/utils.py
@@ -29,47 +29,68 @@ from tqdm.notebook import trange, tqdm
 
 
 # Get the data for Scheduled Trips and RT Trips  
-def find_ran_trips(itp_id, analysis_date):
+
+def load_schedule_data(start_date, end_date, itp_id):
     gtfs_daily = (
         tbl.views.gtfs_schedule_fact_daily_trips()
         >> filter(_.calitp_itp_id == itp_id)
-        >> filter(_.service_date == analysis_date)
+        >> filter((_.service_date >= start_date),
+                 (_.service_date <= end_date))
         >> filter(_.is_in_service == True)
         >> collect()
     )
+    return gtfs_daily
+
+def load_rt_data(start_date, end_date):
     rt = query_sql(
         f"""
         SELECT * FROM `cal-itp-data-infra-staging.natalie_views.test_rt_trips`
-        WHERE date = '{analysis_date}'
+        WHERE date BETWEEN '{start_date}' AND '{end_date}'
         """
     )
-    
-    rt['str_len'] = rt.trip_id.str.len()
-    
-    #joinging 
-    join = (pd.merge(gtfs_daily, rt, how='outer', on='trip_id', indicator='have_rt'))
-    
-    return join
+    return rt
 
-#
-def get_pct_ran(join, date):
-    ## eventually generate a dataframe with the columns (dates and pct_ran)
-    day_pct_ran = {}
-    day_pct_ran['date'] = date
-    if ((len(join))!=0):
-        day_pct_ran['pct_trips_ran'] = ((len(join>>filter(_.have_rt=='both')))/(len(join)))
-    elif ((len(join))==0):
-        day_pct_ran['pct_trips_ran'] = ''
-    pct_ran = pd.DataFrame([day_pct_ran])
+# Get a DF with Percent Ran for each Day
 
-    return pct_ran
-
-def get_pct_ran_df(date_list, itp_id):
+def get_pct_ran_df(itp_id, list_of_dates, gtfs_daily, rt):
+    
     pcts = []
-    for date in date_list: 
-        sched_rt_df = (find_ran_trips(itp_id, date))
-        pct_ran = (get_pct_ran(sched_rt_df, date))
+    
+    # loop through list of dates
+    for single_date in list_of_dates:
+        
+        #filter for single day
+        
+        gtfs_daily2 = (gtfs_daily>>filter(_.service_date == single_date))
+        rt2 = (rt>>filter(_.date == single_date))
+        
+        #outer join schedule and rt data 
+        sched_rt_df = (pd.merge(gtfs_daily2, rt2, how='outer', on='trip_id', indicator='have_rt'))
+
+        
+        day_pct_ran = {}
+        day_pct_ran['date'] = single_date
+        if ((len(sched_rt_df))!=0):
+            day_pct_ran['pct_trips_ran'] = ((len(sched_rt_df>>filter(_.have_rt=='both')))/(len(gtfs_daily2)))
+        elif ((len(sched_rt_df))==0):
+            day_pct_ran['pct_trips_ran'] = ''
+        pct_ran = pd.DataFrame([day_pct_ran])
+        
+        # add columns with counts 
+        pct_ran['n_have_rt'] = (len(sched_rt_df>>filter(_.have_rt=='both')))
+        pct_ran['n_missing_rt'] = (len(sched_rt_df>>filter(_.have_rt=='right_only')))
+        pct_ran['unmatched_rt'] = (len(sched_rt_df>>filter(_.have_rt=='left_only')))
+        
+        # add columns for number of unique trip_ids
+        pct_ran['nunique_sched'] = (gtfs_daily2.trip_id.nunique())
+        pct_ran['nunique_rt'] = (rt2.trip_id.nunique())
+
         pcts.append(pct_ran)                                                    
         #code help from: https://stackoverflow.com/questions/28669482/appending-pandas-dataframes-generated-in-a-for-loop
+   
+    #add each date together 
     pcts = pd.concat(pcts)
+    
+    #arrange by date
+    pcts = pcts>>arrange(_.date)
     return pd.DataFrame(pcts)
