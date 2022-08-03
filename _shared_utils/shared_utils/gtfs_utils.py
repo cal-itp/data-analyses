34dceef29277d5285a9e77ed3218350eb22476b0
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-08-03 23:49:16 +0000

    (gtfs_utils): repeating queries for gtfs schedule

--- _shared_utils/shared_utils/gtfs_utils.py
+++ _shared_utils/shared_utils/gtfs_utils.py
@@ -1,5 +1,7 @@
 """
-GTFS utils
+GTFS utils.
+
+Queries to grab trips, stops, routes.
 
 TODO: move over some of the rt_utils
 over here, if it addresses GTFS schedule data more generally,
@@ -7,7 +9,20 @@ such as cleaning/reformatting arrival times.
 
 Leave the RT-specific analysis there.
 """
+import datetime
+
+import geopandas as gpd
 import pandas as pd
+from calitp.tables import tbl
+from shared_utils import geography_utils
+from siuba import *
+
+# import os
+# os.environ["CALITP_BQ_MAX_BYTES"] = str(200_000_000_000)
+
+
+YESTERDAY_DATE = datetime.date.today() + datetime.timedelta(days=-1)
+
 
 METROLINK_SHAPE_TO_ROUTE = {
     "AVin": "Antelope Valley Line",
@@ -62,3 +77,150 @@ def fill_in_metrolink_trips_df_with_shape_id(trips: pd.DataFrame) -> pd.DataFram
     )
 
     return df
+
+
+def get_route_info(
+    selected_date: str | datetime.date = YESTERDAY_DATE,
+    itp_id_list: list[int] = None,
+    route_cols: list[str] = None,
+    get_df: bool = True,
+) -> pd.DataFrame:
+
+    # Route info query
+    dim_routes = tbl.views.gtfs_schedule_dim_routes() >> distinct()
+
+    routes = (
+        tbl.views.gtfs_schedule_fact_daily_feed_routes()
+        >> filter(_.date == selected_date)
+        >> inner_join(
+            _, dim_routes, on=["route_key", "calitp_extracted_at", "calitp_deleted_at"]
+        )
+        >> distinct()
+    )
+
+    if itp_id_list is not None:
+        routes = routes >> filter(_.calitp_itp_id.isin(itp_id_list))
+
+    if route_cols is not None:
+        routes = routes >> select(*route_cols)
+
+    if get_df is True:
+        routes = routes >> collect()
+
+    return routes
+
+
+def get_stops(
+    selected_date: str | datetime.date = YESTERDAY_DATE,
+    itp_id_list: list[int] = None,
+    stop_cols: list[str] = None,
+    get_df: bool = True,
+) -> gpd.GeoDataFrame:
+
+    # Stops query
+    dim_stops = tbl.views.gtfs_schedule_dim_stops() >> distinct()
+
+    stops = (
+        tbl.views.gtfs_schedule_fact_daily_feed_stops()
+        >> filter(_.date == selected_date)
+        >> inner_join(
+            _, dim_stops, on=["stop_key", "calitp_extracted_at", "calitp_deleted_at"]
+        )
+        >> distinct()
+    )
+
+    if itp_id_list is not None:
+        stops = stops >> filter(_.calitp_itp_id.isin(itp_id_list))
+
+    if stop_cols is not None:
+        stops = stops >> select(*stop_cols)
+
+    if get_df is True:
+        stops = geography_utils.create_point_geometry(stops >> collect()).drop(
+            columns=["stop_lon", "stop_lat"]
+        )
+
+    return stops
+
+
+def get_trips(
+    selected_date: str | datetime.date = YESTERDAY_DATE,
+    itp_id_list: list[int] = None,
+    trip_cols: list[str] = None,
+    get_df: bool = True,
+) -> pd.DataFrame:
+
+    # Trips query
+    dim_trips = tbl.views.gtfs_schedule_dim_trips() >> distinct()
+
+    trips = (
+        tbl.views.gtfs_schedule_fact_daily_trips()
+        >> filter(_.service_date == selected_date, _.is_in_service == True)
+        >> inner_join(
+            _,
+            dim_trips,
+            on=[
+                "trip_key",
+                "trip_id",
+                "route_id",
+                "service_id",
+                "calitp_itp_id",
+                "calitp_url_number",
+                "calitp_extracted_at",
+                "calitp_deleted_at",
+            ],
+        )
+        >> distinct()
+    )
+
+    if itp_id_list is not None:
+        trips = trips >> filter(_.calitp_itp_id.isin(itp_id_list))
+
+    if trip_cols is not None:
+        trips = trips >> select(*trip_cols)
+
+    if get_df is True:
+        trips = trips >> collect()
+
+    return trips
+
+
+def get_trips_with_stop_times(
+    selected_date: str | datetime.date = YESTERDAY_DATE,
+    itp_id_list: list[int] = None,
+    departure_hours: list[int] = None,
+    trip_stop_time_cols: list[str] = None,
+    get_df: bool = True,
+) -> pd.DataFrame:
+
+    trips = get_trips(
+        selected_date=selected_date,
+        itp_id_list=itp_id_list,
+        trip_cols=None,
+        get_df=False,
+    )
+
+    # Join to stop_times, since stop_times always depends on trip_id
+    # No other way to filter stop_times by date
+    trips_with_stop_times = tbl.views.gtfs_schedule_dim_stop_times() >> inner_join(
+        _,
+        # Drop these columns,
+        # the inner join will either return 0 rows or _x, _y
+        # Can't use in merge_cols
+        # because they are different values
+        trips >> select(-_.calitp_hash, -_.calitp_extracted_at, -_.calitp_deleted_at),
+        on=["calitp_itp_id", "calitp_url_number", "trip_id"],
+    )
+
+    if itp_id_list is not None:
+        trips_with_stop_times = trips_with_stop_times >> filter(
+            _.calitp_itp_id.isin(itp_id_list)
+        )
+
+    if trip_stop_time_cols is not None:
+        trips_with_stop_times = trips_with_stop_times >> select(*trip_stop_time_cols)
+
+    if get_df is True:
+        trips_with_stop_times = trips_with_stop_times >> collect()
+
+    return trips_with_stop_times
