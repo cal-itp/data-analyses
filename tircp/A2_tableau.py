94355edd6b233c2cb4de00eb535e457315405e33
amandaha8 <amandahastuyck@gmail.com>
2022-08-09 18:17:53 +0000

    changing indention, move cleanup by organizing into notebooks

--- tircp/A2_tableau.py
+++ tircp/A2_tableau.py
@@ -3,18 +3,7 @@ Tableau
 """
 import numpy as np
 import pandas as pd
-from siuba import *
 from calitp import *
-from plotnine import *
-import intake
-import shared_utils
-
-import altair as alt
-import altair_saver
-from shared_utils import geography_utils
-from shared_utils import altair_utils
-from shared_utils import calitp_color_palette as cp
-from shared_utils import styleguide
 
 #GCS File Path:
 GCS_FILE_PATH = "gs://calitp-analytics-data/data-analyses/tircp/"
@@ -26,10 +15,10 @@ import crosswalks
 #Import cleaned up data
 import A1_data_prep
 
-'''
+"""
 Functions
-'''
-#Categorizing expended percentage into bins
+"""
+# Categorizing expended percentage into bins
 def expended_percent(row):
     if (row.Expended_Percent > 0) and (row.Expended_Percent < 0.26):
         return "1-25"
@@ -40,13 +29,13 @@ def expended_percent(row):
     elif (row.Expended_Percent > 0.75) and (row.Expended_Percent < 0.95):
         return "76-99"
     elif row.Expended_Percent == 0.0:
-         return "0"
+        return "0"
     else:
         return "100"
     
 # Categorize years and expended_percent_group into bins
 def progress(df):
-    ### 2015 ###
+    # 2015
     if (df["project_award_year"] == 2015) and (df["Expended_Percent_Group"] == "1-25") | (
             df["Expended_Percent_Group"] == "26-50"
         ):
@@ -56,7 +45,7 @@ def progress(df):
         ) | (df["Expended_Percent_Group"] == "51-75"):
          return "On Track"
 
-     ### 2016 ###
+     # 2016
     elif (df["project_award_year"] == 2016) and (df["Expended_Percent_Group"] == "1-25") | (
             df["Expended_Percent_Group"] == "26-50"
         ):
@@ -66,7 +55,7 @@ def progress(df):
         ) | (df["Expended_Percent_Group"] == "76-99"):
          return "On Track"
 
-     ### 2018 ###
+     # 2018
     elif (df["project_award_year"] == 2018) and (df["Expended_Percent_Group"] == "1-25"):
         return "Behind"
     elif (df["project_award_year"] == 2018) and (
@@ -76,7 +65,7 @@ def progress(df):
     elif (df["project_award_year"] == 2018) and (df["Expended_Percent_Group"] == "76-99"):
          return "Ahead"
 
-     ### 2020 ###
+     # 2020
     elif (df["project_award_year"] == 2020) and (df["Expended_Percent_Group"] == "1-25"):
            return "Behind"
     elif (df["project_award_year"] == 2020) and (df["Expended_Percent_Group"] == "26-50"):
@@ -86,93 +75,87 @@ def progress(df):
         ) | (df["Expended_Percent_Group"] == "76-99"):
             return "Ahead"
 
-    ### 0 Expenditures ###
+    # 0 Expenditures
     elif df["Expended_Percent_Group"] == "0":
         return "No expenditures recorded"
 
-    ### Else ###
     else:
         return "100% of allocated funds spent"
-'''
-Columns
-'''
-columns_to_keep = ['project_award_year','project_grant_recipient',
-       'project_project_title', 'project_ppno', 'project_district',
-       'project_technical_assistance_calitp__y_n_',
-       'project_technical_assistance_fleet__y_n_',
-       'project_technical_assistance_network_integration__y_n_',
-       'project_technical_assistance_priority_population__y_n_',
-       'project_total_project_cost', 'project_tircp_award_amount__$_',
-       'project_allocated_amount', 'project_unallocated_amount',
-       'project_expended_amount', 'project_award_cycle',
-       'project_estimated_tircp_ghg_reductions',
-       'project_cost_per_ghg_ton_reduced', 'project_increased_ridership',
-       'project_service_integration', 'project_improve_safety',
-       'project_project_readiness','project_county']    
+
 '''
 Script
 '''
-def tableau():  
-    #Load in cleaned project sheets
+def tableau():
+    # Load in cleaned project sheets
     df = A1_data_prep.clean_project()
-  
-    # Keeping only certain columns.
-    df = df[columns_to_keep]
 
-    #Create new cols
+    # Create new cols
     df = df.assign(
-    Expended_Percent = (df["project_expended_amount"] / df["project_allocated_amount"]),
-    Allocated_Percent = (df["project_allocated_amount"] / df["project_tircp_award_amount__$_"]),
-    Unallocated_Amount = (df["project_tircp_award_amount__$_"] - df["project_allocated_amount"]),
-    Projects_Funded_Percent = (df['project_tircp_award_amount__$_']/df['project_total_project_cost'])
+        Expended_Percent=(
+            df["project_expended_amount"] / df["project_allocated_amount"]
+        ),
+        Allocated_Percent=(
+            df["project_allocated_amount"] / df["project_tircp_award_amount__$_"]
+        ),
+        Unallocated_Amount=(
+            df["project_tircp_award_amount__$_"] - df["project_allocated_amount"]
+        ),
+        Projects_Funded_Percent=(
+            df["project_tircp_award_amount__$_"] / df["project_total_project_cost"]
+        ),
     )
-   
+
     # filling in for 0's
-    new_cols_list = ["Expended_Percent", "Allocated_Percent", "Unallocated_Amount", 'Projects_Funded_Percent'] 
+    new_cols_list = [
+        "Expended_Percent",
+        "Allocated_Percent",
+        "Unallocated_Amount",
+        "Projects_Funded_Percent",
+    ]
     df[new_cols_list] = df[new_cols_list].fillna(0)
-    
-    #Replace distircts & counties with their full names 
-    df['project_district'] = df['project_district'].replace(crosswalks.full_ct_district)
-    df['project_county'] = df['project_county'].replace(crosswalks.full_county)
-    
-    #Apply functions
-    #Categorize projects into expended % bins
+
+    # Replace distircts & counties with their full names
+    df["project_district"] = df["project_district"].replace(crosswalks.full_ct_district)
+    df["project_county"] = df["project_county"].replace(crosswalks.full_county)
+
+    # Apply functions
+    # Categorize projects into expended % bins
     df["Expended_Percent_Group"] = df.apply(lambda x: expended_percent(x), axis=1)
-    
-    #Categorize projects whether they are ahead/behind/0 expenditures/etc
+
+    # Categorize projects whether they are ahead/behind/0 expenditures/etc
     df["Progress"] = df.apply(progress, axis=1)
-    
-    #Categorize projects whether they are large/small/med based on TIRCPamount
-    
-    #Rename TIRCP column to something cleaner
-    df= df.rename(columns={'project_tircp_award_amount__$_': "tircp"})
+
+    # Categorize projects whether they are large/small/med based on TIRCPamount
+    # Rename TIRCP column to something cleaner
+    df = df.rename(columns={"project_tircp_award_amount__$_": "tircp"})
     # Which projects are large,small, medium
     p75 = df.tircp.quantile(0.75).astype(float)
     p50 = df.tircp.quantile(0.50).astype(float)
     p25 = df.tircp.quantile(0.25).astype(float)
-    
-    def project_size (row):
-        if ((row.tircp > 0) and (row.tircp < p25)):
-             return "Small"
-        elif ((row.tircp > p25) and (row.tircp < p50)):
-             return "Medium"
-        elif (row.tircp > p50):
-             return "Large"
-        elif (row.tircp == 0):
+
+    def project_size(row):
+        if (row.tircp > 0) and (row.tircp < p25):
+            return "Small"
+        elif (row.tircp > p25) and (row.tircp < p50):
+            return "Medium"
+        elif row.tircp > p50:
+            return "Large"
+        elif row.tircp == 0:
             return "$0 recorded for TIRCP"
         else:
             return "Medium"
-        
+
     df["Project_Category"] = df.apply(lambda x: project_size(x), axis=1)
-    
-    #Clean up column names
-    df.columns = (df.columns
-                  .str.replace('[_]', ' ')
-                  .str.replace('project','')
-                  .str.title()
-                  .str.strip()
-                 )
-    #Write to GCS
+
+    # Clean up column names
+    df.columns = (
+        df.columns.str.replace("[_]", " ")
+        .str.replace("project", "")
+        .str.title()
+        .str.strip()
+    )
+
+    # Write to GCS
     with pd.ExcelWriter(f"{GCS_FILE_PATH}Script_Tableau_Sheet.xlsx") as writer:
         df.to_excel(writer, sheet_name="Data", index=False)
     return df
--- tircp/A3_semiannual_report.py
+++ tircp/A3_semiannual_report.py
@@ -1,191 +1,276 @@
 '''
 Semiannual Report
 '''
-import numpy as np
 import pandas as pd
-from siuba import *
 from calitp import *
-from plotnine import *
-import intake
 import shared_utils
-
-import altair as alt
-import altair_saver
-from shared_utils import geography_utils
-from shared_utils import altair_utils
-from shared_utils import calitp_color_palette as cp
-from shared_utils import styleguide
 
 #GCS File Path:
 GCS_FILE_PATH = "gs://calitp-analytics-data/data-analyses/tircp/"
 FILE_NAME = "TIRCP_July_8_2022.xlsx"
 
 #Crosswalk
-import crosswalks 
+import A5_crosswalks as crosswalks
 
 #Import cleaned up data
 import A1_data_prep
 
 '''
-Functions
-'''
-#Function for the summary table 
-def summary_SAR_table_two(df):
-    #pivot
-    df = (df
-          .drop_duplicates()
-          .groupby(['project_award_year'])
-          .agg({'project_project_#':'count',
-                'project_tircp_award_amount__$_':'sum', 
-                'project_allocated_amount':'sum',
-                'project_expended_amount':'sum'})
-          .reset_index()
-         )
-    #renaming columns to match report
-    df = (df.rename(columns = {'project_project_#':'Number_of_Awarded_Projects',
-                               'project_tircp_award_amount__$_': 'Award_Amount',
-                               'project_allocated_amount':'Amount_Allocated',
-                               'project_expended_amount': 'Expended_Amount',
-                               'project_award_year': 'Award_Year'})
-         )
-    #create percentages
-    df['Expended_Percent_of_Awarded'] = (df['Expended_Amount']/df['Award_Amount'])
-    df['Expended_Percent_of_Allocated'] = (df['Expended_Amount']/df['Amount_Allocated'])
-    df['Percent_Allocated'] = (df['Amount_Allocated']/df['Award_Amount'])
-    #transpose 
-    df = df.set_index('Award_Year').T
-    #grand totals for monetary columns
-    list_to_add = ['Award_Amount','Amount_Allocated','Expended_Amount', 'Number_of_Awarded_Projects']
-    df['Grand_Total']=df.loc[list_to_add, :].sum(axis=1)
-    #grand total project_expended_amount of each monetary column to fill in percentages below.
-    Exp = df.at['Expended_Amount','Grand_Total']
-    Alloc = df.at['Amount_Allocated','Grand_Total']
-    TIRCP = df.at['Award_Amount','Grand_Total']
-    #filling in totals of percentages
-    df.at['Expended_Percent_of_Awarded','Grand_Total'] = (Exp/TIRCP)
-    df.at['Expended_Percent_of_Allocated','Grand_Total'] = (Exp/Alloc)
-    df.at['Percent_Allocated','Grand_Total'] = (Alloc/TIRCP)
-    #switching rows to correct order
-    df = (df.reindex(['Number_of_Awarded_Projects',
-                      'Award_Amount', 'Amount_Allocated',
-                     'Percent_Allocated','Expended_Amount', 
-                      'Expended_Percent_of_Awarded', 'Expended_Percent_of_Allocated'])
-    )
-    return df 
-'''
 Columns
 &
 Lists
 '''
-#Columns to keep 
-allocation_cols = ['allocation_award_year','allocation_expended_amount','allocation_allocation_amount',
-                                'allocation_components','allocation_grant_recipient', 
-                                   'allocation_implementing_agency','allocation_ppno',
-                                   'allocation_phase','allocation_led','allocation_allocation_date',
-                                   'allocation_completion_date','allocation__3rd_party_award_date',
-                                  'allocation_ea', 'allocation_sb1_funding',  'allocation_ggrf_funding',
-                                   'allocation_project_id']
-project_cols = ['project_project_manager','project_award_year', 'project_project_#','project_project_title',
-                             'project_ppno',  'project_tircp_award_amount__$_',
-                             'project_expended_amount','project_allocated_amount','project_grant_recipient']
+# Columns to keep
+allocation_cols = [
+    "allocation_award_year",
+    "allocation_expended_amount",
+    "allocation_allocation_amount",
+    "allocation_components",
+    "allocation_grant_recipient",
+    "allocation_implementing_agency",
+    "allocation_ppno",
+    "allocation_phase",
+    "allocation_led",
+    "allocation_allocation_date",
+    "allocation_completion_date",
+    "allocation__3rd_party_award_date",
+    "allocation_ea",
+    "allocation_sb1_funding",
+    "allocation_ggrf_funding",
+    "allocation_project_id",
+]
+project_cols = [
+    "project_project_manager",
+    "project_award_year",
+    "project_project_#",
+    "project_project_title",
+    "project_ppno",
+    "project_tircp_award_amount__$_",
+    "project_expended_amount",
+    "project_allocated_amount",
+    "project_grant_recipient",
+]
 
-numeric_cols = ['allocation_expended_amount','allocation_allocation_amount',
-            'project_tircp_award_amount__$_','project_expended_amount',
-            'Percent_of_Allocation_Expended', 'Percent_of_Award_Fully_Allocated']
+numeric_cols = [
+    "allocation_expended_amount",
+    "allocation_allocation_amount",
+    "project_tircp_award_amount__$_",
+    "project_expended_amount",
+    "Percent_of_Allocation_Expended",
+    "Percent_of_Award_Fully_Allocated",
+]
 
-#Date Columns 
-dates = ["allocation_allocation_date", "allocation__3rd_party_award_date",
-             "allocation_completion_date", "allocation_led"]
+dates = [
+    "allocation_allocation_date",
+    "allocation__3rd_party_award_date",
+    "allocation_completion_date",
+    "allocation_led",
+]
 
-#Columns for aggregating
-group_by_cols = ['project_award_year','project_project_#','project_project_manager',
-                 'allocation_grant_recipient', 'allocation_implementing_agency',
-                 'project_project_title', 'Percent_of_Award_Fully_Allocated','TIRCP_Award_Amount',
-                 'allocation_components','project_ppno','allocation_phase',
-                 "allocation_allocation_date",  "CON_Contract_Award_Date",
-                 "Phase_Completion_Date", 'allocation_ea', 'allocation_project_id',]
+group_by_cols = [
+    "project_award_year",
+    "project_project_#",
+    "project_project_manager",
+    "allocation_grant_recipient",
+    "allocation_implementing_agency",
+    "project_project_title",
+    "Percent_of_Award_Fully_Allocated",
+    "TIRCP_Award_Amount",
+    "allocation_components",
+    "project_ppno",
+    "allocation_phase",
+    "allocation_allocation_date",
+    "CON_Contract_Award_Date",
+    "Phase_Completion_Date",
+    "allocation_ea",
+    "allocation_project_id",
+]
 
-sum_cols = ['allocation_allocation_amount','allocation_expended_amount','allocation_sb1_funding',
-    'allocation_ggrf_funding']
+sum_cols = [
+    "allocation_allocation_amount",
+    "allocation_expended_amount",
+    "allocation_sb1_funding",
+    "allocation_ggrf_funding",
+]
 
-max_cols = ['Percent_of_Allocation_Expended','Allocated_Before_July_31_2020']
+max_cols = ["Percent_of_Allocation_Expended", "Allocated_Before_July_31_2020"]
 
+list_to_add = ['Award_Amount',
+               'Amount_Allocated',
+               'Expended_Amount', 
+               'Number_of_Awarded_Projects']
+
+list_to_add = [
+    "Award_Amount",
+    "Amount_Allocated",
+    "Expended_Amount",
+    "Number_of_Awarded_Projects",
+]
+'''
+Create Summary Page
+'''
+def summary_SAR_table(df):
+    # Group by and aggregate
+    df = (
+        df.drop_duplicates()
+        .groupby(["project_award_year"])
+        .agg(
+            {
+                "project_project_#": "count",
+                "project_tircp_award_amount__$_": "sum",
+                "project_allocated_amount": "sum",
+                "project_expended_amount": "sum",
+            }
+        )
+        .reset_index()
+    )
+    # Renaming columns to match report
+    df = df.rename(
+        columns={
+            "project_project_#": "Number_of_Awarded_Projects",
+            "project_tircp_award_amount__$_": "Award_Amount",
+            "project_allocated_amount": "Amount_Allocated",
+            "project_expended_amount": "Expended_Amount",
+            "project_award_year": "Award_Year",
+        }
+    )
+    # Create percentage columns
+    df["Expended_Percent_of_Awarded"] = df["Expended_Amount"] / df["Award_Amount"]
+    df["Expended_Percent_of_Allocated"] = df["Expended_Amount"] / df["Amount_Allocated"]
+    df["Percent_Allocated"] = df["Amount_Allocated"] / df["Award_Amount"]
+
+    # Transpose to be the right order
+    df = df.set_index("Award_Year").T
+
+    # Grand totals for monetary columns
+    df["Grand_Total"] = df.loc[list_to_add, :].sum(axis=1)
+
+    # Grand total project_expended_amount of each monetary column to fill in percentages below.
+    Exp = df.at["Expended_Amount", "Grand_Total"]
+    Alloc = df.at["Amount_Allocated", "Grand_Total"]
+    TIRCP = df.at["Award_Amount", "Grand_Total"]
+
+    # Filling in totals of percentages
+    df.at["Expended_Percent_of_Awarded", "Grand_Total"] = Exp / TIRCP
+    df.at["Expended_Percent_of_Allocated", "Grand_Total"] = Exp / Alloc
+    df.at["Percent_Allocated", "Grand_Total"] = Alloc / TIRCP
+
+    # Switching rows to correct order
+    df = df.reindex(
+        [
+            "Number_of_Awarded_Projects",
+            "Award_Amount",
+            "Amount_Allocated",
+            "Percent_Allocated",
+            "Expended_Amount",
+            "Expended_Percent_of_Awarded",
+            "Expended_Percent_of_Allocated",
+        ]
+    )
+    return df
 '''
 Complete Semiannual Report
 '''
 def full_sar_report():
-    #Load in raw sheets
+    # Load in raw sheets
     df_project = A1_data_prep.clean_project()
     df_allocation = A1_data_prep.clean_allocation()
     previous_sar = A1_data_prep.load_previous_sar()
-    
-    #Function for summary table portion of the report
-    summary = summary_SAR_table_two(df_project)
-    
-    #Only keeping certain columns
-    df_project = (df_project[project_cols])
-    df_allocation =(df_allocation[allocation_cols])
-    
-    #Join the 2 dataframes
-    m1 = df_allocation.merge(df_project, how = "left", 
-                                 left_on = ["allocation_ppno", "allocation_award_year"],
-                                 right_on = ["project_ppno", "project_award_year"])
-    #drop duplicates
-    m1 = m1.drop_duplicates() 
-    
-    #Fill in missing dates with a fake one so it'll show up in the group by 
-    missing_date = pd.to_datetime('2100-01-01')
-    for i in dates:
-        m1[i] = (m1[i]
-                     .fillna(missing_date)
-                     .apply(pd.to_datetime)
-                    )
-    
-    #Add new columns with percentages and a new column to flag whether an allocation date is 
-    #AFTER  7-31-2020 then blank, if BEFORE 7-31-2020 then X
-    m1 = m1.assign(
-    Percent_of_Allocation_Expended = (m1['allocation_expended_amount']/
-                                      m1['allocation_allocation_amount']),
-    Percent_of_Award_Fully_Allocated = (m1['allocation_allocation_amount']/
-                                        m1['project_tircp_award_amount__$_']),
-    Allocated_Before_July_31_2020 =   m1.apply(lambda x: ' ' if x.allocation_allocation_date 
-                                        > pd.Timestamp(2020, 7, 31, 0) else 'X', axis=1))
-    
-        
-    #Filter out projects that are excluded 
-    m1 = (m1[(m1.allocation_allocation_amount > 0 ) & (m1.Percent_of_Allocation_Expended < 0.99)]) 
-    
-    #Fill in null values based on datatype of each column
-    m1 = m1.fillna(m1.dtypes.replace({'float64': 0.0, 'int64': 0}))
-    
-    #Rename cols 
-    m1 = m1.rename(columns = {'allocation_led': 'Phase_Completion_Date',
-         'project_tircp_award_amount__$_': 'TIRCP_Award_Amount',
-         'allocation__3rd_party_award_date':'CON_Contract_Award_Date'})
 
-    #Pivot
-    df_pivoted =m1.groupby(group_by_cols).agg({**{e:'max' for e in max_cols}, **{e:'sum' for e in sum_cols}})
+    # Function for summary table portion of the report
+    summary = summary_SAR_table(df_project)
+
+    # Only keeping certain columns
+    df_project = df_project[project_cols]
+    df_allocation = df_allocation[allocation_cols]
+
+    # Join the 2 dataframes
+    m1 = df_allocation.merge(
+        df_project,
+        how="left",
+        left_on=["allocation_ppno", "allocation_award_year"],
+        right_on=["project_ppno", "project_award_year"],
+    )
+    # drop duplicates
+    m1 = m1.drop_duplicates()
+
+    # Fill in missing dates with a fake one so it'll show up in the group by
+    missing_date = pd.to_datetime("2100-01-01")
+    for i in dates:
+        m1[i] = m1[i].fillna(missing_date).apply(pd.to_datetime)
+
+    """
+    Add new columns with percentages and a new column to flag whether an allocation date is
+    AFTER  7-31-2020 then blank, if BEFORE 7-31-2020 then X
+    """ 
+    m1 = m1.assign(
+        Percent_of_Allocation_Expended=(
+            m1["allocation_expended_amount"] / m1["allocation_allocation_amount"]
+        ),
+        Percent_of_Award_Fully_Allocated=(
+            m1["allocation_allocation_amount"] / m1["project_tircp_award_amount__$_"]
+        ),
+        Allocated_Before_July_31_2020=m1.apply(
+            lambda x: " "
+            if x.allocation_allocation_date > pd.Timestamp(2020, 7, 31, 0)
+            else "X",
+            axis=1,
+        ),
+    )
+
+    # Filter out projects that are excluded
+    m1 = m1[
+        (m1.allocation_allocation_amount > 0)
+        & (m1.Percent_of_Allocation_Expended < 0.99)
+    ]
+
+    # Fill in null values based on datatype of each column
+    m1 = m1.fillna(m1.dtypes.replace({"float64": 0.0, "int64": 0}))
+
+    # Rename cols
+    m1 = m1.rename(
+        columns={
+            "allocation_led": "Phase_Completion_Date",
+            "project_tircp_award_amount__$_": "TIRCP_Award_Amount",
+            "allocation__3rd_party_award_date": "CON_Contract_Award_Date",
+        }
+    )
+
+    # Pivot
+    df_pivoted = m1.groupby(group_by_cols).agg(
+        {**{e: "max" for e in max_cols}, **{e: "sum" for e in sum_cols}}
+    )
+
+    """
+    Apply styling to show difference between current SAR and previous SAR
+    https://stackoverflow.com/questions/17095101/compare-two-dataframes-and-output-their-differences-side-by-side
+    """
+    # Reset index from dataframe above
+    df_current = df_pivoted.reset_index()
+    df_all = pd.concat(
+        [df_current, previous_sar], keys=["Current_SAR", "Previous_SAR"], axis=1
+    )
+    df_all = df_all.swaplevel(axis="columns")[df_current.columns[1:]]
     
-    #Apply styling to show difference between current SAR and previous SAR
-    #https://stackoverflow.com/questions/17095101/compare-two-dataframes-and-output-their-differences-side-by-side
-    #Reset index from dataframe above
-    df_current = df_pivoted.reset_index() 
-    df_all = pd.concat([df_current, previous_sar], keys=['Current_SAR', 'Previous_SAR'], axis =1)
-    df_all = df_all.swaplevel(axis='columns')[df_current.columns[1:]]
-    
-    def highlight_diff(data, color='pink'):
-        attr = 'background-color: {}'.format(color)
-        other = data.xs('Current_SAR', axis='columns', level=-1)
-        return pd.DataFrame(np.where(data.ne(other, level=0), attr, ''),
-                        index=data.index, columns=data.columns)
-    
+    """
+    Note to self: move this out
+    """
+    def highlight_diff(data, color="pink"):
+        attr = "background-color: {}".format(color)
+        other = data.xs("Current_SAR", axis="columns", level=-1)
+        return pd.DataFrame(
+            np.where(data.ne(other, level=0), attr, ""),
+            index=data.index,
+            columns=data.columns,
+        )
+
     df_highlighted = df_all.style.apply(highlight_diff, axis=None)
-    
-    #Save to GCS
+
+    # Save to GCS
     with pd.ExcelWriter(f"{GCS_FILE_PATH}Script_Semi_Annual_Report.xlsx") as writer:
         summary.to_excel(writer, sheet_name="Summary", index=True)
         df_pivoted.to_excel(writer, sheet_name="FY", index=True)
         df_current.to_excel(writer, sheet_name="Unpivoted_Current_Version", index=False)
         df_highlighted.to_excel(writer, sheet_name="highlighted")
-        
+
     return df_current, df_pivoted, summary
