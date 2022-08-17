62080a2598a4aa7a951c8881048c275153d50e66
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-08-17 23:30:21 +0000

    Test full run

--- traffic_ops/create_routes_data.py
+++ traffic_ops/create_routes_data.py
@@ -1,25 +1,16 @@
 """
 Create routes file with identifiers including
 route_id, route_name, agency_id, agency_name.
-
-Operator-routes in shapes.txt need route line geometry.
-Operator-routes not in shapes.txt use stop sequence 
-to generate route line geometry.
 """
-import os
-os.environ["CALITP_BQ_MAX_BYTES"] = str(100_000_000_000)
-
 import dask.dataframe as dd
 import dask_geopandas as dg
 import geopandas as gpd
 import pandas as pd
 
-from calitp.tables import tbl
 from datetime import datetime
-from siuba import *
 
 import prep_data
-from shared_utils import geography_utils, gtfs_utils, portfolio_utils
+from shared_utils import geography_utils, portfolio_utils
 
 
 def merge_trips_to_routes(trips: dd.DataFrame, 
@@ -41,25 +32,68 @@ def merge_trips_to_routes(trips: dd.DataFrame,
     
     # Left only means in trips, but shape_id not found in shapes.txt
     # right only means in routes, but no route that has that shape_id 
-    # We probably should keep how = "left"?
-    # left only means we can assemble from stop sequence?
+    # only 1% falls into right_only
     m1 = dd.merge(
             trips,
             routes[shape_id_cols + ["geometry"]].drop_duplicates(),
             on = shape_id_cols,
-            how = "left",
+            how = "inner",
             #validate = "m:1",
-            indicator=True
         ).compute()
 
-    # routes is a gdf, so turn it back into gdf
+    # routes is a gdf, but we lost it putting it as right df in merge, so turn it back into gdf
     m2 = gpd.GeoDataFrame(m1, geometry="geometry", 
                           crs = geography_utils.CA_NAD83Albers
                          ).to_crs(geography_utils.WGS84)
-    
-    m2 = dg.from_geopandas(m2, npartitions=1)
-    
+        
     return m2
+
+
+def add_route_agency_name(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    
+    route_name_used = portfolio_utils.add_route_name(
+        SELECTED_DATE = prep_data.ANALYSIS_DATE,
+    )
+    
+    route_cols_to_drop = ["route_short_name", "route_long_name", "route_desc"]
+    for c in route_cols_to_drop:
+        if c in df.columns:
+            df = df.drop(columns = c)
+    
+    with_route_name = pd.merge(
+        df, 
+        route_name_used.drop_duplicates(subset=["calitp_itp_id", "route_id"]),
+        on = ["calitp_itp_id", "route_id"],
+        how = "inner",
+        validate = "m:1"
+    )
+    
+    # Attach agency_name
+    agency_names = portfolio_utils.add_agency_name(
+        SELECTED_DATE = prep_data.ANALYSIS_DATE)
+    
+    with_agency_name = pd.merge(
+        with_route_name,
+        agency_names,
+        on = "calitp_itp_id",
+        how = "left",
+        validate = "m:1"
+    )
+    
+    # Only keep latest ITP IDS
+    latest_itp_id = portfolio_utils.latest_itp_id()
+    
+    with_latest_id = (pd.merge(
+        with_agency_name, 
+        latest_itp_id,
+        on = "calitp_itp_id",
+        how = "inner",
+        validate = "m:1")
+        # Any renaming to be done before exporting
+        .rename(columns = prep_data.RENAME_COLS)
+    )
+    
+    return with_latest_id
 
 
 # Assemble routes file
@@ -77,33 +111,12 @@ def make_routes_shapefile():
     time1 = datetime.now()
     print(f"Read in data and merge shapes to routes: {time1-time0}")        
     
-    # Attach agency_name
-    agency_names = portfolio_utils.add_agency_name(
-        SELECTED_DATE = prep_data.ANALYSIS_DATE)
-    
-    routes_with_names = pd.merge(
-        df,
-        agency_names,
-        on = "calitp_itp_id",
-        how = "left",
-        validate = "m:1"
-    )
-    
-    latest_itp_id = portfolio_utils.latest_itp_id()
-    
-    routes_with_names = (pd.merge(
-        routes_with_names, 
-        latest_itp_id,
-        on = "calitp_itp_id",
-        how = "inner",
-        validate = "m:1")
-     # Any renaming to be done before exporting
-     .rename(columns = prep_data.RENAME_COLS)
-     .sort_values(["itp_id", "route_id"])
-     .reset_index(drop=True)
-    )
+    routes_with_names = (add_route_agency_name(df)
+                         .sort_values(["itp_id", "route_id"])
+                         .reset_index(drop=True)
+                        )
     
     time3 = datetime.now()
     print(f"Routes script total execution time: {time3-time0}")
 
-    return routes_assembled2
+    return routes_with_names
--- traffic_ops/create_stops_data.py
+++ traffic_ops/create_stops_data.py
@@ -17,11 +17,11 @@ from siuba import *
 
 import prep_data
 from shared_utils import geography_utils, portfolio_utils
-
+from create_routes_data import add_route_agency_name
 
 # Attach all the various route information    
 def attach_route_info_to_stops(stops: dg.GeoDataFrame, 
-                               trips: dg.GeoDataFrame) -> dg.GeoDataFrame:
+                               trips: dg.GeoDataFrame) -> gpd.GeoDataFrame:
     # From trip table, we have trip_key
     # Compare this against the index table, where we have trip_key and stop_key
     trip_keys_on_day = list(trips.trip_key.unique())
@@ -41,37 +41,11 @@ def attach_route_info_to_stops(stops: dg.GeoDataFrame,
     ).merge(trips, 
             on = ["calitp_itp_id", "trip_key"],
             how = "inner"
-    )
+    ).to_crs(geography_utils.WGS84).compute()
     
-    route_name_used = portfolio_utils.add_route_name(SELECTED_DATE = prep_data.ANALYSIS_DATE)
-    
-    keep_cols = [
-        'calitp_itp_id', 'stop_id', 'stop_name', 'geometry',
-        'route_id', 'route_name_used',
-    ]
-    
-    stops_with_route_names = dd.merge(
-        stops_on_day,
-        route_name_used,
-        on = ["calitp_itp_id", "route_id"],
-        how = "inner",
-    )[keep_cols]
-    
-    # Attach agency_name
-    agency_names = portfolio_utils.add_agency_name(
-        SELECTED_DATE = prep_data.ANALYSIS_DATE)
-    
-    stops_with_names = dd.merge(
-        stops_with_route_names,
-        agency_names,
-        on = "calitp_itp_id",
-        how = "left",
-        #validate = "m:1"
-    )
-    
-    stops_cleaned = stops.to_crs(geography_utils.WGS84).compute()
-    
-    return stops_cleaned
+    stops_with_names = add_route_agency_name(stops_on_day)
+
+    return stops_with_names
 
 
 def make_stops_shapefile():
@@ -89,18 +63,10 @@ def make_stops_shapefile():
     time2 = datetime.now()
     print(f"Attach route and operator info to stops: {time2-time1}")
     
-    latest_itp_id = portfolio_utils.latest_itp_id()
-
-    df2 = (pd.merge(df, 
-                    latest_itp_id,
-                    on = "calitp_itp_id",
-                    how = "inner")
-           # Any renaming to be done before exporting
-           .rename(columns = prep_data.RENAME_COLS)
-           .sort_values(["itp_id", "route_id", "stop_id"])
-           .reset_index(drop=True)
-          )
+    stops_assembled = (df.sort_values(["itp_id", "route_id", "stop_id"])
+                       .reset_index(drop=True)
+    )
     
     print(f"Stops script total execution time: {time2-time0}")
     
-    return df2
+    return stops_assembled
