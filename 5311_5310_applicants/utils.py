0aaa428d65015b319cc65e3d1c84e0147da9fa6a
amandaha8 <amandahastuyck@gmail.com>
2022-08-18 23:07:00 +0000

    Took prelim look at merged transit stacks, fare-systems, and blackcat data

--- 5311_5310_applicants/utils.py
+++ 5311_5310_applicants/utils.py
@@ -16,3 +16,15 @@ def organization_cleaning(df, column_wanted: str):
     )
     return df
 
+'''
+Puts all the elements in the column "col to summarize" onto one line and separates them by commas. 
+For ex: an agency typically purchases 1+ products but in the original dataset,
+each product has its own line: this function can grab all the products by agency and puts them on the same line.
+'''
+def summarize_rows(df, col_to_group: str, col_to_summarize: str):
+    df_col_to_summarize = (df
+    .groupby(col_to_group)[col_to_summarize]
+    .apply(','.join)
+    .reset_index()
+     )
+    return df_col_to_summarize
