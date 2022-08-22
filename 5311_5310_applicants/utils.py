8977ba42cfcee5782fb70c4e7c798bbb8a555371
amandaha8 <amandahastuyck@gmail.com>
2022-08-22 23:55:32 +0000

    Continued cleaning organization name, split CAD AVL and fare system components into own cols, export df to google sheets for manual fill in

--- 5311_5310_applicants/utils.py
+++ 5311_5310_applicants/utils.py
@@ -28,3 +28,29 @@ def summarize_rows(df, col_to_group: str, col_to_summarize: str):
     .reset_index()
      )
     return df_col_to_summarize
+
+# Remove underscores, title case, and strip whitespaces
+def clean_up_columns(df):
+    df.columns = df.columns.str.replace("_", " ").str.title().str.strip()
+    return df
+
+# Function that moves all elements in a particular column that is separated by commas onto one line 
+def service_comps_summarize(df, components_wanted: list):
+
+    # Filter out
+    df = df[df["service_components_component_name"].isin(components_wanted)]
+
+    # Puts all the components  an organization purchases onto one line, instead of a few different ones into a df
+    comps = summarize_rows(
+        df, "service_components_service_name", "service_components_component_name"
+    )
+
+    # Puts all product names onto one line into a new df
+    prods = summarize_rows(
+        df, "service_components_service_name", "service_components_product_name"
+    )
+
+    # Merge comps and prods together
+    m1 = pd.merge(comps, prods, how="inner", on="service_components_service_name")
+
+    return m1
