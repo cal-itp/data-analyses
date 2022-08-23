4d41e88a98abc664421486c765002c0c5f56de88
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-08-23 18:35:26 +0000

    Run 2/8 cached files again

--- high_quality_transit_areas/update_vars.py
+++ high_quality_transit_areas/update_vars.py
@@ -1,7 +1,7 @@
 import datetime as dt
 from shared_utils import rt_utils
 
-analysis_date = dt.date(2022, 7, 13) 
+analysis_date = dt.date(2022, 2, 8) 
 date_str = analysis_date.strftime(rt_utils.FULL_DATE_FMT)
 
 CACHED_VIEWS_EXPORT_PATH = f"{rt_utils.GCS_FILE_PATH}cached_views/"
