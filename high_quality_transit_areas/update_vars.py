35dbf0d1456f23b82d8204acde64370ab1fd55db
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-08-30 16:18:55 +0000

    add logger, run aug for hqta

--- high_quality_transit_areas/update_vars.py
+++ high_quality_transit_areas/update_vars.py
@@ -1,8 +1,7 @@
 import datetime as dt
-from shared_utils import rt_utils
+from shared_utils import rt_utils, rt_dates
 
-analysis_date = dt.date(2022, 7, 13) 
-date_str = analysis_date.strftime(rt_utils.FULL_DATE_FMT)
+analysis_date = rt_dates.DATES["aug2022"] 
 
 CACHED_VIEWS_EXPORT_PATH = f"{rt_utils.GCS_FILE_PATH}cached_views/"
 ALL_OPERATORS_FILE = "./hqta_operators.json"
