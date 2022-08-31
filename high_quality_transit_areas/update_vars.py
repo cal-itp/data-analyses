5d585ff67a72b526c3b664cbef2896b535ca271e
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-08-31 21:23:50 +0000

    Rerun july A2 to grab metro brt correctly, and rerun D1, D2

--- high_quality_transit_areas/update_vars.py
+++ high_quality_transit_areas/update_vars.py
@@ -1,7 +1,7 @@
 import datetime as dt
 from shared_utils import rt_utils, rt_dates, gtfs_utils
 
-analysis_date = gtfs_utils.format_date(rt_dates.DATES["aug2022"])
+analysis_date = gtfs_utils.format_date(rt_dates.DATES["jul2022"])
 
 CACHED_VIEWS_EXPORT_PATH = f"{rt_utils.GCS_FILE_PATH}cached_views/"
 ALL_OPERATORS_FILE = "./hqta_operators.json"
