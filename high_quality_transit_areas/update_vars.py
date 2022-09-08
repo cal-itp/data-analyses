a0cf4a796107d4d98e7d5a8b2b9224aef02379a3
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-09-08 22:16:05 +0000

    Move compiling/caching routelines, stops, trips, stop_times from traffic_ops to hqta and use this

--- high_quality_transit_areas/update_vars.py
+++ high_quality_transit_areas/update_vars.py
@@ -4,5 +4,6 @@ from shared_utils import rt_utils, rt_dates, gtfs_utils
 analysis_date = gtfs_utils.format_date(rt_dates.DATES["aug2022"])
 
 CACHED_VIEWS_EXPORT_PATH = f"{rt_utils.GCS_FILE_PATH}cached_views/"
+COMPILED_CACHED_VIEWS = f"{rt_utils.GCS_FILE_PATH}compiled_cached_views/"
 ALL_OPERATORS_FILE = "./hqta_operators.json"
 VALID_OPERATORS_FILE = "./valid_hqta_operators.json"
