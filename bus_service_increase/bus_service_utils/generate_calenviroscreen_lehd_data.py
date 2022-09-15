72612ac695f301d356abea282fc7d786b77791c7
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-09-15 21:58:51 +0000

    add bus_service_utils, update paths and make it importable

---
+++ bus_service_increase/bus_service_utils/generate_calenviroscreen_lehd_data.py
@@ -0,0 +1,21 @@
+"""
+Download LEHD data from Urban Institute data catalog
+
+Combine with CalEnviroscreen data, 
+clean both CalEnviroScreen and LEHD, generate
+needed columns, and upload to GCS.
+"""
+from bus_service_utils import calenviroscreen_lehd_utils
+
+LEHD_DATASETS = calenviroscreen_lehd_utils.LEHD_DATASETS
+
+if __name__ == "__main__":
+    LEHD_DATE_DOWNLOAD = "2021/04/19/"
+
+    #calenviroscreen_lehd_utils.download_lehd_data(
+    #    LEHD_DATE_DOWNLOAD, LEHD_DATASETS)
+    
+    calenviroscreen_lehd_utils.generate_calenviroscreen_lehd_data(
+        calenviroscreen_quartile_groups = 3,
+        lehd_datasets = LEHD_DATASETS,
+        GCS = True)
---
+++ bus_service_increase/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="bus_service_utils",
+    packages=find_packages(),
+    version="0.1.1",
+    description="Shared utility functions for bus service data analyses",
+    author="Cal-ITP",
+    license="Apache",
+    include_package_data=True,
+    package_dir={"bus_service_utils": "bus_service_utils"},
+)
