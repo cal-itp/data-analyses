0a4c48869344d1e168be6aec42dc5fb8c6c3af83
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-09-15 23:31:26 +0000

    add bus_service_utils/chart_utils

---
+++ bus_service_increase/bus_service_utils/chart_utils.py
@@ -0,0 +1,32 @@
+"""
+Common chart / viz functions.
+"""
+import pandas as pd
+
+CHART_LABELING = {
+    "arrivals_per_1k_pj" : "Arrivals per 1k by Pop / Job Density",
+    "equity_group": "CalEnviroScreen",
+    "popjobdensity_group": "Pop / Job Density",
+}
+
+def labeling(word: str, labeling_dict: dict) -> str:
+    """
+    Supply a labeling dictionary where 
+    keys are existing column names and 
+    values are what's to be displayed on visualization.
+    """
+    if word in labeling_dict.keys():
+        word = labeling_dict[word]
+    else: 
+        word = word.replace('_', ' ').title()
+    return word
+
+
+def set_legend_order(label_dict: dict) -> list:
+    """
+    Returns the custom sort order for altair legend.
+    Put the values in the dictionary in the order you want.
+    """
+    legend_order = list(label_dict.values())
+
+    return legend_order 
