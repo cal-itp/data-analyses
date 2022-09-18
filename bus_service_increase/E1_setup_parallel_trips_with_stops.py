02be49361b16695ea07804e2cfd143b76f861243
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-09-18 22:38:17 +0000

    Update googlemaps picking of trip, use 5/4 instead of 1/6, keep all routes

--- bus_service_increase/E1_setup_parallel_trips_with_stops.py
+++ bus_service_increase/E1_setup_parallel_trips_with_stops.py
@@ -7,61 +7,43 @@ Create a df that has the selected trip per route
 with all the stop info (and stop sequence) attached.
 """
 import datetime
+import dask.dataframe as dd
+import dask_geopandas as dg
 import geopandas as gpd
 import os
 import pandas as pd
 
-os.environ["CALITP_BQ_MAX_BYTES"] = str(100_000_000_000)
+os.environ["CALITP_BQ_MAX_BYTES"] = str(130_000_000_000)
 
-from calitp.tables import tbl
-from calitp import query_sql
-from siuba import *
-
-import utils
 import shared_utils
+from bus_service_utils import utils
 
+ANALYSIS_DATE = shared_utils.rt_dates.PMAC["Q2_2022"]
+COMPILED_CACHED = f"{shared_utils.rt_utils.GCS_FILE_PATH}compiled_cached_views/"
 
-def grab_service_hours(df: pd.DataFrame, 
-                       SELECTED_DATE: str | datetime.datetime):
-    daily_trip_info = (
-        tbl.views.gtfs_schedule_fact_daily_trips()
-        >> filter(_.service_date == SELECTED_DATE)
-        >> select(_.calitp_itp_id, 
-               _.trip_key, _.service_hours, 
-               _.trip_first_departure_ts, _.trip_last_arrival_ts
-              ) 
-        >> collect()
+def grab_service_hours(selected_date: str, 
+                       valid_trip_keys: list) -> pd.DataFrame:
+    daily_service_hours = shared_utils.gtfs_utils.get_trips(
+        selected_date = selected_date,
+        itp_id_list = None,
+        # Keep more columns, route_id, shape_id, direction_id so the metrolink fix 
+        # can be propagated
+        trip_cols = ["calitp_itp_id", "service_date", "trip_key",
+                     "route_id", "shape_id", "direction_id", "service_hours"],
+        get_df = True,
+        custom_filtering = {"trip_key": valid_trip_keys}
     )
     
-    df2 = pd.merge(df, 
-                   daily_trip_info,
-                   on = ["calitp_itp_id", "trip_key"],
-                   how = "inner",
-                   # m:1 because trips has stop_level data by trips
-                   # 1 is on the right beause service_hours is trip-level
-                   validate = "m:1"
-                  )
-    
-    return df2
+    daily_service_hours.to_parquet(
+        f"{utils.GCS_FILE_PATH}service_hours_{selected_date}.parquet")
+
 
 def select_one_trip(df: pd.DataFrame) -> pd.DataFrame:
-    drop_cols = ["stop_sequence", "stop_id", "departure_time", 
-                 "trip_first_departure_ts", "trip_last_arrival_ts", 
-                ]
-    
-    # Some departure_time is None
-    df = df[df.departure_time.notna()].assign(
-        departure_hr = pd.to_datetime(df.trip_first_departure_ts, unit='s').dt.hour                   
-    ).drop(columns = drop_cols).drop_duplicates()
-    
-    # Make sure no NaNs make it through
-    df = df[(df.departure_hr.notna()) & (df.departure_hr < 24)].reset_index(drop=True)
-    
+        
     # Across trip_ids, for the same route_id, there are differing max_stop_sequence
     # Can't use max_stop_sequence to find longest route_length
     # Use service hours instead to find faster trip during free-flowing traffic
     group_cols = ["calitp_itp_id", "route_id"]
-    
     
     # Select a trip that closest to 25th percentile (lower means faster!)
     # This groupby ruins the index, throws an error, so just merge in as separate df
@@ -77,138 +59,82 @@ def select_one_trip(df: pd.DataFrame) -> pd.DataFrame:
             )
     
     # Select trip that is closest to 25th percentile (min_diff)
-    # ERROR: Should be abs(df.service_hours - df.p25)
-    # But, cannot change df here because the identifier includes trip_id
-    # and don't want to rerun google maps requests
-    df["difference"] = df.service_hours - df.p25
+    df["difference"] = abs(df.service_hours - df.p25)
     df["min_diff"] = df.groupby(group_cols)["difference"].transform("min")
 
-    df['faster_trip'] = df.apply(lambda x: 
-                                 1 if x.difference == x.min_diff else 0, axis=1)
+    df['faster_trip'] = df.apply(
+        lambda x: 1 if x.difference == x.min_diff else 0, axis=1)
     
     # If there are multiple trips selected for a route, do a sort/drop duplicates
-    # This df is trip-level (no stop_id, becuase that was dropped at beginning)
     df2 = (df[df.faster_trip==1]
-           .sort_values(group_cols + ["departure_hr"], 
-                        # If there are multiple trips with same service hours, 
-                        # pick one with later departure hr (closer to mid-day)
-                        ascending=[True, True, False])
+           .sort_values(group_cols + ["trip_id"], ascending=[True, True, True])
            .drop_duplicates(subset=group_cols)
-           .drop(columns = ["faster_trip", "difference", "min_diff", 
-                            "p25"])
+           .drop(columns = ["faster_trip", "difference", "min_diff", "p25"])
            .reset_index(drop=True)
           )
 
     return df2
 
 
-def subset_to_parallel_routes(df: pd.DataFrame) -> gpd.GeoDataFrame:
-    # Just use route_id to flag parallel, not shape_id
-    # It won't matter anyway, because we will use stop's point geom
-    parallel_routes = shared_utils.utils.download_geoparquet(utils.GCS_FILE_PATH, 
-                                             "parallel_or_intersecting")
+def grab_stops_for_trip_selected(trip_df: dd.DataFrame, 
+                                 selected_date: str) -> dg.GeoDataFrame:
     
-    keep_cols = ["calitp_itp_id", "route_id", "geometry"]
-
-    parallel_routes2 = (parallel_routes[parallel_routes.parallel==1]
-           .reset_index(drop=True)
-           .rename(columns = {"itp_id": "calitp_itp_id"})
-           [keep_cols]
-           .drop_duplicates()
-           .reset_index(drop=True)
-          )
+    stop_times = dd.read_parquet(
+        f"{COMPILED_CACHED}st_{selected_date}.parquet")
     
-    # Put parallel routes on the right because we don't need its line geometry
-    gdf = pd.merge(
-        df,
-        parallel_routes2,
-        on = ["calitp_itp_id", "route_id"],
-        how = "inner",
-        validate = "m:1",
+    stop_times_for_trip = dd.merge(
+        trip_df,
+        stop_times[["trip_key", "stop_id", "stop_sequence"]],
+        on = "trip_key",
+        how = "inner"
     )
     
-    return gdf
-
-
-def grab_stop_geom(df: pd.DataFrame) -> gpd.GeoDataFrame:
-    stop_info = (tbl.views.gtfs_schedule_dim_stops()
-                 >> select(_.calitp_itp_id,
-                       _.stop_id, _.stop_lon, _.stop_lat,
-                      )
-             >> distinct()
-             >> collect()
+    # Add in stop geom
+    stops = dg.read_parquet(
+        f"{COMPILED_CACHED}stops_{selected_date}.parquet"
     )
     
-    df2 = pd.merge(
-        df,
-        (stop_info.sort_values(["calitp_itp_id", "stop_id", "stop_lon"])
-         .drop_duplicates(subset=["calitp_itp_id", "stop_id"])
-        ),
+    stop_times_with_geom = dd.merge(
+        stops[["calitp_itp_id", "stop_id", "stop_name", "geometry"]].drop_duplicates(),
+        stop_times_for_trip,
         on = ["calitp_itp_id", "stop_id"],
-        how = "inner", 
-        validate = "m:1"
+        how = "inner"
+    ).to_crs(shared_utils.geography_utils.WGS84)
+    
+    
+    stop_times_with_geom2 = (stop_times_with_geom.drop(
+        columns = ["calitp_extracted_at", "calitp_deleted_at"])
+        .sort_values(["calitp_itp_id", "route_id", "trip_id", "stop_sequence"])
+        .reset_index(drop=True)
     )
     
-    gdf = (shared_utils.geography_utils.create_point_geometry(df2)
-           .sort_values(["calitp_itp_id", "route_id", 
-                         "trip_id", "stop_sequence"])
-           .reset_index(drop=True)
-           .drop(columns = ["stop_lon", "stop_lat", 
-                            # Keep trip_first_arrival_ts (in case departure_time is NaN)
-                            "trip_last_arrival_ts"])
-          )
-
-    return gdf
-
-
-def make_parallel_routes_df_with_stops(): 
-    DATA_PATH = f"{utils.GCS_FILE_PATH}2022_Jan/"
-
-    # Read in intermediate parquet for trips on selected date
-    trips = pd.read_parquet(f"{DATA_PATH}trips_joined_thurs.parquet")
-    SELECTED_DATE = '2022-1-6' #warehouse_queries.dates['thurs']
-    
-    print("Read in data")
-    
-    # Attach service hours
-    # This df is trip_id-stop_id level
-    trips_with_service_hrs = grab_service_hours(trips, SELECTED_DATE)
-
-    # Narrow down to 1 trip per route_id
-    selected_trip = select_one_trip(trips_with_service_hrs)
-    
-    print("Select 1 trip")
-
-    # Narrow down to just parallel routes
-    selected_parallel_trips = subset_to_parallel_routes(selected_trip)
-
-    # Recall: trips_with_service_hrs contains stop_id
-    # selected_parallel_trips is trip-level
-    # Pare down trips_with_service_hrs with isin()
-    parallel_trips_with_stops = (
-        trips_with_service_hrs[
-                trips_with_service_hrs.trip_key.isin(selected_parallel_trips.trip_key)]
-              .reset_index(drop=True)
-             )
-    
-    print("Select parallel trip and cleanup")
-
-    final_df = grab_stop_geom(parallel_trips_with_stops)
-    
-    # Get rid of dups / drop ITP_ID==200
-    final_df = (final_df[final_df.calitp_itp_id != 200]
-                .sort_values(["calitp_itp_id", "route_id", 
-                              "trip_id", "stop_sequence"])
-                .drop_duplicates(subset=["calitp_itp_id", "route_id", 
-                                         "trip_id", "stop_sequence"])
-                .reset_index(drop=True)
-               )
-    
-    shared_utils.utils.geoparquet_gcs_export(final_df, 
-                                             utils.GCS_FILE_PATH, 
-                                             "parallel_trips_with_stops")
-    print("Exported to GCS")
+    return stop_times_with_geom2
 
 
 if __name__ == "__main__":
-    make_parallel_routes_df_with_stops() 
+    
+    trips = dd.read_parquet(
+        f"{COMPILED_CACHED}trips_{ANALYSIS_DATE}.parquet")
+    
+    valid_trip_keys = trips.trip_key.unique().compute().tolist()
+    #grab_service_hours(ANALYSIS_DATE, valid_trip_keys)
+    
+    daily_service_hours = pd.read_parquet(
+        f"{utils.GCS_FILE_PATH}service_hours_{ANALYSIS_DATE}.parquet")
+
+    df = dd.merge(
+        trips, 
+        daily_service_hours[["trip_key", "service_hours"]],
+        on = "trip_key",
+        how = "inner",
+    ).compute()
+    
+    one_trip = select_one_trip(df)
+
+    trips_with_stops = grab_stops_for_trip_selected(one_trip, ANALYSIS_DATE)
+    
+    shared_utils.utils.geoparquet_gcs_export(
+        trips_with_stops,
+        utils.GCS_FILE_PATH,
+        "parallel_trips_with_stops"
+    )
