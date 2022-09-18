55cbacd96baf3e62a7bf7df024bd26457423eefa
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-09-18 22:33:57 +0000

    rerun A1 generate data for quarterly metrics

--- quarterly_performance_objective/A1_generate_routes_on_shn_data.py
+++ quarterly_performance_objective/A1_generate_routes_on_shn_data.py
@@ -1,20 +1,22 @@
 """
 Script to create PMAC data
 """
+import datetime
+import geopandas as gpd
 import os
+import pandas as pd
+import sys
+
 os.environ["CALITP_BQ_MAX_BYTES"] = str(130_000_000_000)
 
-import geopandas as gpd
-import pandas as pd
-
-from calitp.tables import tbl
-from siuba import *
-
-import pmac_utils
-from bus_service_utils import create_parallel_corridors
-from update_vars import COMPILED_CACHED_GCS
+from bus_service_utils import create_parallel_corridors, gtfs_build
 from shared_utils import gtfs_utils, geography_utils, portfolio_utils
-from bus_service_utils import gtfs_build
+from update_vars import ANALYSIS_DATE, BUS_SERVICE_GCS, COMPILED_CACHED_GCS
+
+from loguru import logger
+
+logger.add("./logs/A1_generate_routes_on_shn_data.log")
+logger.add(sys.stderr, format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", level="INFO")
 
 
 def merge_routelines_with_trips(selected_date: str) -> gpd.GeoDataFrame: 
@@ -73,37 +75,11 @@ def get_total_service_hours(selected_date):
     # Once aggregated dataset written to GCS, remove local cache
     os.remove(f"./data/trips_with_hrs_staging_{selected_date}.parquet")
 
-
-def add_route_geom(analysis_date: str)-> gpd.GeoDataFrame:
-    """
-    Combine df with parallel/on_shn/other designation
-    with route line geom.
-    """
-    df = pmac_utils.flag_parallel_intersecting_routes(analysis_date)
-    
-    trips_with_geom = merge_routelines_with_trips(analysis_date)
-    
-    bus_route_with_geom = create_parallel_corridors.process_transit_routes(
-        alternate_df = trips_with_geom)
-    
-    gdf = pd.merge(
-        bus_route_with_geom[["itp_id", "route_id", "route_length", "geometry"]], 
-        df,
-        on = ["itp_id", "route_id"],
-        how = "left",
-    )    
-    
-    gdf = gdf.assign(
-        total_service_hours = gdf.total_service_hours.round(2),
-        # simplify geom to make it faster to plot?
-        geometry = gdf.geometry.simplify(tolerance = 20),
-        route_length_mi = round(gdf.route_length / geography_utils.FEET_PER_MI, 2),
-    ).drop(columns = "route_length")
-    
-    return gdf
     
     
 if __name__ == "__main__":    
+    
+    start = datetime.datetime.now()
     
     # Use concatenated routelines and trips from traffic_ops work
     # Use the datasets with Amtrak added back in (rt_delay always excludes Amtrak)
@@ -119,16 +95,27 @@ if __name__ == "__main__":
         FILE_NAME = f"routes_on_shn_{ANALYSIS_DATE}"
     )  
     
+    time1 = datetime.datetime.now()
+    logger.info(f"routes on SHN created: {time1 - start}")
+
     # Grab other routes where at least 30% of route is within 0.5 mile of SHN
     create_parallel_corridors.make_analysis_data(
         hwy_buffer_feet= geography_utils.FEET_PER_MI * 0.5, 
         alternate_df = trips_with_geom,
-        pct_route_threshold = 0.3,
+        pct_route_threshold = 0.2,
         pct_highway_threshold = 0,
         DATA_PATH = BUS_SERVICE_GCS, 
         FILE_NAME = f"parallel_or_intersecting_{ANALYSIS_DATE}"
     )
     
+    time2 = datetime.datetime.now()
+    logger.info(f"routes within half mile buffer created: {time2 - time1}")
     
     # Get aggregated service hours by shape_id
     #get_total_service_hours(ANALYSIS_DATE)
+    
+    time3 = datetime.datetime.now()
+    logger.info(f"downloaded service hours at shape_id level: {time3 - time2}")
+    
+    end = datetime.datetime.now()
+    logger.info(f"execution time: {end - start}")
---
+++ quarterly_performance_objective/update_vars.py
@@ -0,0 +1,6 @@
+from shared_utils import rt_dates, rt_utils
+from bus_service_utils import utils
+
+BUS_SERVICE_GCS = f"{utils.GCS_FILE_PATH}"
+COMPILED_CACHED_GCS = f"{rt_utils.GCS_FILE_PATH}compiled_cached_views/"
+ANALYSIS_DATE = rt_dates.PMAC["Q2_2022"] 
