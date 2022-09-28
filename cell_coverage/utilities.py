c765159862332ab5d54ed513097d57623c2f046a
amandaha8 <amandahastuyck@gmail.com>
2022-09-28 17:15:28 +0000

    more experimenting with sjoins

--- cell_coverage/utilities.py
+++ cell_coverage/utilities.py
@@ -2,6 +2,8 @@
 from shared_utils import geography_utils
 import geopandas as gpd
 import fsspec
+from calitp import *
+
 GCS_FILE_PATH = "gs://calitp-analytics-data/data-analyses/cellular_coverage/"
 
 # Function to clip the data coverage map to California only.
@@ -21,3 +23,39 @@ def create_california_coverage(file_zip_name:str, new_file_name:str):
 
     # Save this into a parquet so don't have to clip all the time
     utils.geoparquet_gcs_export(fcc_ca_gdf, GCS_FILE_PATH, new_file_name)
+
+# Return a cleaned up NTD dataframe 
+def ntd_vehicles():
+    
+    # Open sheet
+    df = pd.read_excel(
+    f"gs://calitp-analytics-data/data-analyses/5311 /2020-Vehicles_1.xlsm",
+    sheet_name="Vehicle Type Count by Agency",)
+    
+    # Only grab California
+    df = df.loc[ntd_df2["state"] == "CA"]
+    
+    # Only get bus related columns
+    columns_wanted = [
+    "Agency",
+    "State",
+    "Bus",
+    "Over-The-Road Bus",
+    "Articulated Bus",
+    "Double Decker Bus",
+    "School Bus",
+    "Van",
+    "Cutaway",
+    "Minivan"]
+    
+    # Have to add snakecase after b/c some columns have integers
+    # Drop unwanted columns
+    df = to_snakecase(df[columns_wanted])
+    
+    # Add up buses
+    df["total_buses"] = df.sum(numeric_only=True, axis=1)
+    
+    # Drop agencies with 0 buses
+    df = df.loc[ntd_df2['total_buses'] !=0]
+    
+    return df
