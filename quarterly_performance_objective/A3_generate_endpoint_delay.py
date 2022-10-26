611af71bd571a9e7652b36590726d39117671d82
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-10-26 23:22:24 +0000

    Add docstrings for A3, A4 functions

--- quarterly_performance_objective/A3_generate_endpoint_delay.py
+++ quarterly_performance_objective/A3_generate_endpoint_delay.py
@@ -2,37 +2,18 @@
 Download RT endpoint delay
 """
 import datetime
-import gcsfs
 import pandas as pd
 import sys
 
-# Add rt utils needed
-sys.path.append("../rt_delay/")
-
-from calitp.tables import tbl
-from siuba import *
 from loguru import logger
 
-import rt_filter_map_plot
-from shared_utils import rt_utils, utils
+from shared_utils import rt_utils, utils, gtfs_utils
+from rt_analysis import rt_filter_map_plot
+
 from update_vars import ANALYSIS_DATE, COMPILED_CACHED_GCS
 
 logger.add("./logs/A3_generate_endpoint_delay.log")
 logger.add(sys.stderr, format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", level="INFO")
-
-fs = gcsfs.GCSFileSystem()
-
-MONTH_DAY = f"{ANALYSIS_DATE.split('-')[1]}_{ANALYSIS_DATE.split('-')[2]}"
-
-def operators_with_rt(selected_date: str):
-    fs_list = fs.ls(f"{rt_utils.GCS_FILE_PATH}rt_trips/")
-
-    ## now finds ran operators on specific analysis date
-
-    ran_operators = [int(path.split('rt_trips/')[1].split('_')[0])
-                     for path in fs_list if MONTH_DAY in path]
-    
-    return ran_operators
 
 
 def aggregate_endpoint_delays(itp_id_list: list) -> pd.DataFrame:
@@ -64,13 +45,15 @@ if __name__=="__main__":
     logger.info(f"Analysis date: {ANALYSIS_DATE}")
     start = datetime.datetime.now()
     
-    ran_operators = operators_with_rt(MONTH_DAY)
+    operators_status_dict = rt_utils.get_operators(
+        ANALYSIS_DATE, gtfs_utils.ALL_ITP_IDS)
     
     aggregate_endpoint_delays = aggregate_endpoint_delays(ran_operators)
     
     time1 = datetime.datetime.now()
     logger.info(f"concatenate all endpoint delays for operators: {time1 - start}")
-
+    
+    # Save in GCS
     utils.geoparquet_gcs_export(
         aggregate_endpoint_delays,
         COMPILED_CACHED_GCS,
@@ -78,21 +61,3 @@ if __name__=="__main__":
     )
  
     logger.info(f"execution time: {datetime.datetime.now() - start}")
-
-    '''
-    airtable_organizations = (
-        tbl.airtable.california_transit_organizations()
-        >> select(_.itp_id, _.name, _.caltrans_district,
-                  _.website, _.ntp_id, _.drmt_organization_name)
-        >> arrange(_.caltrans_district, _.itp_id)
-        >> collect()
-        >> filter(_.itp_id.isin(ran_operators))
-        >> mutate(itp_id = _.itp_id.astype('int64'), 
-                  analysis_date = ANALYSIS_DATE
-                 )
-    )
-    
-    airtable_organizations.to_parquet(
-        f"{DATA_PATH}{ANALYSIS_DATE.isoformat()}_working_organizations.parquet")
-
-    '''
