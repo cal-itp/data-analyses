e15e7076aa29927525f579363f3953c32731efa8
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-10-26 23:35:50 +0000

    Move report data into B1_report_metrics

--- quarterly_performance_objective/B1_report_metrics.py
+++ quarterly_performance_objective/B1_report_metrics.py
@@ -1,11 +1,15 @@
 """
 Functions to calculate summary stats for report.
+
+Two reports: current quarter, historical report.
+Since we need to the same dataset across notebooks, 
+generate the various pieces needed in the report too.
 """
 import geopandas as gpd
 import pandas as pd
 
 from shared_utils import geography_utils
-
+from update_vars import ANALYSIS_DATE, BUS_SERVICE_GCS
 
 def add_percent(df: pd.DataFrame, col_list: list) -> pd.DataFrame:
     """
@@ -93,7 +97,8 @@ def get_delay_summary_table(df: pd.DataFrame) -> pd.DataFrame:
 
 def by_district_on_shn_breakdown(df: pd.DataFrame, sum_cols: list) -> pd.DataFrame:
     """
-    Get service hours or delay hours by district.
+    Get service hours or delay hours by district, and 
+    add in percent and average metrics.
     """
     by_district = geography_utils.aggregate_by_geography(
         df[df.category=="on_shn"],
@@ -124,3 +129,36 @@ def by_district_on_shn_breakdown(df: pd.DataFrame, sum_cols: list) -> pd.DataFra
     ).rename(columns = {"avg": f"avg_{numerator_col}"})
     
     return by_district
+
+
+def route_type_names(row): 
+    if row.route_type in ['0', '1', '2']:
+        return "Rail"
+    elif row.route_type == '3':
+        return "Bus"
+    elif row.route_type == '4':
+        return "Ferry"
+    else:
+        return "Unknown"
+
+    
+def prep_data_for_report(analysis_date: str) -> gpd.GeoDataFrame:
+    df = gpd.read_parquet(
+        f"{BUS_SERVICE_GCS}routes_categorized_with_delay_{analysis_date}.parquet")
+    
+    # Some interest in excluding modes like rail from District 4
+    df = df.assign(
+        route_type_name = df.apply(lambda x: route_type_names(x), axis=1),
+        delay_hours = round(df.delay_seconds / 60 ** 2, 2)
+    ).drop(columns = "delay_seconds")
+
+    #df[df.category=="on_shn"].route_type_name.value_counts()
+    # This shows that only Bus and Unknown are present for on_shn
+    
+    # Should I subset to df[df._merge=="both"]?
+    # both means that it found a corresponding match in itp_id-route_id 
+    # since it's been aggregated up to route_id level (shape_id can mismatch more easily)
+    # Decide here, this is the subset of data I will use for rest of notebook
+    plot_df = df[df._merge=="both"].reset_index(drop=True)
+    
+    return plot_df
