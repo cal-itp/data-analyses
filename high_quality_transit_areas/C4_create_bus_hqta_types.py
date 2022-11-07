7fa0d8fc51bd184d0c6225c7c62ced4f11619a56
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-11-07 21:26:15 +0000

    Remove C3, update C4 script

--- high_quality_transit_areas/C4_create_bus_hqta_types.py
+++ high_quality_transit_areas/C4_create_bus_hqta_types.py
@@ -6,6 +6,9 @@ These are hqta_types:
 the highest trip count
 * hq_corridor_bus: stops along the HQ transit corr (may not be highest trip count)
 
+# Ex: how to split it up, then apply using map_partitions
+# https://stackoverflow.com/questions/61920105/dask-applying-a-function-over-a-large-dataframe-which-is-more-than-ram
+
 From combine_and_visualize.ipynb
 """
 import dask.dataframe as dd
@@ -15,15 +18,12 @@ import geopandas as gpd
 import pandas as pd
 import sys
 
-from calitp.tables import tbl
-from siuba import *
 from loguru import logger
 
-import C1_prep_for_clipping as prep_clip
-import C3_clean_clipped_intersections as clean_clip
+import C1_prep_pairwise_intersections as prep_clip
 from shared_utils import utils, geography_utils, gtfs_utils
 from utilities import catalog_filepath, GCS_FILE_PATH
-from update_vars import analysis_date
+from update_vars import analysis_date, COMPILED_CACHED_VIEWS
 
 logger.add("./logs/C4_create_bus_hqta_types.log", retention="6 months")
 logger.add(sys.stderr, 
@@ -31,102 +31,20 @@ logger.add(sys.stderr,
            level="INFO")
 
 # Input files
-ALL_BUS = catalog_filepath("all_bus")
+ALL_INTERSECTIONS = catalog_filepath("all_intersections")
 
-segment_cols = ["calitp_itp_id", "hqta_segment_id"]
-stop_cols = ["calitp_itp_id", "stop_id"]
-
-
-def query_all_stops(analysis_date: dt.date) -> gpd.GeoDataFrame:
+def buffer_around_intersections(buffer_size: int = 50) -> dg.GeoDataFrame: 
     """
-    Query and find all stops on selected date 
-    for all operators (except ITP ID 200)
-    and return a gpd.GeoDataFrame.
+    Draw 50 m buffers around intersections to better catch stops
+    that might fall within it.
     """
-    keep_stop_cols = [
-        "calitp_itp_id", "stop_id", "stop_lon", "stop_lat"
-    ]
+    gdf = dg.read_parquet(ALL_INTERSECTIONS)
     
-    ALL_IDS_NO_200 = (tbl.gtfs_schedule.agency()
-         >> distinct(_.calitp_itp_id)
-         >> filter(_.calitp_itp_id != 200)
-         >> collect()
-        ).calitp_itp_id.tolist()
-    
-    stops = gtfs_utils.get_stops(
-        selected_date = analysis_date,
-        itp_id_list = ALL_IDS_NO_200,
-        stop_cols = keep_stop_cols,
-        get_df = True,
-        crs = geography_utils.CA_NAD83Albers,
-        custom_filtering = None
+    gdf = gdf.assign(
+        geometry = gdf.geometry.buffer(50)
     )
-    
-    return stops
 
-
-def merge_clipped_with_hqta_segments() -> gpd.GeoDataFrame:
-    """
-    Merge the cleaned up clipped areas (large areas dropped) 
-    with hqta segments (which contains stop_id).
-    
-    Use an inner merge because we only want the operators
-    who have stops that appear in clipped bus corridor intersections.
-    These will be called "frequent bus stops in intersections".
-    
-    Return a gpd.GeoDataFrame because we need to use gdf.explode(),
-    available only in geopandas, not dask_geopandas.
-    """
-    # Clean up the clipped df and remove large shapes
-    clipped_df = clean_clip.process_clipped_intersections()
-    
-    # For hqta_segment level data, only 1 stop is attached to each segment
-    # It's the stop with the highest trip count
-    hqta_segment = dg.read_parquet(ALL_BUS)
-    
-    keep_cols = segment_cols + ["stop_id", "hq_transit_corr"]
-
-    stops_in_bus_intersections = dd.merge(
-        # Put clipped geometries on the left for merge
-        # Keep the clipped geom, don't want the full hqta_segment geom
-        clipped_df,
-        hqta_segment[keep_cols],
-        on = segment_cols,
-        how = "inner"
-    )
-    
-    # Change to gdf because gdf.explode only works with geopandas, not dask gdf
-    return stops_in_bus_intersections.compute()
-
-
-def explode_bus_intersections(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    """
-    Explode so that the multipolygon becomes multiple rows of polygons
-    
-    Each polygon contains one clipped area
-    Explode works with geopandas gdf, but not dask gdf
-    """
-    keep_cols = segment_cols + ["stop_id", "hq_transit_corr", "geometry"]
-    
-    one_intersection_per_row = gdf[keep_cols].explode(ignore_index=True)
-    
-    return one_intersection_per_row
-
-
-def only_major_bus_stops(all_stops: gpd.GeoDataFrame, 
-                         bus_intersections: gpd.GeoDataFrame):
-    """
-    Pare down all stops to include only the operators
-    who have stops that show up in bus corridor intersections (clipped results).
-    """
-    all_stops_for_major_operators = dd.merge(
-        all_stops,
-        bus_intersections[["calitp_itp_id"]].drop_duplicates(),
-        on = "calitp_itp_id",
-        how = "inner"
-    )
-    
-    return all_stops_for_major_operators
+    return gdf 
 
 
 def create_major_stop_bus(all_stops: gpd.GeoDataFrame, 
@@ -140,9 +58,10 @@ def create_major_stop_bus(all_stops: gpd.GeoDataFrame,
     intersections.
     """
     # Narrow down all stops to only include stops from operators
-    # that have at least 1 stop that is in freq_bus_stops
-    major_stops = only_major_bus_stops(all_stops, bus_intersections)
-
+    # that also have some bus corridor intersection result
+    included_operators = bus_intersections.calitp_itp_id.unique()
+    major_stops = all_stops[all_stops.calitp_itp_id.isin(included_operators)]
+    
     major_bus_stops_in_intersections = (
         dg.sjoin(
             major_stops,
@@ -179,6 +98,8 @@ def create_stops_along_corridors(all_stops: gpd.GeoDataFrame) -> gpd.GeoDataFram
     """
     bus_corridors = prep_clip.prep_bus_corridors()
     
+    stop_cols = ["calitp_itp_id", "stop_id"]
+    
     stops_in_hq_corr = (dg.sjoin(
                             all_stops, 
                             bus_corridors[["geometry"]],
@@ -202,16 +123,12 @@ if __name__ == "__main__":
     logger.info(f"Analysis date: {analysis_date}")
     start = dt.datetime.now()
     
-    # Narrow down to the stops of operators who also have
-    # stops in the clipped results.
-    freq_bus_stops_in_intersections = merge_clipped_with_hqta_segments()
+    # Start with the gdf of all the hqta_segments
+    # that have a sjoin with an orthogonal route
+    bus_intersections = buffer_around_intersections(buffer_size=100)
 
-    # This exploded geometry is what will be used in spatial join on all stops
-    # to see which stops fall in this
-    bus_intersections = explode_bus_intersections(freq_bus_stops_in_intersections)
-    
     # Grab point geom with all stops
-    all_stops = query_all_stops(analysis_date)
+    all_stops = gpd.read_parquet(f"{COMPILED_CACHED_VIEWS}stops_{analysis_date}.parquet")
     logger.info("grab all stops")
     
     # Create hqta_type == major_stop_bus
