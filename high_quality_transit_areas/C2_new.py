9ca35ec53601dcebb31786f07dad860bfed1a286
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-11-07 17:53:47 +0000

    Check clipped results, seems ok, use itertuples, can't get dask geopandas

--- high_quality_transit_areas/C2_new.py
+++ high_quality_transit_areas/C2_new.py
@@ -11,8 +11,8 @@ Big Blue Bus takes 10 min to run.
 
 From combine_and_visualize.ipynb
 """
-import dask.dataframe as dd
-import dask_geopandas as dg
+#import dask.dataframe as dd
+#import dask_geopandas as dg
 import datetime as dt
 import geopandas as gpd
 import glob
@@ -64,7 +64,7 @@ def attach_geometry_to_pairs(corridors: gpd.GeoDataFrame,
         on = "hqta_segment_id",
         how = "inner"
     )
-    
+
     pairs_with_geom2 = pd.merge(
         (corridors[segment_cols]
          .rename(columns = rename_cols)),
@@ -72,15 +72,15 @@ def attach_geometry_to_pairs(corridors: gpd.GeoDataFrame,
         on = "intersect_hqta_segment_id",
         how = "inner"
     )
-    
+
     gdf = (pairs_with_geom2.reindex(columns = col_order)
            .sort_values(["hqta_segment_id", "intersect_hqta_segment_id"])
            .reset_index(drop=True)
           )
     
     return gdf
-
-
+   
+    
 def find_intersections(pairs_table: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     """
     We have pairwise table already, and now there's geometry attached to 
@@ -88,6 +88,9 @@ def find_intersections(pairs_table: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     
     Use iterrtuples to loop through and store results.
     Convert back to gdf at the end.
+    
+    https://stackoverflow.com/questions/33817190/intersection-of-two-linestrings-geopandas
+    https://stackoverflow.com/questions/43221208/iterate-over-pandas-dataframe-using-itertuples
     """
     results = []
     segments = []
@@ -120,8 +123,8 @@ if __name__ == "__main__":
 
     start = dt.datetime.now()
         
-    intersecting_pairs = dd.read_parquet(PAIRWISE_FILE)
-    corridors = dg.read_parquet(SUBSET_CORRIDORS)
+    intersecting_pairs = pd.read_parquet(PAIRWISE_FILE)
+    corridors = gpd.read_parquet(SUBSET_CORRIDORS)
     
     pairs_table = attach_geometry_to_pairs(corridors, intersecting_pairs)
     
@@ -132,10 +135,9 @@ if __name__ == "__main__":
     
     time2 = dt.datetime.now()
     logger.info(f"find intersections: {time2 - time1}")
-    
-    
+        
     utils.geoparquet_gcs_export(
-        results,
+        results_gdf,
         DASK_GCS,
         "all_clipped"
     )
