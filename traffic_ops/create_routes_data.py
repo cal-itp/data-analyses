07225f1b9d3854e73c3dd2d4999675a9b7b38f0c
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-11-09 22:52:00 +0000

    add ca_transit_routes_feed to shared_data_catalog

--- traffic_ops/create_routes_data.py
+++ traffic_ops/create_routes_data.py
@@ -7,8 +7,6 @@ import dask_geopandas as dg
 import geopandas as gpd
 import pandas as pd
 
-from datetime import datetime
-
 import prep_data
 from shared_utils import geography_utils, portfolio_utils
 from bus_service_utils import gtfs_build
@@ -20,36 +18,48 @@ remove_trip_cols = ["service_date", "calitp_extracted_at", "calitp_deleted_at"]
 
 
 def merge_trips_to_routes(trips: dd.DataFrame, 
-                          routes: dg.GeoDataFrame) -> dg.GeoDataFrame:
+                          routes: dg.GeoDataFrame, 
+                          group_cols: list = ["calitp_itp_id", "shape_id"]
+                         ) -> dg.GeoDataFrame:
     # Routes or trips can contain multiple calitp_url_numbers 
     # for same calitp_itp_id-shape_id. Drop these now
     # dask can only sort by 1 column!
-    shape_id_cols = ["calitp_itp_id", "shape_id"]
-    
-    routes = (routes.sort_values("calitp_url_number")
-              .drop_duplicates(subset=shape_id_cols)
+    # when publishing to open data portal, we want it at operator level, not feed level
+    if group_cols == ["calitp_itp_id", "shape_id"]:
+        routes = (routes.sort_values("calitp_url_number")
+              .drop_duplicates(subset=group_cols)
               .reset_index(drop=True)
-    )
-    
-    trips = (trips.sort_values("calitp_url_number")
+        )
+        
+        trips = (trips.sort_values("calitp_url_number")
              .drop_duplicates(subset=["calitp_itp_id", "trip_id"])
              .reset_index(drop=True)
             .drop(columns = remove_trip_cols)
-    )
+        )
+        
+    else:
+        routes = (routes
+          .drop_duplicates(subset=group_cols)
+          .reset_index(drop=True)
+        )
+        trips = (trips.drop_duplicates(
+            subset=["calitp_itp_id", "calitp_url_number", "trip_id"])
+             .reset_index(drop=True)
+            .drop(columns = remove_trip_cols)
+        )
+
     
     # Left only means in trips, but shape_id not found in shapes.txt
     # right only means in routes, but no route that has that shape_id 
     # only 1% falls into right_only
-    keep_cols = [
-        'calitp_itp_id', 'route_id', 'shape_id', 
+    keep_cols = group_cols + ['route_id', 
         'route_type', 'geometry',
     ]
-    
     
     m1 = gtfs_build.merge_routes_trips(
         routes, 
         trips, 
-        shape_id_cols,
+        group_cols,
         crs = f"EPSG: {routes.crs.to_epsg()}"
     )
     
@@ -109,28 +119,24 @@ def add_route_agency_name(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     return with_latest_id
 
 
-# Assemble routes file
-def make_routes_shapefile():
-    time0 = datetime.now()
-    
+def create_routes_file_for_export(analysis_date: str, 
+                                  group_cols: list) -> gpd.GeoDataFrame:
     # Read in local parquets
     trips = dd.read_parquet(
-        f"{prep_data.COMPILED_CACHED_GCS}trips_{prep_data.ANALYSIS_DATE}_all.parquet")
+        f"{prep_data.COMPILED_CACHED_GCS}trips_{analysis_date}_all.parquet")
     routes = dg.read_parquet(
-        f"{prep_data.COMPILED_CACHED_GCS}routelines_{prep_data.ANALYSIS_DATE}_all.parquet")
+        f"{prep_data.COMPILED_CACHED_GCS}routelines_{analysis_date}_all.parquet")
 
-    df = merge_trips_to_routes(trips, routes)
-    
-    time1 = datetime.now()
-    print(f"Read in data and merge shapes to routes: {time1-time0}")        
+    df = merge_trips_to_routes(
+        trips, 
+        routes, 
+        group_cols = group_cols
+    )
     
     routes_with_names = (add_route_agency_name(df)
-                         .sort_values(["itp_id", "route_id"])
-                         .reset_index(drop=True)
-                         .drop_duplicates()
-                        )
+                 .sort_values(["itp_id", "route_id"])
+                 .reset_index(drop=True)
+                 .drop_duplicates()
+                )
     
-    time3 = datetime.now()
-    print(f"Routes script total execution time: {time3-time0}")
-
     return routes_with_names
