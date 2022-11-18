a1a82949ac6d0ae952ef69d1d8750c6b4b62262c
tiffanychu90 <tiffany.chu@dot.ca.gov>
2022-11-18 17:33:18 +0000

    move logs to open when script executed, simplify completeness check with just stop_times file

--- high_quality_transit_areas/operators_for_hqta.py
+++ high_quality_transit_areas/operators_for_hqta.py
@@ -23,12 +23,8 @@ from shared_utils import rt_utils
 from update_vars import (analysis_date, CACHED_VIEWS_EXPORT_PATH, 
                         VALID_OPERATORS_FILE)
 
-
-logger.add("./logs/operators_for_hqta.log", retention="6 months")
-logger.add(sys.stderr, format="{time} {level} {message}", level="INFO")
-
-
-def get_list_of_cached_itp_ids(analysis_date: str, all_itp_ids: list = None) -> list:
+def get_list_of_cached_itp_ids(analysis_date: str, 
+                               all_itp_ids: list = None) -> list:
     """
     ALL_ITP_IDS: list. 
                 Allow passing an alternate list of IDs in. 
@@ -47,7 +43,8 @@ def get_list_of_cached_itp_ids(analysis_date: str, all_itp_ids: list = None) -> 
 
     for itp_id in all_itp_ids:
         response1 = rt_utils.check_cached(
-            f"routelines_{itp_id}_{analysis_date}.parquet", subfolder="cached_views/")
+            f"routelines_{itp_id}_{analysis_date}.parquet", 
+            subfolder="cached_views/")
         response2 = rt_utils.check_cached(
             f"trips_{itp_id}_{analysis_date}.parquet", subfolder="cached_views/")
         response3 = rt_utils.check_cached(
@@ -81,33 +78,40 @@ def itp_ids_from_json(file: str = VALID_OPERATORS_FILE) -> list:
     return data["VALID_ITP_IDS"]
 
 
-def check_for_completeness(export_path: str = CACHED_VIEWS_EXPORT_PATH, 
-                           all_itp_ids: list = [],
-                           analysis_date: str = analysis_date,
-                           valid_operators_file: str = VALID_OPERATORS_FILE):
-        
-    # Go through and check that the files are non-empty for all 4
-    # If it is, then add it to our smaller list of operators that we expect to run through
-    # rest of the HQTA workflow
+def check_for_completeness(
+    export_path: str = CACHED_VIEWS_EXPORT_PATH, 
+    all_itp_ids: list = [],
+    analysis_date: str = analysis_date,
+    valid_operators_file: str = VALID_OPERATORS_FILE) -> list:
+    """
+    Go through and check that the files are non-empty for all 4.
+    
+    Since stop_times is only downloaded if the first 3 are non-empty,
+    just check for presence of stop_times file.
+    
+    Returns a potentially smaller list of operators that 
+    we expect to run through rest of the HQTA workflow
+    """
     IDS_WITH_FULL_INFO = [] 
     
     for itp_id in all_itp_ids:  
-        routelines = dask_geopandas.read_parquet(
-                f"{export_path}routelines_{itp_id}_{analysis_date}.parquet")
-        trips = dd.read_parquet(f"{export_path}trips_{itp_id}_{analysis_date}.parquet")
-        stop_times = dd.read_parquet(f"{export_path}st_{itp_id}_{analysis_date}.parquet")
-        stops = dask_geopandas.read_parquet(
-            f"{export_path}stops_{itp_id}_{analysis_date}.parquet")
-       
-        if ( (len(routelines.index) > 0) and (len(trips.index) > 0) and 
-            (len(stop_times.index) > 0) and (len(stops.index) > 0) 
-           ):  
+        stop_times = dd.read_parquet(
+            f"{export_path}st_{itp_id}_{analysis_date}.parquet")
+
+        if len(stop_times.index) > 0:  
             IDS_WITH_FULL_INFO.append(itp_id)
     
     return IDS_WITH_FULL_INFO
 
     
 if __name__=="__main__":
+    
+    logger.add("./logs/operators_for_hqta.log", retention="6 months")
+    logger.add(sys.stderr, 
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", 
+               level="INFO")
+    
+    logger.info(f"Analysis date: {analysis_date}")
     start = dt.datetime.now()
     
     # These are all the IDs that have some cached files in GCS
