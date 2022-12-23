e13f5c93bd711f32b1827ae7e4ee46c2d279dd15
amandaha8 <amandahastuyck@gmail.com>
2022-12-23 01:20:34 +0000

    originally utils file was long moved things around, attempt to debu sjoin

--- cell_coverage/A1_provider_prep.py
+++ cell_coverage/A1_provider_prep.py
@@ -42,7 +42,6 @@ def create_california_coverage(file_zip_name:str, new_file_name:str):
     # Save this into a parquet so don't have to clip all the time
     utils.geoparquet_gcs_export(fcc_ca_gdf, GCS_FILE_PATH, new_file_name)
 
-
 # Clip the provider map to a boundary and return the areas of a boundary
 # that is NOT covered by the provider.
 def find_difference_and_clip(
@@ -59,8 +58,6 @@ def find_difference_and_clip(
 
     return no_coverage
 
-# Breakout provider gdf by counties, find the areas of each county
-# that doesn't have coverage, concat everything and dissolve to one row.
 def concat_all_areas(all_gdf:list, gcs_file_path: str, file_name:str):
     """
     Districts/counties are separated out into different gdfs that contain 
@@ -107,8 +104,7 @@ def breakout_counties(provider, gcs_file_path:str, file_name:str, counties_wante
     
     return full_gdf
 
-# Sjoin the provider gdf to a single district then 
-# find the difference
+# Sjoin the provider gdf to a single district then & find the difference
 def iloc_find_difference_district(
     provider_df: dg.GeoDataFrame, 
     district_df: gpd.GeoDataFrame,
@@ -219,8 +215,10 @@ def correct_kern():
 Final Provider Files
 """
 # Areas that don't have AT&T cell coverage across CA
+# att_all_counties.parquet was created using breakout_counties
+# ATT_no_coverage_complete_CA.parquet was created using complete_difference_provider_district_level
 def load_att(): 
-    att_file =  "ATT_no_coverage_complete_CA.parquet"
+    att_file =  "att_all_counties.parquet"
     gdf = gpd.read_parquet(f"{GCS_FILE_PATH}{att_file}")
     return gdf
 
@@ -241,7 +239,7 @@ def simplify_geometry(provider: gpd.GeoDataFrame):
     provider = provider.to_crs(geography_utils.CA_StatePlane)
 
     # Simplify
-    provider["geometry"] = provider.geometry.simplify(tolerance=15)
+    provider["geometry"] = provider.geometry.simplify(tolerance=50)
 
     provider = provider.to_crs(geography_utils.WGS84)
 
@@ -256,113 +254,3 @@ def simplify_geometry_all_providers():
     att = simplify_geometry(load_att())
     tmobile = simplify_geometry(load_tmobile())
     return verizon, att, tmobile
-
- 
-"""
-Analysis Functions
-"""
-# Overlay Federal Communications Commission map with original bus routes df.
-def comparison(gdf_left, gdf_right, provider_name: str):
-
-    # Overlay
-    overlay_df = gpd.overlay(
-        gdf_left, gdf_right, how="intersection", keep_geom_type=False
-    )
-
-    # Create a new route length for portions covered by cell coverage
-    overlay_df = overlay_df.assign(
-        route_length=overlay_df.geometry.to_crs(geography_utils.CA_StatePlane).length
-    )
-    
-    utils.geoparquet_gcs_export(overlay_df, GCS_FILE_PATH, f"{provider_name}_overlaid_with_unique_routes")
-    return overlay_df
-
-
-# Take the FCC provider shape file, compare it against the original df
-# Find % of route covered by a provider compared to the original route length.
-def route_cell_coverage(provider_gdf, original_routes_df, suffix: str):
-    """
-    Args:
-        provider_gdf: the provider gdf clipped to CA
-        original_routes_df: the original df with all the routes
-        suffix (str): suffix to add behind dataframe
-    Returns:
-        Returns a gdf with the percentage of the routes covered by a provider
-    """
-    # Overlay the dfs
-    overlay =  comparison(original_routes_df, provider_gdf)
-
-    # Aggregate lengths of routes by route id, name, agency, and itp_id
-    # 10/18: removed route name
-    overlay2 = (overlay.dissolve(
-         by=["route_id","agency", "itp_id"],
-         aggfunc={
-         "route_length": "sum"}).reset_index()) 
-
-    # Merge original dataframe with old route with provider-route overlay
-    # To compare original route length and old route length
-    # 10/18: removed route name
-    m1 = overlay2.merge(
-        original_routes_df,
-        how="inner",
-        on=["route_id","agency", "itp_id"],
-        suffixes=["_overlay", "_original_df"],
-    )
-    
-    
-    # Create % of route covered by data vs. not 
-    m1["percentage"] = (
-        m1["route_length_overlay"] / m1["route_length_original_df"]
-    ) * 100
-    
-    # Create bins  
-    bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    m1["binned"] = pd.cut(m1["percentage"], bins)
-    
-    # Drop unwanted cols 
-    unwanted_cols = ["route_type", "geometry_original_df"]
-    m1 = m1.drop(columns = unwanted_cols)
-    
-    # Sort
-    m1 = m1.sort_values(["route_id","route_name", "agency"]) 
-    
-    # Add suffix to certain columns to distinguish which provider 
-    # https://stackoverflow.com/questions/53380310/how-to-add-suffix-to-column-names-except-some-columns
-    m1 = m1.rename(columns={c: c+ suffix for c in m1.columns if c in ['percentage', 'binned', 'route_length_overlay',
-                                                                     'geometry_overlay']})
-
-    # Ensure m1 is a GDF 
-    m1 = gpd.GeoDataFrame(m1, geometry = f"geometry_overlay{suffix}", crs = "EPSG:4326")
-    return m1
-
-"""
-Other Functions
-"""
-# Export geospatial file to a geojson 
-def geojson_gcs_export(gdf, GCS_FILE_PATH, FILE_NAME):
-    """
-    Save geodataframe as parquet locally,
-    then move to GCS bucket and delete local file.
-
-    gdf: geopandas.GeoDataFrame
-    GCS_FILE_PATH: str. Ex: gs://calitp-analytics-data/data-analyses/my-folder/
-    FILE_NAME: str. Filename.
-    """
-    gdf.to_file(f"./{FILE_NAME}.geojson", driver="GeoJSON")
-    fs.put(f"./{FILE_NAME}.geojson", f"{GCS_FILE_PATH}{FILE_NAME}.geojson")
-    os.remove(f"./{FILE_NAME}.geojson")
-
-# Clean organization names - strip them of dba, etc
-def organization_cleaning(df, column_wanted: str):
-    df[column_wanted] = (
-        df[column_wanted]
-        .str.strip()
-        .str.split(",")
-        .str[0]
-        .str.replace("/", "")
-        .str.split("(")
-        .str[0]
-        .str.split("/")
-        .str[0]
-    )
-    return df
