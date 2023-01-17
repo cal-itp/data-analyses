401320ae952854eb5befdf3b6ca4af13d6ac8e0e
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-01-17 19:52:17 +0000

    (utils): remove old, unused functions, finish moving to v2

--- _shared_utils/shared_utils/gtfs_utils_v2.py
+++ _shared_utils/shared_utils/gtfs_utils_v2.py
@@ -1,54 +1,5 @@
 """
 Migrate gtfs_utils from v1 to v2 warehouse.
-
-Changes from v1 to v2:
-1. new feed_key to organization_name table created.
-STATUS: stays in gtfs_utils_v2 for now
-
-    Should probably stage in dbt anyway to see what feeds / names
-    are there for the same day?
-    Rather than subsetting by feed_keys, join this new table to
-    all subsequent queries, so analysts can query by org name too.
-
-2. get_trips: expand and move to dbt
-Status: dbt table in progress, function updated here.
-
-    Rework trips query to better reflect how we use this query as our
-    base query.
-    Bring in route columns we might use to subset
-    (route_type, route_short_name, route_long_name).
-
-    Move this joining into dbt to be a mart_trips table that analysts go to first.
-
-3. get_route_info: deprecate.
-Statsu: dbt table in progress, removed from here.
-
-    Instead, expand the trips query to incorporate our most-used
-    route columns from dim_routes to help with subsetting. See #2
-
-4. get_route_shapes: change make_routes_gdf to take pd.DataFrame,
-    keep in gtfs_utils_v2
-Status: dbt table in progress, function updated here
-
-    Use dask.apply to apply the make_linestring function.
-    Move this new make_routes_gdf over to geography_utils when it's ready
-    and replace that.
-
-5. get_stops: move to dbt
-Status: dbt table in progress, function updated here
-
-    set this up so that point geometry can be created in dbt.
-    Add a mart_stops table that analysts go to first.
-
-6. get_stop_times: TODO
-    TODO. think more on what functions typically come from stop_times.
-    Aggregation, for sure, and that can be handled if it returns dask.dataframe
-    as in previous get_stop_times.
-
-    With new timestamp that's numeric, we might want to add subsetting by departure
-    hour on top of that so we don't have to do the calculation of seconds to
-    departure hour more.
-
 """
 
 # import os
@@ -57,7 +8,6 @@ Status: dbt table in progress, function updated here
 import datetime
 from typing import Literal, Union
 
-import dask.dataframe as dd
 import geopandas as gpd
 import pandas as pd
 import siuba  # need this to do type hint in functions
@@ -408,33 +358,6 @@ def get_trips(
     return trips >> subset_cols(trip_cols)
 
 
-def make_routes_gdf(
-    df: pd.DataFrame,
-    crs: str = "EPSG:4326",
-) -> gpd.GeoDataFrame:
-    """
-    Parameters:
-
-    crs: str, a projected coordinate reference system.
-        Defaults to EPSG:4326 (WGS84)
-    """
-    # Use apply to use map_partitions
-    # https://stackoverflow.com/questions/70829491/dask-map-partitions-meta-when-using-lambda-function-to-add-column
-    ddf = dd.from_pandas(df, npartitions=1)
-
-    ddf["geometry"] = ddf.pt_array.apply(
-        geography_utils.make_linestring, meta=("geometry", "geometry")
-    )
-    shapes = ddf.compute()
-
-    # convert to geopandas; re-project if needed
-    gdf = gpd.GeoDataFrame(
-        shapes.drop(columns="pt_array"), geometry="geometry", crs=geography_utils.WGS84
-    ).to_crs(crs)
-
-    return gdf
-
-
 def get_shapes(
     selected_date: Union[str, datetime.date],
     operator_feeds: list[str] = [],
@@ -460,7 +383,9 @@ def get_shapes(
     )
 
     if get_df:
-        shapes_gdf = make_routes_gdf(shapes, crs=crs)[shape_cols + ["geometry"]]
+        shapes_gdf = geography_utils.make_routes_gdf(shapes, crs=crs)[
+            shape_cols + ["geometry"]
+        ]
 
         return shapes_gdf
 
