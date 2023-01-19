7f681db778275fa57b924c3de6fcd1afd530ef11
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-01-19 20:35:44 +0000

    download stop times successfully

--- high_quality_transit_areas/download_stop_times.py
+++ high_quality_transit_areas/download_stop_times.py
@@ -1,122 +1,103 @@
 """
-Just run stop_times download.
+Download all stop_times for a day.
 """
 import os
 os.environ["CALITP_BQ_MAX_BYTES"] = str(500_000_000_000)
 
 import datetime as dt
-import gcsfs
 import geopandas as gpd
 import pandas as pd
-import siuba
 import sys 
 
-from calitp.tables import tbls
-from dask import delayed, compute
-from dask.delayed import Delayed # use this for type hint
-from siuba import *
 from loguru import logger
 
-import operators_for_hqta
-
-from update_vars import analysis_date, CACHED_VIEWS_EXPORT_PATH
-from shared_utils import gtfs_utils_v2, geography_utils, rt_utils, utils
+from shared_utils import gtfs_utils_v2
+from update_vars import analysis_date, COMPILED_CACHED_VIEWS
 
 
-@delayed
-def get_stop_times(feed_key: str, analysis_date: str) -> pd.DataFrame:
+def feeds_across_trips_shapes_stops(analysis_date: str) -> list:
     """
-    Download stop times for the trips that ran on selected date.
-        
-    Write pd.DataFrame in GCS.
+    Return only the feeds that are in common 
+    across the trips, shapes, and stops tables.
+    
+    Use this to subset the trips table down when 
+    inputting that for stop_times.
+    
+    Bug: shapes doesn't have Metrolink feeds, because shape_array_keys are None.
     """
-    dataset = "st"
-    filename = f"{dataset}_{feed_key}_{analysis_date}.parquet"
+    keep_cols = ["feed_key"]
+    
+    trip_feeds = pd.read_parquet(
+        f"{COMPILED_CACHED_VIEWS}trips_{analysis_date}.parquet", 
+        columns = keep_cols
+    ).drop_duplicates().feed_key.unique().tolist()
+    
+    stop_feeds = gpd.read_parquet(
+        f"{COMPILED_CACHED_VIEWS}stops_{analysis_date}.parquet",
+        columns = keep_cols
+    ).drop_duplicates().feed_key.unique().tolist()
+    
+    shape_feeds = gpd.read_parquet(
+        f"{COMPILED_CACHED_VIEWS}routelines_{analysis_date}.parquet",
+        columns = keep_cols
+    ).drop_duplicates().feed_key.unique().tolist()
+    
+    feeds_in_common = list(set(trip_feeds) & set(shape_feeds) & set(stop_feeds))
+    
+    return feeds_in_common
 
+
+if __name__=="__main__":
+    # Connect to dask distributed client, put here so it only runs for this script
+    #from dask.distributed import Client
+    
+    #client = Client("dask-scheduler.dask.svc.cluster.local:8786")
+    
+    logger.add("./logs/download_data.log", retention="3 months")
+    logger.add(sys.stderr, 
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", 
+               level="INFO")
+    
+    logger.info(f"Analysis date: {analysis_date}")
+    start = dt.datetime.now()
+    
     full_trips = pd.read_parquet(
-        f"{CACHED_VIEWS_EXPORT_PATH}trips_{feed_key}_{analysis_date}.parquet")
+        f"{COMPILED_CACHED_VIEWS}trips_{analysis_date}.parquet")
+    
+    FEEDS_TO_RUN = full_trips.feed_key.unique().tolist()
+    #FEEDS_TO_RUN = feeds_across_trips_shapes_stops(analysis_date)
+
+    logger.info(f"# operators to run: {len(FEEDS_TO_RUN)}")
+    
+    # There may be some feeds missing with complete info
+    # subset it now -- should we drop Metrolink knowingly?
+    #trips_on_day = full_trips[
+    #    full_trips.feed_key.isin(FEEDS_TO_RUN)
+    #].reset_index(drop=True)
+    
+    # st already used, keep for continuity
+    dataset = "st"
+    logger.info(f"*********** Download {dataset} data ***********")
+
+    keep_stop_time_cols = [
+        "feed_key",   
+        "trip_id", 
+        "stop_id", "stop_sequence", "timepoint",
+        "arrival_sec", "departure_sec",
+    ]
     
     stop_times = gtfs_utils_v2.get_stop_times(
         selected_date = analysis_date,
-        operator_feeds = [feed_key],
-        stop_time_cols = None,
+        operator_feeds = FEEDS_TO_RUN,
+        stop_time_cols = keep_stop_time_cols,
         get_df = True,
-        trip_df = full_trips,
-    )
+        trip_df = full_trips
+    ) 
     
-    if not stop_times.empty:
-        stop_times.to_parquet(f"{CACHED_VIEWS_EXPORT_PATH}{filename}")
-        logger.info(f"{feed_key}: {dataset} exported to GCS")
+    stop_times.to_parquet(
+        f"{COMPILED_CACHED_VIEWS}{dataset}_{analysis_date}.parquet") 
 
+    end = dt.datetime.now()
+    logger.info(f"execution time: {end-start}")
 
-def check_route_trips_stops_are_cached(feed_key: str, analysis_date: str):
-    """
-    Check that routelines, trips, stops parquets are present.
-    If not, don't bother downloading stop_times (computationally expensive!).
-    
-    This way, if an operator is missing files, they are 
-    always missing at least 2.
-    """
-    response1 = rt_utils.check_cached(
-        f"routelines_{feed_key}_{analysis_date}.parquet", 
-        subfolder="cached_views/")
-    response2 = rt_utils.check_cached(
-        f"trips_{feed_key}_{analysis_date}.parquet", 
-        subfolder="cached_views/")
-    response3 = rt_utils.check_cached(
-        f"stops_{feed_key}_{analysis_date}.parquet", 
-        subfolder="cached_views/")    
-    
-    all_responses = [response1, response2, response3]
-    
-    if all(r is not None for r in all_responses):
-        return True
-    else:
-        return False
-
-    
-def compute_delayed_stop_times(
-    feed_key: str, 
-    analysis_date: str, 
-    stop_times_df: Delayed, 
-):
-    """
-    Unpack the stop_times table from the dask delayed function. 
-    """
-    df = compute(stop_times_df)[0]
-    
-    if not stop_times.empty:
-
-        df.to_parquet(
-            f"{CACHED_VIEWS_EXPORT_PATH}st_{feed_key}_{analysis_date}.parquet")
-        logger.info(f"{feed_key}: st exported to GCS")
-        
-        
-if __name__ == "__main__":
-    
-    hqta_operators_df = operators_for_hqta.scheduled_operators_for_hqta(
-        analysis_date)
-    
-    FEEDS_TO_RUN = sorted(hqta_operators_df.feed_key.unique().tolist())    
-
-    FEEDS_NEED_STOP_TIMES = []
-
-    # Store a list where operators have 3 complete files - need stop_times later
-    for feed_key in FEEDS_TO_RUN:
-        if check_route_trips_stops_are_cached(feed_key, analysis_date):
-            FEEDS_NEED_STOP_TIMES.append(feed_key)
-            
-    
-    # Check that routes, trips, stops were downloaded successfully
-    # If all 3 are present, then download stop_times. Otherwise, skip stop_times.
-    for feed_key in FEEDS_NEED_STOP_TIMES:
-        time0 = dt.datetime.now()
-        logger.info(f"*********** Download stop_times for: {feed_key} ***********")
-
-        stop_times = get_stop_times(feed_key, analysis_date)
-        
-        # Turn the delayed dask objects into df or gdf and export to GCS
-        compute_delayed_stop_times(feed_key, analysis_date, stop_times)
-        
-        time1 = dt.datetime.now()
-        logger.info(f"download files for {feed_key}: {time1-time0}")
+    #client.close()
