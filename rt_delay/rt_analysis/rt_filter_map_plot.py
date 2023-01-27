7074c5267ca0366c2b3da441722ef78c384e9508
Eric Dasmalchi <edasmalchi@gmail.com>
2023-01-27 21:22:49 +0000

    another DASH, fix mapping

--- rt_delay/rt_analysis/rt_filter_map_plot.py
+++ rt_delay/rt_analysis/rt_filter_map_plot.py
@@ -627,7 +627,7 @@ class RtFilterMapper:
         speed_metric = int(round(speed_metric))
         self.corridor['schedule_metric_minutes'] = schedule_metric
         self.corridor['speed_metric_minutes'] = speed_metric
-        self.corridor['routes'] = self.filter['route_names'] if self.filter['route_names'] else 'All'
+        self.corridor['routes'] = self.filter['route_names'] if self.filter and self.filter['route_names'] else 'All'
         self.corridor['filter'] = self.filter_formatted
         return {'schedule_metric_minutes': schedule_metric,
                'speed_metric_minutes': speed_metric}
