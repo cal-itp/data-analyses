61409912b54b26c7f3df82f258b07230d9b36ea8
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-02-01 00:24:07 +0000

    Use new v1 tables and merge with endpoint delay

--- quarterly_performance_objective/B2_route_service_hours_delay.py
+++ quarterly_performance_objective/B2_route_service_hours_delay.py
@@ -9,10 +9,11 @@ import sys
 
 from loguru import logger
 
-from update_vars import ANALYSIS_DATE, BUS_SERVICE_GCS, COMPILED_CACHED_GCS
-from shared_utils import geography_utils, utils
+from update_vars import BUS_SERVICE_GCS, COMPILED_CACHED_GCS
+from shared_utils import geography_utils, utils, rt_dates
 
-def calculate_route_level_delays(selected_date: str) -> pd.DataFrame:
+def calculate_route_level_delays(selected_date: str, 
+                                 route_cols: list) -> pd.DataFrame:
     """
     Aggregate endpoint_delays to route-level, from trip-level.
     """
@@ -21,11 +22,11 @@ def calculate_route_level_delays(selected_date: str) -> pd.DataFrame:
     
     # Drop duplicates
     delay_df2 = delay_df.drop_duplicates(
-        subset=["calitp_itp_id", "route_id", "trip_id", "stop_id"])
+        subset=route_cols + ["trip_id", "stop_id"])
     
     route_delay = geography_utils.aggregate_by_geography(
         delay_df2, 
-        group_cols = ["calitp_itp_id", "route_id"],
+        group_cols = route_cols,
         sum_cols = ["delay_seconds"]
     )
     
@@ -34,7 +35,8 @@ def calculate_route_level_delays(selected_date: str) -> pd.DataFrame:
 
 def merge_delay_with_route_categories(
     route_delay_df: pd.DataFrame, 
-    routes_categorized: gpd.GeoDataFrame
+    routes_categorized: gpd.GeoDataFrame,
+    route_cols: list
 ) -> gpd.GeoDataFrame:
     """
     Merge service hours df with delay df. 
@@ -46,9 +48,9 @@ def merge_delay_with_route_categories(
     """
         
     delay_on_shn_routes = pd.merge(
-        routes_categorized.rename(columns = {"itp_id": "calitp_itp_id"}),
+        routes_categorized,
         route_delay_df, 
-        on = ["calitp_itp_id", "route_id"],
+        on = route_cols,
         how = "outer",
         validate = "1:1",
         indicator="merge_delay"
@@ -59,41 +61,42 @@ def merge_delay_with_route_categories(
 
 if __name__ == "__main__":
     
-    logger.add("./logs/A4_route_service_hours_delay.log")
+    ANALYSIS_DATE = rt_dates.PMAC["Q4_2022"]
+    VERSION = "v1"
+    
+    logger.add("./logs/B2_route_service_hours_delay.log")
     logger.add(sys.stderr, 
                format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
                level="INFO")
     
-    logger.info(f"Analysis date: {ANALYSIS_DATE}")
+    logger.info(f"Analysis date: {ANALYSIS_DATE}  warehouse {VERSION}")
     start = datetime.datetime.now()
     
+    if VERSION == "v1":
+        route_cols = ["calitp_itp_id", "route_id"]
+        
+    elif VERSION == "v2":
+        route_cols = ["feed_key", "route_id"]
+        
     routes_categorized = gpd.read_parquet(
-        f"{BUS_SERVICE_GCS}routes_categorized_{ANALYSIS_DATE}.parquet")
-    
-    # Should I subset to df[df._merge=="both"]?
-    # both means that it found a corresponding match in itp_id-route_id 
-    # since it's been aggregated up to route_id level (shape_id can mismatch more easily)
-    # right_only means found in trips table, but later did not find service hours for it
-    logger.info(f"merge results for service hours only: {routes_categorized._merge.value_counts()}")
-    
-    no_service_hours = routes_categorized[routes_categorized._merge=="right_only"]
-    logger.info(f"right only merges for service hours: {no_service_hours.itp_id.value_counts()}")
+        f"{BUS_SERVICE_GCS}routes_categorized_{ANALYSIS_DATE}_{VERSION}.parquet")
     
     # Get endpoint delay up to route-level
-    route_delay = calculate_route_level_delays(ANALYSIS_DATE)
+    route_delay = calculate_route_level_delays(ANALYSIS_DATE, route_cols)
     
     # Merge the routes that are categorized with route-level delay
     # Keep merge variable to understand why some don't have delay info
     # (it's ok, since not every route will have RT...but why are there some
     # that are in RT but not found in schedule?)
-    df = merge_delay_with_route_categories(route_delay, routes_categorized)
+    df = merge_delay_with_route_categories(route_delay, 
+                                           routes_categorized, route_cols)
     
     logger.info(f"schedule-RT delay outer join merge: {df.merge_delay.value_counts()}")
     
     utils.geoparquet_gcs_export(
         df, 
         BUS_SERVICE_GCS,
-        f"routes_categorized_with_delay_{ANALYSIS_DATE}"
+        f"routes_categorized_with_delay_{ANALYSIS_DATE}_{VERSION}"
     )
     
     logger.info(f"exported to GCS")
