77e95d20b5f783f2c36bcc51cd6ca86bc3d6f611
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-02-02 01:32:39 +0000

    Run Q1 2023 service hours with v2, deal with filenaming differences for v1, v2

--- quarterly_performance_objective/update_vars.py
+++ quarterly_performance_objective/update_vars.py
@@ -5,3 +5,16 @@ BUS_SERVICE_GCS = f"{utils.GCS_FILE_PATH}"
 COMPILED_CACHED_GCS = f"{rt_utils.GCS_FILE_PATH}compiled_cached_views/"
 CURRENT_QUARTER = "Q4_2022"
 ANALYSIS_DATE = rt_dates.PMAC[CURRENT_QUARTER] 
+
+
+def get_filename(
+    file_name_prefix: str, 
+    analysis_date: str, 
+    warehouse_version: str
+) -> str:
+    if analysis_date.startswith("2022"):
+        file_name = f"{file_name_prefix}{analysis_date}_{warehouse_version}.parquet"
+    else:
+        file_name = f"{file_name_prefix}{analysis_date}.parquet"
+
+    return file_name
