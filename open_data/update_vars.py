965d0089ea46b7e0331d6522f7ace98f45c06da1
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-02-23 00:30:13 +0000

    more helper functions for wrangling schedule tables, use for prepping route and stop segments

--- open_data/update_vars.py
+++ open_data/update_vars.py
@@ -3,8 +3,5 @@ from shared_utils import rt_dates
 analysis_date = rt_dates.DATES["feb2023"]
 
 GCS_FILE_PATH = "gs://calitp-analytics-data/data-analyses/"
-CACHED_VIEWS_EXPORT_PATH = f"{GCS_FILE_PATH}rt_delay/cached_views/"
-COMPILED_CACHED_VIEWS = f"{GCS_FILE_PATH}rt_delay/compiled_cached_views/"
-SEGMENT_GCS = f"{GCS_FILE_PATH}rt_segment_speeds/"
 TRAFFIC_OPS_GCS = f"{GCS_FILE_PATH}traffic_ops/"
-
+HQTA_GCS = f"{GCS_FILE_PATH}high_quality_transit_areas/"
