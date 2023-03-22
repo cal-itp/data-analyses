f36e1e7b8650ce76b1b3bf236ac730ed71698fe8
Eric Dasmalchi <edasmalchi@gmail.com>
2023-03-22 23:09:39 +0000

    successful v2 map ac, bbb -- organization + backwards compatibility pending

--- rt_delay/rt_analysis/v2_queries.py
+++ rt_delay/rt_analysis/v2_queries.py
@@ -29,18 +29,18 @@ fs = gcsfs.GCSFileSystem()
 BUCKET_NAME = "calitp-analytics-data"
 VP_FILE_PATH = f"gs://{BUCKET_NAME}/data-analyses/rt_segment_speeds/"
 
-trip_cols = ['trip_key', 'gtfs_dataset_key', 'activity_date',
-   'trip_id', 'route_id', 'route_short_name',
-   'shape_id', 'direction_id', 'route_type',
-   'route_long_name', 'route_desc']
+trip_cols = ['feed_key', 'trip_key', 'gtfs_dataset_key',
+             'activity_date', 'trip_id', 'route_id',
+             'route_short_name', 'shape_id', 'direction_id',
+             'route_type', 'route_long_name', 'route_desc']
 
 st_cols = ['feed_key', 'trip_id', 'stop_id', 'arrival_time',
        'departure_time', 'timepoint', 'stop_sequence', 'continuous_drop_off',
        'continuous_pickup', 'arrival_sec', 'departure_sec']
 # must include _sec for util to work...
-
 stop_cols = ['feed_key', 'stop_id', 'stop_name', 'pt_geom']
-# must includ pt_geom to return gdf
+# must include pt_geom to return gdf
+shape_cols = ['feed_key', 'shape_id']
 
 def get_ix_query(itp_id: int, analysis_date: dt.date):
     '''
@@ -61,7 +61,7 @@ def get_ix_query(itp_id: int, analysis_date: dt.date):
     >> inner_join(_, daily_service, by = 'schedule_gtfs_dataset_key')
     >> filter(_.activity_date == analysis_date)
     >> select(_.feed_key, _.schedule_gtfs_dataset_key, _.vehicle_positions_gtfs_dataset_key,
-             _.organization_itp_id, _.organization_name)
+             _.organization_itp_id, _.organization_name, _.activity_date)
     )
     
     return org_feeds_datasets
@@ -73,11 +73,10 @@ def get_vehicle_positions(ix_query):
     # starts with warehouse vehicle locations table
     '''
     
-    assert ix_query.activity_date.iloc[0] == dt.date(2023, 3, 15), 'hardcoded to 3/15 for now :)'
-    vp_all = gpd.read_parquet(f'{VP_FILE_PATH}vp_2023-03-15.parquet')
     ix_df = ix_query >> collect()
+    assert ix_df.activity_date.iloc[0].date() == dt.date(2023, 3, 15), 'hardcoded to 3/15 for now :)'
+    vp_all = gpd.read_parquet(f'{VP_FILE_PATH}vp_2023-03-15.parquet')
     org_vp = vp_all >> filter(_.gtfs_dataset_key.isin(ix_df.vehicle_positions_gtfs_dataset_key))
     org_vp = org_vp >> select(-_.location_timestamp)
     org_vp = org_vp.to_crs(CA_NAD83Albers)
     return org_vp
-
