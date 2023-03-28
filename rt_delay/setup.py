8d3def10e851e17c239ca70fed2c7dce3e2d5690
Eric Dasmalchi <edasmalchi@gmail.com>
2023-03-28 23:56:05 +0000

    remove temporary v2_queries module

--- rt_delay/setup.py
+++ rt_delay/setup.py
@@ -3,12 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name="rt_analysis",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.2.0",
     description="Module for GTFS-RT transit speed analysis",
     author="Cal-ITP",
     license="Apache",
     include_package_data=True,
-    py_modules = ['rt_parser', 'rt_filter_map_plot', 'sccp_tools', 'v2_queries'],
-    # v2_queries is temporarily here for ease of debug... (future shared_utils/rt_utils_v2?)
+    py_modules = ['rt_parser', 'rt_filter_map_plot', 'sccp_tools'],
     package_dir={"rt_delay": "rt_analysis"}
 )
