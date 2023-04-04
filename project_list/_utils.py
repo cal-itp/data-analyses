847031494230e01fcad4546b8b2c9059a051cf14
amandaha8 <amandahastuyck@gmail.com>
2023-04-04 18:22:24 +0000

    moved harmonization stuff to script

--- project_list/_utils.py
+++ project_list/_utils.py
@@ -11,18 +11,8 @@ def clean_columns(df, strings_to_remove:list):
     Example: I want to remove 'tircp_' 
     before all the column names.
     """
-    
     strings_to_remove = r"({})".format("|".join(strings_to_remove))
     
     df.columns = df.columns.str.replace(strings_to_remove, "", regex=True)
     
     return df 
-
-def add_project_category(df, project_category: str):
-    """
-    Add project category column
-    to dataframe.
-    """
-    df['project_category'] = project_category
-    
-    return df
