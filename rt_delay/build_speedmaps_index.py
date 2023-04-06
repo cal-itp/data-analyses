23def856f5996fe0abec7fb3d614e378694341b7
Eric Dasmalchi <edasmalchi@gmail.com>
2023-04-06 23:57:23 +0000

    break out scripts, start testing

---
+++ rt_delay/build_speedmaps_index.py
@@ -0,0 +1,48 @@
+import os
+os.environ["CALITP_BQ_MAX_BYTES"] = str(1_000_000_000_000) ## 1TB?
+
+from siuba import *
+import pandas as pd
+import datetime as dt
+
+from calitp_data_analysis.tables import tbls
+import shared_utils
+
+ANALYSIS_DATE = dt.date(2023, 3, 15)
+PROGRESS_PATH = f'./_rt_progress_{ANALYSIS_DATE}.parquet'
+
+def build_speedmaps_index(analysis_date: dt.date) -> pd.DataFrame:
+    '''
+    An index table for tracking down a given org's schedule/rt feeds
+    returns LazyTbl
+    '''
+    analysis_dt = dt.datetime.combine(analysis_date, dt.time(0, 0))
+    
+    dim_orgs = (tbls.mart_transit_database.dim_organizations()
+                >> filter(_._valid_from <= analysis_dt, _._valid_to > analysis_dt)
+                >> select(_.source_record_id, _.caltrans_district)
+               )
+    
+    orgs_with_vp = (tbls.mart_transit_database.dim_provider_gtfs_data()
+    >> filter(_._valid_from <= analysis_dt, _._valid_to > analysis_dt,
+              _.reports_site_assessed, _.vehicle_positions_gtfs_dataset_key != None)
+    >> inner_join(_, dim_orgs, on = {'organization_source_record_id': 'source_record_id'})
+    >> select(_.organization_itp_id, _.organization_name, _.organization_source_record_id,
+             _.caltrans_district, _._is_current, _.vehicle_positions_gtfs_dataset_key)
+    >> collect()
+    )
+    assert not orgs_with_vp.isnull().values.any()
+    orgs_with_vp['analysis_date'] = analysis_date
+    orgs_with_vp = orgs_with_vp >> distinct(_.organization_name,
+                    _.organization_itp_id, _.organization_source_record_id,
+                    _.caltrans_district, _._is_current, _.analysis_date
+                    # ,_.vehicle_positions_gtfs_dataset_key
+                                           )
+    return orgs_with_vp
+
+if __name__ == "__main__":
+    
+    # do_stuff()
+    speedmaps_index = build_speedmaps_index(ANALYSIS_DATE)
+    speedmaps_index_joined = shared_utils.rt_utils.check_intermediate_data(speedmaps_index)
+    speedmaps_index_joined.to_parquet(PROGRESS_PATH)
