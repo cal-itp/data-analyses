52aea91b88cbbd4697f14e5f26d730b1481d17fe
Eric Dasmalchi <edasmalchi@gmail.com>
2023-04-17 17:40:33 +0000

    speedmap bugfixes, cast vp back to timestamp in rt_utils for now

--- _shared_utils/shared_utils/gtfs_utils_v2.py
+++ _shared_utils/shared_utils/gtfs_utils_v2.py
@@ -384,9 +384,9 @@ def get_shapes(
         if not shape_cols:
             shape_cols = list((shapes >> select(-_.pt_array)).columns)
 
-            shapes_gdf = geography_utils.make_routes_gdf(shapes, crs=crs)[shape_cols + ["geometry"]]
+        shapes_gdf = geography_utils.make_routes_gdf(shapes, crs=crs)[shape_cols + ["geometry"]]
 
-            return shapes_gdf
+        return shapes_gdf
 
     else:
         return shapes >> subset_cols(shape_cols)
