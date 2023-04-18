1bdff2a51a5cf48eaa9a68b6a72baa5104e2c9b8
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-04-18 21:03:32 +0000

    finish joining schedule to rt

--- rt_predictions/schedule_stop_time_wrangling.py
+++ rt_predictions/schedule_stop_time_wrangling.py
@@ -8,11 +8,87 @@ from segment_speed_utils.project_vars import (PREDICTIONS_GCS,
 trip_cols = ["gtfs_dataset_key", "service_date", "trip_id"]
 stop_cols = trip_cols + ["stop_id"] 
 
+def prep_scheduled_stop_times(analysis_date: str) -> dd.DataFrame: 
+    stop_times = helpers.import_scheduled_stop_times(
+        analysis_date,
+        columns = ["feed_key", "trip_id",
+                   "stop_id", "stop_sequence", 
+                   "arrival_sec"],
+    )
+    
+    stop_times = stop_times.assign(
+        service_date = pd.to_datetime(analysis_date).date(),
+        arrival_datetime = dd.to_datetime(
+            stop_times.arrival_sec, unit="s").dt.time.astype(str)
+    )
+    
+    stop_times = stop_times.assign(
+        scheduled_arrival = dd.to_datetime(
+            stop_times.service_date.astype(str) + " " + 
+            stop_times.arrival_datetime
+        )
+    ).drop(columns = ["arrival_sec", "arrival_datetime"])
+    
+    return stop_times
+
+
+def scheduled_stop_times_with_rt_dataset_key(
+    analysis_date: str,
+) -> dd.DataFrame:
+    """
+    Get scheduled stop_times and attach gtfs_dataset_key.
+    """
+    stop_times = prep_scheduled_stop_times(analysis_date)
+    
+    trips = helpers.import_scheduled_trips(
+        analysis_date, 
+        columns = ["feed_key", "trip_id", "shape_id", "route_id"]
+    ).compute()
+    
+    stop_times2 = dd.merge(
+        stop_times,
+        trips,
+        on = ["feed_key", "trip_id"],
+        how = "inner"
+    )
+    
+    trip_start = (stop_times2.groupby(["feed_key", "trip_id"])
+                  .agg({"scheduled_arrival": "min"})
+                  .reset_index()
+                  .rename(columns = {
+                      "scheduled_arrival": "scheduled_trip_start"})
+                  .compute()
+                 )
+    
+    stop_times_with_trip_start = dd.merge(
+        stop_times2,
+        trip_start,
+        on = ["feed_key", "trip_id"], 
+        how = "inner"
+    )
+    
+    crosswalk = sched_rt_utils.crosswalk_scheduled_trip_grouping_with_rt_key(
+        analysis_date,
+        ["feed_key", "trip_id"],
+        # keep shape_id because some of the aggregations may have to 
+        # take place at shape-level first, before route-level
+        feed_types = ["trip_updates"]
+    )
+
+    stop_times_with_rt_key = dd.merge(
+        stop_times_with_trip_start,
+        crosswalk, 
+        on = ["feed_key", "trip_id"],
+        how = "inner"
+    )
+    
+    return stop_times_with_rt_key
+
 
 def derive_schedule_relationship(
-    stop_time_updates, 
-    scheduled_stop_times
-) -> pd.DataFrame:
+    stop_time_updates: pd.DataFrame, 
+    scheduled_stop_times: dd.DataFrame,
+) -> dd.DataFrame:
     """
     Can we infer that all missing values for schedule_relationship 
     should be the default of SCHEDULED?
@@ -21,7 +97,8 @@ def derive_schedule_relationship(
     """
     trips_in_rt = stop_time_updates[trip_cols].drop_duplicates()
     trips_in_schedule = scheduled_stop_times[
-        trip_cols + ["scheduled_trip_start"]].drop_duplicates()
+        trip_cols + ["scheduled_trip_start",
+                     "shape_id", "route_id"]].drop_duplicates()
     
     if isinstance(trips_in_schedule, dd.DataFrame):
         trips_in_schedule = trips_in_schedule.compute()
@@ -52,58 +129,19 @@ def derive_schedule_relationship(
     
     st_with_start = st_with_start.assign(
         schedule_relationship = st_with_start.schedule_relationship.fillna(
-            st_with_start.derived_schedule_relationship)
-    ).drop(columns = "derived_schedule_relationship")
+            st_with_start.derived_schedule_relationship),
+    ).drop(columns = ["derived_schedule_relationship"])
     
-    return st_with_start
-    
-    
-def scheduled_stop_times_with_rt_dataset_key(
-    analysis_date: str,
-    **kwargs
-) -> dd.DataFrame:
-    """
-    Get scheduled stop_times and attach gtfs_dataset_key.
-    """
-    stop_times = helpers.import_scheduled_stop_times(
-        analysis_date,
-        columns = ["feed_key", "trip_id", "stop_id", "stop_sequence", 
-                  "arrival_sec"],
-        **kwargs
-    ).assign(service_date = pd.to_datetime(analysis_date).date())
-    
-    trip_start = (stop_times.groupby(["feed_key", "trip_id"])
-                  .agg({"arrival_sec": "min"})
-                  .reset_index()
-                  .rename(columns = {"arrival_sec": "scheduled_trip_start"})
-                  .compute()
-                 )
-    
-    trip_start = trip_start.assign(
-        scheduled_trip_start = pd.to_datetime(
-            trip_start.scheduled_trip_start, unit="s")
+    # Now merge in scheduled arrival time by stop 
+    # since the df is back at stop-level
+    st_with_schedule = dd.merge(
+        st_with_start,
+        scheduled_stop_times[stop_cols + ["stop_sequence", 
+                                          "scheduled_arrival"]], 
+        on = stop_cols + ["stop_sequence"],
+        how = "left",
     )
     
-    stop_times_with_trip_start = dd.merge(
-        stop_times,
-        trip_start,
-        on = ["feed_key", "trip_id"], 
-        how = "inner"
-    )
-    
-    crosswalk = sched_rt_utils.crosswalk_scheduled_trip_grouping_with_rt_key(
-        analysis_date,
-        ["feed_key", "trip_id", "shape_id"],
-        # keep shape_id because some of the aggregations may have to 
-        # take place at shape-level first, before route-level
-        feed_types = ["trip_updates"]
-    )
+    return st_with_schedule
 
-    stop_times_with_rt_key = dd.merge(
-        stop_times_with_trip_start,
-        crosswalk, 
-        on = ["feed_key", "trip_id"],
-        how = "inner"
-    )
     
-    return stop_times_with_rt_key
