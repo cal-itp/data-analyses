610cb93cab19e667ee2c219dbe920269de693ea5
amandaha8 <amandahastuyck@gmail.com>
2023-04-20 21:45:58 +0000

    addednew chart for metric 3

--- rt_predictions/chart_utils.py
+++ rt_predictions/chart_utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 def altair_dropdown(df, column_for_dropdown:str, title_of_dropdown:str):
     """
     Create a dropdown menu. Selects the first 
-    operator as the default display option.
+    value as the default display option.
     """
     dropdown_list = df[column_for_dropdown].unique().tolist()
     initialize_first_op = sorted(dropdown_list)[0]
@@ -40,7 +40,9 @@ def describe_to_df(df, operator: str, metric_cols: list) -> pd.DataFrame:
     """
     # Filter for operator
     df = df[df._gtfs_dataset_name == operator].reset_index(drop=True)
-
+    
+    operator = operator.replace('TripUpdates','').strip()
+    
     final = pd.DataFrame()
 
     for i in metric_cols:
@@ -72,13 +74,16 @@ def prep_df_for_chart(df,
     Round certain columns, round it to one decimal place,
     sort by stop sequence, etc. 
     """
-
+    # Mulitply percentage column
     df[percentage_column] = df[percentage_column] * 100
-
+    
+    # Sort by operator, trip_id, and stop sequence
+    # Stops go from smallest to largest
     df = df.sort_values(
         by=["_gtfs_dataset_name", "trip_id", "stop_sequence"]
     ).reset_index(drop=True)
 
+    # Subset
     df = df[columns_to_keep]
 
     # Rounds down. 96 becomes 90.
@@ -91,44 +96,7 @@ def prep_df_for_chart(df,
 
     return df
 
-def scatter_plot(
-    df,
-    x_col: str,
-    y_col: str,
-    color_col: str,
-    dropdown_col: str,
-    dropdown_col_title: str,
-):
-    """
-    Create scatterplot.
-    """
-    selection = altair_dropdown(df, dropdown_col, dropdown_col_title)
-
-    chart = (
-        alt.Chart(df)
-        .mark_circle(size=250)
-        .encode(
-            x=alt.X(
-                f"{x_col}:N",
-                scale=alt.Scale(domain=df[x_col].unique().tolist()),
-            ),
-            y=alt.Y(y_col, scale=alt.Scale(domain=[0, 100])),
-            color=alt.Color(
-                color_col,
-                scale=alt.Scale(range=cp.CALITP_SEQUENTIAL_COLORS, domain=[0, 100]),
-            ),
-            tooltip=df.columns.to_list(),
-        )
-        .properties(title=f"Operator - Metric {y_col}")
-        .interactive()
-    )
-
-    chart = chart_size(chart)
-    chart = chart.add_selection(selection).transform_filter(selection)
-
-    return chart
-
-def scatter_plot_operator(
+def scatter_plot_domain(
     df,
     operator,
     x_col: str,
@@ -138,11 +106,12 @@ def scatter_plot_operator(
     dropdown_col_title: str,
 ):
     """
-    Create scatterplot.
+    Create scatterplot with a bounded x_col/y_col.
+    For Metrics 1 and 4.
     """
     df = df[df['Gtfs Dataset Name'] == operator].reset_index(drop = True)
     
-    operator_title = df['Gtfs Dataset Name'].iloc[0]
+    operator_title = df['Gtfs Dataset Name'].iloc[0].replace('TripUpdates','').strip()
     
     selection = altair_dropdown(df, dropdown_col, dropdown_col_title)
 
@@ -168,3 +137,51 @@ def scatter_plot_operator(
     chart = chart.add_selection(selection).transform_filter(selection)
 
     return chart
+
+def basic_scatter_plot(
+    df,
+    operator,
+    x_col:str,
+    y_col:str,
+    dropdown_col:str,
+    dropdown_col_title:str):
+    """
+    Create a scatterplot with only a bounded
+    X column. 
+    
+    Used for metric 3. 
+    """
+    df = df[df['Gtfs Dataset Name'] == operator].reset_index(drop = True)
+    operator_title = df['Gtfs Dataset Name'].iloc[0].replace('TripUpdates','').strip()
+    
+    selection = altair_dropdown(df, dropdown_col, dropdown_col_title)
+    
+    chart = (
+        alt.Chart(df)
+        .mark_circle(size=200)
+        .encode(
+            x=alt.X(
+                f"{x_col}:Q",
+                scale=alt.Scale(domain=[df[x_col].min(), df[x_col].max()]),
+            ),
+            y=alt.Y(y_col),
+            color=alt.Color(y_col,scale=alt.Scale(range=cp.CALITP_DIVERGING_COLORS)),
+            tooltip=df.columns.to_list(),
+        )
+        .properties(title=f"{operator_title} - Metric {y_col}")
+        .interactive()
+    )
+    
+    chart = chart_size(chart)
+    chart = chart.add_selection(selection).transform_filter(selection)
+    return chart
+
+def quick_descriptives(df: pd.DataFrame, 
+                       operator: str,
+                       cols_to_describe: list):
+    print(f"------------- {operator}-------------")
+    subset_df = df[df._gtfs_dataset_name==operator] 
+    
+    for c in cols_to_describe:
+        print(subset_df[c].describe())
+        print("\n")
