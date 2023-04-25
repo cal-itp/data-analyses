e10658dfcfed5ab1c215e5d0b51bcfff79ddccdf
amandaha8 <amandahastuyck@gmail.com>
2023-04-25 23:19:16 +0000

    read data from arcgis

--- project_list/_harmonization_utils.py
+++ project_list/_harmonization_utils.py
@@ -62,100 +62,119 @@ def funding_vs_expenses(df):
     else:
         return "Partially funded"
 
+columns_to_keep = [
+        "project_title",
+        "lead_agency",
+        "project_year",
+        "project_category",
+        "grant_program",
+        "project_description",
+        "total_project_cost",
+        "fully_funded",
+        "total_available_funds",
+        "location",
+         "city",
+        "county",
+        "data_source",
+        "notes",
+        "funding_notes",
+        "project_id",
+    ]
+
 # ADD YEAR
-def harmonizing(df, 
-                agency_name: str,
-                project_name:str,
-                project_description:str,
-                project_category:str,
-                project_cost:str,
-                location:str,
-                county:str,
-                city:str,
-                program:str,
-                fund_cols:list,
-                cost_in_millions:bool = True):
+def harmonizing(
+    df,
+    agency_name_col: str,
+    project_name_col: str,
+    project_description_col: str,
+    project_category_col: str,
+    project_cost_col: str,
+    location_col: str,
+    county_col: str,
+    city_col: str,
+    project_year_col: str,
+    program_col: str,
+    data_source: str,
+    fund_cols: list,
+    cost_in_millions: bool = True,
+):
     """
     Take a dataset and change the column names/types to
     default names and formats.
     
-    Add metric if the project is fully funded or not. 
+    Add metric if the project is fully funded or not.
     """
+    rename_columns = {
+        agency_name_col: "lead_agency",
+        project_name_col: "project_title",
+        project_description_col: "project_description",
+        project_category_col: "project_category",
+        project_cost_col: "total_project_cost",
+        location_col: "location",
+        county_col: "county",
+        city_col: "city",
+        project_year_col: "project_year",
+        program_col: "grant_program"
+    }
     # Rename columns
-    rename_columns = {agency_name: 'lead_agency',
-                      project_name: 'project_title',
-                      project_description: 'project_description',
-                      project_category:'project_category',
-                      project_cost: 'total_project_cost',
-                      location: 'location',
-                      county: 'county',
-                      city: 'city'}
-    
-    df = df.rename(columns = rename_columns)
-    
+    df = df.rename(columns=rename_columns)
+
     # Coerce cost/fund columns to right type
     cost_columns = df.columns[df.columns.str.contains("(cost|funds)")].tolist()
     for i in cost_columns:
-        df[i]= df[i].apply(pd.to_numeric, errors = 'coerce').fillna(0)
-    
+        df[i] = df[i].apply(pd.to_numeric, errors="coerce").fillna(0)
+
     # Clean up string columns
-    string_cols = df.select_dtypes(include=['object']).columns.to_list()
+    string_cols = df.select_dtypes(include=["object"]).columns.to_list()
     for i in string_cols:
-        df[i] = df[i].str.strip().str.title()
-        
+        df[i] = df[i].str.replace("_", " ").str.strip().str.title()
+    
     # Clean agency names
-    df = organization_cleaning(df, 'lead_agency')
-    
+    df = organization_cleaning(df, "lead_agency")
+
     # Add data source
-    df['data_source'] = program 
-    
+    df["data_source"] = data_source
+
     # Divide cost columns by millions
     # If bool is set to True
     if cost_in_millions:
         for i in cost_columns:
             df[i] = df[i].divide(1_000_000)
-    else:
-        df
-    
-   # Create columns even if they don't exist, just to harmonize 
-   # before concatting.
-    if 'county' not in df:
-        df['county'] = "None"
-    if 'city' not in df:
-        df['city'] = "None"
-    if 'notes' not in df:
-        df['notes'] = "None" 
-    
+
+    # Create columns even if they don't exist, just to harmonize
+    # before concatting.
+    create_columns = ["county","city","notes", "project_year", "project_category"]
+    for column in create_columns:
+        if column not in df:
+            df[column] = "None"
+    if "grant_program" not in df:
+        df["grant_program"] = data_source
+
     # Determine if the project completely funded or not?
     # Add up all available funds
-    df['total_available_funds'] = df[fund_cols].sum(axis=1)
+    df["total_available_funds"] = df[fund_cols].sum(axis=1)
+    df["fully_funded"] = df.apply(harmonization_utils.funding_vs_expenses, axis=1)
     
-    # Compare if available funds is greater or equal to
-    # total project cost
-    df['fully_funded'] = df.apply(funding_vs_expenses, axis=1)
+    # Add new column with funding breakout 
+    # Since it's summarized above and the details are suppressed.
+    prefix = "_" 
+    for column in fund_cols:
+        df[f"{prefix}{column}"] = df[column].astype(str)
+    str_fund_cols = [prefix + sub for sub in fund_cols]
+    # https://stackoverflow.com/questions/65532480/how-to-combine-column-names-and-values
+    def combine_funding(x):
+        return ', '.join([col + ': ' + x[col] for col in str_fund_cols])
+    df['funding_notes'] = df.apply(combine_funding, axis = 1)
+    df['funding_notes'] = df['funding_notes'].str.replace('_',' ')
+    
+    # Create unique project id - first LOST project is LOST-1, 
+    # second LOST is LOST-2, LOST-3, LOST-4, etc 
+    df['project_id'] =  df.data_source + '-' + df.groupby('data_source').cumcount().astype('str')
     
     # Only keep certain columns
-    columns_to_keep = ['project_title','lead_agency','project_category','project_description',
-                       'total_project_cost','fully_funded','total_available_funds',
-                       'location','county','city','notes','data_source']
     df = df[columns_to_keep]
-    
+
     # Fill in any nulls
-    df = df.fillna(df.dtypes.replace({'float64': 0.0, 'object': 'None'}))
+    df = df.fillna(df.dtypes.replace({"float64": 0.0, "object": "None"}))
 
     return df
-
-def add_all_projects():
-    
-    # Load original dataframes
-    state_rail_plan = load_state_rail_plan()
-    lost = load_lost()
-    
-    # Clean dataframes
-    state_rail_plan = harmonizing(state_rail_plan, 'lead_agency', 'project_name','project_description','project_category','total_project_cost', 'corridor', '', '', 'State Rail Plan', []) 
-    lost = harmonizing(lost, 'agency', 'project_title','project_description', 'project_category','cost__in_millions_', 'location', 'county','city', 'LOST', ['estimated_lost_funds','estimated_federal_funds', 'estimated_state_funds','estimated_local_funds', 'estimated_other_funds'], False) 
-    
-    # Concat
-    all_projects = pd.concat([lost, state_rail_plan])
-    
-    return all_projects
