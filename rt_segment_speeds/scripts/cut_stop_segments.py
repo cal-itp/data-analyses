908c4cf1cab0acc54b28355762790b07368097a7
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-05-03 22:07:00 +0000

    refactor normal cases for stop segments

--- rt_segment_speeds/scripts/cut_stop_segments.py
+++ rt_segment_speeds/scripts/cut_stop_segments.py
@@ -20,169 +20,153 @@ from segment_speed_utils.project_vars import (SEGMENT_GCS, analysis_date,
                                               PROJECT_CRS, CONFIG_PATH)
 
 
-def get_shape_coords_up_to_stop(
-    shape_geom: shapely.geometry.LineString,
-    shape_path_dist: list, 
-    stop_break_dist: list,
-    start_end_tuple: tuple
-) -> shapely.geometry.LineString:
-    """
-    For every pair of start/end shape_meters, grab all the 
-    shape path's coords in between.
-    
-    Ex: if (start, end) = (50, 150), then grab the subset of the 
-    shape distance array that spans [50, 150], which could be 
-    [50, 55, 70, 100, 120, 135, 150]
-    """
-    start_dist, end_dist = start_end_tuple
-    
-    # Get the subset of shape_path points that
-    # covers start_dist to end_dist
-    # https://stackoverflow.com/questions/16343752/numpy-where-function-multiple-conditions
-    shape_path_subset = shape_path_dist[
-        (np.where(
-            (shape_path_dist >= start_dist) & 
-            (shape_path_dist <= end_dist))
-        )]
-    
-    # Now add the start_dist and end_dist to the subset
-    shape_subset_with_endpoints = np.unique(np.array(
-            [start_dist] + shape_path_subset.tolist() + [end_dist]))
-    
-    # In each row, get the array, and interpolate it to convert 
-    # distances to points and string together into line geometry
-    segment_points = [shape_geom.interpolate(i) 
-                      for i in shape_subset_with_endpoints]
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import shapely
 
-    if len(segment_points) >= 2:
-        segment_line = shapely.geometry.LineString(segment_points)
-    else:
-        segment_line = np.nan
-    
-    return segment_line
+from dask import delayed, compute
+
+from shared_utils import utils
+from segment_speed_utils import array_utils, wrangle_shapes
+from segment_speed_utils.project_vars import SEGMENT_GCS, analysis_date
 
 
-def cut_stop_segments_for_shape(row: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+def get_prior_shape_meters(
+    gdf: gpd.GeoDataFrame
+) -> gpd.GeoDataFrame:
     """
-    For a row (shape_id), grab the shape_geom, array of 
-    stop's shape_meters.
-    
-    Loop over each stop's shape_meters and grab the subset of relevant
-    shape's array values.
+    For each shape, sort by stop sequence, and fill in the prior stop's 
+    projected shape_meters.
+    If it's missing, fill it in with itself (no line can be drawn, because it's
+    just the same point, but do this so our arrays work downstream). This will
+    be the case for the first stop, since there's no segment that 
+    can be drawn from the previous stop.
     """
     
-    shape_key = getattr(row, "shape_array_key")
-    shape_geom = getattr(row, "geometry")
-    stop_break_dist = getattr(row, "stop_break_distances")
-    shape_path_dist = getattr(row, "shape_distances")
+    shape_cols = ["shape_array_key"]
+
+    gdf = gdf.assign(
+        prior_shape_meters = (gdf.sort_values(shape_cols + ["stop_sequence"])
+                              .groupby(shape_cols, group_keys = False)
+                              .shape_meters
+                              .apply(lambda x: x.shift(1))
+                             )
+    )
     
-    shape_segments = []
+    # If it's missing, then set these equal so we don't get Nones when
+    # working with shapely points
+    gdf = gdf.assign(
+        prior_shape_meters = gdf.prior_shape_meters.fillna(gdf.shape_meters)
+    )
     
-    # Use np.indices instead of enumerate
-    for i in np.indices(stop_break_dist.shape).flatten():
-        # Skip if i == 0, because that's the start of the shape
-        # and it has prior element to look against
-        
-        # We want to skip the first element and the last element
-        # The last point on the shape runs just past the last stop
-        if (i > 0) and (i < np.indices(stop_break_dist.shape).flatten().argmax()):
-            # grab the elements in the array
-            # grab the element prior and the current element
-            # [, i+1 ] works similar to range(), it just includes i, not i+1
-            one_segment = get_shape_coords_up_to_stop(
-                shape_geom,
-                shape_path_dist, 
-                stop_break_dist,
-                stop_break_dist[i-1: i+1]
+    return gdf
+
+
+def subset_shape_coords_by_indices(
+    shape_coords_list: list, 
+    indices_array: np.ndarray
+) -> list:
+    """
+    For first stop, there is no prior stop. Return empty list.
+    For other stops, return the subset of shape_coords that spans
+    the prior to current stop.
+    """
+    
+    if len(indices_array) > 0:
+        return array_utils.subset_array_by_indices(
+            shape_coords_list, 
+            (indices_array[0], indices_array[-1])
             )
-            
-            shape_segments.append(one_segment)
- 
+    else:
+        return np.array([])
     
-    shape_segment_cutoffs = pd.DataFrame()
-    
-    shape_segment_cutoffs = shape_segment_cutoffs.assign(
-        geometry = shape_segments,
-        shape_meters = pd.Series(stop_break_dist),
-        shape_array_key = shape_key,
-    )
-    
-    return shape_segment_cutoffs
-   
-    
-def clean_up_stop_segments(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    """
-    After cutting stop-to-stop segments, do some cleanup.
-    There will be some None geometries and possibly duplicates.
-    Handle them here.
-    """
-    # Drop None geometries
-    gdf = gdf[gdf.geometry.notna()].reset_index(drop=True)
 
-    # Look for duplicates - split df into duplicated and not duplicated
-    # https://stackoverflow.com/questions/22904523/select-rows-with-duplicate-observations-in-pandas
-    shape_cols = ["shape_array_key", "shape_meters"]
-    
-    duplicated_df = gdf[gdf.duplicated(
-        subset=shape_cols, keep=False) == True]
-    
-    rest_of_df = gdf[~gdf.duplicated(subset=shape_cols, keep=False)]
-        
-    # If there are duplicates, we will keep the row with more 
-    # coords (more points) to form the line
-    duplicated_df = duplicated_df.assign(
-        num_coords = duplicated_df.geometry.apply(
-            lambda x: len(x.coords))
-    )
-    
-    no_dups = (duplicated_df.sort_values(
-        shape_cols + ["num_coords"], ascending=[True, True, False])
-        .drop_duplicates(subset=shape_cols)
-        .drop(columns = "num_coords")
-    )
-        
-    # Concatenate 
-    cleaned_df = (pd.concat([rest_of_df, no_dups], axis=0)
-                  .sort_values(shape_cols)
-                  .reset_index(drop=True)
-                 ) 
-    
-    return cleaned_df    
-    
-    
-def cut_stop_segments(gdf_wide: gpd.GeoDataFrame):
+def linestring_from_points(coords_list: list) -> shapely.geometry.LineString:
     """
-    Cut stop-to-stop segments, which includes looping through each row
-    of a wide df.
-    For a given shape geometry (and all its coords), 
-    subset by two stops, and find all the coords (from shape) 
-    in between those 2 stops.
+    Returns a shapely linestring object. Where there is just 1 point,
+    return an empty shapely linestring.
     """
-    segment_cutoffs = gpd.GeoDataFrame()
-    
-    # cut stop-to-stop segments and save projected coords
-    # since df is wide, loop with itertuples to cut the segments and assemble
-    for row in gdf_wide.itertuples():
-        print(f"start shape: {getattr(row, 'shape_array_key')}")
-        shape_segment_cutoffs = cut_stop_segments_for_shape(row)
-        
-        segment_cutoffs = pd.concat(
-            [segment_cutoffs, shape_segment_cutoffs], 
-            axis=0, ignore_index=True)
-    
-    # assemble and set as gdf
-    segments_assembled = gpd.GeoDataFrame(
-        segment_cutoffs[
-            segment_cutoffs.shape_meters > 0
-        ].reset_index(drop=True),
-        geometry = "geometry", 
-        crs = PROJECT_CRS
-    )
-    
-    # Clean up stop-to-stop segments
-    cleaned_segments = clean_up_stop_segments(segments_assembled)
+    if len(coords_list) > 1:
+        return shapely.geometry.LineString(coords_list)
+    else: 
+        return shapely.geometry.LineString()
 
-    return cleaned_segments
+
+def project_and_cut_segments_for_one_shape(
+    full_gdf: gpd.GeoDataFrame, 
+    one_shape: str
+) -> gpd.GeoDataFrame:
+    """
+    Use shapely.project. For each stop along the shape, 
+    find the subset of relevant coords to keep for our stop_segment.
+    Use projected distances as a way to subset the arrays,
+    and interpolate it back to shapely points, and string together 
+    as shapely linestrings at the end.
+    """
+    gdf = full_gdf[full_gdf.shape_array_key==one_shape].reset_index(drop=True)
+    shape_geometry = gdf.geometry.iloc[0]
+    
+    # (1) Get list of coords from shapely linestring and convert it to a list
+    # of projected distances. 
+    # Simplify to get rid of duplicate coords (if there are any)
+    # Ex: we can take a subset of distances between stop 2 and 3 [100m, 500m]
+    shape_coords_list = [shapely.Point(i) for 
+                         i in shape_geometry.simplify(0).coords]
+    
+    shape_projected_dist = wrangle_shapes.project_list_of_coords(
+        shape_geometry, use_shapely_coords = True)
+    
+    #  (2) Grab a subset of the array
+    # for a given pair of (prior_stop, current_stop)
+    # This gives us the indices of the subset_array we want
+    idx_shape_dist = [
+        array_utils.cut_shape_by_origin_destination(
+            shape_projected_dist, 
+            (origin_stop, destination_stop)
+        ) for origin_stop, destination_stop in 
+        zip(gdf.prior_shape_meters, gdf.shape_meters)
+    ]
+    
+    # (3) Concatenate the endpoints, so that the segments
+    # can get as close as possible to the stop
+    subset_shape_dist_with_endpoints = [
+        np.concatenate([
+            [origin_stop],
+            shape_projected_dist[idx],
+            [destination_stop]
+        ]) for idx, origin_stop, destination_stop 
+        in zip(idx_shape_dist, gdf.prior_shape_meters, gdf.shape_meters)
+    ]
+
+    # (4) Interpolate this entire array and convert it from projected distances
+    # to shapely points
+    subset_shape_geom_with_endpoints = [ 
+        wrangle_shapes.interpolate_projected_points(
+            shape_geometry, 
+            dist_array
+        ) for dist_array in subset_shape_dist_with_endpoints
+    ]
+    
+    # (5) Convert this array into a shapely linestring
+    subset_shape_geom_ls = [
+        linestring_from_points(i)
+        for i in subset_shape_geom_with_endpoints
+    ]
+
+    # (6) Assign this geoseries as the stop_segment_geometry column
+    # Set the CRS (we lose this)
+    # Note: if we change step 5 to a geoseries (with CRS), 
+    # we lose the shapely objects, and it's a geoseries of None geometries. 
+    # Leave as list, then set CRS here
+    gdf2 = (gdf.assign(
+        stop_segment_geometry = subset_shape_geom_ls
+        ).drop(columns = "geometry")
+        .set_geometry("stop_segment_geometry")
+        .set_crs(gdf.crs)
+    )
+    
+    return gdf2
     
     
 def finalize_stop_segments(
@@ -239,22 +223,54 @@ if __name__ == "__main__":
     
     start = datetime.datetime.now()
 
-    STOP_SEG_DICT = helpers.get_parameters(CONFIG_PATH, "stop_segments")
-    EXPORT_FILE = STOP_SEG_DICT["segments_file"]
+    #STOP_SEG_DICT = helpers.get_parameters(CONFIG_PATH, "stop_segments")
+    #EXPORT_FILE = STOP_SEG_DICT["segments_file"]
     
-    df = gpd.read_parquet(
-        f"{SEGMENT_GCS}stops_projected_wide_{analysis_date}.parquet")
+    # Get list of shapes that go through normal stop segment cutting
+    shapes_to_cut = pd.read_parquet(
+        f"{SEGMENT_GCS}stops_projected_{analysis_date}/",
+        filters = [[("loop_or_inlining", "==", 0)]],
+        columns = ["shape_array_key"]
+    ).drop_duplicates().shape_array_key
     
-    stop_segments = cut_stop_segments(df)
+    gdf = gpd.read_parquet(
+        f"{SEGMENT_GCS}stops_projected_{analysis_date}/",
+        filters = [[("loop_or_inlining", "==", 0)]],
+        columns = ["shape_array_key",
+            "stop_id", "stop_sequence", 
+            "shape_meters", 
+            "geometry", 
+            ]
+    )
+    
+    gdf = get_prior_shape_meters(gdf)
+    
+    results = []
+    
+    for shape in shapes_to_cut:
+        segments = delayed(project_and_cut_segments_for_one_shape)(
+            gdf, shape)
+        results.append(segments)
     
     time1 = datetime.datetime.now()
     logger.info(f"Cut stop segments: {time1-start}")
     
-    # Merge in the stop projected geom so that each stop can be 
-    # attached to the segment leading up to that stop
-    stops_projected = gpd.read_parquet(
-        f"{SEGMENT_GCS}stops_projected_{analysis_date}.parquet")
+    results2 = [compute(i)[0] for i in results]
+    results_gdf = (pd.concat(results2, axis=0)
+                   .sort_values(["shape_array_key", "stop_sequence"])
+                   .reset_index(drop=True)
+                  )
     
+    utils.geoparquet_gcs_export(
+        results_gdf,
+        SEGMENT_GCS,
+        f"stop_segments_normal_{analysis_date}"
+    )
+    
+    time2 = datetime.datetime.now()
+    logger.info(f"Export results: {time2-time1}")
+    
+    '''
     arrowized_segments = finalize_stop_segments(
         stops_projected, 
         stop_segments
@@ -268,6 +284,6 @@ if __name__ == "__main__":
         SEGMENT_GCS,
         f"{EXPORT_FILE}_{analysis_date}"
     )
-
+    '''
     end = datetime.datetime.now()
     logger.info(f"execution time: {end-start}")
