800208fcca73130cf4a43bac0a212a0a6e8f6dc7
amandaha8 <amandahastuyck@gmail.com>
2023-05-31 21:50:32 +0000

    added new script for speeds, double check for tiger

---
+++ rt_segment_speeds/_speed_utils.py
@@ -0,0 +1,118 @@
+import _threshold_utils as threshold_utils
+import altair as alt
+
+import pandas as pd
+import geopandas as gpd
+from segment_speed_utils.project_vars import analysis_date
+from shared_utils import calitp_color_palette as cp
+
+GCS_PATH = "gs://calitp-analytics-data/data-analyses/rt_segment_speeds/"
+
+def open_speeds_stops(date: str)-> pd.DataFrame:
+    """
+    Open up speeds_stops parquet.
+    
+    Args:
+        date: analysis date
+    """
+    speed_stops_subset = ['gtfs_dataset_key', '_gtfs_dataset_name', 'shape_array_key',
+       'stop_sequence', 'trip_id','speed_mph']
+    
+    df = pd.read_parquet(f"{GCS_PATH}speeds_stop_segments_{date}")
+    
+    df = df[speed_stops_subset]
+    
+    return df
+
+def open_avg_speeds(date:str)-> pd.DataFrame:
+    """
+    Open up avg_speeds_stop_segments parquet.
+    
+    Args:
+        date: analysis date
+    """
+    df = gpd.read_parquet(f"{GCS_PATH}avg_speeds_stop_segments_{date}.parquet")
+    cols_to_drop = ['geometry','geometry_arrowized','district','district_name']
+    df = df.drop(columns = cols_to_drop)
+    
+    return df
+
+def merge_all_speeds(date:str) -> pd.DataFrame:
+    """
+    Merge avg_speeds_stop_segments and
+    speed_stops parquets, drop duplicates, and
+    renumber stop sequences that are out of order. 
+    
+    Args:
+        date: analysis date
+    """
+    avg_speeds = open_avg_speeds(date)
+    speed_stops = open_speeds_stops(date)
+    
+    merge_cols = ['gtfs_dataset_key','shape_array_key', 'stop_sequence']
+    m1 = pd.merge(avg_speeds, speed_stops, on = merge_cols, how = 'inner')
+    print(f"{len(m1) - len(m1.drop_duplicates())} duplicate rows will be dropped") 
+    
+    m1 = m1.drop_duplicates().reset_index(drop = True)
+    
+    # Renumber stop sequences since some of them are out of order. 
+    # Ex: instead of stop 1,2,3 it could be stop 151,169, 300
+    # Sort df first.
+    sort_cols = ['shape_array_key', 'gtfs_dataset_key', 'trip_id', 'stop_sequence']
+    m1 = m1.sort_values(sort_cols).reset_index(drop = True)
+    
+    # Count stops by operator (gtfs key), route (shape array), and trip
+    group_cols = ['gtfs_dataset_key','shape_array_key', 'trip_id']
+    m1['sorted_stop_seq'] = m1.groupby(group_cols).cumcount().astype(int)+1
+    
+    # Check that stops were renumbered correctly. Should only be 0.
+    # Total original stops should equal newly renumbered ones. 
+    check1 = m1.groupby(['shape_array_key']).agg({'sorted_stop_seq':'count', 'stop_sequence':'count'}).reset_index(drop = True)
+    check1["stops_renumbered_correctly"] = check1.sorted_stop_seq-check1.stop_sequence
+    print(check1.stops_renumbered_correctly.value_counts())
+    
+    return m1
+
+def find_shapes_with_many_stops(date:str) -> pd.DataFrame:
+    """
+    Find the routes with total stops 
+    in the 95th+ percentile.
+    
+    Args:
+        date: analysis_date
+        
+    Returns:
+        DF with the total stops by shape_array_key,
+        gtfs_dataset_key, trip_id and total stops to check
+        out routes.
+        
+        List of unique shape_array_keys.
+    """
+    df = merge_all_speeds(date)
+    
+    # Find the total number of stops by operator, shape_array_key (route), and trip_id
+    agg = (df.groupby(['gtfs_dataset_key','_gtfs_dataset_name','shape_array_key','trip_id'])
+                .agg({'stop_sequence':'nunique'})
+                .sort_values(['stop_sequence'], ascending = False)
+                .reset_index()
+                .rename(columns = {'stop_sequence':'total_stops'})
+               )
+    
+    # Categorize 
+    p95 =  agg.total_stops.quantile(0.95).astype(float)
+    p99 =  agg.total_stops.quantile(0.99).astype(float)
+    
+    def stop_categories(row):
+        if ((row.total_stops >= p95) and (row.total_stops <= p99)):
+               return f"95th {p95}-{p99} stops"
+        elif row.total_stops > p99:
+               return f"99th {p99}+ stops"
+        else:
+            return "other"
+        
+    agg["stop_percentiles"] = agg.apply(lambda x: stop_categories(x), axis=1)
+    
+    # Keep only the routes in the 95th percentile of stops
+    agg2 = agg[agg.stop_percentiles.isin([f"95th {p95}-{p99} stops", f"99th {p99}+ stops"])].reset_index(drop = True)
+    
+    return agg2, agg2.shape_array_key.unique().tolist()
