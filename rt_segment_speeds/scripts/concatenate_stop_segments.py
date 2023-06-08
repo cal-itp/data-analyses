c0f541c92d1a360c24cad753549e74961fe18d77
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-06-08 21:47:55 +0000

    move sjoin of segments to districts into concat_segments, assemble df for export

--- rt_segment_speeds/scripts/concatenate_stop_segments.py
+++ rt_segment_speeds/scripts/concatenate_stop_segments.py
@@ -1,10 +1,15 @@
 """
-Concatenate the two stop segment files and arrowize segments
+Concatenate the two stop segment files,
+spatial join to district,
+and arrowize segments
 """
+import os
+os.environ['USE_PYGEOS'] = '0'
 import geopandas as gpd
 import pandas as pd
 
-from shared_utils import utils
+from shared_utils import utils, portfolio_utils
+from shared_utils.geography_utils import WGS84
 from segment_speed_utils import helpers, sched_rt_utils, wrangle_shapes
 from segment_speed_utils.project_vars import (SEGMENT_GCS, analysis_date, 
                                               CONFIG_PATH)
@@ -33,6 +38,46 @@ def finalize_stop_segments(
     return stop_segments_with_rt_key
 
 
+def spatial_join_to_caltrans_districts(
+    gdf: gpd.GeoDataFrame
+) -> gpd.GeoDataFrame: 
+    """
+    Spatial join any gdf to Caltrans districts (open data portal).
+    Use centroid for simpler sjoin. 
+    For segments, we're taking the centroid of a linestring.
+    """
+    gdf = gdf.assign(
+        centroid = gdf.geometry.centroid,
+    )
+    
+    caltrans_districts = gpd.read_parquet(
+        "gs://calitp-analytics-data/data-analyses/shared_data/"
+        "caltrans_districts.parquet"
+    ).to_crs(WGS84)
+    
+    caltrans_districts = caltrans_districts.assign(
+        district_name = caltrans_districts.DISTRICT.map(
+            portfolio_utils.district_name_dict)
+    ).rename(columns = {"DISTRICT": "district"})
+    
+    # Spatial join to Caltrans district
+    sjoin_results = gpd.sjoin(
+        gdf[["seg_idx", "centroid"]].set_geometry("centroid"), 
+        caltrans_districts.to_crs(gdf.crs),
+        how = "inner",
+        predicate = "intersects"
+    ).drop(columns = "index_right")[["seg_idx", "district", "district_name"]]
+    
+    gdf3 = pd.merge(
+        gdf,
+        sjoin_results,
+        on = "seg_idx",
+        how = "inner"
+    ).set_geometry("geometry").drop(columns = "centroid")
+    
+    return gdf3
+
+
 if __name__ == "__main__":
     
     STOP_SEG_DICT = helpers.get_parameters(CONFIG_PATH, "stop_segments")
@@ -58,8 +103,10 @@ if __name__ == "__main__":
     
     gdf = finalize_stop_segments(gdf)
     
+    gdf2 = spatial_join_to_caltrans_districts(gdf)
+    
     arrowized_segments = wrangle_shapes.add_arrowized_geometry(
-        gdf)
+        gdf2)
     
     utils.geoparquet_gcs_export(
         arrowized_segments, 
