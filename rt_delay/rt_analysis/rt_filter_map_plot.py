363d0da4d1efd883e3ce183a25afaddc8dc2d20c
Eric Dasmalchi <edasmalchi@gmail.com>
2023-06-29 00:30:53 +0000

    move spa link generation, add cols to variance

--- rt_delay/rt_analysis/rt_filter_map_plot.py
+++ rt_delay/rt_analysis/rt_filter_map_plot.py
@@ -494,11 +494,11 @@ class RtFilterMapper:
         gdf = self.detailed_map_view.copy().dropna(subset=['fast_slow_ratio']) >> arrange(_.trips_per_hour)
         display_cols = ['fast_slow_ratio', 'p20_mph', 'p80_mph', 'miles_from_last',
                        'route_short_name', 'trips_per_hour', 'shape_id',
-                       'stop_sequence']
+                       'stop_sequence', 'stop_id', 'route_id', 'stop_name']
         display_aliases = ['80th %ile to 20th %ile speed ratio (variation in speeds)',
                            '20th %ile Speed (miles per hour)', ' 80th %ile Speed (miles per hour)', 'Segment distance (miles)',
                           'Route', 'Frequency (trips per hour)', 'Shape ID',
-                          'Stop Sequence']
+                          'Stop Sequence', 'Stop ID', 'Route ID', 'Stop Name']
         tooltip_dict = {'aliases': display_aliases}
         cmap = VARIANCE_FIXED_COLORSCALE
         self.variance_cmap = cmap
@@ -542,7 +542,9 @@ class RtFilterMapper:
             print(f'writing to {path}')
             with fs.open(path, 'wb') as writer:  # write out to public-facing GCS?
                 with gzip.GzipFile(fileobj=writer, mode="w") as gz:
-                    gz.write(geojson_bytes)            
+                    gz.write(geojson_bytes)
+            base64state = base64.urlsafe_b64encode(json.dumps(self.spa_map_state).encode()).decode()
+            self.spa_map_url = f'https://leaflet-speedmaps--cal-itp-data-analyses.netlify.app/?state={base64state}'
             return
         
         if map_type == '_20p_speeds':
@@ -600,9 +602,8 @@ class RtFilterMapper:
         Width/height defaults are current best option for JupyterBook, don't change for portfolio use
         width, height: int (pixels)
         '''
-        assert hasattr(self, 'spa_map_state'), 'must export map and set state first using self.map_gz_export'
-        base64state = base64.urlsafe_b64encode(json.dumps(self.spa_map_state).encode()).decode()
-        i = IFrame(f'https://leaflet-speedmaps--cal-itp-data-analyses.netlify.app/?state={base64state}', width=width, height=height)
+        assert hasattr(self, 'spa_map_url'), 'must export map and set state first using self.map_gz_export'
+        i = IFrame(self.spa_map_url, width=width, height=height)
         display(i)
         return
 
