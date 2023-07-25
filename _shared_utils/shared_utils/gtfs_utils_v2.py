ae8e18e61adb02f6183fde06f4c1083d0cad58d6
Eric Dasmalchi <edasmalchi@gmail.com>
2023-07-25 19:38:36 +0000

    fix metrolink feed query

--- _shared_utils/shared_utils/gtfs_utils_v2.py
+++ _shared_utils/shared_utils/gtfs_utils_v2.py
@@ -169,11 +169,10 @@ def get_metrolink_feed_key(selected_date: Union[str, datetime.date], get_df: boo
 
     metrolink_feed = (
         tbls.mart_gtfs.fct_daily_schedule_feeds()
-        >> filter(_.date == selected_date)
-        >> select(_.feed_key, _.gtfs_dataset_key)
-        >> inner_join(_, metrolink_in_airtable, on="gtfs_dataset_key")
-        >> rename(name=_.gtfs_dataset_name)
-        >> subset_cols(["feed_key", "name"])
+        >> filter(_.date == analysis_date)
+        >> inner_join(_, metrolink_in_airtable, on=["gtfs_dataset_key", "gtfs_dataset_name"])
+        >> rename(name = _.gtfs_dataset_name)
+        >> shared_utils.gtfs_utils_v2.subset_cols(["feed_key", "name"])
         >> collect()
     )
 
