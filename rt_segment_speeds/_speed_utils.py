1e6917502a9ec1cbc7ee437fd7fe217c0cc75d18
amandaha8 <amandahastuyck@gmail.com>
2023-08-08 22:07:13 +0000

    changed col names

--- rt_segment_speeds/_speed_utils.py
+++ rt_segment_speeds/_speed_utils.py
@@ -31,7 +31,7 @@ def count_trips_routes(df:pd.DataFrame):
     Args:
         df can be any df (from flag_stage3 or merge_all_speeds)
     """
-    cols_to_keep = ['shape_array_key','_gtfs_dataset_name','gtfs_dataset_key', 'trip_id', 'n_trips']
+    cols_to_keep = ['shape_array_key','gtfs_dataset_name','gtfs_dataset_key', 'trip_id', 'n_trips']
     m1 = (df
      .sort_values(['n_trips'], ascending = False)
      .drop_duplicates(['shape_array_key'])
@@ -162,7 +162,7 @@ def keep_only_zeroes(flagged:pd.DataFrame)-> pd.DataFrame:
     print(f"{flagged.trip_id.nunique()-df2.trip_id.nunique()} unique trips flagged.")
     print(f"{df2.shape_array_key.nunique()} routes flagged out of {flagged.shape_array_key.nunique()}.")
     print(f"{df2.shape_array_key.nunique()/flagged.shape_array_key.nunique() * 100} routes have 1+ row that has zeroes for meters/sec elapsed")
-    print(f"{flagged._gtfs_dataset_name.nunique()-df2._gtfs_dataset_name.nunique()} operators are not flagged.")
+    print(f"{flagged.gtfs_dataset_name.nunique()-df2.gtfs_dataset_name.nunique()} operators are not flagged.")
     
     return df2
 
@@ -305,7 +305,7 @@ def load_vp_stage3(flagged_df:pd.DataFrame, date:str) -> pd.DataFrame:
                    ('gtfs_dataset_key', 'in', gtfs_dataset_key)]],)
     
     # Merge to capture original df information and filter down further
-    vp2 = pd.merge(flagged_df, vp, how = "inner", on = ['gtfs_dataset_key', 'trip_id','stop_sequence','shape_array_key','_gtfs_dataset_name'])
+    vp2 = pd.merge(flagged_df, vp, how = "inner", on = ['gtfs_dataset_key', 'trip_id','stop_sequence','shape_array_key','gtfs_dataset_name'])
     
     return vp2
 
