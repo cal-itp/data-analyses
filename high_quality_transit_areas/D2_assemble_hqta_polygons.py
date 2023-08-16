51ccea243f045a2b1421146ea56a9b63bb12cd91
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-08-16 17:22:00 +0000

    update get_fs imports

--- high_quality_transit_areas/D2_assemble_hqta_polygons.py
+++ high_quality_transit_areas/D2_assemble_hqta_polygons.py
@@ -10,8 +10,7 @@ import intake
 import pandas as pd
 import sys
 
-from calitp_data.storage import get_fs
-from calitp_data.config import is_cloud
+from calitp_data_analysis import get_fs
 from loguru import logger
 
 import C1_prep_pairwise_intersections as prep_clip
