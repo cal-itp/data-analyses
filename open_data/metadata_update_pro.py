f134e8b7316503fef8053972dbf3b0bc4383276a
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-08-31 19:11:11 +0000

    break out metadata update by sections, simplify script to overwrite for each dataset

--- open_data/metadata_update_pro.py
+++ open_data/metadata_update_pro.py
@@ -4,22 +4,25 @@ Overwrite XML metadata using JSON.
 Analyst inputs a dictionary of values to overwrite.
 Convert JSON back to XML to feed in ArcGIS.
 """
-import os
+import json
 import pandas as pd
 import xml.etree.ElementTree as ET
 import xmltodict
 
-import validation_pro
-from update_vars import DEFAULT_XML_TEMPLATE, XML_FOLDER
+from pathlib import Path
+from typing import Union
 
-TEMPLATE_XML = f"./{XML_FOLDER}{DEFAULT_XML_TEMPLATE}"
+from update_vars import DEFAULT_XML_TEMPLATE, XML_FOLDER, META_JSON
 
 # This prefix keeps coming up, but xmltodict has trouble processing or replacing it
 x = "ns0:"
 main = f"{x}MD_Metadata"
+char_key = "ns1:CharacterString"
+dt_key = "ns1:Date"
+code_key = "@codeListValue"
+txt_key = "text"
 
-
-def xml_to_json(path: str) -> dict:  
+def xml_to_json(path: Union[str, Path]) -> dict:  
     """
     Convert XML to JSON
     https://stackoverflow.com/questions/48821725/xml-parsers-expat-expaterror-not-well-formed-invalid-token
@@ -57,14 +60,18 @@ def lift_necessary_dataset_elements(metadata_json: dict) -> dict:
    
     # Coordinate Reference System Info
     d[f"{x}referenceSystemInfo"] = m[f"{x}referenceSystemInfo"] 
-    
-    # Distribution Info
-    d[f"{x}distributionInfo"] = m[f"{x}distributionInfo"]   
-    
+        
     return d
 
 
 def overwrite_default_with_dataset_elements(metadata_json: dict) -> dict:
+    """
+    Grab the default template.
+    Replace entire sections that are dataset specific with 
+    this dataset's attributes.
+    """
+    TEMPLATE_XML = XML_FOLDER.joinpath(DEFAULT_XML_TEMPLATE)
+
     default_template = xml_to_json(TEMPLATE_XML)
     
     # Grab the necessary elements from my dataset
@@ -81,179 +88,170 @@ def overwrite_default_with_dataset_elements(metadata_json: dict) -> dict:
     return default_template
 
 
-def update_arcpy_metadata_class(dataset_info: dict) -> :
+def overwrite_topic_section(metadata: dict, dataset_info: dict) -> dict: 
     """
-    This step needs to take place in ArcPro before we export XML.
-    """
-        
-    for_arcpy = [
-        "dataset_name", # title
-        "theme_keywords", # tags
-        "purpose", # summary
-        "description", # abstract
-        "public_access", # accessConstraints
-    ]
-    
-    subset_dict = {i: dataset_info[i] for i in for_arcpy}
-    
-    #test_metadata.exportMetadata("test.xml")
-    
-    return subset_dict
-
-
-def overwrite_overview(metadata: dict, dataset_info: dict) -> dict:
-    """
-    Overwrite the metadata for ArcPro Overview section 
+    Overwrite the metadata for ArcPro 
+    Overview > Item Description and Topic & Keywords
+    and Resource > Details / Extents section(s)
     with dictionary of dataset info supplied.
     """
     d = dataset_info
     
-    # This is how most values are keyed in for last dict
-    key = "ns1:CharacterString"
-    key_dt = "ns1:Date"
-    enum = "@codeListValue"
-    t = "text"
-    
-    id_info = metadata[f"{x}identificationInfo"][f"{x}MD_DataIdentification"]
-    
-    
-    citation_info[f"{x}title"][key] = d["dataset_name"]  
-    
-    
-    id_info[f"{x}abstract"][key] = d["abstract"]
-    id_info[f"{x}purpose"][key] = d["purpose"]
-    (id_info[f"{x}descriptiveKeywords"][0]
-     [f"{x}MD_Keywords"][f"{x}keyword"]) = d["theme_keywords"]
-    id_info[f"{x}topicCategory"][f"{x}MD_TopicCategoryCode"] = d["theme_topic"]
-    id_info[f"{x}extent"][0][f"{x}EX_Extent"][f"{x}description"][key] = d["place"]
-    
-    citation_info = id_info[f"{x}citation"][f"{x}CI_Citation"]
-  
-    
-    return metadata
-
-
-
-def overwrite_id_info(metadata: dict, dataset_info: dict) -> dict:
-    """
-    Overwrite the metadata after dictionary of dataset info is supplied
-    """
-    d = dataset_info
-    # This is how most values are keyed in for last dict
-    key = "ns1:CharacterString"
-    key_dt = "ns1:Date"
-    enum = "@codeListValue"
-    t = "text"
-    
     ## Identification Info
     id_info = metadata[f"{x}identificationInfo"][f"{x}MD_DataIdentification"]
-    
-    id_info[f"{x}abstract"][key] = d["abstract"]
-    id_info[f"{x}purpose"][key] = d["purpose"]
-    (id_info[f"{x}descriptiveKeywords"][0]
-     [f"{x}MD_Keywords"][f"{x}keyword"]) = d["theme_keywords"]
-    id_info[f"{x}topicCategory"][f"{x}MD_TopicCategoryCode"] = d["theme_topic"]
-    id_info[f"{x}extent"][0][f"{x}EX_Extent"][f"{x}description"][key] = d["place"]
-    
-    citation_info = id_info[f"{x}citation"][f"{x}CI_Citation"]
-    citation_info[f"{x}title"][key] = d["dataset_name"]    
-    
-    beginning_cite = citation_info[f"{x}date"][0][f"{x}CI_Date"] 
-    beginning_cite[f"{x}date"][key_dt] = d["creation_date"]
-    beginning_cite[f"{x}dateType"][f"{x}CI_DateTypeCode"][enum] = "creation"
-    beginning_cite[f"{x}dateType"][f"{x}CI_DateTypeCode"][t] = "creation"    
-    
-    end_cite = citation_info[f"{x}date"][1][f"{x}CI_Date"] 
-    end_cite[f"{x}date"][key_dt] = d["beginning_date"]
-    end_cite[f"{x}dateType"][f"{x}CI_DateTypeCode"][enum] = "revision"
-    end_cite[f"{x}dateType"][f"{x}CI_DateTypeCode"][t] = "revision"      
-        
-    citation_contact = citation_info[f"{x}citedResponsibleParty"][f"{x}CI_ResponsibleParty"]
-    citation_contact[f"{x}individualName"][key] = d["contact_person"]
-    citation_contact[f"{x}organisationName"][key] = d["contact_organization"]  
-    citation_contact[f"{x}positionName"][key] = d["publish_entity"]
-    (citation_contact[f"{x}contactInfo"][f"{x}CI_Contact"][f"{x}address"]
-     [f"{x}CI_Address"][f"{x}electronicMailAddress"][key]) = d["contact_email"]
-    
     status_info = id_info[f"{x}status"][f"{x}MD_ProgressCode"]
-    status_info[enum] = d["status"]
-    status_info[t] = d["status"]
     
-    maint_info = id_info[f"{x}resourceMaintenance"][f"{x}MD_MaintenanceInformation"]
-    (maint_info[f"{x}maintenanceAndUpdateFrequency"]
-     [f"{x}MD_MaintenanceFrequencyCode"][enum]) = d["frequency"]
-    (maint_info[f"{x}maintenanceAndUpdateFrequency"]
-     [f"{x}MD_MaintenanceFrequencyCode"][t]) = d["frequency"]
-    maint_info[f"{x}dateOfNextUpdate"][key_dt] = d["end_date"]
-    maint_info = overwrite_contact_info(maint_info, d)
+    ## Topic & Keywords
+    id_info[f"{x}topicCategory"][f"{x}MD_TopicCategoryCode"] = d["theme_topic"]
+    
+    # this might be GIS theme keywords?
+    (id_info[f"{x}descriptiveKeywords"][0]
+     [f"{x}MD_Keywords"][f"{x}keyword"][char_key]) = d["theme_topic"]
         
-    (id_info[f"{x}resourceConstraints"][0][f"{x}MD_LegalConstraints"]
-     [f"{x}useLimitation"][key]) = d["public_access"]   
-    (id_info[f"{x}resourceConstraints"][1][f"{x}MD_LegalConstraints"]
-     [f"{x}useLimitation"][key]) = d["boilerplate_desc"]
-    (id_info[f"{x}resourceConstraints"][2][f"{x}MD_LegalConstraints"]
-     [f"{x}useLimitation"][key]) = d["boilerplate_license"]   
-    (id_info[f"{x}resourceConstraints"][2][f"{x}MD_LegalConstraints"]
-     [f"{x}useConstraints"][f"{x}MD_RestrictionCode"][enum]) = "license"
-    (id_info[f"{x}resourceConstraints"][2][f"{x}MD_LegalConstraints"]
-     [f"{x}useConstraints"][f"{x}MD_RestrictionCode"][t]) = "license"    
+    ## Resource > Details - Status
+    status_info[code_key] = d["status"]
 
+    status_info[txt_key] = d["status"]
+    
+    ## Resource > Extents - Place
+    (id_info[f"{x}extent"][f"{x}EX_Extent"]
+     [f"{x}description"][char_key]) = d["place"]
+    
     return metadata
-    
-    
-def overwrite_contact_info(metadata: dict, dataset_info: dict) -> dict: 
-    d = dataset_info
-    key = "ns1:CharacterString"
 
-    ## Contact Info
-    contact_info = metadata[f"{x}contact"][f"{x}CI_ResponsibleParty"]
+
+def overwrite_citation_section(metadata: dict, dataset_info: dict) -> dict:
+    """
+    Overwrite the metadata for ArcPro 
+    Overview > Citation and Citation Contacts section(s) 
+    with dictionary of dataset info supplied.
+    """
+    d = dataset_info
     
-    contact_info[f"{x}positionName"][key] = d["publish_entity"]
-    contact_info[f"{x}organisationName"][key] = d["contact_organization"]
-    contact_info[f"{x}individualName"][key] = d["contact_person"]
+    ## Identification Info > Citation
+    id_info = metadata[f"{x}identificationInfo"][f"{x}MD_DataIdentification"]
+    citation_info = id_info[f"{x}citation"][f"{x}CI_Citation"]
+                    
+    ## Citation Dates
+    (citation_info[f"{x}date"][0][f"{x}CI_Date"]
+     [f"{x}date"][dt_key]) = d["creation_date"]
     
-    (contact_info[f"{x}contactInfo"][f"{x}CI_Contact"]
+    (citation_info[f"{x}date"][1][f"{x}CI_Date"]
+     [f"{x}date"][dt_key]) = d["revision_date"]
+    
+    ## Citation Contacts
+    (citation_info[f"{x}citedResponsibleParty"][0]
+     [f"{x}CI_ResponsibleParty"][f"{x}organisationName"][char_key]) = d["contact_organization"]
+
+    (citation_info[f"{x}citedResponsibleParty"][1]
+     [f"{x}CI_ResponsibleParty"][f"{x}individualName"][char_key]) = d["contact_person"]
+    
+    (citation_info[f"{x}citedResponsibleParty"][1]
+     [f"{x}CI_ResponsibleParty"][f"{x}organisationName"][char_key]) = d["contact_organization"]
+    
+    (citation_info[f"{x}citedResponsibleParty"][1]
+     [f"{x}CI_ResponsibleParty"][f"{x}positionName"][char_key]) = d["publish_entity"]
+    
+    (citation_info[f"{x}citedResponsibleParty"][1]
+     [f"{x}CI_ResponsibleParty"][f"{x}contactInfo"][f"{x}CI_Contact"]
      [f"{x}address"][f"{x}CI_Address"]
-     [f"{x}electronicMailAddress"][key]) = d["contact_email"] 
-    
+     [f"{x}electronicMailAddress"][char_key]) = d["contact_email"]
+        
     return metadata
 
 
-def overwrite_data_quality_info(metadata: dict, dataset_info: dict) -> dict:
+def overwrite_resource_section(metadata: dict, dataset_info: dict) -> dict:
+    """
+    Overwrite the metadata for ArcPro 
+    Resource > Maintenance & Constraints section(s) 
+    with dictionary of dataset info supplied.    
+    """
     d = dataset_info
-    key = "ns1:CharacterString"
+    
+    ## Identification Info > Resource Maintenance / Resource Constraints
+    id_info = metadata[f"{x}identificationInfo"][f"{x}MD_DataIdentification"]    
+    maint_info = id_info[f"{x}resourceMaintenance"]
+    constraint_info = id_info[f"{x}resourceConstraints"]
+    
+    ## Maintenance
+    (maint_info[f"{x}MD_MaintenanceInformation"]
+     [f"{x}maintenanceAndUpdateFrequency"]
+     [f"{x}MD_MaintenanceFrequencyCode"][code_key]) = d["frequency"]
+    
+    (maint_info[f"{x}MD_MaintenanceInformation"]
+     [f"{x}maintenanceAndUpdateFrequency"]
+     [f"{x}MD_MaintenanceFrequencyCode"][txt_key]) = d["frequency"]    
+    
+    ## Constraints
+    (constraint_info[0][f"{x}MD_Constraints"]
+     [f"{x}useLimitation"][char_key]) = d["public_access"]        
+
+    (constraint_info[1][f"{x}MD_LegalConstraints"]
+     [f"{x}useLimitation"][char_key]) = d["boilerplate_desc"] 
+    
+    (constraint_info[2][f"{x}MD_LegalConstraints"]
+     [f"{x}useLimitation"][char_key]) = d["boilerplate_license"] 
+
+    return metadata
+    
+
+def overwrite_data_quality_section(metadata: dict, dataset_info: dict) -> dict:
+    """
+    Overwrite the metadata for ArcPro 
+    Resource > Quality section(s)
+    with dictionary of dataset info supplied.  
+    """
+    d = dataset_info
+    
+    qual_info = metadata[f"{x}dataQualityInfo"][f"{x}DQ_DataQuality"]
     
     ## Data Quality
-    data_qual_info = metadata[f"{x}dataQualityInfo"][f"{x}DQ_DataQuality"]
-    (data_qual_info[f"{x}report"][f"{x}DQ_RelativeInternalPositionalAccuracy"]
-     [f"{x}measureDescription"][key]) = d["horiz_accuracy"]
+    (qual_info[f"{x}report"][f"{x}DQ_RelativeInternalPositionalAccuracy"]
+     [f"{x}measureDescription"][char_key]) = d["horiz_accuracy"]
     
-    (data_qual_info[f"{x}lineage"][f"{x}LI_Lineage"]
+    (qual_info[f"{x}lineage"][f"{x}LI_Lineage"]
      [f"{x}processStep"][f"{x}LI_ProcessStep"]
-     [f"{x}description"][key]) = d["methodology"]
+     [f"{x}description"][char_key]) = d["methodology"]
+    
+    return metadata
+
+
+def overwrite_distribution_section(metadata: dict, dataset_info: dict) -> dict:
+    """
+    Overwrite the metadata for ArcPro 
+    Resource > Distribution section(s)
+    with dictionary of dataset info supplied.  
+    """
+    d = dataset_info
+    
+    dist_info = (metadata[f"{x}distributionInfo"][f"{x}MD_Distribution"]
+                 [f"{x}transferOptions"][f"{x}MD_DigitalTransferOptions"]
+                 [f"{x}onLine"][f"{x}CI_OnlineResource"]
+                )
+
+    ## Distribution Info
+    dist_info[f"{x}linkage"][f"{x}URL"] = d["readme"]
     
     return metadata
     
 
-def overwrite_metadata_json(metadata_json: dict, 
-                            dataset_info: dict, first_run: bool = False) -> dict:
+def overwrite_metadata_json(metadata_json: dict, dataset_info: dict) -> dict:
     d = dataset_info
     new_metadata = metadata_json.copy()
     
-    new_metadata[main] = overwrite_id_info(new_metadata[main], d)
-        
-    new_metadata[main] = overwrite_data_quality_info(new_metadata[main], d)
-                
-    #m["eainfo"]["detailed"]["enttyp"]["enttypd"] = d["data_dict_type"]    
-    #m["eainfo"]["detailed"]["enttyp"]["enttypds"] = d["data_dict_url"]    
-      
+    new_metadata[main] = overwrite_topic_section(new_metadata[main], d)
+    new_metadata[main] = overwrite_citation_section(new_metadata[main], d)    
+    new_metadata[main] = overwrite_resource_section(new_metadata[main], d)
+    new_metadata[main] = overwrite_data_quality_section(new_metadata[main], d)
+    new_metadata[main] = overwrite_distribution_section(new_metadata[main], d)
+    
     return new_metadata 
 
 
-def update_metadata_xml(
-    xml_file: str, 
-    dataset_info: dict, 
-    metadata_folder: str = XML_FOLDER
+def update_dataset_metadata_xml(
+    dataset_name: str, 
+    metadata_path: Union[str, Path] = META_JSON, 
+    metadata_folder: Union[str, Path] = XML_FOLDER
 ):
     """
     xml_file: string.
@@ -265,34 +263,33 @@ def update_metadata_xml(
         Analyst needs to replace the values where needed.
     """
     
-    # Read in original XML as JSON
+    # (1) Read in original XML as JSON
+    xml_file = Path(metadata_folder).joinpath(f"{dataset_name}.xml")
     esri_metadata = xml_to_json(xml_file)
     print("Read in XML as JSON")
     
-    # Apply template
+    # (2) Read in JSON for all the metadata and grab this dataset's dict
+    with open(metadata_path) as f:
+        all_metadata = json.load(f)
+    
+    dataset_info = all_metadata[dataset_name]
+    
+    # (3) Apply template and bring in dataset's spatial info
     metadata_templated = overwrite_default_with_dataset_elements(esri_metadata)
     print("Default template applied.")
-
-    # These rely on functions, so they can't be used in pydantic easily
-    dataset_info = validation_pro.fix_values_in_validated_dict(dataset_info) 
-    
-    # Validate the dict input with pydantic
-    DATASET_INFO_VALIDATED = metadata_input(**dataset_info).dict()
-    
-    # Overwrite the metadata with dictionary input
-    new_metadata = overwrite_metadata_json(metadata_templated, DATASET_INFO_VALIDATED)
+        
+    # (4) Overwrite other portions of metadata with dictionary input
+    new_metadata = overwrite_metadata_json(metadata_templated, dataset_info)
     print("Overwrite JSON using dict")
 
+    # (5) Convert new metadata dict and write back as xml
     new_xml = xmltodict.unparse(new_metadata, encoding='utf-8', pretty=True)
     print("Convert JSON back to XML")
     
-    # Overwrite existing XML file
-    OUTPUT_FOLDER = "run_in_esri/"
-    if not os.path.exists(f"./{metadata_folder}{OUTPUT_FOLDER}"):
-        os.makedirs(f"./{metadata_folder}{OUTPUT_FOLDER}")
-    
-    FILE = f"{xml_file.split(metadata_folder)[1]}"
-        
-    with open(f"./{metadata_folder}{OUTPUT_FOLDER}{FILE}", 'w') as f:
+    # (6) Save new XML file in a sub-directory
+    OUTPUT_FOLDER = metadata_folder.joinpath(Path("run_in_esri"))
+    Path.mkdir(OUTPUT_FOLDER, exist_ok = True)
+            
+    with open(OUTPUT_FOLDER.joinpath(f"{dataset_name}.xml"), 'w') as f:
         f.write(new_xml)
     print("Save over existing XML")
--- open_data/update_vars.py
+++ open_data/update_vars.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from shared_utils import rt_dates
 
 analysis_date = rt_dates.DATES["apr2023c"]
@@ -9,5 +10,6 @@ HQTA_GCS = f"{GCS_FILE_PATH}high_quality_transit_areas/"
 SEGMENT_GCS = f"{GCS_FILE_PATH}rt_segment_speeds/"
 
 ESRI_BASE_URL = "https://gisdata.dot.ca.gov/arcgis/rest/services/CHrailroad/"
-DEFAULT_XML_TEMPLATE = "default_pro.xml"
-XML_FOLDER = "xml/"
+DEFAULT_XML_TEMPLATE = Path("default_pro.xml")
+XML_FOLDER = Path("xml")
+META_JSON = Path("metadata.json")
