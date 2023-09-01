2d15593c88d555be78ee4124ddd2a6866ca1b67a
amandaha8 <amandahastuyck@gmail.com>
2023-09-01 22:16:48 +0000

    upload lp2000 work, clean up lrtp

--- project_list/_specific_list_utils.py
+++ project_list/_specific_list_utils.py
@@ -3,6 +3,7 @@ These functions are to help search through lists
 for specific types of projects
 """
 import pandas as pd
+import geopandas as gpd
 from calitp_data_analysis.sql import to_snakecase
 import _harmonization_utils as harmonization_utils
 
@@ -40,3 +41,51 @@ def find_keywords(df, columns_to_search: list, keywords_search: list):
 
     return df2
 
+def filter_projects(
+    df,
+    columns_to_search: list,
+    keywords_search: list,
+    file_name: str,
+    save_to_gcs: bool = True,
+):
+
+    # Filter out for Cordon
+    df = find_keywords(df, columns_to_search, keywords_search)
+    df2 = (
+        df[
+            (df.lower_case_project_title_keyword_search != "keyword not found")
+            | (df.lower_case_project_description_keyword_search != "keyword not found")
+        ]
+    ).reset_index(drop=True)
+
+    # Delete out non HOV projects that were accidentally picked up
+    projects_to_delete = [
+        "SR 17 Corridor Congestion Relief in Los Gatos",
+        "Interstate 380 Congestion Improvements",
+    ]
+    df2 = df2[~df2.project_title.isin(projects_to_delete)].reset_index(drop=True)
+
+    # Change cases
+    for i in ["project_title", "project_description"]:
+        df2[i] = df2[i].str.title()
+
+    # Drop invalid geometries
+    gdf = df2[df2.geometry != None].reset_index(drop=True)
+    gdf = gdf.set_geometry("geometry")
+    gdf.geometry = gdf.geometry.set_crs("EPSG:4326")
+    gdf = gdf[gdf.geometry.is_valid].reset_index(drop=True)
+    gdf = gdf.fillna(gdf.dtypes.replace({"float64": 0.0, "object": "None"}))
+
+    # One version that's a df
+    columns_to_drop = ["lower_case_project_title", "lower_case_project_description"]
+    df2 = df2.drop(columns=columns_to_drop + ["geometry"])
+    df2 = df2.fillna(df.dtypes.replace({"float64": 0.0, "object": "None"}))
+
+    if save_to_gcs:
+        df2.to_excel(
+            f"{harmonization_utils.GCS_FILE_PATH}LRTP/{file_name}.xlsx",
+            index=False,
+        )
+        gdf.to_file(f"./{file_name}.geojson", driver="GeoJSON")
+
+    return gdf, df2
