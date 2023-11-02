6f475912657b675d8a5d125c8d14b5291fa91eca
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-11-02 18:03:45 +0000

    run q4 metric

--- quarterly_performance_objective/update_vars.py
+++ quarterly_performance_objective/update_vars.py
@@ -3,6 +3,6 @@ from bus_service_utils import utils
 
 BUS_SERVICE_GCS = f"{utils.GCS_FILE_PATH}"
 COMPILED_CACHED_GCS = f"{rt_utils.GCS_FILE_PATH}compiled_cached_views/"
-CURRENT_QUARTER = "Q3_2023"
+CURRENT_QUARTER = "Q4_2023"
 ANALYSIS_DATE = rt_dates.PMAC[CURRENT_QUARTER] 
 VERSION = "v2" # warehouse version (post migration, we are on v2)
