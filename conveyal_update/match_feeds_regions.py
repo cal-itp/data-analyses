240a1f8de8b58e5a8291c371801e3fcc1dd5a2e0
Eric Dasmalchi <edasmalchi@gmail.com>
2023-11-06 19:52:53 +0000

    keep working on scripts

---
+++ conveyal_update/match_feeds_regions.py
@@ -0,0 +1,38 @@
+import os
+os.environ['USE_PYGEOS'] = '0'
+from shared_utils import gtfs_utils_v2
+
+import pandas as pd
+import geopandas as gpd
+import shapely
+
+import conveyal_vars
+
+regions = conveyal_vars.conveyal_regions
+target_date = conveyal_vars.target_date
+feeds_on_target = pd.read_parquet(f'{conveyal_vars.gcs_path}feeds_{target_date.isoformat()}.parquet')
+
+def create_region_gdf():
+    # https://shapely.readthedocs.io/en/stable/reference/shapely.box.html#shapely.box
+    # xmin, ymin, xmax, ymax
+    to_bbox = lambda x: [x['west'], x['south'], x['east'], x['north']]
+    df = pd.DataFrame(regions).transpose().reset_index().rename(columns={'index':'region'})
+    df['bbox'] = df.apply(to_bbox, axis=1)
+    df['geometry'] = df.apply(lambda x: shapely.geometry.box(*x.bbox), axis = 1)
+    df = df >> select(-_.bbox)
+    region_gdf = gpd.GeoDataFrame(df, crs=WGS84).to_crs(CA_NAD83Albers)
+    return region_gdf
+
+def join_stops_regions(region_gdf: gpd.GeoDataFrame, feeds_on_target: pd.DataFrame):
+    all_stops = gtfs_utils_v2.get_stops(selected_date=target_date, operator_feeds=feeds_on_target.feed_key).to_crs(CA_NAD83Albers)
+    region_join = gpd.sjoin(region_gdf, all_stops)
+    regions_and_feeds = region_join >> distinct(_.region, _.feed_key)
+    return regions_and_feeds
+
+if __name__ == '__main__':
+    
+    region_gdf = create_region_gdf()
+    regions_and_feeds = join_stops_regions(region_gdf, feeds_on_target)
+    regions_and_feeds = regions_and_feeds >> inner_join(_, feeds_on_target >> select(_.feed_key, _.gtfs_dataset_name, _.base64_url,
+                                                                                _.date), on = 'feed_key')
+    regions_and_feeds.to_parquet(f'{conveyal_vars.gcs_path}regions_feeds_{target_date.isoformat()}.parquet')
