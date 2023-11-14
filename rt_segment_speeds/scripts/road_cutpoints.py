6348d2e5e733f57d644d04ee461162ac2957da19
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-11-14 16:15:28 +0000

    add long and wide road cutpoints script

---
+++ rt_segment_speeds/scripts/road_cutpoints.py
@@ -0,0 +1,73 @@
+"""
+Some prep work related to where road segments 
+begin and end...this is analogous to transit
+"stops" along a shape.
+"""
+import geopandas as gpd
+import pandas as pd
+
+import cut_road_segments
+from segment_speed_utils.project_vars import SEGMENT_GCS, analysis_date
+from calitp_data_analysis import utils
+
+if __name__ == "__main__":
+    
+    road_id_cols = ["linearid", "mtfcc"]
+    segment_identifier_cols = road_id_cols + ["segment_sequence"]
+    
+    road_segments = gpd.read_parquet(
+        f"{SEGMENT_GCS}road_segments_{analysis_date}",
+        filters = [[("mtfcc", "in", ["S1100", "S1200"])]],
+        columns = segment_identifier_cols + ["destination"],
+    )
+    
+    full_roads = cut_road_segments.load_roads(
+        filtering = [("MTFCC", "in", ["S1100", "S1200"])]
+    ).drop(columns = "road_length")
+
+    # Merge cut road segments with full road geometry
+    gdf = pd.merge(
+        full_roads,
+        road_segments,
+        on = road_id_cols,
+        how = "inner"
+    )
+    
+    # Project each segment destination against full road geometry
+    # note: we didn't add road's start at 0
+    gdf = gdf.assign(
+        road_meters = gdf.geometry.project(gdf.destination)
+    ).drop(columns = ["destination"])
+    
+    utils.geoparquet_gcs_export(
+        gdf,
+        f"{SEGMENT_GCS}segments_staging/",
+        f"roads_with_cutpoints_long_{analysis_date}"
+    )
+    
+    # For each linearid, create an array tracking the cutpoints where
+    # segments are created, which can act as "stops"
+    road_info = (gdf.groupby(road_id_cols,
+                 observed=True, group_keys=False)
+        .agg({
+            "segment_sequence": lambda x: list(x),
+            "road_meters": lambda x: list(x),
+        }).reset_index()
+        .rename(columns = {
+            "segment_sequence": "segment_sequence_arr",
+            "road_meters": "road_meters_arr",
+        })
+    )
+    
+    road_info_with_geom = pd.merge(
+        full_roads[road_id_cols + ["geometry"]],
+        road_info,
+        on = road_id_cols,
+        how = "inner"
+    )
+
+    utils.geoparquet_gcs_export(
+        road_info_with_geom,
+        f"{SEGMENT_GCS}segments_staging/",
+        f"roads_with_cutpoints_wide_{analysis_date}"
+    )
