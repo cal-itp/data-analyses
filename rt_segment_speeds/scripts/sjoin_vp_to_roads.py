a79572bf1c0bc712a21b3b78cfc843aa80bdca5a
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-11-22 18:33:00 +0000

    get road meters with cumsum

--- rt_segment_speeds/scripts/sjoin_vp_to_roads.py
+++ rt_segment_speeds/scripts/sjoin_vp_to_roads.py
@@ -1,11 +1,12 @@
 import dask.dataframe as dd
+import dask_geopandas as dg
 import datetime
 import geopandas as gpd
 import pandas as pd
 
 from segment_speed_utils.project_vars import (SEGMENT_GCS, analysis_date,
                                               PROJECT_CRS)
-from segment_speed_utils import wrangle_shapes
+from segment_speed_utils import helpers, wrangle_shapes
 
 
 def sjoin_vp_to_roads(
@@ -16,13 +17,15 @@ def sjoin_vp_to_roads(
     
     vp_gdf = wrangle_shapes.vp_as_gdf(vp)
     
-    roads = roads.assign(
-        geometry = roads.geometry.buffer(25)
+    roads2 = roads.dissolve(road_grouping_cols).reset_index()
+    
+    roads2 = roads2.assign(
+        geometry = roads2.geometry.buffer(25)
     )
         
     results = gpd.sjoin(
         vp_gdf,
-        roads,
+        roads2,
         how = "inner",
         predicate = "within"
     )[["vp_idx", "trip_instance_key"] + 
@@ -50,31 +53,57 @@ def make_wide(
     return results_wide
 
 
-def import_data(
-    analysis_date: str, 
-    direction: str,
-    segment_identifier_cols: list
-) -> tuple[dd.DataFrame, gpd.GeoDataFrame]:
-    
-    opposite_direction = wrangle_shapes.OPPOSITE_DIRECTIONS[direction]
-    
+def import_vp(analysis_date: str):
     vp = dd.read_parquet(
         f"{SEGMENT_GCS}vp_usable_{analysis_date}",
-        columns = ["vp_idx", "trip_instance_key", "x", "y"],
-        filters = [[("vp_primary_direction", "==", direction)]]
+        columns = ["vp_idx", "trip_instance_key", "x", "y", 
+                   "vp_primary_direction"],
     )
     
-    vp = vp.repartition(npartitions=10)
+    rt_trips = vp.trip_instance_key.unique().compute().tolist()
     
-    road_segments = gpd.read_parquet(
+    trip_to_shape = helpers.import_scheduled_trips(
+        analysis_date,
+        columns = ["trip_instance_key", "shape_array_key"],
+        filters = [[("trip_instance_key", "in", rt_trips)]],
+        get_pandas = True
+    )
+    
+    vp2 = dd.merge(
+        vp,
+        trip_to_shape,
+        on = "trip_instance_key",
+        how = "inner"
+    )
+    
+    return vp2
+
+
+def import_roads(
+    analysis_date: str, 
+    vp: dd.DataFrame,
+    road_grouping_cols: list,
+) -> gpd.GeoDataFrame:
+    
+    keep_shapes = vp.shape_array_key.unique().compute().tolist()
+    
+    # Import shapes to roads crosswalk
+    keep_roads = pd.read_parquet(
+        f"{SEGMENT_GCS}shape_road_crosswalk_{analysis_date}.parquet",
+        filters = [[("shape_array_key", "in", keep_shapes)]]
+    ).linearid.unique().tolist()
+        
+    road_segments = dg.read_parquet(
         f"{SEGMENT_GCS}road_segments_{analysis_date}",
         filters = [[
-            ("mtfcc", "in", ["S1100", "S1200"]), 
-            ("primary_direction", "!=", opposite_direction)]],
-        columns =  segment_identifier_cols + ["geometry"]
+            ("linearid", "in", keep_roads),
+            #("mtfcc", "in", ["S1100", "S1200"]), 
+        ]],
+        columns =  road_grouping_cols + ["primary_direction", 
+                                         "geometry"]
     )
-    
-    return vp, road_segments
+              
+    return road_segments
 
 
 if __name__ == "__main__":
@@ -82,20 +111,26 @@ if __name__ == "__main__":
     start = datetime.datetime.now()
     
     road_cols = ["linearid", "mtfcc"]
-    segment_identifier_cols = road_cols + ["segment_sequence", 
-                                           "primary_direction"]
 
-    all_directions = wrangle_shapes.ALL_DIRECTIONS
-    
-    dfs = [
-        import_data(
-            analysis_date, d, segment_identifier_cols)
+    all_directions = wrangle_shapes.ALL_DIRECTIONS + ["Unknown"]
+
+    vp = import_vp(analysis_date).persist()
+    roads = import_roads(analysis_date, vp, road_cols).persist()
+                
+    vp_dfs = [
+        vp[vp.vp_primary_direction == d].repartition(npartitions=20)
         for d in all_directions
     ]
     
-
-    vp_cols_dtypes = dfs[0][0][["vp_idx", "trip_instance_key"]].dtypes.to_dict()
-    road_cols_dtypes = dfs[0][1][road_cols].dtypes.to_dict()
+    road_dfs = [
+        roads[roads.primary_direction != 
+              wrangle_shapes.OPPOSITE_DIRECTIONS[d]
+             ].repartition(npartitions=40) 
+        for d in all_directions
+    ]
+    
+    vp_cols_dtypes = vp[["vp_idx", "trip_instance_key"]].dtypes.to_dict()
+    road_cols_dtypes = roads[road_cols].dtypes.to_dict()
     
     results = [
         vp_subset.map_partitions(
@@ -107,23 +142,23 @@ if __name__ == "__main__":
                 **road_cols_dtypes
             },
             align_dataframes = False
-        ) for vp_subset, road_subset in dfs
+        ) for vp_subset, road_subset in zip(vp_dfs, road_dfs)
     ]
     print("map partitions")
     
     full_results = dd.multi.concat(results, axis=0).reset_index(drop=True)
-    full_results = full_results.repartition(npartitions=10).persist()
+    full_results = full_results.repartition(npartitions=3).persist()
     
     time1 = datetime.datetime.now()
     print(f"map partitions and persist full results: {time1 - start}")
     
     full_results.to_parquet(
-        f"{SEGMENT_GCS}vp_sjoin/vp_road_segments_{analysis_date}_seg",
+        f"{SEGMENT_GCS}vp_sjoin/vp_road_segments_{analysis_date}",
         overwrite=True
     )
     
     full_results = dd.read_parquet(
-        f"{SEGMENT_GCS}vp_sjoin/vp_road_segments_{analysis_date}_seg"
+        f"{SEGMENT_GCS}vp_sjoin/vp_road_segments_{analysis_date}"
     )
         
     results_wide = full_results.map_partitions(
