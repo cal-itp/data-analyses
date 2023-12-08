58cfc84aec4b1c2df8c3e1d619b3bfb04e1363f1
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-12-08 19:08:58 +0000

    combine apr, oct and save to public bucket

--- rt_segment_speeds/scripts/multiday_avg.py
+++ rt_segment_speeds/scripts/multiday_avg.py
@@ -1,3 +1,8 @@
+"""
+Calculate speed columns (p20, p50, p80) across
+multiple days worth of 
+speeds by stop segments parquets.
+"""
 import datetime
 import pandas as pd
 import sys
@@ -7,6 +12,7 @@ from loguru import logger
 
 from segment_speed_utils import helpers, gtfs_schedule_wrangling, sched_rt_utils
 from segment_speed_utils.project_vars import SEGMENT_GCS, CONFIG_PATH
+from shared_utils import schedule_rt_utils
 from avg_speeds_by_segment import calculate_avg_speeds
 
 
@@ -18,7 +24,7 @@ def crosswalk_shape_key_to_shape_id(
     so that we can aggregate across days (in case shape_array_key 
     changes, but shape_id doesn't).
     """
-    crosswalk = pd.concat([
+    shape_crosswalk = pd.concat([
         helpers.import_scheduled_trips(
             d,
             columns = ["gtfs_dataset_key", 
@@ -27,8 +33,26 @@ def crosswalk_shape_key_to_shape_id(
         ) for d in date_list], 
         axis=0, ignore_index=True
     ).drop_duplicates().reset_index(drop=True)
+        
+    crosswalk = schedule_rt_utils.sample_gtfs_dataset_key_to_organization_crosswalk(
+        shape_crosswalk.rename(columns = {"schedule_gtfs_dataset_key": "gtfs_dataset_key"}),
+        date_list[0],
+        quartet_data = "schedule",
+        dim_gtfs_dataset_cols = [
+            "key",
+            "base64_url",
+        ],
+        dim_organization_cols = ["source_record_id", "name"]
+    )
+
+    shape_crosswalk_with_org = pd.merge(
+        shape_crosswalk,
+        crosswalk,
+        on = "schedule_gtfs_dataset_key",
+        how = "inner"
+    )
     
-    return crosswalk
+    return shape_crosswalk_with_org
 
 
 def trip_crosswalk_for_daytype_and_peak_category(
@@ -40,14 +64,10 @@ def trip_crosswalk_for_daytype_and_peak_category(
     """
     df = pd.concat([
         sched_rt_utils.get_trip_time_buckets(d).assign(
-            service_date = pd.to_datetime(d)
+            day_name = pd.to_datetime(d).day_name()
         ) for d in date_list],
         axis=0, ignore_index=True
     ).pipe(gtfs_schedule_wrangling.add_peak_offpeak_column)
-
-    df = df.assign(
-        day_name = df.service_date.dt.day_name()
-    )
     
     df = df.assign(
         weekday_weekend = df.apply(
@@ -60,8 +80,9 @@ def trip_crosswalk_for_daytype_and_peak_category(
     return df[keep_cols].drop_duplicates().reset_index(drop=True)
 
 
-def concatenate_speeds(date_list: list):
+def concatenate_speeds(date_list: list) -> pd.DataFrame:
     """
+    Concatenate speeds by stop segments across multiple days.
     """
     dfs = [
         pd.read_parquet(
@@ -88,20 +109,30 @@ def concatenate_speeds(date_list: list):
     )
     
     return df_concat
+    
 
-
-def check_time_span(date_list: list): 
-    time_span = list(set(
+def time_span_labeling(date_list: list) -> tuple[str]: 
+    """
+    If we grab a week's worth of trips, we'll
+    use this week's average to stand-in for the entire month.
+    Label with month and year.
+    """
+    time_span_str = list(set(
         [datetime.datetime.strptime(d, "%Y-%m-%d").strftime("%b%Y").lower() 
          for d in date_list]
     ))
     
-    if len(time_span) == 1:
-        return time_span[0]
+    time_span_num = list(set(
+        [datetime.datetime.strptime(d, "%Y-%m-%d").strftime("%m_%Y").lower() 
+         for d in date_list]
+    ))    
+    
+    if len(time_span_str) == 1:
+        return time_span_str[0], time_span_num[0]
 
     else:
-        print(f"multiple months: {time_span}")
-        return time_span
+        print(f"multiple months: {time_span_str}")
+        return time_span_str, time_span_num
 
 
 if __name__ == "__main__":
@@ -123,7 +154,9 @@ if __name__ == "__main__":
     df = delayed(concatenate_speeds)(analysis_date_list)
 
     group_cols = [
-        "schedule_gtfs_dataset_key", "shape_id", 
+        "schedule_gtfs_dataset_key", 
+        "base64_url", "organization_source_record_id", "organization_name",         
+        "shape_id", 
         "stop_sequence", "stop_id", 
         "peak_offpeak", "weekday_weekend"
     ]
@@ -134,11 +167,24 @@ if __name__ == "__main__":
     )
     
     results = compute(daytype_peak_speeds)[0]
+    print(f"compute average: {datetime.datetime.now() - start}")
     
-    time_span = check_time_span(analysis_date_list)
+    time_span_str, time_span_num = time_span_labeling(analysis_date_list)
+    month = int(time_span_num.split('_')[0])
+    year = int(time_span_num.split('_')[1])
+    
+    # Downgrade some dtypes for public bucket
+    results = results.assign(
+        month = month, 
+        year = year,   
+    ).astype({
+        "month": "int16", 
+        "year": "int16",
+        "n_trips": "int16"
+    })
     
     results.to_parquet(
-        f"{SEGMENT_GCS}rollup/{EXPORT_FILE}_{time_span}.parquet"
+        f"{SEGMENT_GCS}rollup/{EXPORT_FILE}_{time_span_str}.parquet"
     )
     
     end = datetime.datetime.now()
