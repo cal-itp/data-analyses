510dba6cd9e457ceb30bc1a5931d490d4d782765
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-12-14 18:07:55 +0000

    run gtfs_funnel for dec

--- gtfs_funnel/update_vars.py
+++ gtfs_funnel/update_vars.py
@@ -6,7 +6,7 @@ months = ["nov", "oct", "sep", "aug",
          "jul", "jun", "may", "apr", "mar"]         
 
 analysis_date_list = [
-    rt_dates.DATES[f"{m}2023"] for m in months
+    rt_dates.DATES["dec2023"] 
 ]
 
 CONFIG_PATH = Path("config.yml")
