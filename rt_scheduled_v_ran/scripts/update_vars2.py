b30eb1a13a964ba64b95d2f467b92f79e92353a3
amandaha8 <amandahastuyck@gmail.com>
2023-12-18 19:48:55 +0000

    fixed log times, ran for june to aug

--- rt_scheduled_v_ran/scripts/update_vars2.py
+++ rt_scheduled_v_ran/scripts/update_vars2.py
@@ -1,7 +1,10 @@
 from shared_utils import rt_dates
 
-months = ["nov", "oct", "sep", "aug",
-        "jul", "jun", "may", "apr", "mar"]         
+#months = ["nov", "oct", "sep", "aug",
+#        "jul", "jun", "may", "apr", "mar"]         
+#
+
+months = ["aug","jul","jun"]
 
 analysis_date_list = [
     rt_dates.DATES[f"{m}2023"] for m in months
