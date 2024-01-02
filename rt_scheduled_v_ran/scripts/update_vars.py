5fc142b89cfabc20a6440574f352c2f99a0974a9
amandaha8 <amandahastuyck@gmail.com>
2024-01-02 22:54:52 +0000

    ran dec and troubleshoot why routes results are so different

--- rt_scheduled_v_ran/scripts/update_vars.py
+++ rt_scheduled_v_ran/scripts/update_vars.py
@@ -1,7 +1,9 @@
 from shared_utils import rt_dates
 
-months = ["dec","nov", "oct", "sep", "aug",
-        "jul", "jun", "may", "apr", "mar"]         
+months = ["dec"]
+
+#months = ["dec","nov", "oct", "sep", "aug",
+#        "jul", "jun", "may", "apr", "mar"]         
 
 analysis_date_list = [
     rt_dates.DATES[f"{m}2023"] for m in months
