a5cec6e33e21c5949766c70b90b581652060079a
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-01-13 06:03:46 +0000

    (segment_speed_utils): refactor, add time_helpers, move sched_rt_utils into other spots

--- rt_segment_speeds/segment_speed_utils/segment_calcs.py
+++ rt_segment_speeds/segment_speed_utils/segment_calcs.py
@@ -8,6 +8,17 @@ from typing import Union
 
 from shared_utils import rt_utils
 
+def speed_from_meters_elapsed_sec_elapsed(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Convert total meters elapsed and seconds elapsed 
+    into miles per hour.
+    """
+    df = df.assign(
+        speed_mph = (df.meters_elapsed.divide(df.sec_elapsed) * 
+                     rt_utils.MPH_PER_MPS)
+    )
+    return df
+
 
 def derive_speed(
     df: pd.DataFrame, 
@@ -37,12 +48,42 @@ def derive_speed(
                            np.timedelta64(1, 's')).abs(),
         )
     
-    df = df.assign(
-        speed_mph = (df.meters_elapsed.divide(df.sec_elapsed) * 
-                     rt_utils.MPH_PER_MPS)
-    )
+    df = speed_from_meters_elapsed_sec_elapsed(df)
     
     return df
+
+
+def calculate_avg_speeds(
+    df: pd.DataFrame,
+    group_cols: list
+) -> pd.DataFrame:
+    """
+    Calculate the median, 20th, and 80th percentile speeds 
+    by groups.
+    """
+    # pd.groupby and pd.quantile is so slow
+    # create our own list of speeds and use np
+    df2 = (df.groupby(group_cols, 
+                      observed=True, group_keys=False)
+           .agg({"speed_mph": lambda x: sorted(list(x))})
+           .reset_index()
+           .rename(columns = {"speed_mph": "speed_mph_list"})
+    )
+                        
+    df2 = df2.assign(
+        p50_mph = df2.apply(lambda x: np.percentile(x.speed_mph_list, 0.5), axis=1),
+        n_trips = df2.apply(lambda x: len(x.speed_mph_list), axis=1).astype("int16"),
+        p20_mph = df2.apply(lambda x: np.percentile(x.speed_mph_list, 0.2), axis=1),
+        p80_mph = df2.apply(lambda x: np.percentile(x.speed_mph_list, 0.8), axis=1),
+    )
+    
+    stats = df2.drop(columns = "speed_mph_list")
+    
+    # Clean up for map
+    speed_cols = [c for c in stats.columns if "_mph" in c]
+    stats[speed_cols] = stats[speed_cols].round(2)
+    
+    return stats
 
                                    
 def convert_timestamp_to_seconds(
