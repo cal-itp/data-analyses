c4a26132eb755222c5defbf2b7694ceb324b4c95
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-01-13 06:04:59 +0000

    clearer logic for averaging for single and multi

--- rt_segment_speeds/scripts/stop_arrivals_to_speed.py
+++ rt_segment_speeds/scripts/stop_arrivals_to_speed.py
@@ -7,8 +7,67 @@ import sys
 
 from loguru import logger
 
-from segment_speed_utils import helpers, segment_calcs
+from segment_speed_utils import helpers, gtfs_schedule_wrangling, segment_calcs
 from segment_speed_utils.project_vars import SEGMENT_GCS, CONFIG_PATH
+
+def attach_operator_natural_identifiers(
+    df: pd.DataFrame, 
+    analysis_date: str
+) -> pd.DataFrame:
+    """
+    For each gtfs_dataset_key-shape_array_key combination,
+    re-attach the natural identifiers and organizational identifiers.
+    Return a df with all the identifiers we need during downstream 
+    aggregations, such as by route-direction.
+    """
+    # Get shape_id back
+    shape_identifiers = helpers.import_scheduled_trips(
+        analysis_date,
+        columns = ["gtfs_dataset_key", 
+                   "shape_array_key", "shape_id", 
+                   "route_id", "direction_id"],
+        get_pandas = True
+    )
+    
+    # Get crosswalk from schedule_gtfs_dataset_key to organization
+    crosswalk = helpers.import_schedule_gtfs_key_organization_crosswalk(
+        analysis_date,
+    ).drop(columns = "itp_id")
+    
+    # Add time-of-day, which is associated with trip_instance_key
+    time_of_day = gtfs_schedule_wrangling.get_trip_time_buckets(analysis_date)
+    
+    # Add shape_stop_pair
+    # Since all trips for a shape must adhere to a single trip's cutpoints,
+    # the stop_pair is not associated with the trip, but the shape
+    # to allow for easier aggregation
+    shape_stop_pairs = pd.read_parquet(
+        f"{SEGMENT_GCS}stops_projected_{analysis_date}.parquet",
+        columns = ["shape_array_key", "stop_sequence", 
+                   "stop_id", "shape_stop_pair"]
+    )
+    
+    df_with_natural_ids = pd.merge(
+        df,
+        shape_identifiers,
+        on = "shape_array_key",
+        how = "inner"
+    ).merge(
+        shape_stop_pairs,
+        on = ["shape_array_key", "stop_sequence", "stop_id"],
+        how = "inner"
+    ).merge(
+        time_of_day,
+        on = "trip_instance_key",
+        how = "inner"
+    ).merge(
+        crosswalk,
+        on = "schedule_gtfs_dataset_key",
+        how = "left"
+    )
+    
+    return df_with_natural_ids
+
 
 def calculate_speed_from_stop_arrivals(
     analysis_date: str, 
@@ -50,8 +109,11 @@ def calculate_speed_from_stop_arrivals(
         segment_calcs.derive_speed, 
         ("prior_stop_meters", "stop_meters"), 
         ("prior_arrival_time_sec", "arrival_time_sec")
+    ).pipe(
+        attach_operator_natural_identifiers, 
+        analysis_date
     )
-    
+        
     speed.to_parquet(
         f"{SEGMENT_GCS}{SPEED_FILE}.parquet")
     
