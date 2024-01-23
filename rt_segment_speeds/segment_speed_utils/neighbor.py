07c2a5e9ba9e690df31924df8e17b1cc7e6529b1
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-01-23 20:05:39 +0000

    grab vp_idx and timestamp boundaries

--- rt_segment_speeds/segment_speed_utils/neighbor.py
+++ rt_segment_speeds/segment_speed_utils/neighbor.py
@@ -32,10 +32,56 @@ def add_nearest_vp_idx(
     stop: shapely.Point, 
     vp_idx_arr: np.ndarray
 ) -> int:
-    
+    """
+    Index into where the nearest vp is to the stop,
+    and return that vp_idx value from the vp_idx array.
+    """
     idx = nearest_snap(vp_linestring, stop)
     
     return vp_idx_arr[idx]
+
+
+def add_trio(
+    nearest_value: int,
+    vp_idx_arr: np.ndarray,
+    timestamp_arr: np.ndarray,
+):
+    """
+    Try to grab at least 2 vp_idx, but optimally 3 vp_idx,
+    including the nearest_vp_idx to 
+    act as boundaries to interpolate against.
+    3 points hopefully means 2 intervals where the stop could occur in
+    to give us successful interpolation result.
+    
+    We don't have a guarantee that the nearest_vp_idx is before
+    the stop, so let's just keep more boundary points.
+    """
+    start_idx = np.where(vp_idx_arr == nearest_value)[0][0]
+    
+    array_length = len(vp_idx_arr)
+
+    # if we only have 2 values, we want to return all the possibilities
+    if array_length <= 2:
+        return vp_idx_arr, timestamp_arr
+    
+    # if it's the first value of the array 
+    # and array is long enough for us to grab another value    
+    elif (start_idx == 0) and (array_length > 2):
+        start_pos = start_idx
+        end_pos = start_idx + 3
+        return vp_idx_arr[start_pos : end_pos], timestamp_arr[start_pos: end_pos]
+    
+    # if it's the last value in the array, still grab 3 vp_idx
+    elif start_idx == array_length - 1:
+        start_pos = start_idx-2
+        return vp_idx_arr[start_pos: ], timestamp_arr[start_pos: ]   
+    
+    # (start_idx > 0) and (array_length > 2):
+    else: 
+        start_pos = start_idx - 1
+        end_pos = start_idx + 2
+        return vp_idx_arr[start_pos: end_pos], timestamp_arr[start_pos: end_pos]
+    
 
 
 def merge_stop_vp_for_nearest_neighbor(
