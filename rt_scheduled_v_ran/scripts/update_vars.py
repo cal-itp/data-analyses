0b9a96c774f8e5fc6a8b8c663c724eb4d59513e4
amandaha8 <amandahastuyck@gmail.com>
2024-02-07 16:53:54 +0000

    updated update_vars, jotted down notes

--- rt_scheduled_v_ran/scripts/update_vars.py
+++ rt_scheduled_v_ran/scripts/update_vars.py
@@ -1,8 +1,6 @@
 from shared_utils import rt_dates
-
-months = ["nov", "oct", "sep", "aug",
-      "jul", "jun", "may", "apr", "mar"]         
-
+import yaml
+from pathlib import Path
 
 #months = ["dec","nov", "oct", "sep", "aug",
 #        "jul", "jun", "may", "apr", "mar"]         
@@ -10,3 +8,8 @@ months = ["nov", "oct", "sep", "aug",
 analysis_date_list = [
     rt_dates.DATES[f"{m}2023"] for m in months
 ]
+
+CONFIG_PATH = Path("config.yml")
+
+with open(CONFIG_PATH) as f: 
+    CONFIG_DICT = yaml.safe_load(f)  
