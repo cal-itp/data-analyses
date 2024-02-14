2341e1e62ce42bdaffad56e8fe046d1d1931ec35
amandaha8 <amandahastuyck@gmail.com>
2024-02-14 18:45:24 +0000

    added cleaning func for charts

--- rt_scheduled_v_ran/scripts/update_vars.py
+++ rt_scheduled_v_ran/scripts/update_vars.py
@@ -7,3 +7,8 @@ months = ["nov", "dec"]
 analysis_date_list = [
     rt_dates.DATES[f"{m}2023"] for m in months
 ]
+
+CONFIG_PATH = Path("config.yml")
+
+with open(CONFIG_PATH) as f: 
+    CONFIG_DICT = yaml.safe_load(f)  
