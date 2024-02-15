75779503285fdda5e96169ddc7bcbd79649f5421
amandaha8 <amandahastuyck@gmail.com>
2024-02-15 00:32:54 +0000

    set up config file, fixed metric func

--- rt_scheduled_v_ran/scripts/update_vars.py
+++ rt_scheduled_v_ran/scripts/update_vars.py
@@ -2,7 +2,7 @@ from shared_utils import rt_dates
 import yaml
 from pathlib import Path
 
-months = ["nov", "dec"]         
+months = ["oct"]         
 
 analysis_date_list = [
     rt_dates.DATES[f"{m}2023"] for m in months
