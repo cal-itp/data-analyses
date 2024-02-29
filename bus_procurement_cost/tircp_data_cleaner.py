90861d4cb092c08de4e2932d9da43052233915e1
csuyat-dot <christian.suyat@dot.ca.gov>
2024-02-29 19:24:58 +0000

    adjusted cuntions in dgs cleaner. compelte refactor on tircp cleaner

--- bus_procurement_cost/tircp_data_cleaner.py
+++ bus_procurement_cost/tircp_data_cleaner.py
@@ -1,161 +1,131 @@
-import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import shared_utils
+from calitp_data_analysis.sql import to_snakecase
+from fta_data_cleaner import gcs_path
+from dgs_data_cleaner import new_prop_finder
+from dgs_data_cleaner import new_bus_size_finder
 
-gcs_path = 'gs://calitp-analytics-data/data-analyses/bus_procurement_cost/'
-file_name = 'TIRCP Tracking Sheets 2_1-10-2024.xlsx'
-sheet_name = 'Project Tracking'
-
-def get_data(path, file, sheet):
-    '''
-    read in tircp data from project tracking tab
-    '''
-    df = pd.read_excel(path+file, sheet_name=sheet)
+def col_row_updater(df: pd.DataFrame, col1: str, val1, col2: str, new_val):
+    """
+    function used to update values at specificed columns and row value.
+    """
+    df.loc[df[col1] == val1, col2] = new_val
     
-    return df
+    return
 
-project = get_data(gcs_path, file_name, sheet_name)
+def clean_tircp_columns() -> pd.DataFrame:
+    """
+    main function that reads in and cleans TIRCP data.
+    """
+    file_name = "TIRCP Tracking Sheets 2_1-10-2024.xlsx"
+    tircp_name = "Project Tracking"
 
-# only keep first couple of columns
-project = project.iloc[:, :20]
+    # read in data
+    df = pd.read_excel(f"{gcs_path}{file_name}", sheet_name=tircp_name)
 
-# dropping specific columns
-drop_col = [
-    "Master Agreement Expiration Date",
-    "Project Manager",
-    "Regional Coordinator",
-    "Technical Assistance-CALITP (Y/N)",
-    "Technical Assistance-Fleet (Y/N)",
-    "Technical Assistance-Network Integration (Y/N)",
-    "Technical Assistance-Priority Population (Y/N)",
-    "Allocated Amount",
-]
+    # keep specific columns
+    keep_col = [
+        "Award Year",
+        "Project #",
+        "Grant Recipient",
+        "Project Title",
+        "PPNO",
+        "District",
+        "County",
+        "Project Description",
+        "bus_count",
+        "Master Agreement Number",
+        "Total Project Cost",
+        "TIRCP Award Amount ($)",
+    ]
 
-project.drop(columns=drop_col, inplace=True)
+    df1 = df[keep_col]
 
-# replace columns space with _ & lower everything
-project.columns = project.columns.str.replace(" ", "_")
-project.columns = project.columns.str.lower()
+    # snakecase
+    df2 = to_snakecase(df1)
 
-#clean `grant_recipient column
-agency_dict = {
-    "Antelope Valley Transit Authority ": "Antelope Valley Transit Authority (AVTA)",
-    "Humboldt Transit Authority": "Humboldt Transit Authority (HTA)",
-    "Orange County Transportation Authority": "Orange County Transportation Authority (OCTA)",
-    "Capitol Corridor Joint Powers Authority": "Capitol Corridor Joint Powers Authority (CCJPA)",
-    "Los Angeles County Metropolitan Transportation Authority": "Los Angeles County Metropolitan Transportation Authority (LA Metro)",
-    "Monterey-Salinas Transit": "Monterey-Salinas Transit District (MST)",
-    "Sacramento Regional Transit (SacRT)": "Sacramento Regional Transit District (SacRT)",
-    "Sacramento Regional Transit District": "Sacramento Regional Transit District (SacRT)",
-    "Sacramento Regional Transit District (SacRT) ": "Sacramento Regional Transit District (SacRT)",
-    "San Diego Association of Governments": "San Diego Association of Governments (SANDAG)",
-    "Santa Clara Valley Transportation Authority (SCVTA)": "Santa Clara Valley Transportation Authority (VTA)",
-    "Southern California  Regional Rail Authority (SCRRA)": "Southern California Regional Rail Authority (SCRRA - Metrolink)",
-    "Southern California Regional Rail Authority": "Southern California Regional Rail Authority (SCRRA - Metrolink)",
-}
+    # dict of replacement values
+    value_replace_dict = {
+        "Antelope Valley Transit Authority ": "Antelope Valley Transit Authority (AVTA)",
+        "Humboldt Transit Authority": "Humboldt Transit Authority (HTA)",
+        "Orange County Transportation Authority": "Orange County Transportation Authority (OCTA)",
+        "Capitol Corridor Joint Powers Authority": "Capitol Corridor Joint Powers Authority (CCJPA)",
+        "Los Angeles County Metropolitan Transportation Authority": "Los Angeles County Metropolitan Transportation Authority (LA Metro)",
+        "Monterey-Salinas Transit": "Monterey-Salinas Transit District (MST)",
+        "Sacramento Regional Transit (SacRT)": "Sacramento Regional Transit District (SacRT)",
+        "Sacramento Regional Transit District": "Sacramento Regional Transit District (SacRT)",
+        "Sacramento Regional Transit District (SacRT) ": "Sacramento Regional Transit District (SacRT)",
+        "San Diego Association of Governments": "San Diego Association of Governments (SANDAG)",
+        "Santa Clara Valley Transportation Authority (SCVTA)": "Santa Clara Valley Transportation Authority (VTA)",
+        "Southern California  Regional Rail Authority (SCRRA)": "Southern California Regional Rail Authority (SCRRA - Metrolink)",
+        "Southern California Regional Rail Authority": "Southern California Regional Rail Authority (SCRRA - Metrolink)",
+        "3, 4": "VAR",
+    }
+    
+    # replacing values in agency & county col
+    df3 = df2.replace(
+        {"grant_recipient": value_replace_dict}
+    ).replace(
+        {"county": value_replace_dict}
+    )
+    
+    # using update function to update values at specific columns and rows
+    col_row_updater(df3, 'ppno', 'CP106', 'bus_count', 42)
+    col_row_updater(df3, 'ppno', 'CP005', 'bus_count', 29)
+    col_row_updater(df3, 'ppno', 'CP028', 'bus_count', 12)
+    col_row_updater(df3, 'ppno', 'CP048', 'bus_count', 5)
+    col_row_updater(df3, 'ppno', 'CP096', 'bus_count', 6)
+    col_row_updater(df3, 'ppno', 'CP111', 'bus_count', 5)
+    col_row_updater(df3, 'ppno', 'CP130', 'bus_count', 7)
+    col_row_updater(df3, 'total_project_cost', 203651000, 'bus_count', 8)
+    
+    # columns to change dtype to str
+    dtype_update = [
+        'ppno',
+        'district'
+    ]
+    
+    df3[dtype_update] = df3[dtype_update].astype('str')
+    
+    # assigning new columns using imported functions.
+    df4 = df3.assign(
+        prop_type = df3['project_description'].apply(new_prop_finder),
+        bus_size_type = df3['project_description'].apply(new_bus_size_finder)
+    )
 
-project.replace({"grant_recipient": agency_dict}, inplace=True)
+    return df4
 
+def agg_buses_only(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    filters df to only include projects with bus procurement. 
+    does not include engineering, planning or construction only projects.
+    then, aggregates the df by agency name and ppno. Agencies may have multiple projects that procure different types of buses
+    """
+    df2 = df[df["bus_count"] > 0]
+    
+    df3 = (
+        df2.groupby(
+            [
+                "grant_recipient",
+                "ppno",
+                "prop_type",
+                "bus_size_type",
+            ]
+        )
+        .agg({"total_project_cost": "sum", "bus_count": "sum"})
+        .reset_index()
+    )
+    return df3
 
-# clean `county` column
-# change county value from '3, 4' to 'VAR' like the other rows.
-project.at[3, "county"] = "VAR"
-
-# updating bus count value for certain row 
-project.loc[project['ppno'] == 'CP106', 'bus_count'] = 42
-
-#filtering project to only have bus_count >0
-bus_only = project[project["bus_count"] > 0]
-
-#preparing for new `prop_type` column
-prop_type = [
-    "electric buses",
-    "electric commuter",
-    "Electric Buses",
-    "battery electric",
-    "Batery Electric",
-    "battery-electric",
-    "fuel-cell",
-    "fuel cell",
-    "Fuel Cell",
-    "zero emission",
-    "Zero Emission",
-    "zero-emission electric buses",
-    "zero-emission buses",
-    "zero‐emission",
-    "zero-emission",
-    "zeroemission",
-    "CNG",
-    "cng",
-]
-
-def prop_type_finder(description):
-    '''
-    function for finding prop type from project description column, and returning the keyword it matched on
-    '''
-    for keyword in prop_type:
-        if keyword in description:
-            return keyword
-    return "not specified"
-
-bus_only["prop_type"] = bus_only["project_description"].apply(prop_type_finder)
-
-#dictionary to consolidate/validate prop_types
-prop_dict = {
-    "battery electric": "BEB",
-    "battery-electric": "BEB",
-    "electric buses": "electric (not specified)",
-    "electric commuter": "electric (not specified)",
-    "fuel cell": "FCEB",
-    "fuel-cell": "FCEB",
-    "zero-emission buses": "zero-emission bus (not specified)",
-    "zero emission": "zero-emission bus (not specified)",
-    "zero-emission": "zero-emission bus (not specified)",
-    "zero‐emission": "zero-emission bus (not specified)",
-}
-
-bus_only.replace({"prop_type": prop_dict}, inplace=True)
-
-#prepare for new column `bus_size_type`
-bus_size = [
-    "standard",
-    "30-foot",
-    "40 foot",
-    "40-foot",
-    "45-foot",
-    "45 foot",
-    "40ft",
-    "60-foot",
-    "articulated",
-    "cutaway",
-    "coach-style",
-    "over-the-road",
-    "feeder bus",
-]
-
-# re writing prop type funct for bus size
-def bus_size_finder(description):
-    '''
-    similar to prop_type function. returns bus size keyword that matched in the project description col'''
-    for keyword in bus_size:
-        if keyword in description:
-            return keyword
-    return "not specified"
-
-bus_only["bus_size_type"] = bus_only["project_description"].apply(bus_size_finder)
-
-#dictionary to consolidate bus sizes
-
-size_dict={'40 foot': 'conventional (40-ft like)' ,
- '40-foot': 'conventional (40-ft like)',
- '45-foot': 'conventional (40-ft like)',
- 'coach-style':'over-the-road',
- 'feeder bus': 'conventional (40-ft like)',
- }
-
-bus_only.replace({"bus_size_type": size_dict}, inplace=True)
-
-#export as parquet
-bus_only.to_parquet(
-    "gs://calitp-analytics-data/data-analyses/bus_procurement_cost/tircp_project_bus_only.parquet"
-)
+if __name__ == "__main__":
+    
+    # initial df
+    df1 = clean_tircp_columns()
+    
+    # aggregate 
+    df2 = agg_buses_only(df1)
+    
+    # export both df's as parquets to GCS
+    df1.to_parquet(f'{gcs_path}clean_tircp_project.parquet')
+    df2.to_parquet(f'{gcs_path}clean_tircp_project_bus_only.parquet')
