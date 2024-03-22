fb24be8a23bd108e4877c119885fa4eb7c7f2b6d
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-03-22 21:01:57 +0000

    consolidate into 1 function

--- rt_segment_speeds/scripts/cut_road_segments.py
+++ rt_segment_speeds/scripts/cut_road_segments.py
@@ -1,19 +1,16 @@
 """
 Cut road segments.
 """
-import dask.dataframe as dd
-import dask_geopandas as dg
+#import dask.dataframe as dd
 import datetime
 import geopandas as gpd
 import pandas as pd
 import sys
 
-from dask import delayed
+from dask import delayed, compute
 from loguru import logger
-from typing import Literal
 
 from calitp_data_analysis.sql import to_snakecase
-from segment_speed_utils import helpers
 from segment_speed_utils.project_vars import (SHARED_GCS, 
                                               PROJECT_CRS, 
                                               ROAD_SEGMENT_METERS
@@ -24,7 +21,7 @@ from shared_utils import rt_utils
 """
 TIGER
 """
-def load_roads(filtering: tuple) -> gpd.GeoDataFrame:
+def load_roads(**kwargs) -> gpd.GeoDataFrame:
     """
     Load roads based on what you filter for MTFCC values (road types).
     Do some basic cleaning/dissolving.
@@ -43,8 +40,8 @@ def load_roads(filtering: tuple) -> gpd.GeoDataFrame:
     # https://stackoverflow.com/questions/56522977/using-predicates-to-filter-rows-from-pyarrow-parquet-parquetdataset
     df = gpd.read_parquet(
         f"{SHARED_GCS}all_roads_2020_state06.parquet",
-        filters = filtering,
         columns=["LINEARID", "MTFCC", "FULLNAME", "geometry"],
+        **kwargs,
     ).to_crs(PROJECT_CRS).pipe(to_snakecase)
     
     # If a road has mutliple rows but the same
@@ -95,10 +92,21 @@ def load_roads(filtering: tuple) -> gpd.GeoDataFrame:
     
     
 def cut_segments(
-    gdf: gpd.GeoDataFrame,
+    roads: gpd.GeoDataFrame,
     group_cols: list,
     segment_length_meters: int
 ) -> gpd.GeoDataFrame:
+    
+    short_roads = roads[
+        roads.road_length <= segment_length_meters
+    ].drop(columns = "road_length").assign(
+        segment_sequence = 0
+    ).astype({"segment_sequence": "int16"}).reset_index(drop=True)
+    
+    gdf = roads[
+        roads.road_length > segment_length_meters
+    ].drop(columns = "road_length").reset_index(drop=True)
+    
     gdf["segment_geometry"] = gdf.apply(
         lambda x: 
         geography_utils.create_segments(x.geometry, int(segment_length_meters)), 
@@ -109,78 +117,17 @@ def cut_segments(
         gdf,
         group_cols,
         segment_col = "segment_geometry",
-    )[group_cols + ["segment_sequence", "geometry"]
-     ].set_geometry("geometry").set_crs(PROJECT_CRS)
+    )[
+        group_cols + ["segment_sequence", "geometry"]
+    ].set_geometry("geometry").set_crs(PROJECT_CRS)
     
-    return gdf2 
-
-
-def cut_local_roads(
-    roads: gpd.GeoDataFrame, 
-    group_cols: list,
-    segment_length_meters: int
-) -> gpd.GeoDataFrame:
-    
-    # If roads are less than our segment length
-    # keep these intact...they are freebies, already segmented for us (1 segment)
-    short_roads = roads[
-        roads.road_length <= segment_length_meters
-    ].drop(columns = "road_length").reset_index(drop=True)
-
-    short_roads = short_roads.assign(
-        segment_sequence = 0
-    ).astype({"segment_sequence": "int16"})
-
-    long_roads = roads[
-        roads.road_length > segment_length_meters
-    ].drop(columns = "road_length").reset_index(drop=True)
-
-    
-    long_roads = long_roads.repartition(npartitions=50)
-    
-    time1 = datetime.datetime.now()
-
-    # Concatenate the short roads and the segmented roads
-    road_dtypes = long_roads[group_cols].dtypes.to_dict()
-    
-    segmented_long_roads = long_roads.map_partitions(
-        cut_segments,
-        group_cols,
-        segment_length_meters,
-        meta = {
-            **road_dtypes,
-            "segment_sequence": "int16",
-            "geometry": "geometry"
-        },
-        align_dataframes = False
-    ).persist()
-    
-    print("first persist")
-    print(segmented_long_roads.dtypes)
-        
-    gdf = dd.multi.concat(
-        [short_roads, segmented_long_roads],
-        axis=0
+    segmented = pd.concat(
+        [short_roads, gdf2], axis=0
     ).reset_index(drop=True)
     
-    gdf = gdf.repartition(npartitions=20)
+    segmented = gpd.GeoDataFrame(segmented, geometry="geometry", crs=PROJECT_CRS)
     
-    gdf_dtypes = gdf.dtypes.to_dict()
-    
-    gdf2 = gdf.map_partitions(
-        add_segment_direction,
-        meta = {
-            **gdf_dtypes, 
-            "origin": "geometry",
-            "destination": "geometry",
-            "primary_direction": "object"
-            }
-    ).persist()
-    
-    time2 = datetime.datetime.now()
-    print(f"map partitions to cut segments and add direction: {time2 - time1}")
-    
-    return gdf2
+    return segmented 
 
 
 def add_segment_direction(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
@@ -190,6 +137,7 @@ def add_segment_direction(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     one running on other side, we need to distinguish between
     these 2 rows with same linearid-mtfcc-fullname.
     """
+    df = df.dropna(subset="geometry")
     df = rt_utils.add_origin_destination(df)
     
     df = df.assign(
@@ -216,43 +164,21 @@ if __name__ == '__main__':
     ROAD_SEGMENT_METERS = 1_609*2
     road_cols = ["linearid", "mtfcc", "fullname"]
 
-    # Always cut all the primary and secondary roads
-    road_type = "primarysecondary"
-    road_type_values = ["S1100", "S1200"]
-
-    roads = load_roads(filtering = [("MTFCC", "in", road_type_values)])
-    primary_secondary_roads = cut_segments(
-        roads, road_cols, ROAD_SEGMENT_METERS)
-    primary_secondary_roads = add_segment_direction(primary_secondary_roads)
+    roads = delayed(load_roads)()
+    
+    road_segments = delayed(cut_segments)(
+        roads, 
+        ["linearid", "mtfcc", "fullname"], 
+        ROAD_SEGMENT_METERS
+    ).pipe(add_segment_direction)
+    
+    road_segments = compute(road_segments)[0]
     
     utils.geoparquet_gcs_export(
-        primary_secondary_roads,
+        road_segments,
         SHARED_GCS,
-        f"segmented_roads_twomile_2020_{road_type}"
-    )
-    
-    del roads, primary_secondary_roads
-    time1 = datetime.datetime.now()
-    logger.info(f"cut primary/secondary roads: {time1 - start}")
-        
-    road_type = "local"  
-    road_type_values = ["S1400"]
-    roads = delayed(load_roads)(filtering = [("MTFCC", "in", road_type_values)])
-    
-    local_gddf = dd.from_delayed(roads).repartition(npartitions=50)
-    
-    local_roads = cut_local_roads(
-        local_gddf, road_cols, ROAD_SEGMENT_METERS
-    ).compute()
-    
-    utils.geoparquet_gcs_export(
-        local_roads,
-        SHARED_GCS,
-        f"segmented_roads_twomile_2020_{road_type}"
-    )
-    
-    time2 = datetime.datetime.now()
-    logger.info(f"cut local roads: {time2 - time1}")
+        f"segmented_roads_twomile_2020"
+    )            
         
     end = datetime.datetime.now()
     logger.info(f"execution time: {end - start}")
