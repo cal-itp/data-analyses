c211625a16a141a524629d2b152fb317ac7ba8db
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-03-28 20:00:26 +0000

    summary speeds rerun for all dates, make sure new function works

--- rt_segment_speeds/scripts/average_summary_speeds.py
+++ rt_segment_speeds/scripts/average_summary_speeds.py
@@ -74,9 +74,6 @@ def single_day_summary_averages(analysis_date: str, dict_inputs: dict):
         [analysis_date]
     ).reset_index(drop=True)
     
-    #trip_avg = average_summary_speed(
-    #    df, trip_group_cols, [analysis_date])
-    
     trip_avg.to_parquet(
         f"{SEGMENT_GCS}{TRIP_FILE}_{analysis_date}.parquet"
     )
@@ -89,11 +86,6 @@ def single_day_summary_averages(analysis_date: str, dict_inputs: dict):
         (trip_avg.sec_elapsed >= MIN_TRIP_SECONDS) & 
         (trip_avg.sec_elapsed <= MAX_TRIP_SECONDS)
     ]
-
-    #route_dir_avg = average_summary_speed(
-    #    trip_avg_filtered, OPERATOR_COLS + ROUTE_DIR_COLS, 
-    #    [analysis_date]
-    #)
     
     route_dir_avg = metrics.concatenate_peak_offpeak_allday_averages(
         trip_avg_filtered,
@@ -172,9 +164,14 @@ def multi_day_summary_averages(analysis_date_list: list, dict_inputs: dict):
     )
     print("concatenated files") 
     
-    trip_avg = delayed(average_summary_speed)(
-        df, trip_group_cols + ["weekday_weekend"], analysis_date_list)
-    
+    trip_avg = delayed(metrics.weighted_average_speeds_across_segments)(
+        df,
+        trip_group_cols + ["peak_offpeak", "weekday_weekend"],
+    ).pipe(
+        gtfs_schedule_wrangling.merge_operator_identifiers, 
+        analysis_date_list
+    ).reset_index(drop=True)
+
     trip_avg_filtered = trip_avg[
         (trip_avg.meters_elapsed >= METERS_CUTOFF) & 
         (trip_avg.sec_elapsed >= MIN_TRIP_SECONDS) & 
@@ -183,12 +180,15 @@ def multi_day_summary_averages(analysis_date_list: list, dict_inputs: dict):
     
     time_span_str, time_span_num = time_helpers.time_span_labeling(
         analysis_date_list)
-        
-    route_dir_avg = delayed(average_summary_speed)(
-        trip_avg_filtered, 
-        OPERATOR_COLS + ROUTE_DIR_COLS + ["weekday_weekend"], 
+    
+    route_dir_avg = delayed(metrics.concatenate_peak_offpeak_allday_averages)(
+        trip_avg_filtered,
+        OPERATOR_COLS + ROUTE_DIR_COLS + ["weekday_weekend"],
+        metric_type = "summary_speeds"
+    ).pipe(
+        gtfs_schedule_wrangling.merge_operator_identifiers, 
         analysis_date_list
-    )
+    ).reset_index(drop=True)
     
     route_dir_avg = compute(route_dir_avg)[0]
     
@@ -236,6 +236,7 @@ if __name__ == "__main__":
     
     dict_inputs = helpers.get_parameters(CONFIG_PATH, "rt_stop_times")
     
+    
     for analysis_date in analysis_date_list:
         
         start = datetime.datetime.now()
@@ -244,7 +245,6 @@ if __name__ == "__main__":
         end = datetime.datetime.now()
         
         logger.info(f"average rollups for {analysis_date}: {end - start}")
-    
         
     '''
     for one_week in [rt_dates.oct_week, rt_dates.apr_week]:
