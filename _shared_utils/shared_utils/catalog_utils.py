e306da3322b14cfeb6dba1032a813bfea2b566bc
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-04-04 17:44:24 +0000

    add segment speed datasets, fix relative import

--- _shared_utils/shared_utils/catalog_utils.py
+++ _shared_utils/shared_utils/catalog_utils.py
@@ -7,13 +7,14 @@ from typing import Literal
 import intake
 from omegaconf import OmegaConf  # this is yaml parser
 
+repo_name = "data-analyses/"
 shared_utils_dir = "_shared_utils/shared_utils/"
 
 
 def get_catalog(catalog_name: Literal["shared_data_catalog", "gtfs_analytics_data"]) -> Path:
     """ """
-    catalog_file_path = Path(f"{shared_utils_dir}{catalog_name}.yml")
-    catalog_path = Path.cwd().parent.joinpath(catalog_file_path)
+    catalog_file_path = Path(f"{repo_name}{shared_utils_dir}{catalog_name}.yml")
+    catalog_path = Path.cwd().home().joinpath(catalog_file_path)
 
     if catalog_name == "gtfs_analytics_data":
         return OmegaConf.load(catalog_path)
