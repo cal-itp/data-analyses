0081b7bfcece917438f9ea8131c971268ab983e0
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-05-01 21:02:10 +0000

    also publish csv and geojson

--- gtfs_digest/publish_public_data.py
+++ gtfs_digest/publish_public_data.py
@@ -8,8 +8,11 @@ import pandas as pd
 from pathlib import Path
 from typing import Literal
 
-from update_vars import GTFS_DATA_DICT 
+from calitp_data_analysis import utils
 from shared_utils import publish_utils
+from update_vars import GTFS_DATA_DICT 
+  
+PUBLIC_GCS = GTFS_DATA_DICT.gcs_paths.PUBLIC_GCS
     
 def grab_filepaths(
     table_section: Literal["digest_tables", "schedule_tables"], 
@@ -28,20 +31,66 @@ def grab_filepaths(
     return [f"{GCS}{f}.parquet" for f in filepaths]
     
 
+def export_parquet_as_csv_or_geojson(
+    filename: str,
+    filetype: Literal["df", "gdf"],
+):
+    """
+    For parquets, we want to export as csv.
+    For geoparquets, we want to export as geojson.
+    """
+    if filetype=="df":
+        df = pd.read_parquet(filename)
+        df.to_csv(
+            f"{PUBLIC_GCS}gtfs_digest/"
+            f"{Path(filename).stem}.csv", index=False
+        )
+        
+        
+    elif filetype=="gdf":
+        df = gpd.read_parquet(filename)
+        utils.geojson_gcs_export(
+            df,
+            f"{PUBLIC_GCS}gtfs_digest/",
+            Path(filename).stem,
+            geojson_type = "geojson"
+        )
+        
+    return
+        
+        
+
 if __name__ == "__main__":
     
-    digest_keys = [
-        "route_schedule_vp", "route_segment_speeds", 
-        "operator_profiles", "operator_routes_map", 
+    digest_gdf_keys = [
+        "operator_routes_map"
+    ]
+    
+    digest_df_keys = [
+        "route_schedule_vp", 
+        "operator_profiles",  
         "operator_sched_rt"
     ]  
     
-    digest_filepaths = grab_filepaths("digest_tables", digest_keys)
-    schedule_filepaths = grab_filepaths(
-        "schedule_tables", ["monthly_scheduled_service"])
+    df_filepaths = (
+        grab_filepaths("digest_tables", digest_df_keys) + 
+        grab_filepaths("schedule_tables", ["monthly_scheduled_service"])
+    )
     
-    for f in digest_filepaths + schedule_filepaths:
+    gdf_filepaths = grab_filepaths("digest_tables", digest_gdf_keys)
+    
+    # copy our private files to public GCS
+    # for df ones, export as csv too
+    # for gdf ones, export as geojson
+    for f in df_filepaths + gdf_filepaths:
         publish_utils.write_to_public_gcs(
             f,
-            f"{Path(f).name}"
+            f"gtfs_digest/{Path(f).name}",
+            PUBLIC_GCS
         )
+    
+    for f in df_filepaths:
+        export_parquet_as_csv_or_geojson(f, filetype="df")
+        
+    for f in gdf_filepaths:
+        export_parquet_as_csv_or_geojson(f, filetype="gdf")
