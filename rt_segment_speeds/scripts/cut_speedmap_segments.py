5d2014240494caf6e632947a1ed414fd30f6a5d7
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-05-17 18:33:14 +0000

    test speedmap pipeline and check all the columns are filled in

--- rt_segment_speeds/scripts/cut_speedmap_segments.py
+++ rt_segment_speeds/scripts/cut_speedmap_segments.py
@@ -6,7 +6,7 @@ import sys
 
 from loguru import logger
 
-from segment_speed_utils import helpers
+from segment_speed_utils import gtfs_schedule_wrangling, helpers
 from update_vars import SEGMENT_GCS, GTFS_DATA_DICT
 from calitp_data_analysis import utils, geography_utils
 
@@ -54,12 +54,6 @@ def cut_longer_segments(
                      )
     )
     
-    # TODO: this might be unnecessarily complicated
-    # leave for now, but maybe we can get away with just segment_sequence2
-    # although for aggregation, we want to ensure segments have same endpoints
-    # if we want to stack it, and maybe segment_sequence2 isn't sufficient?
-    # we don't want to stack segment1 with segment1 
-    
     # To get a new stop_sequence that is numeric, 
     # would have to calculate cumulative distance in the segment now
     gdf2["seg_length"] = gdf2.geometry.length
@@ -90,30 +84,33 @@ def get_proxy_stops(
     longer_segments: gpd.GeoDataFrame,
     analysis_date: str
 ) -> gpd.GeoDataFrame: 
-    
-    # todo: update references to shapely.Point(x.geometry.coords[0])
-    # we can use shapely.get_point()
-    keep_cols =  ["trip_instance_key", "shape_array_key",
-                   "stop_sequence", "stop_id", "stop_pair", 
-                   #"stop_primary_direction",
-                   "geometry"]
+    """
+    Proxy stops for speedmap segments are segment endpoint (origin).
+    If a segment gets chopped into 2, the first segment origin is an actual stop.
+    The second segment's origin is the 1,000th meter.
+    """
+    keep_cols =  [
+        "trip_instance_key", "shape_array_key",
+        "stop_sequence", "stop_id", 
+        "stop_pair",
+        "geometry"
+    ]
 
     proxy_stops = longer_segments.assign(
         geometry = longer_segments.apply(
             lambda x: shapely.get_point(x.geometry, 0), axis=1)
     ).rename(
         columns = {"stop_id1": "stop_id"}
-    )[keep_cols + ["stop_sequence1"]].to_crs(geography_utils.WGS84)
+    )[keep_cols + ["segment_id", "stop_sequence1"]].to_crs(geography_utils.WGS84)
 
     # stop_primary_direction can be populated when it's appended
     # with the stop_times, and we can sort by trip-stop_sequence1
     # and pd.ffill (forward fill)
     stop_times = helpers.import_scheduled_stop_times(
         analysis_date,
-        columns = ["trip_instance_key", "shape_array_key",
-                   "stop_sequence", "stop_id", "stop_pair", 
-                   "stop_primary_direction",
-                   "geometry"],
+        columns = keep_cols + [
+            "schedule_gtfs_dataset_key", 
+            "stop_pair_name", "stop_primary_direction"],
         with_direction = True,
         get_pandas = True,
         crs = geography_utils.WGS84,
@@ -129,14 +126,20 @@ def get_proxy_stops(
         trip_stop_cols
     ).reset_index(drop=True)
     
+    # Fill in the missing info from proxy stops that would have come from stop_times
     gdf = gdf.assign(
         stop_primary_direction = (gdf.groupby(trip_stop_cols)
                                   .stop_primary_direction
                                   .ffill()
                                  ),
-        #TODO: create stop_sequence2 
-        # is this needed or will stop_sequence1 be sufficient
-        # segments go from current stop to next stop, 
+        schedule_gtfs_dataset_key = (gdf.groupby(trip_stop_cols)
+                                     .schedule_gtfs_dataset_key
+                                     .ffill()
+                                    ),
+        stop_pair_name = (gdf.groupby(trip_stop_cols)
+                          .stop_pair_name
+                          .ffill()
+                         ),
     ).pipe(gtfs_schedule_wrangling.fill_missing_stop_sequence1)
     
     # After filling in stop_primary_direction, we need a drop_duplicates
@@ -187,7 +190,7 @@ if __name__ == "__main__":
 
         time1 = datetime.datetime.now()
         logger.info(f"cut longer segments: {time1 - start}")
-        '''      
+              
         speedmap_segments = pd.concat([
             stop_segments.loc[stop_segments.segment_length < SEGMENT_LENGTH],
             longer_segments], 
@@ -208,11 +211,11 @@ if __name__ == "__main__":
             SEGMENT_GCS,
             f"{SPEEDMAP_SEGMENTS}_{analysis_date}"
         )
-        ''' 
+         
         time2 = datetime.datetime.now()
         logger.info(f"concatenate segments and export: {time2 - time1}")
 
-        #del stop_segments, speedmap_segments
+        del stop_segments, speedmap_segments
         
         speedmap_stops = get_proxy_stops(longer_segments, analysis_date)
         end = datetime.datetime.now()
@@ -226,3 +229,4 @@ if __name__ == "__main__":
         )
           
         logger.info(f"execution time: {end - start}")
+        
