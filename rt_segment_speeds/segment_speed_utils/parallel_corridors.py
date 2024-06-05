68f3e199cedb69b596fc58c7dc29ee0cee59b981
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-06-05 21:51:55 +0000

    simplify functions for categorizing parallel

--- rt_segment_speeds/segment_speed_utils/parallel_corridors.py
+++ rt_segment_speeds/segment_speed_utils/parallel_corridors.py
@@ -13,52 +13,51 @@ import geopandas as gpd
 import pandas as pd
 
 from calitp_data_analysis import geography_utils, utils
+from segment_speed_utils import gtfs_schedule_wrangling, helpers
+from shared_utils import catalog_utils
 
-DATA_PATH = "./data/"
+def process_transit_routes(analysis_date: str) -> gpd.GeoDataFrame:
+    """
+    For each route, select the longest shape for each route
+    to overlay with SHN.
+    Also count how many routes there are for each operator.
+    """
+    longest_shape = gtfs_schedule_wrangling.longest_shape_by_route_direction(analysis_date)
+    
+    gdf = longest_shape.assign(
+        total_routes = longest_shape.groupby("feed_key").route_key.transform("nunique")
+    ).sort_values(
+        ["route_key", "route_length"], 
+        ascending = [True, False]
+    ).drop_duplicates(subset="route_key").reset_index(drop=True)
+    
+    # Get this to same CRS as highways
+    gdf = gdf.assign(
+        route_feet = gdf.geometry.to_crs(geography_utils.CA_StatePlane).length
+    ).drop(columns = "route_length").to_crs(geography_utils.CA_StatePlane)
+    
+    
+    return gdf
 
-def process_transit_routes(
-    df: gpd.GeoDataFrame, 
-    operator_cols: list = ["feed_key"]
+
+def process_highways(
+    group_cols: list,
+    buffer_feet: int
 ) -> gpd.GeoDataFrame:
     """
-    At operator level, pick the route with the longest length 
-    to overlay with SHN.
-    At operator level, sum up how many unique routes there are.
-    """
-    ## Clean transit routes
-    df = df.assign(
-        route_length = df.to_crs(
-            geography_utils.CA_StatePlane).geometry.length
-    ).to_crs(geography_utils.CA_StatePlane)
-    
-    # Get it down to route_id and pick longest shape
-    df2 = (df.sort_values(operator_cols + ["route_id", "route_length"], 
-                          ascending = [True for i in operator_cols] + [True, False])
-           .drop_duplicates(subset=operator_cols + ["route_id"])
-           .reset_index(drop=True)
-    )
-    
-    # Then count how many unique route_ids appear for operator (that's denominator)
-    route_cols = ["route_id", "total_routes", "route_length", "geometry"]
-    keep_cols = operator_cols + route_cols
-
-    
-    df3 = df2.assign(
-        total_routes = df2.groupby(operator_cols)["route_id"].transform(
-            "nunique").astype("Int64")
-    )[keep_cols]
-    
-    return df3
-
-def prep_highway_directions_for_dissolve(
-    group_cols: list = ["Route", "County", "District", "RouteType"]
-) -> gpd.GeoDataFrame:    
-    '''
     Put in a list of group_cols, and aggregate highway segments with 
     the direction info up to the group_col level.
-    '''
-    df = (gpd.read_parquet("gs://calitp-analytics-data/data-analyses/"
-                           "shared_data/state_highway_network.parquet")
+    
+    For each highway, store what directions it runs in 
+    as dummy variables. This will allow us to dissolve 
+    the geometry and get fewer rows for highways
+    without losing direction info.
+    """
+    SHN_FILE = catalog_utils.get_catalog("shared_data_catalog").state_highway_network.urlpath
+    
+    direction_cols = ["NB", "SB", "EB", "WB"]
+
+    df = (gpd.read_parquet(SHN_FILE)
           .to_crs(geography_utils.CA_StatePlane))
     
     # Get dummies for direction
@@ -66,48 +65,31 @@ def prep_highway_directions_for_dissolve(
     direction_dummies = pd.get_dummies(df.Direction, dtype=int)
     df = pd.concat([df.drop(columns = "Direction"), 
                     direction_dummies], axis=1)
-
-    direction_cols = ["NB", "SB", "EB", "WB"]
     
+    # For each highway, allow multiple dummies to be 1 (as long as highway had that direction,
+    # we'll allow dummy to be 1. A highway can be tagged as WB and SB, and we want to keep info for both).
     for c in direction_cols:
         df[c] = df.groupby(group_cols)[c].transform('max').astype(int)
-
-    return df
-
-
-def process_highways(
-    buffer_feet: int = geography_utils.FEET_PER_MI
-) -> gpd.GeoDataFrame:
-    """
-    For each highway, store what directions it runs in 
-    as dummy variables. This will allow us to dissolve 
-    the geometry and get fewer rows for highways
-    without losing direction info.
-    """
-    
-    group_cols = ["Route", "County", "District", "RouteType"]
-    df = prep_highway_directions_for_dissolve(group_cols)
     
     # Buffer first, then dissolve
     # If dissolve first, then buffer, kernel times out
     df = df.assign(
-        highway_length = df.geometry.length,
+        highway_feet = df.geometry.length,
         geometry = df.geometry.buffer(buffer_feet),
         Route = df.Route.astype(int),
     )
     
-    direction_cols = ["NB", "SB", "EB", "WB"]
-    df = df.dissolve(by=group_cols + direction_cols).reset_index()
+    df2 = df.dissolve(by=group_cols + direction_cols).reset_index()
     
-    df[direction_cols] = df[direction_cols].astype(int)
+    df2[direction_cols] = df2[direction_cols].astype(int)
         
-    return df
+    return df2
 
 
 def overlay_transit_to_highways(
-    hwy_buffer_feet: int = geography_utils.FEET_PER_MI,
-    transit_routes_df: gpd.GeoDataFrame = None, 
-    operator_cols: list = ["feed_key", "name"]
+    analysis_date: str,
+    hwy_buffer_feet: int,
+    pct_route_threshold: float,
 ) -> gpd.GeoDataFrame:
     """
     Function to find areas of intersection between
@@ -116,10 +98,14 @@ def overlay_transit_to_highways(
     Returns: geopandas.GeoDataFrame, with geometry column reflecting
     the areas of intersection.
     """
+    hwy_group_cols = ["Route", "County", "District", "RouteType"]
     
     # Can pass a different buffer zone to determine parallel corridors
-    highways = process_highways(buffer_feet = hwy_buffer_feet)
-    transit_routes = process_transit_routes(transit_routes_df, operator_cols)
+    highways = process_highways(
+        group_cols = hwy_group_cols, 
+        buffer_feet = hwy_buffer_feet
+    )
+    transit_routes = process_transit_routes(analysis_date)
     
     # Overlay
     # Note: an overlay based on intersection changes the geometry column
@@ -134,107 +120,77 @@ def overlay_transit_to_highways(
     # Using new geometry column, calculate what % that intersection 
     # is of the route and hwy
     gdf = gdf.assign(
-        pct_route = (gdf.geometry.length / gdf.route_length).round(3),
-        pct_highway = (gdf.geometry.length / gdf.highway_length).round(3),
+        pct_route = (gdf.geometry.length / gdf.route_feet).round(3),
         Route = gdf.Route.astype(int),
-    )
-    
-    '''
-    Set pct_highway to have max of 1
-    LA Metro Line 910 (Silver Line) runs on the 110 freeway in both directions.
-    Has to do with the fact that highway length was calculated only 
-    for 1 direction (~centerline). 
-    The length was calculated, 1 mi buffer drawn, then the 
-    dissolve (computationally expensive).
-    '''
-
-    gdf = gdf.assign(
-        pct_highway = gdf.apply(lambda x: 1 if x.pct_highway > 1 
-                                else x.pct_highway, axis=1),
     )
     
     # Fix geometry - don't want the overlay geometry, which is intersection
     # Want to use a transit route's line geometry    
     gdf2 = pd.merge(
-        transit_routes[operator_cols + ["route_id", "geometry"]
-                      ].drop_duplicates(),
+        transit_routes[["route_key", "geometry"]],
         gdf.drop(columns = "geometry"),
-        on = operator_cols + ["route_id"],
-        how = "left",
+        on = "route_key",
+        how = "inner",
         # Allow 1:m merge because the same transit route 
         # can overlap with various highways
         validate = "1:m",
-        indicator=True
     )
     
-    return gdf2
-
-
-def parallel_or_intersecting(
-    df: gpd.GeoDataFrame, 
-    pct_route_threshold: float = 0, 
-    pct_highway_threshold: float = 0
-) -> gpd.GeoDataFrame:
+    gdf3 = gdf2.loc[gdf2.pct_route >= pct_route_threshold]
     
-    # Play with various thresholds to decide how to designate parallel
-    df = df.assign(
-        parallel = df.apply(lambda x: 
-                            1 if (
-                                (x.pct_route >= pct_route_threshold) and 
-                                (x.pct_highway >= pct_highway_threshold)
-                            ) else 0, axis=1),
+    return gdf3
+
+
+def routes_by_on_shn_parallel_categories(analysis_date: str) -> gpd.GeoDataFrame:
+    """
+    Categorize routes into on_shn / parallel / other.
+    Only unique routes are kept (route_key).
+    """
+    # Tweak these if needed - for now, on SHN is defined as being within 50 ft of SHN,
+    # and parallel to SHN is within 0.5 mile of SHN.
+    # since these are concentric circle buffers, we'll allow a route to be assigned
+    # to a category based on ranking (shn > parallel > other).
+    # A route (longest shape) can overlay with multiple highways, but we're looking across all highways
+    # and if the route qualifies as on_shn for any of the highways its near, it is on_shn.
+    SHN_HWY_BUFFER_FEET = 50 
+    SHN_PCT_ROUTE = 0.2 # we'll use the same for both categories
+    PARALLEL_HWY_BUFFER_FEET = geography_utils.FEET_PER_MI * 0.5
+    
+    transit_routes = process_transit_routes(analysis_date)
+
+    on_shn = overlay_transit_to_highways(
+        analysis_date,
+        hwy_buffer_feet = SHN_HWY_BUFFER_FEET,
+        pct_route_threshold = SHN_PCT_ROUTE,
+    )[["route_key"]].drop_duplicates()
+    
+    parallel_or_intersecting = overlay_transit_to_highways(
+        analysis_date,
+        hwy_buffer_feet = PARALLEL_HWY_BUFFER_FEET, 
+        pct_route_threshold = SHN_PCT_ROUTE,
+    )[["route_key"]].drop_duplicates()
+    
+    df = pd.merge(
+        transit_routes.assign(other=1),
+        on_shn.assign(on_shn=3),
+        on = "route_key",
+        how = "left"
+    ).merge(
+        parallel_or_intersecting.assign(parallel=2),
+        on = "route_key",
+        how = "left"
     )
     
-    return df
-
-
-# Use this in notebook
-# Can pass different parameters if buffer or thresholds need adjusting
-def make_analysis_data(
-    hwy_buffer_feet: int = geography_utils.FEET_PER_MI,
-    transit_routes_df: gpd.GeoDataFrame = None,
-    pct_route_threshold: float = 0.5,
-    pct_highway_threshold: float = 0.1,
-    data_path: str = "", 
-    file_name: str = "parallel_or_intersecting",
-    operator_cols: list = ["feed_key", "name"]
-):
-    '''
-    hwy_buffer_feet: int, number of feet to draw hwy buffers, defaults to 1 mile
-    transit_routes_df: a table, typically shapes, with route_id too
-    pct_route_threshold: float, between 0-1
-    pct_highway_threshold: float, between 0-1
-    data_path: str, file path for saving parallel transit routes dataset
-    file_name: str, file name for saving parallel transit routes dataset
+    #https://stackoverflow.com/questions/29919306/find-the-column-name-which-has-the-maximum-value-for-each-row
+    category_cols = ["on_shn", "parallel", "other"]
+    keep_cols = [
+        "feed_key", "schedule_gtfs_dataset_key",
+        "route_id", "route_key",
+        "total_routes", "category"
+    ]
     
-    '''
-    # Get overlay between highway and transit routes
-    # Draw buffer around highways
-    gdf = overlay_transit_to_highways(
-        hwy_buffer_feet, transit_routes_df, operator_cols)
+    df2 = df.assign(
+        category = df[category_cols].idxmax(axis=1)
+    )[keep_cols]
 
-    # Remove transit routes that do not overlap with highways
-    gdf = (gdf[gdf._merge!="left_only"]
-           .drop(columns = "_merge")
-           .reset_index(drop=True)
-          )
-    
-    # Categorize whether route is parallel or intersecting based on threshold
-    gdf2 = parallel_or_intersecting(
-        gdf, 
-        pct_route_threshold, 
-        pct_highway_threshold
-    )
-    
-    if "gs://" in data_path:
-        utils.geoparquet_gcs_export(
-            gdf2, 
-            data_path, 
-            file_name
-        )
-    else:
-        gdf2.to_parquet(f"{data_path}{file_name}.parquet")
-        
-    # For map, need highway to be 250 ft buffer
-    #highways = process_highways(buffer_feet=250)
-    #highways.to_parquet(f"{DATA_PATH}highways.parquet")
+    return df2
