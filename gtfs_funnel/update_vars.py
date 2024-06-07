1bb9f58a9f743014ca3b9be444fa148facc5f57c
amandaha8 <amandahastuyck@gmail.com>
2024-06-07 21:14:43 +0000

    corrected stuff back to a list and clarified which update_vars to update

--- gtfs_funnel/update_vars.py
+++ gtfs_funnel/update_vars.py
@@ -6,7 +6,7 @@ all_dates = (rt_dates.y2024_dates + rt_dates.y2023_dates +
             )
 
 apr_week = rt_dates.get_week("apr2024", exclude_wed=True)
-may_date = rt_dates.DATES["may2024"]
+may_date = [rt_dates.DATES["may2024"]] 
 analysis_date_list = may_date
 
 GTFS_DATA_DICT = catalog_utils.get_catalog("gtfs_analytics_data")
