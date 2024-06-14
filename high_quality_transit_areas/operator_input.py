b49352d4681c22770e4fc2608e724dcbccc10602
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-06-14 16:59:59 +0000

    add muni stop_names to their BRT list, allow helpers to return df instead of gdf

--- high_quality_transit_areas/operator_input.py
+++ high_quality_transit_areas/operator_input.py
@@ -10,10 +10,41 @@ Here, it's just 136 stops.
 """
 import pandas as pd
 
+from segment_speed_utils import helpers
+from shared_utils import rt_dates
 from update_vars import GCS_FILE_PATH
 
+def get_muni_stops(analysis_date: str) -> pd.DataFrame:
+    """
+    From our stops table, grab Muni stop_ids and
+    stop_names.
+    If stop_id ever changes, maybe we can match 
+    on stop_names.
+    """
+    muni_feed = helpers.import_scheduled_trips(
+        analysis_date,
+        columns = ["feed_key"],
+        filters = [[("name", "==", "Bay Area 511 Muni Schedule")]],
+        get_pandas = True
+    ).feed_key.iloc[0]
+    
+    stops = helpers.import_scheduled_stops(
+        analysis_date,
+        columns = ["stop_id", "stop_name"],
+        filters = [[("feed_key", "==", muni_feed)]],
+        get_pandas = True
+    )
+    
+    return stops
+
 if __name__ == "__main__":
-    FILE = "SFMTA_muni_high_quality_transit_stops_2024-02-01.csv"
+    
+    sfmta_date = "2024-02-01"
+    analysis_date = rt_dates.DATES["feb2024"]
+    
+    all_muni_stops = get_muni_stops(analysis_date)
+    
+    FILE = f"SFMTA_muni_high_quality_transit_stops_{sfmta_date}.csv"
 
     muni_stops = (
         pd.read_csv(f"{GCS_FILE_PATH}operator_input/{FILE}", 
@@ -22,9 +53,18 @@ if __name__ == "__main__":
         .rename(columns={"bs_id": "stop_id"})
     )
     
-    muni_stops.to_parquet(
+    muni_stops_with_names = pd.merge(
+        muni_stops,
+        all_muni_stops,
+        on = "stop_id",
+        how = "inner"
+    )
+    
+    muni_stops_with_names.to_parquet(
         f"{GCS_FILE_PATH}operator_input/"
         f"muni_brt_stops.parquet"
     )
-    
+        
     print(f"saved muni stops")
+    
+
