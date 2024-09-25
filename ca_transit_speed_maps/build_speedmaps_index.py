578c0f6f231421250c4df67a4d6ce99ecc85dfda
Eric Dasmalchi <edasmalchi@gmail.com>
2024-09-25 18:04:29 +0000

    re-add route_id, ensure unique col names in average_segment_speeds

--- ca_transit_speed_maps/build_speedmaps_index.py
+++ ca_transit_speed_maps/build_speedmaps_index.py
@@ -4,15 +4,22 @@ os.environ['USE_PYGEOS'] = '0'
 
 from siuba import *
 import pandas as pd
+import geopandas as gpd
 import datetime as dt
 
 from calitp_data_analysis.tables import tbls
 from shared_utils import rt_dates, rt_utils
 
+from segment_speed_utils.project_vars import (
+    COMPILED_CACHED_VIEWS,
+    PROJECT_CRS,
+    SEGMENT_GCS,
+)
+
 ANALYSIS_DATE = dt.date.fromisoformat(rt_dates.DATES['aug2024'])
 PROGRESS_PATH = f'./_rt_progress_{ANALYSIS_DATE}.parquet'
 
-def build_speedmaps_index(analysis_date: dt.date) -> pd.DataFrame:
+def build_speedmaps_index(analysis_date: dt.date, how: str = 'new') -> pd.DataFrame:
     '''
     An index table for tracking down a given org's schedule/rt feeds.
     Note that in limited cases, multiple orgs may share the same datasets
@@ -31,22 +38,30 @@ def build_speedmaps_index(analysis_date: dt.date) -> pd.DataFrame:
               _.vehicle_positions_gtfs_dataset_key != None)
     >> inner_join(_, dim_orgs, on = {'organization_source_record_id': 'source_record_id'})
     >> select(_.organization_itp_id, _.organization_name, _.organization_source_record_id,
-             _.caltrans_district, _._is_current, _.vehicle_positions_gtfs_dataset_key)
+             _.caltrans_district, _._is_current, _.vehicle_positions_gtfs_dataset_key,
+			 _.schedule_gtfs_dataset_key)
     >> collect()
     )
-    print(orgs_with_vp >> filter(_.caltrans_district.isna()))
+    assert (orgs_with_vp >> filter(_.caltrans_district.isna())).empty
     orgs_with_vp = orgs_with_vp >> filter(-_.caltrans_district.isna())
     assert not orgs_with_vp.isnull().values.any()
     orgs_with_vp['analysis_date'] = analysis_date
     orgs_with_vp = orgs_with_vp >> distinct(_.organization_name,
                     _.organization_itp_id, _.organization_source_record_id,
-                    _.caltrans_district, _._is_current, _.analysis_date
+                    _.caltrans_district, _._is_current, _.analysis_date,
+											_.schedule_gtfs_dataset_key
                                            )
-    return orgs_with_vp
+    if how == 'new':
+        speedmap_segs = gpd.read_parquet(f'{SEGMENT_GCS}rollup_singleday/speeds_shape_speedmap_segments_{analysis_date}.parquet') #  aggregated
+        new_ix = (orgs_with_vp >> filter(_.schedule_gtfs_dataset_key.isin(speedmap_segs.schedule_gtfs_dataset_key.unique()))).copy()
+        new_ix['status'] = 'speedmap_segs_available'
+        return new_ix
+    else:
+        return orgs_with_vp
 
 if __name__ == "__main__":
     
     print(f'analysis date from shared_utils/rt_dates: {ANALYSIS_DATE}')
     speedmaps_index = build_speedmaps_index(ANALYSIS_DATE)
-    speedmaps_index_joined = rt_utils.check_intermediate_data(speedmaps_index)
-    speedmaps_index_joined.to_parquet(PROGRESS_PATH)
+    # speedmaps_index = rt_utils.check_intermediate_data(speedmaps_index)
+    speedmaps_index.to_parquet(PROGRESS_PATH)
