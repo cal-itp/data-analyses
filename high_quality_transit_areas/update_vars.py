7f87713def97887e78512fc685c6e74d4b91988e
Eric Dasmalchi <edasmalchi@gmail.com>
2024-10-03 18:10:15 +0000

    add fixed peaks, test out stop-level averaging

--- high_quality_transit_areas/update_vars.py
+++ high_quality_transit_areas/update_vars.py
@@ -1,4 +1,5 @@
 from shared_utils import rt_dates
+import datetime as dt
 
 analysis_date = rt_dates.DATES["sep2024"]
 
@@ -10,3 +11,6 @@ SEGMENT_BUFFER_METERS = 50 # buffer around segment to sjoin to stops
 HALF_MILE_BUFFER_METERS = 805 # half mile ~ 805 meters
 CORRIDOR_BUFFER_METERS = HALF_MILE_BUFFER_METERS - SEGMENT_BUFFER_METERS # 755 meters
 EXPORT_PATH = f"{GCS_FILE_PATH}export/{analysis_date}/"
+
+AM_PEAK = (dt.time(hour=6), dt.time(hour=9))
+PM_PEAK = (dt.time(hour=15), dt.time(hour=19))
