852e80ed64c3c74d90b179f933c0c0853592a553
tiffanychu90 <tiffany.chu@dot.ca.gov>
2024-10-04 00:13:38 +0000

    do not drop duplicates for feed to organization_name

--- _shared_utils/shared_utils/schedule_rt_utils.py
+++ _shared_utils/shared_utils/schedule_rt_utils.py
@@ -151,19 +151,16 @@ def get_organization_id(
         sorting = [True for c in merge_cols]
         keep_cols = ["organization_source_record_id"]
 
-        # Eventually, we need to move to 1 organization name, so there's
-        # no fanout when we merge it on
-        # Until then, handle it by dropping duplicates and pick 1 name
-        dim_provider_gtfs_data2 = (
-            dim_provider_gtfs_data2.sort_values(
-                merge_cols + ["_valid_to", "_valid_from"], ascending=sorting + [False, False]
-            )
-            .drop_duplicates(merge_cols)
-            .reset_index(drop=True)[merge_cols + keep_cols]
-        )
+        # We allow fanout when merging a feed to multiple organization names,
+        # but we should handle it by selectig a preferred
+        # rather than alphabetical.
+        # (organization names Foothill Transit and City of Duarte)
+        dim_provider_gtfs_data2 = dim_provider_gtfs_data2.sort_values(
+            merge_cols + ["_valid_to", "_valid_from"], ascending=sorting + [False, False]
+        ).reset_index(drop=True)[merge_cols + keep_cols]
 
         df2 = pd.merge(df, dim_provider_gtfs_data2, on=merge_cols, how="inner")
-        # return dim_provider_gtfs_data2
+
         return df2
 
 
