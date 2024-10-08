0f8349277325d7d68ae961f714bd15d293a178bc
Eric Dasmalchi <edasmalchi@gmail.com>
2024-10-08 18:44:04 +0000

    test scenarios, proceed with keeping multi-route aggregation

--- high_quality_transit_areas/update_vars.py
+++ high_quality_transit_areas/update_vars.py
@@ -12,5 +12,7 @@ HALF_MILE_BUFFER_METERS = 805 # half mile ~ 805 meters
 CORRIDOR_BUFFER_METERS = HALF_MILE_BUFFER_METERS - SEGMENT_BUFFER_METERS # 755 meters
 EXPORT_PATH = f"{GCS_FILE_PATH}export/{analysis_date}/"
 
-AM_PEAK = (dt.time(hour=6), dt.time(hour=9))
+AM_PEAK = (dt.time(hour=6), dt.time(hour=9)) #  time range to calculate average peak frequencies
 PM_PEAK = (dt.time(hour=15), dt.time(hour=19))
+CORR_SINGLE_ROUTE = False #  only consider frequency from a single route at each stop (not currently recommended)
+BUS_MTS_SINGLE_ROUTE = False #  currently matches corridors but subject to statutory interpretation, see README.md
