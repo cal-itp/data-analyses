327b586cfc0c4e82dcef32554c544b561caaf87c
Eric Dasmalchi <edasmalchi@gmail.com>
2024-10-08 20:08:54 +0000

    update script and vars with seperate hq corr and major stop frequence thresholds

--- high_quality_transit_areas/update_vars.py
+++ high_quality_transit_areas/update_vars.py
@@ -14,5 +14,7 @@ EXPORT_PATH = f"{GCS_FILE_PATH}export/{analysis_date}/"
 
 AM_PEAK = (dt.time(hour=6), dt.time(hour=9)) #  time range to calculate average peak frequencies
 PM_PEAK = (dt.time(hour=15), dt.time(hour=19))
+HQ_TRANSIT_THRESHOLD = 4  #  trips per hour, from statute, see README.md
+MS_TRANSIT_THRESHOLD = 3  #  different statutory threshold as of October 2024
 CORR_SINGLE_ROUTE = False #  only consider frequency from a single route at each stop (not currently recommended)
-BUS_MTS_SINGLE_ROUTE = False #  currently matches corridors but subject to statutory interpretation, see README.md
+BUS_MS_SINGLE_ROUTE = False #  currently matches corridors but subject to statutory interpretation, see README.md
