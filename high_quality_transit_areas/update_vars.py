92730af2f430855cf41968eb357fd3e2f1172020
Eric Dasmalchi <edasmalchi@gmail.com>
2024-10-09 17:05:16 +0000

    wip

--- high_quality_transit_areas/update_vars.py
+++ high_quality_transit_areas/update_vars.py
@@ -16,5 +16,5 @@ AM_PEAK = (dt.time(hour=6), dt.time(hour=9)) #  time range to calculate average 
 PM_PEAK = (dt.time(hour=15), dt.time(hour=19))
 HQ_TRANSIT_THRESHOLD = 4  #  trips per hour, from statute, see README.md
 MS_TRANSIT_THRESHOLD = 3  #  different statutory threshold as of October 2024
-CORR_SINGLE_ROUTE = False #  only consider frequency from a single route at each stop (not currently recommended)
-BUS_MS_SINGLE_ROUTE = False #  currently matches corridors but subject to statutory interpretation, see README.md
+CORR_SINGLE_ROUTE = False #  only consider frequency from a single route at each stop (not recommended, see README)
+BUS_MS_SINGLE_ROUTE = False #  only consider frequency from a single route at each stop (not recommended, see README)
