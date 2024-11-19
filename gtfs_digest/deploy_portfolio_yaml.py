5935ec16366f4d4f09ca740c784b33256dbd3d4a
amandaha8 <amandahastuyck@gmail.com>
2024-11-19 20:55:14 +0000

    yml is working!

--- gtfs_digest/deploy_portfolio_yaml.py
+++ gtfs_digest/deploy_portfolio_yaml.py
@@ -3,33 +3,31 @@ Create the GTFS Digest yaml that
 sets the parameterization for the analysis site.
 """
 import pandas as pd
+import yaml
 
-from pathlib import Path
-
-import _operators_prep as op_prep
-from segment_speed_utils.project_vars import RT_SCHED_GCS
 from shared_utils import portfolio_utils
+from _operators_prep import operators_schd_vp_rt
 
-PORTFOLIO_SITE_YAML = Path("../portfolio/sites/gtfs_digest.yml")
+SITE_YML = "../portfolio/sites/gtfs_digest.yml"
 
-if __name__ == "__main__":
-    
-    df = (op_prep.operators_schd_vp_rt()
-          .sort_values(["caltrans_district", "organization_name"])
-          .reset_index(drop=True)
-         )
-    
-    portfolio_utils.create_portfolio_yaml_chapters_with_sections(
-        PORTFOLIO_SITE_YAML ,
-        df,
-        chapter_info = {
-            "column": "caltrans_district",
-            "name": "district",
-            "caption_prefix": "District ",
-            "caption_suffix": "",
-        },
-        section_info = {
-            "column": "organization_name",
-            "name": "organization_name",
-        },
-    )
+df = operators_schd_vp_rt()
+
+portfolio_utils.create_portfolio_yaml_chapters_with_sections(
+    SITE_YML,
+    df,
+    chapter_info = {
+        "column": "caltrans_district",
+        "name": "district",
+        "caption_prefix": "District ",
+        "caption_suffix": "",
+    },
+    section_info = {
+        "column": "organization_name",
+        "name": "organization_name",
+    },
+)
+
+with open(SITE_YML) as f:
+    site_yaml = yaml.safe_load(f)
+      
+site_yaml
