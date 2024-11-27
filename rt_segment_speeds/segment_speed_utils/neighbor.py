753237a86d7e2027907cb18d6664b5fead5dcfdf
tiffanychu90 <tiffany.ku@dot.ca.gov>
2024-11-27 21:51:31 +0000

    refactor nearest neighbor to use vp_condensed

--- rt_segment_speeds/segment_speed_utils/neighbor.py
+++ rt_segment_speeds/segment_speed_utils/neighbor.py
@@ -55,6 +55,7 @@ def merge_stop_vp_for_nearest_neighbor(
         
     return gdf
 
+
 def subset_arrays_to_valid_directions(
     vp_direction_array: np.ndarray,
     vp_geometry: shapely.LineString,
@@ -63,16 +64,24 @@ def subset_arrays_to_valid_directions(
     stop_direction: str,
 ) -> np.ndarray: 
     """
+    Each row stores several arrays related to vp.
+    vp_direction is an array, vp_idx is an array,
+    and the linestring of vp coords can be coerced into an array.
+    
+    When we're doing nearest neighbor search, we want to 
+    first filter the full array down to valid vp
+    before snapping it.
     """
+    N_NEAREST_POINTS = 10
+    
     opposite_direction = vp_transform.OPPOSITE_DIRECTIONS[stop_direction] 
     
     # These are the valid index values where opposite direction 
     # is excluded       
     valid_indices = (vp_direction_array != opposite_direction).nonzero()   
 
-    vp_coords_line = shapely.LineString(
-        np.array(vp_geometry.coords)[valid_indices]
-    )
+    vp_coords_line = np.array(vp_geometry.coords)[valid_indices]
+    
     vp_idx_arr = np.asarray(vp_idx_array)[valid_indices]  
             
     np_inds = geo_utils.nearest_snap(
@@ -84,11 +93,10 @@ def subset_arrays_to_valid_directions(
     # if we want 10 nearest neighbors and 8th, 9th, 10th are all
     # the same result, the 8th will have a result, then 9th and 10th will
     # return the length of the array (which is out-of-bounds)
-
     np_inds2 = np_inds[np_inds < vp_idx_arr.size]
-        
+    
     nearest_vp_arr = vp_idx_arr[np_inds2]
-        
+    
     return nearest_vp_arr
 
 
@@ -99,71 +107,21 @@ def add_nearest_neighbor_result_array(
 ) -> pd.DataFrame:
     """
     Add the nearest k_neighbors result.
-    """
-    N_NEAREST_POINTS = 10
-    
+    """    
     nearest_vp_arr_series = []
     
     for row in gdf.itertuples():
         
-        # These are the ones that do not run in the 
-        # opposite direction as stop_primary_direction
-        # Ex: if stop1 to stop2 is going eastbound, 
-        # we do not allow westbound traveling vp    
-        vp_coords_line = getattr(row, "vp_primary_direction")
-        stop_direction = getattr(row, "stop_primary_direction")
-        opposite_direction = vp_transform.OPPOSITE_DIRECTIONS[stop_direction] 
-        
-        # These are the valid index values where opposite direction 
-        # is excluded       
-        valid_indices = (vp_coords_line != opposite_direction).nonzero()   
-        
-        vp_coords_line = np.array(getattr(row, "vp_geometry").coords)[valid_indices]
-        vp_coords_line = shapely.LineString(vp_coords_line)
-        stop_geometry = getattr(row, "stop_geometry")
-        vp_idx_arr = np.asarray(getattr(row, "vp_idx"))[valid_indices]  
-            
-        np_inds = geo_utils.nearest_snap(
-            vp_coords_line, stop_geometry, N_NEAREST_POINTS
+        nearest_vp_arr = subset_arrays_to_valid_directions(
+            getattr(row, "vp_primary_direction"),
+            getattr(row, "vp_geometry"),
+            getattr(row, "vp_idx"),
+            getattr(row, "stop_geometry"),
+            getattr(row, "stop_primary_direction"),
         )
         
-        # nearest neighbor returns self.N 
-        # if there are no nearest neighbor results found
-        # if we want 10 nearest neighbors and 8th, 9th, 10th are all
-        # the same result, the 8th will have a result, then 9th and 10th will
-        # return the length of the array (which is out-of-bounds)
-        
-        np_inds2 = np_inds[np_inds < vp_idx_arr.size]
-        
-        nearest_vp_arr = vp_idx_arr[np_inds2]
-        
         nearest_vp_arr_series.append(nearest_vp_arr)
-    
-    gdf2 = gdf.assign(
-        nearest_vp_arr = nearest_vp_arr_series
-    ).drop(columns = ["vp_primary_direction", "vp_idx", "vp_geometry"])
-    
-    return gdf2
-
-def add_nearest_neighbor_result_array2(
-    gdf: gpd.GeoDataFrame, 
-    analysis_date: str,
-    **kwargs
-) -> pd.DataFrame:
-    """
-    Add the nearest k_neighbors result.
-    """
-    N_NEAREST_POINTS = 10
-    
-    nearest_vp_arr = np.vectorize(
-        subset_arrays_to_valid_directions)(
-        gdf.vp_primary_direction,
-        gdf.vp_geometry,
-        gdf.vp_idx,
-        gdf.stop_geometry,
-        gdf.stop_primary_direction,
-    )
-    
+        
     gdf2 = gdf.assign(
         nearest_vp_arr = nearest_vp_arr_series
     ).drop(columns = ["vp_primary_direction", "vp_idx", "vp_geometry"])
