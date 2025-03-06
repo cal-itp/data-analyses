25712a5833ea5af4fce219f2dc7d057fa5113f2c
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-03-06 20:39:17 +0000

    update digest toc to use renamed caltrans district names

--- gtfs_digest/_operators_prep.py
+++ gtfs_digest/_operators_prep.py
@@ -1,4 +1,4 @@
-from shared_utils import catalog_utils
+from shared_utils import catalog_utils, potfolio_utils
 import pandas as pd
 
 GTFS_DATA_DICT = catalog_utils.get_catalog("gtfs_analytics_data")
@@ -10,17 +10,23 @@ def operators_schd_vp_rt()->pd.DataFrame:
     """
     schd_vp_url = f"{GTFS_DATA_DICT.digest_tables.dir}{GTFS_DATA_DICT.digest_tables.route_schedule_vp}.parquet"
     
-    schd_vp_df = (pd.read_parquet(schd_vp_url, 
-                       filters=[[("sched_rt_category", "in", ["schedule_and_vp", "schedule_only"])]],
-                       columns = [ "schedule_gtfs_dataset_key",
-                                    "caltrans_district",
-                                    "organization_name",
-                                    "name",
-                                    "sched_rt_category",
-                                    "service_date",]
-                                     )
-                     )
-
+    schd_vp_df = pd.read_parquet(
+        schd_vp_url, 
+        filters=[[("sched_rt_category", "in", ["schedule_and_vp", "schedule_only"])]],
+        columns = ["schedule_gtfs_dataset_key",
+                   "caltrans_district", 
+                   "organization_name", 
+                   "name", 
+                   "sched_rt_category", 
+                   "service_date"]
+    )
+    
+    schd_vp_df = schd_vp_df.assign(
+        caltrans_district = schd_vp_df.caltrans_district.map(portfolio_utils.CALTRANS_DISTRICT_DICT)
+    )
+    
+    # TODO: Check what the next step is doing? -
+    # The line before got rid of 07 - Los Angeles and now uses new name 
    
     schd_vp_df = schd_vp_df.loc[schd_vp_df.caltrans_district != '07 - Los Angeles']
     
