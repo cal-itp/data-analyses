d6d64b39584ea3facb6a937250e89c6207dd3c2f
amandaha8 <amandahastuyck@gmail.com>
2025-03-06 23:59:04 +0000

    updated gtfs digest yml to exclude dup orgs and include ferries

--- gtfs_digest/_operators_prep.py
+++ gtfs_digest/_operators_prep.py
@@ -1,12 +1,11 @@
-from shared_utils import catalog_utils
+from shared_utils import catalog_utils, portfolio_utils
 import pandas as pd
 
 GTFS_DATA_DICT = catalog_utils.get_catalog("gtfs_analytics_data")
     
 def operators_schd_vp_rt()->pd.DataFrame:
     """
-    Operators who have schedule only OR have 
-    both schedule and realtime data.
+    Generate the yaml for our portfolio.
     """
     schd_vp_url = f"{GTFS_DATA_DICT.digest_tables.dir}{GTFS_DATA_DICT.digest_tables.route_schedule_vp}.parquet"
     
@@ -21,6 +20,9 @@ def operators_schd_vp_rt()->pd.DataFrame:
                                      )
                      )
 
+    schd_vp_df = schd_vp_df.assign(
+        caltrans_district = schd_vp_df.caltrans_district.map(portfolio_utils.CALTRANS_DISTRICT_DICT)
+    )
     
     schd_vp_df2 = (
     schd_vp_df.dropna(subset="caltrans_district")
@@ -41,6 +43,20 @@ def operators_schd_vp_rt()->pd.DataFrame:
     .reset_index(drop=True)
     )
     
+    # Manually filter out certain operators for schedule_gtfs_dataset_keys
+    # that have multiple operators because keeping another value is preferable. 
+    operators_to_exclude = ["City of Alameda"]
+    schd_vp_df2 = schd_vp_df2.loc[~schd_vp_df2.organization_name.isin(operators_to_exclude)]
     
-    final = schd_vp_df2[["caltrans_district","organization_name"]]
+    # Keep only one instance of a schedule_gtfs_dataset_key
+    schd_vp_df3 = (
+    schd_vp_df2.drop_duplicates(
+        subset=[
+            "schedule_gtfs_dataset_key",
+        ]
+    )
+    .reset_index(drop=True)
+    )
+    
+    final = schd_vp_df3[["caltrans_district","organization_name"]]
     return final
