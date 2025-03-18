7e2a8727a938beb4a95302f045ade99eb62d801c
csuyat-dot <christian.suyat@dot.ca.gov>
2025-03-18 19:05:08 +0000

    adjusting the change col function, removed the year col from the groupby portion

--- ntd/annual_ridership_report/annual_ridership_module.py
+++ ntd/annual_ridership_report/annual_ridership_module.py
@@ -20,20 +20,22 @@ def add_change_columns(
     df: pd.DataFrame) -> pd.DataFrame:
     """
     Calculates (value) change of UPT from previous year.
-    Sorts the df by ntd id. then adds  new columns: 
+    Sorts the df by ntd id, year, mode, service. then shifts the upt value down one row (to the next year,mode,service row) . then adds  new columns: 
         1. previous year/month UPT
+        2. change_1yr
     """
 
     sort_cols2 =  ["ntd_id",
                    "year",
-                   #"mode", 
-                   #"tos",
+                   "mode", 
+                   "service",
                    #"period_month", 
                    #"period_year"
                   ] # got the order correct with ["period_month", "period_year"]! sorted years with grouped months
+    
     group_cols2 = ["ntd_id",
-                   #"mode", 
-                   #"tos"
+                   "mode", 
+                   "service"
                   ]
     
     #df[["period_year","period_month"]] = df[["period_year","period_month"]].astype(int)
