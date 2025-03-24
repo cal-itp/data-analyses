6b3340dea4ec4de352d8b131ab86b7dbc7a36b1b
amandaha8 <amandahastuyck@gmail.com>
2025-03-24 23:32:52 +0000

    rolled up all the metrics to be quarterly

--- gtfs_digest/merge_data.py
+++ gtfs_digest/merge_data.py
@@ -7,7 +7,7 @@ import geopandas as gpd
 import pandas as pd
 import numpy as np
 
-from segment_speed_utils import gtfs_schedule_wrangling, time_series_utils
+from segment_speed_utils import gtfs_schedule_wrangling, time_series_utils, segment_calcs
 from shared_utils import gtfs_utils_v2, publish_utils
 from update_vars import GTFS_DATA_DICT, SEGMENT_GCS, RT_SCHED_GCS, SCHED_GCS
 
@@ -224,52 +224,171 @@ def set_primary_typology(df: pd.DataFrame) -> pd.DataFrame:
 """
 Quarterly Rollup Functions
 """
-def quarterly_rollup(df:pd.DataFrame, metric_columns:list)->pd.DataFrame:
-    """
-    roll up months to each quarter for certain metrics.
-    """
-    quarterly_metrics = segment_calcs.calculate_weighted_averages(
-    
-    df=df,
-    group_cols=[
-        "quarter",
-        "Period",
-        "Organization",
-        "Route",
-        "dir_0_1",
-        "Direction",
-    ],
-    metric_cols= metric_columns,
-    weight_col="# Trips with VP",
-    )
-    return quarterly_metrics
+schd_metric_cols = [
+    "avg_scheduled_service_minutes",
+    "avg_stop_miles",
+    "frequency",
+    "total_scheduled_service_minutes",
+]
 
+groupby_cols = [
+    "schedule_gtfs_dataset_key",
+    "quarter",
+    "direction_id",
+    "time_period",
+    "route_id",
+]
+rt_metric_cols = [
+    "minutes_atleast1_vp",
+    "minutes_atleast2_vp",
+    "total_rt_service_minutes",
+    "total_vp",
+    "vp_in_shape",
+    "avg_rt_service_minutes",
+    "speed_mph",
+]
+rt_metric_no_weighted_avg = [
+    "is_early",
+    "is_ontime",
+    "is_late",
+]
+crosswalk_cols = [
+    "base64_url",
+    "organization_source_record_id",
+    "organization_name",
+    "caltrans_district",
+    "route_primary_direction",
+    "name",
+    "schedule_source_record_id",
+    "is_express",
+    "is_rapid",
+    "is_rail",
+    "is_coverage",
+    "is_downtown_local",
+    "is_local",
+    "service_date",
+    "typology",
+    "sched_rt_category",
+    "route_long_name",
+    "route_short_name",
+    "route_combined_name",
+]
+group_cols = [
+    "quarter",
+    "schedule_gtfs_dataset_key",
+    "route_id",
+    "direction_id",
+    "time_period",
+]
 def rollup_schd_qtr(peak_offpeak_df:pd.DataFrame)->pd.DataFrame:
     """
-    Roll up # Scheduled Trips to be on a quarterly basis
-    since this metric doesn't change very often. 
+    When rolling up # Scheduled Trips to be on a quarterly basis,
+    this needs to be divided by the number of months recorded
     """
     # Aggregate
     agg1 = (
     peak_offpeak_df.groupby(
-        ["quarter", "Period", "Organization", "Route", "dir_0_1", "Direction"]
+        group_cols + ["service_date"]
     )
-    .agg({"Date":"nunique","# scheduled trips": "sum"})
+    .agg({"service_date":"nunique","# scheduled trips": "sum"})
     .reset_index()
     )
     
     # If a quarter is complete with all 3 months, divide by 3
-    agg1.loc[agg1["Date"] == 3, "# scheduled trips"] = (
-    agg1.loc[agg1["Date"] == 3, "# scheduled trips"] / 3)
+    agg1.loc[agg1["service_date"] == 3, "# scheduled trips"] = (
+    agg1.loc[agg1["service_date"] == 3, "# scheduled trips"] / 3)
     
     # If a quarter is incomplete with only 2 months, divide by 2 
-    agg1.loc[agg1["Date"] == 2, "# scheduled trips"] = (
-    agg1.loc[agg1["Date"] == 2, "# scheduled trips"] / 2
+    agg1.loc[agg1["service_date"] == 2, "# scheduled trips"] = (
+    agg1.loc[agg1["service_date"] == 2, "# scheduled trips"] / 2
 )
     return agg1
+
+def quarterly_metrics(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    GTFS Digest is presented on a monthly candece.
+    Aggregate this dataframe to be on a quarterly grain
+    instead. 
+    """
+    # Create quarters
+    # Turn date to quarters
+    df["quarter"] = pd.PeriodIndex(df.service_date, freq="Q").astype("str")
+    
+    # Create copies of the original df before aggregating because I noticed applying
+    #  segment_calcs.calculate_weighted_averages impacts the original df
+    rt_df = df.copy()
+    schd_df = df.copy()
+    timeliness_df = df.copy()
+
+    # Calculate RT Metrics that need to have a weighted average
+    rt_metrics = segment_calcs.calculate_weighted_averages(
+        df=rt_df,
+        group_cols=groupby_cols,
+        metric_cols=rt_metric_cols,
+        weight_col="n_vp_trips",
+    )
+
+    # Calculate Scheduled Metrics that need to have a weighted average
+    schd_metrics = segment_calcs.calculate_weighted_averages(
+        df=schd_df,
+        group_cols=groupby_cols,
+        metric_cols=schd_metric_cols,
+        weight_col="n_scheduled_trips",
+    )
+    
+
+    # Calculate trips by timeliness which doesn't need weighted average
+    timeliness_df = timeliness_df[groupby_cols + rt_metric_no_weighted_avg]
+    timeliness_df2 = (
+        timeliness_df.groupby(groupby_cols)
+        .agg({"is_early": "sum", "is_ontime": "sum", "is_late": "sum"})
+        .reset_index()
+    )
+
+    # Create a crosswalk with string descriptives such as
+    # organization_name, route_long_name, etc that were excluded from the groupby_cols
+    crosswalk = df[groupby_cols + crosswalk_cols]
+
+    # Merge all the dataframes
+    m1 = (
+        pd.merge(rt_metrics, schd_metrics, on=groupby_cols)
+        .merge(timeliness_df2, on=groupby_cols)
+        .merge(crosswalk, on=groupby_cols)
+    )
+
+    # Re-calculate certain columns
+    m1["pct_in_shape"] = m1.vp_in_shape / m1.total_vp
+    m1["pct_rt_journey_atleast1_vp"] = (
+        m1.minutes_atleast1_vp / m1.total_rt_service_minutes
+    )
+    m1["pct_rt_journey_atleast2_vp"] = (
+        m1.minutes_atleast2_vp / m1.total_rt_service_minutes
+    )
+    m1["pct_sched_journey_atleast1_vp"] = (
+        m1.minutes_atleast1_vp / m1.total_scheduled_service_minutes
+    )
+    m1["pct_sched_journey_atleast2_vp"] = (
+        m1.minutes_atleast2_vp / m1.total_scheduled_service_minutes
+    )
+    m1["vp_per_minute"] = m1.total_vp / m1.total_rt_service_minutes
+    m1["rt_sched_journey_ratio"] = (
+        m1.total_rt_service_minutes / m1.total_scheduled_service_minutes
+    )
+
+    # Rearrange columns to match original df
+    m1 = m1[list(df.columns)]
+
+    # Drop service_date & duplicates
+    m1 = (m1
+          .drop(columns=["service_date"])
+          .drop_duplicates(subset = group_cols)
+          .reset_index(drop=True))
+    return m1
+
 """
 Merging Functions
 """
+
 def merge_data_sources_by_route_direction(
     df_schedule: pd.DataFrame,
     df_rt_sched: pd.DataFrame,
@@ -345,8 +464,8 @@ if __name__ == "__main__":
         rt_dates.y2025_dates
     )
     
-    DIGEST_RT_SCHED = GTFS_DATA_DICT.digest_tables.route_schedule_vp 
-    DIGEST_SEGMENT_SPEEDS = GTFS_DATA_DICT.digest_tables.route_segment_speeds
+    DIGEST_RT_SCHED_MONTH = GTFS_DATA_DICT.digest_tables.monthly_route_schedule_vp 
+    DIGEST_RT_SCHED_QTR = GTFS_DATA_DICT.digest_tables.quarterly_route_schedule_vp 
     
     # These are public schedule_gtfs_dataset_keys
     public_feeds = gtfs_utils_v2.filter_to_public_schedule_gtfs_dataset_keys()
@@ -388,29 +507,13 @@ if __name__ == "__main__":
     
     # Save metrics on a monthly candence.
     df.to_parquet(
-        f"{RT_SCHED_GCS}{DIGEST_RT_SCHED}.parquet"
+        f"{RT_SCHED_GCS}{DIGEST_RT_SCHED_MONTH}.parquet"
     )
-   
-    # Roll up monthly metrics to quarterly
-    # Filter for only rows that are "all day" statistics
-    all_day = df.loc[df["Period"] == "all_day"].reset_index(drop=True)
     
-    # Filter for only rows that are "peak/offpeak" statistics
-    peak_offpeak_df = df.loc[df["Period"] != "all_day"].reset_index(drop=True)
-    
-    # Roll up some metrics that don't change too much
-    # to be quarterly instead of monthly
-    quarter_rollup_all_day = quarterly_rollup(all_day, [
-        "Average VP per Minute",
-        "% VP within Scheduled Shape",
-        "Average Scheduled Service (trip minutes)",
-        "ruler_100_pct",
-        "ruler_for_vp_per_min"
-    ]) 
-    
-    total_scheduled_trips = rollup_schd_qtr(peak_offpeak_df)
-    
-    # Merge these 2 
-    m1 = pd.merge(quarter_rollup_all_day, total_scheduled_trips)
+    # Roll up metrics to be quarterly & Save
+    quarter_df = quarterly_metrics(df)
+    quarter_df.to_parquet(
+        f"{RT_SCHED_GCS}{DIGEST_RT_SCHED_QTR}.parquet"
+    )
     print("Saved Digest RT")
     
