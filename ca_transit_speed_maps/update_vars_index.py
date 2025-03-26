e821f14a236ef0cfcc19d5a171247c3fc04076a6
Eric Dasmalchi <edasmalchi@gmail.com>
2025-03-26 21:53:36 +0000

    remove legacy speedmap site generation, refactor siuba to vanilla pandas

--- ca_transit_speed_maps/update_vars_index.py
+++ ca_transit_speed_maps/update_vars_index.py
@@ -1,8 +1,3 @@
-# import os
-# os.environ["CALITP_BQ_MAX_BYTES"] = str(1_000_000_000_000) ## 1TB?
-# os.environ['USE_PYGEOS'] = '0'
-
-from siuba import *
 import pandas as pd
 import geopandas as gpd
 import datetime as dt
@@ -29,10 +24,10 @@ def build_speedmaps_index(analysis_date: dt.date) -> pd.DataFrame:
     (VCTC combined feeds, SD Airport and SDMTS...)
     '''
     path = f'{catalog.speedmap_segments.dir}{catalog.speedmap_segments.segment_timeofday}_{analysis_date}.parquet'
-    speedmap_segs = gpd.read_parquet(path) >> distinct(
-        _.organization_name, _.organization_source_record_id, _.name, _.base64_url)
+    org_cols = ['organization_name', 'organization_source_record_id', 'name', 'base64_url']
+    speedmap_segs = gpd.read_parquet(path)[org_cols].drop_duplicates()
     districts = schedule_rt_utils.filter_dim_county_geography(analysis_date)
-    new_ix = speedmap_segs >> inner_join(_, districts, on = 'organization_name')
+    new_ix = speedmap_segs.merge(districts, on = 'organization_name')
     new_ix['status'] = 'speedmap_segs_available'
     new_ix['analysis_date'] = analysis_date
     return new_ix
@@ -41,5 +36,4 @@ if __name__ == "__main__":
     
     print(f'analysis date from shared_utils/rt_dates: {ANALYSIS_DATE}')
     speedmaps_index = build_speedmaps_index(ANALYSIS_DATE)
-    # speedmaps_index = rt_utils.check_intermediate_data(speedmaps_index)
     speedmaps_index.to_parquet(PROGRESS_PATH)
