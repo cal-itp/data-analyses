af8bc19e7df35addaf65526e5167643bb64c8487
csuyat-dot <christian.suyat@dot.ca.gov>
2025-03-26 18:44:00 +0000

    testing facets on explore noteobok for report year. started to put together a script and parameterized notebook

---
+++ ntd/new_transit_metrics/new_transit_metrics.py
@@ -0,0 +1,12 @@
+"""
+functions for the new transit metrics portfolio
+"""
+
+def make_new_transit_metric_data():
+    """
+    Reads in annual NTD data from the warehoure. Merges with ntd_id-to-rtpa crosswalk
+    """
+
+def transit_metics
+
+if __name__ = "__main__"
