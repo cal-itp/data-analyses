39236269458cba4e722fbb749aea84c356e5f414
Anna Paten <anna.paten@dot.ca.gov>
2025-04-14 22:57:32 +0000

    geocoding working with geopy

--- ntd/ntd_transit_facilities/constants.py
+++ ntd/ntd_transit_facilities/constants.py
@@ -12,4 +12,10 @@ AGENCY_NAME = "Agency Name"
 FACILITY_TYPE = "Facility Type"
 FACILITY_NAME = "Facility Name"
 NTD_ADDRESS_FIELDS = (STREET_ADDRESS, CITY, STATE, ZIP_CODE)
-DEFAULT_TOOLTIP_FIELDS = (AGENCY_NAME, FACILITY_NAME, FACILITY_TYPE, "Square Feet", "Number of Parking Spaces", "Section of a Larger Facility", "Administrative/Maintenance Facility Flag", "Notes", "Primary Mode Served", "geometry_geocoded", "geocode_success")
+DEFAULT_TOOLTIP_FIELDS = (AGENCY_NAME, FACILITY_NAME, FACILITY_TYPE, "Square Feet", "Number of Parking Spaces", "Section of a Larger Facility", "Administrative/Maintenance Facility Flag", "Notes", "Primary Mode Served", "Geometry Geocoded", "Geocode Result Address")
+
+# Geocoding parameters
+DOTENV_PATH = "_env"
+GOOGLE_MAPS_API_KEY_ENV = "GOOGLE_MAPS_API_KEY"
+GOOGLE_MAPS_ENGINE_NAME = "GoogleV3"
+GEOPANDAS_ADDRESS_NAME = "address"
