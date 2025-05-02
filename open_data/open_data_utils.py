69a521c601d55486286db64e92ca1119840c2636
Eric Dasmalchi <edasmalchi@gmail.com>
2025-05-02 21:56:46 +0000

    remove hardcoded hqta and speeds renames

--- open_data/open_data_utils.py
+++ open_data/open_data_utils.py
@@ -183,30 +183,3 @@ def esri_truncate_columns(columns: list | pd.Index) -> dict:
     truncated_dict = dict(zip(truncated_cols, columns))
     truncated_dict = {key: truncated_dict[key] for key in truncated_dict.keys() if key != truncated_dict[key]}
     return truncated_dict
-
-# # Rename columns when shapefile truncates
-# RENAME_HQTA = {
-#     "agency_pri": "agency_primary",
-#     "agency_sec": "agency_secondary",
-#     "hqta_detai": "hqta_details",
-#     "base64_url": "base64_url_primary",
-#     "base64_u_1": "base64_url_secondary",  
-#     "org_id_pri": "org_id_primary",
-#     "org_id_sec": "org_id_secondary",
-#     "avg_trips_": "avg_trips_per_peak_hr"
-# }
-
-# RENAME_SPEED = {
-#     "stop_seque": "stop_sequence",
-#     "time_of_da": "time_of_day",
-#     "time_perio": "time_period",
-#     "district_n": "district_name",
-#     "direction_": "direction_id",
-#     "common_sha": "common_shape_id",
-#     "avg_sched_": "avg_sched_trip_min", 
-#     "avg_rt_tri": "avg_rt_trip_min",
-#     "caltrans_d": "district_name",
-#     "organization_source_record_id": "org_id",
-#     "organization_name": "agency",
-#     "stop_pair_": "stop_pair_name"
-# }
