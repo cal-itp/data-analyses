13544e9d9e10e347a3a704d220b0db01252101b4
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-05-29 18:12:49 +0000

    look at schedule changes for metro transit center opening

--- lax_metro/download_dim_tables.py
+++ lax_metro/download_dim_tables.py
@@ -8,53 +8,52 @@ from siuba import *
 
 from utils import subset_feeds
 
+def find_feeds(operator_list: list) -> dict:
+    """
+    Find the feed_keys for operators of interest,
+    return a dictionary mapping feed_keys to gtfs_dataset_name.
+    """
+    df = pd.read_parquet(
+        "daily_feeds.parquet",
+        filters = [[("gtfs_dataset_name", "in", operator_list)]],
+    )
+
+    last_feed = pd.merge(
+        df,
+        df.groupby(["gtfs_dataset_name"]).agg({"date": "max"}).reset_index(),
+        on = ["gtfs_dataset_name", "date"],
+        how = "inner"
+    )
+    
+    feed_to_name_dict = dict(
+        zip(last_feed.feed_key, last_feed.gtfs_dataset_name)
+    )
+    
+    return feed_to_name_dict
+
+    
 if __name__ == "__main__":
     
-    daily_feeds = (
-        tbls.mart_gtfs.fct_daily_schedule_feeds()
+    (tbls.mart_gtfs.fct_daily_schedule_feeds()
         >> collect()
-    )
+    ).to_parquet("daily_feeds.parquet")
 
-    daily_feeds.to_parquet("daily_feeds.parquet")
-
-    dim_schedule_feeds = (
-        tbls.mart_gtfs.dim_schedule_feeds()
-        >> filter(_["key"].isin(subset_feeds))
+    feed_to_name_dict = find_feeds(utils.la_operators)
+    print(feed_to_name_dict)
+    
+    (tbls.mart_gtfs.dim_calendar()
+        >> filter(_.feed_key.isin(subset_feeds))
         >> collect()
-    )
+    ).to_parquet("dim_calendar.parquet")
 
-    dim_schedule_feeds.to_parquet("dim_schedule_feeds.parquet")
-
-    dim_calendar = (
-        tbls.mart_gtfs.dim_calendar()
-        >> filter(_["feed_key"].isin(subset_feeds))
+    (tbls.mart_gtfs.dim_trips()
+        >> filter(_.feed_key.isin(subset_feeds))
         >> collect()
-    )
+    ).to_parquet("dim_trips.parquet")
 
-    dim_calendar.to_parquet("dim_calendar.parquet")
-
-    dim_calendar_dates = (
-        tbls.mart_gtfs.dim_calendar_dates()
-        >> filter(_["feed_key"].isin(subset_feeds))
+    (tbls.mart_gtfs.dim_routes()
+        >> filter(_.feed_key.isin(subset_feeds))
         >> collect()
-    )
-
-    dim_calendar_dates.to_parquet("dim_calendar_dates.parquet")
-
-    dim_trips = (
-        tbls.mart_gtfs.dim_trips()
-        >> filter(_["feed_key"].isin(subset_feeds))
-        >> collect()
-    )
-
-    dim_trips.to_parquet("dim_trips.parquet")
-
-    dim_routes = (
-        tbls.mart_gtfs.dim_routes()
-        >> filter(_["feed_key"].isin(subset_feeds))
-        >> collect()
-    )
-
-    dim_routes.to_parquet("dim_routes.parquet")
+    ).to_parquet("dim_routes.parquet")
 
 
