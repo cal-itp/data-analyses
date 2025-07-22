d8db82676554717c46f6839735ba38f495eceee7
Eric Dasmalchi <edasmalchi@gmail.com>
2025-07-22 17:34:34 +0000

    draft tsi by agency

--- transit_service_intensity/utils.py
+++ transit_service_intensity/utils.py
@@ -3,15 +3,17 @@ from calitp_data_analysis import geography_utils
 from shared_utils import catalog_utils, rt_dates, rt_utils
 import pandas as pd
 import geopandas as gpd
+import google.auth
 
 catalog = intake.open_catalog("*.yml")
+credentials, _ = google.auth.default()
 
 def read_census_tracts(
     analysis_date: str,
     crs: str = geography_utils.CA_NAD83Albers_m
 ) -> gpd.GeoDataFrame:
     census_tracts = (
-        catalog.calenviroscreen_lehd_by_tract.read()
+        catalog.calenviroscreen_lehd_by_tract(geopandas_kwargs={"storage_options": {"token": credentials}}).read()
         .to_crs(crs)
         [["Tract", "pop_sq_mi", "Population",
           "geometry"]]
