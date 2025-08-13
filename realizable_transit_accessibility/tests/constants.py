924e0cbe354eb9f4f3020a668621338cc953bd5f
Anna Paten <anna.paten@dot.ca.gov>
2025-08-13 22:04:21 +0000

    cleaned up test data and constants (#1600)

--- realizable_transit_accessibility/tests/constants.py
+++ realizable_transit_accessibility/tests/constants.py
@@ -1,3 +1,4 @@
+# Desired columns in stop_times output
 TEST_STOP_TIMES_DESIRED_COLUMNS_DEFAULT = [
     "trip_id",
     "arrival_time",
@@ -16,6 +17,7 @@ TEST_STOP_TIMES_DESIRED_COLUMNS_EXTRA_COLUMNS = [
     "stop_sequence",
 ]
 
+# Collumn definitions in input data
 RT_ARRIVAL_SEC = "rt_arrival_sec"
 TRIP_INSTANCE_KEY = "trip_instance_key"
 SCHEDULE_ARRIVAL_SEC = "schedule_arrival_sec"
@@ -24,22 +26,13 @@ TRIP_ID = "trip_id"
 STOP_ID = "stop_id"
 SCHEDULE_GTFS_DATASET_KEY = "schedule_gtfs_dataset_key"
 
-# Rename these values if column names change in the schedule/rt dataset
-# Scheduled arrival time, in seconds after twelve hours before noon 
 SCHEDULE_ARRIVAL_SEC_NAME = "scheduled_arrival_sec"
-# RT arrival time, in seconds after twelve hours before noon
 RT_ARRIVAL_SEC_NAME = "rt_arrival_sec"
-# The stop sequence value
 STOP_SEQUENCE_NAME = "stop_sequence"
-# The column containing the trip instance key, that uniquely identifies trips, including between different agencies
 TRIP_INSTANCE_KEY_NAME = "trip_instance_key"
-# The column containing the trip id, which can be used to merge trips from the rt table to the schedule feed
 TRIP_ID_NAME = "trip_id"
-# The coulmn containing the stop id, which should be consistent between the rt table and the schedule feed
 STOP_ID_NAME = "stop_id"
-# The schedule gtfs dataset key
 SCHEDULE_GTFS_DATASET_KEY_NAME = "schedule_gtfs_dataset_key"
-
 
 TEST_DEFAULT_COLUMN_MAP = {
     SCHEDULE_ARRIVAL_SEC: SCHEDULE_ARRIVAL_SEC_NAME,
@@ -49,14 +42,16 @@ TEST_DEFAULT_COLUMN_MAP = {
     TRIP_ID: TRIP_ID_NAME,
     STOP_ID: STOP_ID_NAME,
     SCHEDULE_GTFS_DATASET_KEY: SCHEDULE_GTFS_DATASET_KEY_NAME,
-    RT_ARRIVAL_SEC: "gap_imputed_sec",
+    RT_ARRIVAL_SEC: RT_ARRIVAL_SEC_NAME,
 }
 
+# Kwargs to easily pass to tests
+# For most tests
 DEFAULT_TEST_FEED_GENERATION_KWARGS = {
     "stop_times_desired_columns": TEST_STOP_TIMES_DESIRED_COLUMNS_DEFAULT,
     "stop_times_table_columns": TEST_DEFAULT_COLUMN_MAP,
 }
-
+# For tests checking that non-default columns are present
 EXTRA_COLUMNS_FEED_GENERATION_KWARGS = {
     **DEFAULT_TEST_FEED_GENERATION_KWARGS,
     "stop_times_desired_columns": TEST_STOP_TIMES_DESIRED_COLUMNS_EXTRA_COLUMNS
