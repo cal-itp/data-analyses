98d98351cae53891c03008cf0e0208fe7d050873
Eric Dasmalchi <edasmalchi@gmail.com>
2025-08-14 22:40:43 +0000

    refactor to pandas

--- sb125_analyses/vmt_transit_sketch/_utils.py
+++ sb125_analyses/vmt_transit_sketch/_utils.py
@@ -7,7 +7,7 @@ GCS_PATH = 'gs://calitp-analytics-data/data-analyses/sb125/vmt_transit_sketch/'
 
 def get_tract_geoms():
     ca_tracts = pygris.tracts(state='CA', year=2020)
-    ca_tracts = ca_tracts >> select(_.GEOID, _.geometry)
+    ca_tracts = ca_tracts[['GEOID', 'geometry']]
     ca_tracts = ca_tracts.to_crs(CA_NAD83Albers)
     
     return ca_tracts
