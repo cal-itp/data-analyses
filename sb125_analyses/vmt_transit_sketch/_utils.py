ca9310dfebf47b96477fdb57226096a44b3e84d7
Eric Dasmalchi <edasmalchi@gmail.com>
2025-08-14 23:20:56 +0000

    refactor nb

--- sb125_analyses/vmt_transit_sketch/_utils.py
+++ sb125_analyses/vmt_transit_sketch/_utils.py
@@ -1,13 +1,13 @@
 import pygris
 import geopandas as gpd
 from siuba import *
-from calitp_data_analysis.geography_utils import CA_NAD83Albers
+from calitp_data_analysis.geography_utils import CA_NAD83Albers_m
 
 GCS_PATH = 'gs://calitp-analytics-data/data-analyses/sb125/vmt_transit_sketch/'
 
 def get_tract_geoms():
     ca_tracts = pygris.tracts(state='CA', year=2020)
     ca_tracts = ca_tracts[['GEOID', 'geometry']]
-    ca_tracts = ca_tracts.to_crs(CA_NAD83Albers)
+    ca_tracts = ca_tracts.to_crs(CA_NAD83Albers_m)
     
     return ca_tracts
