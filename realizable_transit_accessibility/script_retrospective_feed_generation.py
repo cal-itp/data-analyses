8083030a7ebacc7160e1411356640ff03785bc39
Anna Paten <anna.paten@dot.ca.gov>
2025-08-21 16:31:40 +0000

    ran black, added a couple comments (#1590)

--- realizable_transit_accessibility/script_retrospective_feed_generation.py
+++ realizable_transit_accessibility/script_retrospective_feed_generation.py
@@ -12,10 +12,20 @@ from shared_utils import catalog_utils, gtfs_utils_v2, rt_dates
 from retrospective_feed_generation.warehouse_utils import *
 import argparse
 
-def process_all_feeds(rt_dates: Iterable[str], schedule_local_paths: Iterable[str], schedule_names: Iterable[str], output_local_paths: Iterable[str], max_stop_gap: int = 5) -> None:
+
+def process_all_feeds(
+    rt_dates: Iterable[str],
+    schedule_local_paths: Iterable[str],
+    schedule_names: Iterable[str],
+    output_local_paths: Iterable[str],
+    max_stop_gap: int = 5,
+) -> None:
+    """Process a set of feeds given dates, schedule paths, schedule names, and output paths"""
     gtfs_dataset_key_dict = {}
     schedule_rt_stop_times_table_dict = {}
+    # For each date, get the warehouse data corresponding with that date and the requested feeds
     for rt_date in np.unique(rt_dates):
+        # Get gtfs dataset keys from the warehouse for the target date
         gtfs_dataset_keys = (
             gtfs_utils_v2.schedule_daily_feed_to_gtfs_dataset_name(
                 selected_date=rt_date, keep_cols=["name", "gtfs_dataset_key"]
@@ -23,18 +33,42 @@ def process_all_feeds(rt_dates: Iterable[str], schedule_local_paths: Iterable[st
             .set_index("name")
             .loc[np.unique(schedule_names), "gtfs_dataset_key"]
         )
-        schedule_rt_stop_times_table_dict[rt_date] = get_schedule_rt_stop_times_table(gtfs_dataset_keys.values, rt_date)
+        # Get the schedule rt stop times table for the specified tables, and save them in a dict keyed by date
+        schedule_rt_stop_times_table_dict[rt_date] = get_schedule_rt_stop_times_table(
+            gtfs_dataset_keys.values, rt_date
+        )
+        # Save the set the series of gtfs dataset keys, and save them in a dict keyed by date
         gtfs_dataset_key_dict[rt_date] = gtfs_dataset_keys
+    # Create a dict of gtfs dataset keys keyed by schedule name (index) and date (columns)
     gtfs_dataset_key_df = pd.DataFrame(gtfs_dataset_key_dict)
-    for rt_date, schedule_name, schedule_local_path, output_local_path in zip(rt_dates, schedule_names, schedule_local_paths, output_local_paths):
+    # For each feed, get the input table and geenrate a feed
+    for rt_date, schedule_name, schedule_local_path, output_local_path in zip(
+        rt_dates, schedule_names, schedule_local_paths, output_local_paths
+    ):
         gtfs_dataset_key = gtfs_dataset_key_df.at[schedule_name, rt_date]
         schedule_rt_table = schedule_rt_stop_times_table_dict[rt_date]
-        schedule_rt_table_one_feed = schedule_rt_table.loc[schedule_rt_table["schedule_gtfs_dataset_key"] == gtfs_dataset_key]
-        process_table_row(schedule_rt_table_one_feed, rt_date, schedule_local_path, output_local_path, max_stop_gap=max_stop_gap)
+        schedule_rt_table_one_feed = schedule_rt_table.loc[
+            schedule_rt_table["schedule_gtfs_dataset_key"] == gtfs_dataset_key
+        ]
+        # Generate a feed
+        process_individual_feed(
+            schedule_rt_table_one_feed,
+            rt_date,
+            schedule_local_path,
+            output_local_path,
+            max_stop_gap=max_stop_gap,
+        )
 
-def process_table_row(schedule_rt_table: pd.DataFrame, rt_date: str, schedule_local_path: str, output_local_path: str, max_stop_gap: int = 5) -> None:
-    """Process a row of the input table""" # TODO: make these docstrings actually useful
-    # Get the schedule rt table with 
+
+def process_individual_feed(
+    schedule_rt_table: pd.DataFrame,
+    rt_date: str,
+    schedule_local_path: str,
+    output_local_path: str,
+    max_stop_gap: int = 5,
+) -> None:
+    """Get retrospective accessibility for one row of the input feed"""
+    # Get the schedule rt table with trips that did not generate VP trips dropped
     schedule_rt_stop_times_single_agency = filter_non_rt_trips(
         schedule_rt_table,
         col.DEFAULT_COLUMN_MAP,
@@ -42,9 +76,11 @@ def process_table_row(schedule_rt_table: pd.DataFrame, rt_date: str, schedule_lo
 
     # Impute certain unrealistic (first/last, nonmonotonic, short gap) stop times
     # Logic here is wip
-    schedule_rt_stop_times_single_agency["gap_imputed_sec"] = impute_unrealistic_rt_times(
-        schedule_rt_stop_times_single_agency,
-        max_gap_length=max_stop_gap,
+    schedule_rt_stop_times_single_agency["gap_imputed_sec"] = (
+        impute_unrealistic_rt_times(
+            schedule_rt_stop_times_single_agency,
+            max_gap_length=max_stop_gap,
+        )
     )
 
     # Load the schedule feed using gtfs-lite and filter it
@@ -77,10 +113,11 @@ def process_table_row(schedule_rt_table: pd.DataFrame, rt_date: str, schedule_lo
     output_feed.write_zip(output_local_path)
     return output_local_path
 
+
 if __name__ == "__main__":
     # Read command line args
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_table", type=str) # The table with input information
+    parser.add_argument("input_table", type=str)  # The table with input information
     args = parser.parse_args()
     # Read the input table
     input_table = pd.read_csv(args.input_table)
