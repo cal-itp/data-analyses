16d2705c47d36ca27ea02e08dc36ca5f979e25ae
Rae Bonfanti <raebot@users.noreply.github.com>
2025-09-26 16:14:04 -0700

    Dependency inject instead of mock in test_catalog_utils
    Co-authored-by: Doc Ritezel <ohrite@users.noreply.github.com>

--- _shared_utils/tests/shared_utils/test_catalog_utils.py
+++ _shared_utils/tests/shared_utils/test_catalog_utils.py
@@ -7,7 +7,7 @@ import shared_utils.catalog_utils as catalog_utils
 
 class TestCatalogUtils:
     @pytest.fixture()
-    def setup(self, mocker, tmp_path: Path) -> Callable:
+    def setup(self, tmp_path: Path) -> Callable:
         def setup_with(nested_directory: str = "", in_home_directory: bool = True) -> list[Path]:
             home_path = tmp_path.joinpath("home")
             home_path.mkdir()
@@ -24,10 +24,7 @@ class TestCatalogUtils:
             shared_utils_path = repo_path.joinpath("_shared_utils/shared_utils")
             shared_utils_path.mkdir(parents=True)
 
-            mocker.patch("shared_utils.catalog_utils.Path.home", return_value=home_path)
-            mocker.patch("shared_utils.catalog_utils.Path.cwd", return_value=current_path)
-
-            return [current_path, shared_utils_path]
+            return [home_path, current_path, shared_utils_path]
 
         return setup_with
 
@@ -35,36 +32,36 @@ class TestCatalogUtils:
         self,
         setup: Callable,
     ) -> None:
-        setup()
+        home_path, current_path, _ = setup()
 
         with pytest.raises(FileNotFoundError, match="No such catalog file found"):
-            catalog_utils.get_catalog_file("test-file")
+            catalog_utils.get_catalog_file("test-file", home_path, current_path)
 
     def test_get_catalog_file_repo_in_home_directory(
         self,
         setup: Callable,
     ) -> None:
-        _, shared_utils_path = setup()
+        home_path, current_path, shared_utils_path = setup()
         shared_utils_path.joinpath("test-file.yml").touch()
 
-        filename = catalog_utils.get_catalog_file("test-file")
+        filename = catalog_utils.get_catalog_file("test-file", home_path, current_path)
         assert filename == shared_utils_path.joinpath("test-file.yml")
 
     def test_get_catalog_file_repo_nested_in_home_directory(
         self,
         setup: Callable,
     ) -> None:
-        _, shared_utils_path = setup("caltrans/")
+        home_path, current_path, shared_utils_path = setup("caltrans/")
         shared_utils_path.joinpath("test-file.yml").touch()
 
-        filename = catalog_utils.get_catalog_file("test-file")
+        filename = catalog_utils.get_catalog_file("test-file", home_path, current_path)
         assert filename == shared_utils_path.joinpath("test-file.yml")
 
     def test_get_catalog_file_repo_outside_of_home_directory(
         self,
         setup: Callable,
     ) -> None:
-        setup(in_home_directory=False)
+        home_path, current_path, _ = setup(in_home_directory=False)
 
         with pytest.raises(RuntimeError, match="The data-analyses repo should be located in your home directory."):
-            catalog_utils.get_catalog_file("test-file")
+            catalog_utils.get_catalog_file("test-file", home_path, current_path)
