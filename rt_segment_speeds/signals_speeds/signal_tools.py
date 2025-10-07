4d42d4864606ba154b895f08142c557865d973c4
Anna Paten <anna.paten@dot.ca.gov>
2025-10-07 22:15:32 +0000

    signal speedmap code cleanup (#1692)

--- rt_segment_speeds/signals_speeds/signal_tools.py
+++ rt_segment_speeds/signals_speeds/signal_tools.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 from calitp_data_analysis.geography_utils import WGS84, CA_NAD83Albers_m
 from shared_utils.rt_utils import arrowize_by_frequency, try_parallel
 from typing import Hashable
-
+from constants import SIGNAL_OWNER_NAME
 
 def sjoin_signals(
     signals_gdf: gpd.GeoDataFrame,
@@ -12,11 +12,13 @@ def sjoin_signals(
     signals_buffer_distance: int = 150,
 ):
     """
-    signals_gdf: one-off format from traffic ops. primarily a spatial process,
-    so exclude freeway ramp meters (only relevant to traffic joining fwy,
-    which usually isn't transit)
-    segments_gdf: geometry is polygons (buffered)
-    segments_lines_gdf: geometry is linestrings (need for later approaching calc)
+    Sjoin the signals and segments gdf within the buffer distance, keeping both geometries as follows
+      - Segment geometry -> line_geom, main geometry
+      - Signal geometry -> signal_pt_geom, kept as a GeoSeries within the GDF
+    signals_gdf: GDF with signal points
+    segments_lines_gdf: GDF with speedmaps segments as lines
+    unique_identifier_signals: Name of a column in signals_gdf that uniquely identifies a traffic signal
+    signals_buffer_distance: The distance around each signal to buffer, defaults to 150
     """
 
     # Get buffered signals with just join fields and geometry
@@ -49,7 +51,7 @@ def sjoin_signals(
 def filter_points_along_corridor(
     points_gdf: gpd.GeoDataFrame, corridor_gdf: gpd.GeoDataFrame, buffer_distance: int
 ) -> gpd.GeoDataFrame:
-    """Filter points that are along the corridors in corridor_gdf"""
+    """Filter points in points_gdf that are within buffer_distance of the corridors in corridor_gdf"""
     return points_gdf.clip(
         corridor_gdf.to_crs(CA_NAD83Albers_m).buffer(buffer_distance)
     )
@@ -60,6 +62,20 @@ def add_transit_metrics_to_signals(
     sjoined_signals_segments: gpd.GeoDataFrame,
     unique_identifier_signals: Hashable,
 ):
+    """
+    Return the signals_gdf with the following additional metrics:
+    - trips_hr_sch: the total trips per hour associated with the signal
+    - route_names_aggregated: the comma-separated short names of each route associated with the signal. Maps 1:1 with organization_names_aggregated
+    - organization_names_aggregated: the comma-separated organization names of each route associated with the signal. Maps 1:1 with route_names_aggregated
+    
+    Parameters:
+    - signals_gdf - a points gdf of traffic signals
+    - sjoined_signals_segments - speedmaps segments. Must be as returned from sjoin_signals
+    - unique_identifier_signals - a unique identifier for each signal. Must be the same as passed to sjoin_signals
+    
+    Returns:
+    signals_gdf with the specified additional fields
+    """
     # Get a gdf of segments with one entry per signal and shape_id
     # Get the distance from the segment to the associated signal
     sjoined_signals_segments["distance_to_signal"] = (
@@ -85,7 +101,6 @@ def add_transit_metrics_to_signals(
     # Get all the routes and organizations that serve a stop
     def aggregate_names(names):
         return ", ".join(names.drop_duplicates().dropna())
-
     signals_gdf_with_metrics["route_names_aggregated"] = (
         speedmaps_grouped_by_signal["route_short_name"]
     ).agg(aggregate_names)
@@ -97,6 +112,7 @@ def add_transit_metrics_to_signals(
 
 
 def ready_speedmap_segments_for_display(sjoined_segments_gdf):
+    """Arrowize and format fields for display for sjoined_segments_gdf"""
     arrowized_gdf = (
         sjoined_segments_gdf.drop(["signal_pt_geom"], axis=1)
         .to_crs(CA_NAD83Albers_m)
@@ -113,9 +129,17 @@ def ready_speedmap_segments_for_display(sjoined_segments_gdf):
 
 
 def ready_signals_for_display(signals_gdf, buffer_distance, ownership_map):
+    """
+    Buffer and format fields for display for a single signals_gdf
+
+    Parameters:
+    signals_gdf - a gdf of signals. Must contain 'signal_owner' as a field.
+    buffer_distance - the distance around each signal to buffer (corresponds to the render size of each signal)
+    ownership_map - maps signal_owner values to ints to be used for color mapping
+    """
     buffered_signals = gpd.GeoDataFrame(
         data=signals_gdf.rename(columns={"trips_hr_sch": "Trips/Hour"}),
         geometry=signals_gdf.to_crs(CA_NAD83Albers_m).buffer(buffer_distance),
     ).reset_index()
-    buffered_signals["color_key"] = buffered_signals["signal_owner"].map(ownership_map)
+    buffered_signals["color_key"] = buffered_signals[SIGNAL_OWNER_NAME].map(ownership_map)
     return buffered_signals
