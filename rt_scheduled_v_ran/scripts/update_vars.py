d2f2dbe91449001848e1bf4caa96c7cb94ef5502
Eric Dasmalchi <edasmalchi@gmail.com>
2025-11-10 20:35:44 +0000

    run speeds pipeline for additional oct dates

--- rt_scheduled_v_ran/scripts/update_vars.py
+++ rt_scheduled_v_ran/scripts/update_vars.py
@@ -7,7 +7,7 @@ apr2024_week = rt_dates.get_week("apr2024", exclude_wed=True)
 oct2024_week = rt_dates.get_week("oct2024", exclude_wed=True)
 
 
-analysis_date_list = [rt_dates.DATES["oct2025b"]]
+analysis_date_list = [rt_dates.DATES["oct2025d"]]
 
 GTFS_DATA_DICT = catalog_utils.get_catalog("gtfs_analytics_data")
 
