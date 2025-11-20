5ac2132a00636b4bd28a289615c644dfa1c5b188
Rae Bonfanti <raebot@users.noreply.github.com>
2025-11-20 12:27:51 -0800

    rm submodule imports and __all__ from __init__.py
    Importing all those submodules in shared_utils/__init__.py meant anyone
    importing anything from share_utils was having to load all of the
    submodules even if their work only needed one or two submodules. This
    added significant drag to imports and resulted in circular import
    warnings in some cases.
    
    __all__ is simply to allow * imports, but I didn't see any usages of
    this in the entire repo.

--- _shared_utils/shared_utils/__init__.py
+++ _shared_utils/shared_utils/__init__.py
@@ -1,39 +1,12 @@
 import sys
 
 from calitp_data_analysis.sql import get_engine
+
 from sqlalchemy.orm import sessionmaker
 
 if hasattr(sys, "_called_from_test"):
     db_engine = get_engine(project="cal-itp-data-infra-staging")
     DBSession = sessionmaker(db_engine)
 else:
-    from . import (
-        arcgis_query,
-        catalog_utils,
-        dask_utils,
-        geo_utils,
-        gtfs_utils_v2,
-        portfolio_utils,
-        publish_utils,
-        rt_dates,
-        rt_utils,
-        schedule_rt_utils,
-        time_helpers,
-    )
-
-    __all__ = [
-        "arcgis_query",
-        "catalog_utils",
-        "dask_utils",
-        "geo_utils",
-        "gtfs_utils_v2",
-        "portfolio_utils",
-        "publish_utils",
-        "rt_dates",
-        "rt_utils",
-        "schedule_rt_utils",
-        "time_helpers",
-    ]
-
     db_engine = get_engine()
     DBSession = sessionmaker(db_engine)
