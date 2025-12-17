46b4df00e0dfb28fef477f4970df219aa9ffaa5b
Rae Bonfanti <raebot@users.noreply.github.com>
2025-12-17 12:25:16 -0800

    Allow setting different test dataset for models in shared_utils

--- _shared_utils/shared_utils/models/base.py
+++ _shared_utils/shared_utils/models/base.py
@@ -1,8 +1,13 @@
+import os
 import sys
 
 
 def get_table_name(dataset, table):
     if hasattr(sys, "_called_from_test"):
-        return f"test_shared_utils.{table}"
+        if "TEST_DATASET" in os.environ:
+            return f"{os.environ['TEST_DATASET']}.{table}"
+        else:
+            raise KeyError("You must set TEST_DATASET in your conftest.py to use these models in tests.")
+
     else:
         return f"{dataset}.{table}"
