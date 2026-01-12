6b321e0aafca6046ad694d5745f4ec9b915cc966
csuyat-dot <christian.suyat@dot.ca.gov>
2026-01-12 21:31:01 +0000

    published november ntd ridership numbers. no errors

--- ntd/monthly_ridership_report/update_vars.py
+++ ntd/monthly_ridership_report/update_vars.py
@@ -2,7 +2,7 @@ import ntd_dates
 
 GCS_FILE_PATH = "gs://calitp-analytics-data/data-analyses/ntd/"
 
-current_month = "oct2025"
+current_month = "nov2025"
 YEAR, MONTH = ntd_dates.parse_monthyear_string(current_month)
 PUBLIC_FILENAME = ntd_dates.get_public_filename(current_month)
 MONTH_CREATED = ntd_dates.DATES[current_month]
@@ -10,7 +10,7 @@ MONTH_CREATED = ntd_dates.DATES[current_month]
 # Check this url each month
 # https://www.transit.dot.gov/ntd/data-product/monthly-module-adjusted-data-release
 # Depending on if they fixed the Excel, there may be an additional suffix
-suffix = "_251201"
+suffix = "_260102"
 FULL_URL = (
     "https://www.transit.dot.gov/sites/fta.dot.gov/files/"
     f"{MONTH_CREATED}/{MONTH}%20{YEAR}%20"
