591f243a89e908b73dc1ed2e0484f8014c64d0c5
Eric Dasmalchi <edasmalchi@gmail.com>
2026-01-22 00:06:37 +0000

    refactor script, ssab too big to save geoparquet

--- transit_service_intensity/prepare_tracts_borders.py
+++ transit_service_intensity/prepare_tracts_borders.py
@@ -1,11 +1,24 @@
 # import intake
 import uuid
+from functools import cache
 
 import geopandas as gpd
 import pandas as pd
+from calitp_data_analysis.gcs_geopandas import GCSGeoPandas
+from calitp_data_analysis.gcs_pandas import GCSPandas
 from segment_speed_utils import helpers
-from update_vars import ANALYSIS_DATE, BORDER_BUFFER_METERS
-from utils import read_census_tracts
+from update_vars import ANALYSIS_DATE, BORDER_BUFFER_METERS, GCS_PATH, GEOM_SUBFOLDER
+from utils import read_uzas
+
+
+@cache
+def gcs_pandas():
+    return GCSPandas()
+
+
+@cache
+def gcs_geopandas():
+    return GCSGeoPandas()
 
 
 def intersection_hash(row, id_col="tract"):
@@ -15,18 +28,18 @@ def intersection_hash(row, id_col="tract"):
     """
     t1 = int(row[f"{id_col}_1"][2:])  # drop state code
     t2 = int(row[f"{id_col}_2"][2:])
-    row_tracts = [t1, t2]
-    row_tracts.sort()  # modifies inplace
-    return hash(tuple(row_tracts))
+    row_areas = [t1, t2]
+    row_areas.sort()  # modifies inplace
+    return hash(tuple(row_areas))
 
 
 def find_borders(
-    tracts_gdf: gpd.GeoDataFrame, border_buffer: int = BORDER_BUFFER_METERS, id_col: str = "tract"
+    areas_gdf: gpd.GeoDataFrame, border_buffer: int = BORDER_BUFFER_METERS, id_col: str = "tract"
 ) -> gpd.GeoDataFrame:
     """ """
-    tracts_gdf = tracts_gdf.copy()
-    tracts_gdf.geometry = tracts_gdf.buffer(border_buffer)
-    borders = gpd.overlay(tracts_gdf, tracts_gdf)
+    areas_gdf = areas_gdf.copy()
+    areas_gdf.geometry = areas_gdf.buffer(border_buffer)
+    borders = gpd.overlay(areas_gdf, areas_gdf)
     borders = borders[borders[f"{id_col}_1"] != borders[f"{id_col}_2"]]
     # for dropping mirrored borders
     borders["intersection_hash"] = borders.apply(intersection_hash, axis=1, id_col=id_col)
@@ -38,8 +51,8 @@ def find_borders(
 
 def find_shapes_in_areas_borders(shape_stops, areas, borders, id_col="tract"):
     """
-    sjoin stops to tracts and border zones by GTFS shape.
-    create tsi_segment_id equal to tract if a single tract
+    sjoin stops to areas and border zones by GTFS shape.
+    create tsi_segment_id equal to area id_col if a single area
     or intersection_id if a border zone
     """
     shape_stops_areas_borders = pd.concat([areas, borders]).sjoin(shape_stops).drop(columns="index_right")
@@ -54,10 +67,11 @@ def find_shapes_in_areas_borders(shape_stops, areas, borders, id_col="tract"):
 
 if __name__ == "__main__":
 
-    print(f"prepare_tracts_borders {ANALYSIS_DATE}")
-    tracts = read_census_tracts(ANALYSIS_DATE)
+    print(f"prepare_areas_borders {ANALYSIS_DATE}")
+    # areas = read_census_tracts(ANALYSIS_DATE)
+    areas = read_uzas()
     shapes = helpers.import_scheduled_shapes(ANALYSIS_DATE)
-    borders = find_borders(tracts)
+    borders = find_borders(areas_gdf=areas, id_col="uace20")
     st = helpers.import_scheduled_stop_times(
         analysis_date=ANALYSIS_DATE, columns=["feed_key", "trip_id", "stop_id"], get_pandas=True
     )
@@ -71,6 +85,8 @@ if __name__ == "__main__":
         .dropna()
     )
 
-    borders.to_parquet(f"borders_{ANALYSIS_DATE}.parquet")
-    shape_stops_tracts_borders = find_shapes_in_areas_borders(shape_stops, tracts, borders)
-    shape_stops_tracts_borders.to_parquet(f"shape_stops_tracts_borders_{ANALYSIS_DATE}.parquet")
+    gcs_geopandas().geo_data_frame_to_parquet(borders, f"{GCS_PATH}{GEOM_SUBFOLDER}borders_{ANALYSIS_DATE}.parquet")
+    shape_stops_areas_borders = find_shapes_in_areas_borders(shape_stops, areas, borders, id_col="uace20")
+    gcs_geopandas().geo_data_frame_to_parquet(
+        shape_stops_areas_borders, f"{GCS_PATH}{GEOM_SUBFOLDER}shape_stops_areas_borders_{ANALYSIS_DATE}.parquet"
+    )
--- transit_service_intensity/update_vars.py
+++ transit_service_intensity/update_vars.py
@@ -1,5 +1,7 @@
 from shared_utils import rt_dates
 
 GCS_PATH = "gs://calitp-analytics-data/data-analyses/transit_service_intensity/"
+INPUT_GEOM_PATHS = {"urbanized_areas": "input_geoms/ca_uza_map.parquet"}
+GEOM_SUBFOLDER = "urbanized_areas/"
 ANALYSIS_DATE = rt_dates.DATES["jul2025"]
 BORDER_BUFFER_METERS = 35
