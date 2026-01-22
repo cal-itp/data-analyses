0fb970b3b21d770a9e35543418a219e59f5d5351
Eric Dasmalchi <edasmalchi@gmail.com>
2026-01-22 22:21:43 +0000

    use GCSPandas helper in funnel vp scripts

--- gtfs_funnel/crosswalk_gtfs_dataset_key_to_organization.py
+++ gtfs_funnel/crosswalk_gtfs_dataset_key_to_organization.py
@@ -6,57 +6,61 @@ Currently, we save an output with our keys, then an output
 without our keys to easily go back to our workflow whenever we get
 feedback we get from users, but this is very redundant.
 """
+
 import datetime
-import os
+from functools import cache
+
 import pandas as pd
-
-from calitp_data_analysis.tables import tbls
+from calitp_data_analysis.gcs_pandas import GCSPandas
 from calitp_data_analysis.sql import get_engine
-
-from shared_utils import schedule_rt_utils
 from segment_speed_utils import helpers
+from shared_utils import schedule_rt_utils
 from update_vars import GTFS_DATA_DICT, SCHED_GCS
+
+
+@cache
+def gcs_pandas():
+    return GCSPandas()
+
 
 db_engine = get_engine()
 
-def create_gtfs_dataset_key_to_organization_crosswalk(
-    analysis_date: str
-) -> pd.DataFrame:
+
+def create_gtfs_dataset_key_to_organization_crosswalk(analysis_date: str) -> pd.DataFrame:
     """
-    For every operator that appears in schedule data, 
+    For every operator that appears in schedule data,
     create a crosswalk that links to organization_source_record_id.
     For all our downstream outputs, at various aggregations,
     we need to attach these over and over again.
     """
-    df = helpers.import_scheduled_trips(
-        analysis_date,
-        columns = ["gtfs_dataset_key", "name"],
-        get_pandas = True
-    ).rename(columns = {"schedule_gtfs_dataset_key": "gtfs_dataset_key"})
+    df = helpers.import_scheduled_trips(analysis_date, columns=["gtfs_dataset_key", "name"], get_pandas=True).rename(
+        columns={"schedule_gtfs_dataset_key": "gtfs_dataset_key"}
+    )
     # rename columns because we must use simply gtfs_dataset_key in schedule_rt_utils function
-    
+
     # Get base64_url, organization_source_record_id and organization_name
     crosswalk = schedule_rt_utils.sample_gtfs_dataset_key_to_organization_crosswalk(
         df,
         analysis_date,
-        quartet_data = "schedule",
-        dim_gtfs_dataset_cols = ["key", "source_record_id", "base64_url"],
-        dim_organization_cols = ["source_record_id", "name", "ntd_id_2022"],
-        dim_county_geography_cols= ["caltrans_district"], # this is where caltrans_district appears by default
+        quartet_data="schedule",
+        dim_gtfs_dataset_cols=["key", "source_record_id", "base64_url"],
+        dim_organization_cols=["source_record_id", "name", "ntd_id_2022"],
+        dim_county_geography_cols=["caltrans_district"],  # this is where caltrans_district appears by default
     )
 
     df_with_org = pd.merge(
-        df.rename(columns = {"gtfs_dataset_key": "schedule_gtfs_dataset_key"}),
+        df.rename(columns={"gtfs_dataset_key": "schedule_gtfs_dataset_key"}),
         crosswalk,
-        on = "schedule_gtfs_dataset_key",
-        how = "inner"
+        on="schedule_gtfs_dataset_key",
+        how="inner",
     )
 
     return df_with_org
 
+
 def load_ntd() -> pd.DataFrame:
     with db_engine.connect() as connection:
-        query = f"""
+        query = """
             SELECT
             number_of_state_counties,
             primary_uza_name,
@@ -76,17 +80,18 @@ def load_ntd() -> pd.DataFrame:
             ntd_id,
             year
             FROM
-            cal-itp-data-infra.mart_ntd.dim_annual_agency_information  
+            cal-itp-data-infra.mart_ntd.dim_annual_agency_information
             WHERE state = 'CA' AND _is_current = TRUE
             """
-        df = pd.read_sql(query,connection)
+        df = pd.read_sql(query, connection)
     df2 = df.sort_values(by=df.columns.tolist(), na_position="last")
     df3 = df2.groupby("agency_name").first().reset_index()
     return df3
 
-def load_mobility()->pd.DataFrame:
+
+def load_mobility() -> pd.DataFrame:
     with db_engine.connect() as connection:
-        query = f"""
+        query = """
             SELECT
             agency_name,
             counties_served,
@@ -98,88 +103,69 @@ def load_mobility()->pd.DataFrame:
             on_demand_vehicles_at_max_service,
             vehicles_at_max_service
             FROM
-            cal-itp-data-infra.mart_transit_database.dim_mobility_mart_providers  
+            cal-itp-data-infra.mart_transit_database.dim_mobility_mart_providers
             """
-        df = pd.read_sql(query,connection)
-    df2 = df.sort_values(
-        by=["on_demand_vehicles_at_max_service","vehicles_at_max_service"], 
-        ascending = [False, False]
-    )
-    df3 = df2.groupby('agency_name').first().reset_index()
+        df = pd.read_sql(query, connection)
+    df2 = df.sort_values(by=["on_demand_vehicles_at_max_service", "vehicles_at_max_service"], ascending=[False, False])
+    df3 = df2.groupby("agency_name").first().reset_index()
     return df3
 
-def merge_ntd_mobility()->pd.DataFrame:
+
+def merge_ntd_mobility() -> pd.DataFrame:
     """
-    Merge NTD (dim_annual_ntd_agency_information) with 
+    Merge NTD (dim_annual_ntd_agency_information) with
     mobility providers (dim_mobility_mart_providers)
     and dedupe and keep 1 row per agency.
     """
     ntd = load_ntd()
     mobility = load_mobility()
-    
-    m1 = pd.merge(
-        mobility,
-        ntd,
-        how="inner",
-        on="agency_name"
-    )
 
-    m1 = m1.drop_duplicates(
-        subset="agency_name"
-    ).reset_index(
-        drop=True
-    ).drop(columns = "agency_name")
-    
+    m1 = pd.merge(mobility, ntd, how="inner", on="agency_name")
+
+    m1 = m1.drop_duplicates(subset="agency_name").reset_index(drop=True).drop(columns="agency_name")
+
     # Wherever possible, allow nullable integers. These columns are integers, but can be
     # missing if we don't find corresponding NTD info
     integrify_cols = [
-        "number_of_state_counties", "number_of_counties_with_service", 
-        "service_area_sq_miles", "service_area_pop",
-        "on_demand_vehicles_at_max_service", "vehicles_at_max_service",
-        "voms_pt", "voms_do", "year",
+        "number_of_state_counties",
+        "number_of_counties_with_service",
+        "service_area_sq_miles",
+        "service_area_pop",
+        "on_demand_vehicles_at_max_service",
+        "vehicles_at_max_service",
+        "voms_pt",
+        "voms_do",
+        "year",
     ]
     m1[integrify_cols] = m1[integrify_cols].astype("Int64")
 
     return m1
 
+
 if __name__ == "__main__":
 
     from update_vars import analysis_date_list
-    
+
     EXPORT = GTFS_DATA_DICT.schedule_tables.gtfs_key_crosswalk
     start = datetime.datetime.now()
-    
+
     for analysis_date in analysis_date_list:
         t0 = datetime.datetime.now()
-        df = create_gtfs_dataset_key_to_organization_crosswalk(
-            analysis_date
-        ).rename(columns = {"ntd_id_2022": "ntd_id"})
-        
+        df = create_gtfs_dataset_key_to_organization_crosswalk(analysis_date).rename(columns={"ntd_id_2022": "ntd_id"})
+
         # Add some NTD data: if I want to delete this, simply take out
         # ntd_df and the crosswalk_df merge.
         ntd_df = merge_ntd_mobility()
-        
-        crosswalk_df = pd.merge(
-            df,
-            ntd_df,
-            on = ["ntd_id"],
-            how = "left"
-        )
+
+        crosswalk_df = pd.merge(df, ntd_df, on=["ntd_id"], how="left")
 
         # Drop ntd_id from ntd_df to avoid confusion
-        # Drop duplicates since we're getting a lot. 
-        crosswalk_df = (crosswalk_df
-                        .drop(columns = ["ntd_id"])
-                        .drop_duplicates()
-                       )
-        
-        crosswalk_df.to_parquet(
-           f"{SCHED_GCS}{EXPORT}_{analysis_date}.parquet"
-        )
+        # Drop duplicates since we're getting a lot.
+        crosswalk_df = crosswalk_df.drop(columns=["ntd_id"]).drop_duplicates()
+
+        gcs_pandas().data_frame_to_parquet(crosswalk_df, f"{SCHED_GCS}{EXPORT}_{analysis_date}.parquet")
         t1 = datetime.datetime.now()
         print(f"finished {analysis_date}: {t1-t0}")
-    
+
     end = datetime.datetime.now()
     print(f"execution time: {end - start}")
-
- 
--- gtfs_funnel/vp_condenser.py
+++ gtfs_funnel/vp_condenser.py
@@ -1,84 +1,86 @@
 """
 Condense vp into arrays by trip-direction.
 """
+
 import datetime
-import geopandas as gpd
-import pandas as pd
 import sys
+from functools import cache
 
-from dask import delayed, compute
-from loguru import logger
-
-from calitp_data_analysis.geography_utils import WGS84
 from calitp_data_analysis import utils
+from calitp_data_analysis.gcs_pandas import GCSPandas
+from calitp_data_analysis.geography_utils import WGS84
+from dask import compute, delayed
+from loguru import logger
 from segment_speed_utils import vp_transform
 from shared_utils import geo_utils
 from update_vars import GTFS_DATA_DICT, SEGMENT_GCS
 
-def condense_vp_to_linestring(
-    analysis_date: str, 
-    dict_inputs: dict
-):
+
+@cache
+def gcs_pandas():
+    return GCSPandas()
+
+
+def condense_vp_to_linestring(analysis_date: str, dict_inputs: dict):
     """
-    Turn vp (df with point geometry) into a condensed 
+    Turn vp (df with point geometry) into a condensed
     linestring version.
-    We will group by trip and save out 
+    We will group by trip and save out
     the vp point geom into a shapely.LineString.
     """
     USABLE_VP = dict_inputs.speeds_tables.vp_dwell
     EXPORT_FILE = dict_inputs.speeds_tables.vp_condensed_line
-    
-    vp = delayed(pd.read_parquet)(
+
+    vp = delayed(gcs_pandas().read_parquet)(
         f"{SEGMENT_GCS}{USABLE_VP}_{analysis_date}/",
-        columns = ["trip_instance_key", "x", "y", 
-                   "vp_idx", "vp_primary_direction", 
-                   "location_timestamp_local", 
-                   "moving_timestamp_local",
-                  ],
-    ).pipe(
-        geo_utils.vp_as_gdf, crs = WGS84
+        columns=[
+            "trip_instance_key",
+            "x",
+            "y",
+            "vp_idx",
+            "vp_primary_direction",
+            "location_timestamp_local",
+            "moving_timestamp_local",
+        ],
+    ).pipe(geo_utils.vp_as_gdf, crs=WGS84)
+
+    vp_condensed = (
+        delayed(vp_transform.condense_point_geom_to_line)(
+            vp,
+            group_cols=["trip_instance_key"],
+            geom_col="geometry",
+            array_cols=[
+                "vp_idx",
+                "location_timestamp_local",
+                "moving_timestamp_local",
+                "vp_primary_direction",
+            ],
+            sort_cols=["vp_idx"],
+        )
+        .set_geometry("geometry")
+        .set_crs(WGS84)
     )
-    
-    vp_condensed = delayed(vp_transform.condense_point_geom_to_line)(
-        vp,
-        group_cols = ["trip_instance_key"],
-        geom_col = "geometry",
-        array_cols = ["vp_idx", 
-                      "location_timestamp_local", "moving_timestamp_local",
-                      "vp_primary_direction",
-                     ],
-        sort_cols = ["vp_idx"]
-    ).set_geometry("geometry").set_crs(WGS84)
-        
+
     vp_condensed = compute(vp_condensed)[0]
-    
-    utils.geoparquet_gcs_export(
-        vp_condensed,
-        SEGMENT_GCS,
-        f"{EXPORT_FILE}_{analysis_date}"
-    )
-        
-    return 
+
+    utils.geoparquet_gcs_export(vp_condensed, SEGMENT_GCS, f"{EXPORT_FILE}_{analysis_date}")
+
+    return
 
 
 if __name__ == "__main__":
-    
+
     from update_vars import analysis_date_list
-    
+
     LOG_FILE = "./logs/vp_preprocessing.log"
     logger.add(LOG_FILE, retention="3 months")
-    logger.add(sys.stderr, 
-               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", 
-               level="INFO")
-        
+    logger.add(sys.stderr, format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", level="INFO")
+
     for analysis_date in analysis_date_list:
         start = datetime.datetime.now()
-        
+
         condense_vp_to_linestring(analysis_date, GTFS_DATA_DICT)
-        
+
         end = datetime.datetime.now()
-        
-        logger.info(
-            f"{analysis_date}: condense vp for trip "
-            f"{end - start}"
-        )    
+
+        logger.info(f"{analysis_date}: condense vp for trip " f"{end - start}")
