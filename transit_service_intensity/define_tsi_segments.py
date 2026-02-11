80105f399ed33ae10e2662246981dfa4cc964d45
Eric Dasmalchi <edasmalchi@gmail.com>
2026-02-11 16:42:11 +0000

    rename away from tract, docstrings and comments

--- transit_service_intensity/define_tsi_segments.py
+++ transit_service_intensity/define_tsi_segments.py
@@ -27,7 +27,10 @@ tqdm.pandas(desc=f"TSI Segments Progress {ANALYSIS_DATE}")
 def overlay_to_borders(
     shape_gdf: gpd.GeoDataFrame, border_gdf: gpd.GeoDataFrame, sensitivity_dist: int = BORDER_BUFFER_METERS * 4
 ):
-    """ """
+    """
+    Find where shapes overlap border zones, keep if >= sensitivity distance.
+    Screen out shapes briefly crossing border, keep shapes running along border.
+    """
     overlaid = shape_gdf.overlay(border_gdf, how="intersection")
     overlaid["length"] = overlaid.geometry.length
     overlaid = overlaid[overlaid.length >= sensitivity_dist]
@@ -35,7 +38,9 @@ def overlay_to_borders(
 
 
 def overlay_to_areas(shape_gdf_no_border: gpd.GeoDataFrame, areas_gdf: gpd.GeoDataFrame, id_col: str = GEOM_ID_COL):
-    """ """
+    """
+    simple overlay with non-border analysis areas
+    """
     areas_gdf = areas_gdf[[id_col, "geometry"]]
     return shape_gdf_no_border.overlay(areas_gdf, how="intersection")
 
@@ -47,15 +52,20 @@ def overlay_areas_borders(
     sensitivity_dist: int = BORDER_BUFFER_METERS * 4,
     id_col=GEOM_ID_COL,
 ):
-    """ """
+    """
+    overlay gtfs shapes to tsi analysis areas and border zones
+    """
     border_gdf = border_gdf.drop(columns=["intersection_hash"])
     # try:
     border_overlaid = overlay_to_borders(shape_gdf, border_gdf, sensitivity_dist)
-    not_border = shape_gdf.overlay(border_overlaid, how="difference")
+    not_border = shape_gdf.overlay(
+        border_overlaid, how="difference"
+    )  # strip shape part in border zone to avoid double-counting distance
     areas_overlaid = overlay_to_areas(not_border, areas_gdf, id_col=id_col)
     areas_and_borders = pd.concat([areas_overlaid, border_overlaid]).explode(index_parts=False).reset_index(drop=True)
     areas_and_borders = areas_and_borders.assign(
         border=~areas_and_borders[f"{id_col}_2"].isna(),
+        # point where gtfs shape crosses into a tsi analysis area
         start=areas_and_borders.geometry.apply(lambda x: shapely.Point(x.coords[0])),
         tsi_segment_id=areas_and_borders[id_col].combine_first(areas_and_borders.intersection_id).astype(str),
         tsi_segment_meters=areas_and_borders.geometry.length,
--- transit_service_intensity/time_distance_in_segments.py
+++ transit_service_intensity/time_distance_in_segments.py
@@ -18,7 +18,6 @@ def gcs_geopandas():
 
 
 def attach_projected_stop_times(analysis_date: str):
-    """ """
     st_dir_cols = ["trip_instance_key", "stop_sequence", "stop_meters", "stop_id"]
     st_dir = helpers.import_scheduled_stop_times(
         analysis_date, columns=st_dir_cols, get_pandas=True, with_direction=True
@@ -32,6 +31,10 @@ def attach_projected_stop_times(analysis_date: str):
 
 
 def read_tsi_segs(analysis_date, shapes):
+    """
+    read in tsi segments and merge with full gtfs shapes.
+    project entry point (start) of each shape x tsi segment against its full shape linestring
+    """
     path = f"{GCS_PATH}{GEOM_SUBFOLDER}tsi_segs_{analysis_date}.parquet"
     tsi_segs = gcs_geopandas().read_parquet(path)
     tsi_segs = tsi_segs.drop(columns=["geometry"])
@@ -45,37 +48,42 @@ def read_tsi_segs(analysis_date, shapes):
     return shape_merged
 
 
-def tract_border_time_by_trip(tracts_borders_trip_df: pd.DataFrame, st_proj_df: pd.DataFrame):
-    """ """
+def area_border_time_by_trip(areas_borders_trip_df: pd.DataFrame, st_proj_df: pd.DataFrame):
+    """
+    Operating on each trip, determine when trip enters each tsi analysis area
+    """
 
-    one_trip = st_proj_df.query("trip_instance_key == @tracts_borders_trip_df.trip_instance_key.iloc[0]").sort_values(
+    one_trip = st_proj_df.query("trip_instance_key == @areas_borders_trip_df.trip_instance_key.iloc[0]").sort_values(
         "stop_sequence"
     )
     shape_array = one_trip.stop_meters.to_numpy()
     dt_float_array = one_trip.arrival_sec.to_numpy()
-    tracts_borders_trip_df["arrival_sec"] = tracts_borders_trip_df.start_meters.apply(
+    # estimate when each trip crosses into a tsi analysis geometry
+    areas_borders_trip_df["arrival_sec"] = areas_borders_trip_df.start_meters.apply(
         rt_utils.time_at_position_numba, shape_array=shape_array, dt_float_array=dt_float_array
     )
-    tracts_borders_trip_df = tracts_borders_trip_df.assign(
-        arrival_sec_next=tracts_borders_trip_df.arrival_sec.shift(-1),
+    areas_borders_trip_df = areas_borders_trip_df.assign(
+        arrival_sec_next=areas_borders_trip_df.arrival_sec.shift(-1),
         trip_instance_key=one_trip.trip_instance_key.iloc[0],
     )
-    tracts_borders_trip_df.loc[tracts_borders_trip_df.index.min(), "arrival_sec"] = one_trip.arrival_sec.min()
-    tracts_borders_trip_df.loc[tracts_borders_trip_df.index.max(), "arrival_sec_next"] = one_trip.arrival_sec.max()
-    tracts_borders_trip_df = tracts_borders_trip_df.assign(
-        segment_seconds=tracts_borders_trip_df.arrival_sec_next - tracts_borders_trip_df.arrival_sec
+    # assign min and max stop times to min and max tsi analysis areas
+    # TODO evaluate this behavior, it preserves total vrh but may misallocate it
+    areas_borders_trip_df.loc[areas_borders_trip_df.index.min(), "arrival_sec"] = one_trip.arrival_sec.min()
+    areas_borders_trip_df.loc[areas_borders_trip_df.index.max(), "arrival_sec_next"] = one_trip.arrival_sec.max()
+    areas_borders_trip_df = areas_borders_trip_df.assign(
+        segment_seconds=areas_borders_trip_df.arrival_sec_next - areas_borders_trip_df.arrival_sec
     )
 
-    return tracts_borders_trip_df
+    return areas_borders_trip_df
 
 
 def dask_calculate_batch(
-    tracts_borders_trip_df: pd.DataFrame, st_proj_df: pd.DataFrame, meta: pd.DataFrame
+    areas_borders_trip_df: pd.DataFrame, st_proj_df: pd.DataFrame, meta: pd.DataFrame
 ) -> pd.DataFrame:
 
-    ddf = dd.from_pandas(tracts_borders_trip_df, npartitions=100)
+    ddf = dd.from_pandas(areas_borders_trip_df, npartitions=100)
     ddf = ddf.groupby("trip_instance_key", group_keys=False).apply(
-        tract_border_time_by_trip, st_proj_df=st_proj_df, meta=meta
+        area_border_time_by_trip, st_proj_df=st_proj_df, meta=meta
     )
     df = ddf.compute()
     return df
@@ -95,17 +103,11 @@ if __name__ == "__main__":
     many_trip_test = (
         tsi_segments_trips.head(10)
         .groupby("trip_instance_key", group_keys=False)
-        .apply(tract_border_time_by_trip, st_proj_df=st_proj)
+        .apply(area_border_time_by_trip, st_proj_df=st_proj)
     )
     meta = many_trip_test[:0]
+    # this step is currently very slow - several hours. Future trip pattern hashing will help.
     trip_tsi_segments = dask_calculate_batch(tsi_segments_trips, st_proj, meta)
 
     path = f"{GCS_PATH}{GEOM_SUBFOLDER}trip_tsi_segments_{ANALYSIS_DATE}.parquet"
     gcs_geopandas().geo_data_frame_to_parquet(trip_tsi_segments, path)
-
-    # trips = tsi_segments_trips.trip_instance_key.unique()
-    # trips1 = trips[:len(trips)//10]
-    # dask_calculate_batch(tsi_segments_trips.query('trip_instance_key.isin(@trips1)'),
-    #                      st_proj, meta).to_parquet(f'trip_tables_batch1_{ANALYSIS_DATE}.parquet')
-    # dask_calculate_batch(tsi_segments_trips.query('not trip_instance_key.isin(@trips1)'),
-    #                      st_proj, meta).to_parquet(f'trip_tables_batch2_{ANALYSIS_DATE}.parquet')
