049dfc464d530ab773389bd8c1c3e5293731e859
Jeff Parr <jeffparr100@gmail.com>
2026-03-10 13:26:24 -0700

    Copy calitp-data-analysis package into local package
    ref: #1882

---
+++ calitp-data-analysis/calitp_data_analysis/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+import gcsfs  # type: ignore[import]
+
+
+def get_fs(gcs_project="", **kwargs):
+    if os.environ.get("CALITP_AUTH") == "cloud":
+        return gcsfs.GCSFileSystem(project=gcs_project, token="cloud", **kwargs)
+    else:
+        return gcsfs.GCSFileSystem(project=gcs_project, token="google_default", **kwargs)
