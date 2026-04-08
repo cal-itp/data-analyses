9908a65bbc4f2bb9c37de393b667d0011a140ddc
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-04-08 00:08:43 +0000

    colorbox for more columns

--- rt_predictions/_color_palette.py
+++ rt_predictions/_color_palette.py
@@ -23,6 +23,8 @@ COLOR_NAME_TO_HEX = {
     "light_gray": "#faf0e6",  # linen in gt
     "valentino": "#7a378b",  # dark purple, mediumorchid4 in gt
     "lizard_green": "#6e8b3d",  # dark olive, darkolivegreen4 in gt
+    "light_goldenrod": "#ffec8b",  # light goldenrod1 in gt
+    "pastel_peppermint": "#e5f5e0",  # white alyssum in gt (from greens)
 }
 
 
