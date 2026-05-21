64e9d38c9359ed7e46483031c4d0d46959a9bec9
Jeff Parr <jeffparr100@gmail.com>
2026-05-21 08:55:44 -0700

    annual_ridership_report

--- ntd/annual_ridership_report/deploy_portfolio_yaml.py
+++ ntd/annual_ridership_report/deploy_portfolio_yaml.py
@@ -1,5 +1,5 @@
 """
-Creates site .yml with chapters for each unique RTPA in the report data, 
+Creates site .yml with chapters for each unique RTPA in the report data,
 then places the new .yml in the portfolio/sites directory
 
 Yaml structure is not nested by district, it is just all RTPAs
@@ -8,20 +8,26 @@ in the navigation panel.
 
 import sys
 
-sys.path.append("../monthly_ridership_report")  # up one level
-sys.path.append("../")
-from pathlib import Path
-from calitp_data_analysis.tables import tbls
-import pandas as pd
-from shared_utils import portfolio_utils
-from update_vars import GCS_FILE_PATH
-from _01_ntd_ridership_utils import ntd_id_to_rtpa_crosswalk
+sys.path.append("../monthly_ridership_report")  # for update_vars
+from pathlib import Path  # noqa: E402
 
-PORTFOLIO_SITE_YAML = Path("../../portfolio/sites/ntd_annual_ridership_report.yml")
+from calitp_data_analysis.gcs_pandas import GCSPandas  # noqa: E402
+from calitp_portfolio.models import load_site  # noqa: E402
+from calitp_portfolio.mutations import generate_parts_flat  # noqa: E402
+from update_vars import GCS_FILE_PATH  # noqa: E402
+
+PORTFOLIO_SITE_YAML = Path(__file__).parent / "ntd_annual_ridership_report.yml"
 
 # read in rtpa data from dim_orgs
 if __name__ == "__main__":
-    df = pd.read_parquet(f"{GCS_FILE_PATH}annual_ridership_report_data.parquet")["rtpa_name"].sort_values().drop_duplicates().to_frame()
-    portfolio_utils.create_portfolio_yaml_chapters_no_sections(
-        PORTFOLIO_SITE_YAML, chapter_name="rtpa", chapter_values=list(df.rtpa_name)
+    df = (
+        GCSPandas()
+        .read_parquet(f"{GCS_FILE_PATH}annual_ridership_report_data.parquet")["rtpa_name"]
+        .sort_values()
+        .drop_duplicates()
+        .to_frame()
     )
+
+    site = load_site(PORTFOLIO_SITE_YAML)
+    site = generate_parts_flat(site, param_key="rtpa", values=list(df.rtpa_name))
+    site.write_yaml(PORTFOLIO_SITE_YAML)
