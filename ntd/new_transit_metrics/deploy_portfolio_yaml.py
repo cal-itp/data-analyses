abf727753e8a6ce6bf75ce046ce4b2be1e21cefa
Jeff Parr <jeffparr100@gmail.com>
2026-05-21 08:55:57 -0700

    new_transit_metrics

--- ntd/new_transit_metrics/deploy_portfolio_yaml.py
+++ ntd/new_transit_metrics/deploy_portfolio_yaml.py
@@ -7,30 +7,34 @@ we use, let's just generate the yaml.
 This Yaml structure is structured by RTPA names, instead of CT districts.
 RTPA names will become the navigation panel on the portfolio site
 """
+
 import sys
 
 sys.path.append("../")  # up one level
 sys.path.append("../monthly_ridership_report")
-import pandas as pd
+from pathlib import Path  # noqa: E402
 
-from pathlib import Path
+from calitp_data_analysis.gcs_pandas import GCSPandas  # noqa: E402
+from calitp_portfolio.models import load_site  # noqa: E402
+from calitp_portfolio.mutations import generate_parts_flat  # noqa: E402
+from update_vars import GCS_FILE_PATH  # noqa: E402
 
-from shared_utils import portfolio_utils
-from update_vars import GCS_FILE_PATH
-
-PORTFOLIO_SITE_YAML = Path("../../portfolio/sites/new_transit_metrics.yml")
+PORTFOLIO_SITE_YAML = Path(__file__).parent / "new_transit_metrics.yml"
 
 if __name__ == "__main__":
-    
-    df = pd.read_parquet(
-        #f"{GCS_FILE_PATH}ntd_id_rtpa_crosswalk_all_reporter_types.parquet",
-        f"{GCS_FILE_PATH}raw_transit_performance_metrics_data.parquet",
-        columns = ["RTPA"]
-    ).drop_duplicates().sort_values("RTPA").reset_index(drop=True)
-    
-    portfolio_utils.create_portfolio_yaml_chapters_no_sections(
-        PORTFOLIO_SITE_YAML, 
-        chapter_name = "rtpa",
-        chapter_values =list(df.RTPA)
+
+    df = (
+        GCSPandas()
+        .read_parquet(
+            # f"{GCS_FILE_PATH}ntd_id_rtpa_crosswalk_all_reporter_types.parquet",
+            f"{GCS_FILE_PATH}raw_transit_performance_metrics_data.parquet",
+            columns=["RTPA"],
+        )
+        .drop_duplicates()
+        .sort_values("RTPA")
+        .reset_index(drop=True)
     )
 
+    site = load_site(PORTFOLIO_SITE_YAML)
+    site = generate_parts_flat(site, param_key="rtpa", values=list(df.RTPA))
+    site.write_yaml(PORTFOLIO_SITE_YAML)
